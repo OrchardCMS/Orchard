35548f6ef142e6aee2d804d00dbc5f13609e7b24
suhacan <none@none>
2009-11-19 19:52:02 +0000

    - Comments: Permissions
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4041429

---
+++ src/Orchard.Web/Packages/Orchard.Comments/Permissions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Orchard.Security.Permissions;
+
+namespace Orchard.Comments {
+    public class Permissions : IPermissionProvider {
+        public static readonly Permission AddComment = new Permission { Description = "Adding a Comment", Name = "AddComment" };
+        public static readonly Permission AddCommentWithoutValidation = new Permission { Description = "Adding a Comment without validation", Name = "AddCommentWithoutValidation" };
+        public static readonly Permission CloseComment = new Permission { Description = "Closing Comments", Name = "CloseComment" };
+        public static readonly Permission CloseCommentOnOwnItems = new Permission { Description = "Closing Comments on own items", Name = "CloseCommentOnOwnItems" };
+        public static readonly Permission ModerateComment = new Permission { Description = "Moderating Comments", Name = "ModerateComment" };
+        public static readonly Permission ModerateCommentOnOwnItems = new Permission { Description = "Moderating Comments On Own Items", Name = "ModerateCommentOnOwnItems" };
+
+        public string PackageName {
+            get {
+                return "Comments";
+            }
+        }
+
+        public IEnumerable<Permission> GetPermissions() {
+            return new List<Permission> {
+                AddComment,
+                AddCommentWithoutValidation,
+                CloseComment,
+                CloseCommentOnOwnItems,
+                ModerateComment,
+                ModerateCommentOnOwnItems
+            };
+        }
+    }
+}
