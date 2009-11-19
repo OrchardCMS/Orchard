baba3c3eca90830e20434630f524e4eb4eae0f57
loudej <none@none>
2009-11-19 05:42:26 +0000

    removing unused files
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4041355

--- src/Orchard/Models/Records/ModelRecord.cs
+++
@@ -1,6 +0,0 @@
-namespace Orchard.Models.Records {
-    public class ModelRecord {
-        public virtual int Id { get; set; }
-        public virtual ModelTypeRecord ModelType { get; set; }
-    }
-}
--- src/Orchard/Models/Records/ModelTypeRecord.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Orchard.Models.Records {
-    public class ModelTypeRecord {
-        public virtual int Id { get; set; }
-        public virtual string Name { get; set; }
-    }
-}
