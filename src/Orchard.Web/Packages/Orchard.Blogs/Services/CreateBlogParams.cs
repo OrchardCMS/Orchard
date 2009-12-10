e19de46233265e4e829e0253692aeaf961f9e360
skewed <none@none>
2009-12-10 07:51:16 +0000

    Removed no longer used Create<model>Params from Blogs
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4043664

--- src/Orchard.Web/Packages/Orchard.Blogs/Services/CreateBlogParams.cs
+++
@@ -1,8 +0,0 @@
-namespace Orchard.Blogs.Services {
-    public class CreateBlogParams {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Slug { get; set; }
-        //public bool Enabled { get; set; }
-    }
-}
