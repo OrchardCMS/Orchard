1ca64376d6871a5d0042403163081cf6a38514ad
rpaquay <none@none>
2009-12-12 07:56:59 +0000

    Add version check for MVC Beta
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4043868

--- src/Orchard.Web/Global.asax.cs
+++ src/Orchard.Web/Global.asax.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Autofac;
 using Autofac.Builder;
-using Autofac.Integration.Web;
-using Autofac.Modules;
 using Orchard.Environment;
 
 namespace Orchard.Web {
@@ -40,9 +39,10 @@ namespace Orchard.Web {
         }
 
         protected void Application_Start() {
+            // This is temporary until MVC2 is officially released.
+            // We want to avoid running against an outdated preview installed in the GAC
+            CheckMvcVersion(new Version("2.0.41116.0")/*MVC2 Beta file version #*/);
             RegisterRoutes(RouteTable.Routes);
-
-
 
             _host = OrchardStarter.CreateHost(MvcSingletons);
             _host.Initialize();
@@ -50,6 +50,50 @@ namespace Orchard.Web {
             //TODO: what's the failed initialization story - IoC failure in app start can leave you with a zombie appdomain
         }
 
+        private void CheckMvcVersion(Version requiredVersion) {
+            Assembly loadedMvcAssembly = typeof(System.Web.Mvc.Controller).Assembly;
+            Version loadedMvcVersion = ReadAssemblyFileVersion(loadedMvcAssembly);
+
+            if (loadedMvcVersion != requiredVersion) {
+                string message;
+                if (loadedMvcAssembly.GlobalAssemblyCache) {
+                    message = string.Format(
+                        "Orchard has been deployed with a version of {0} that has a different file version ({1}) " +
+                        "than the version installed in the GAC ({2}).\r\n" +
+                        "This implies that Orchard will not be able to run properly in this machine configuration.\r\n" +
+                        "Please un-install MVC from the GAC or install a more recent version.",
+                        loadedMvcAssembly.GetName().Name,
+                        loadedMvcVersion,
+                        requiredVersion);
+                }
+                else {
+                    message = string.Format(
+                        "Orchard has been configured to run with a file version {1} of \"{0}\" " +
+                        "but the version deployed with the application is {2}.\r\n" +
+                        "This probably implies that Orchard is deployed with a newer version " +
+                        "and the source code hasn't been updated accordingly.\r\n" +
+                        "Update the Orchard.Web application source code (look for \"CheckMvcVersion\") to " + 
+                        "specify the correct file version number.\r\n",
+                        loadedMvcAssembly.GetName().Name,
+                        loadedMvcVersion,
+                        requiredVersion);
+                }
+
+                throw new HttpException(500, message);
+            }
+        }
+
+        private Version ReadAssemblyFileVersion(Assembly assembly) {
+            object[] attributes = assembly.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), true);
+            if (attributes == null || attributes.Length != 1) {
+                string message = string.Format("Assembly \"{0}\" doesn't have a \"{1}\" attribute",
+                    assembly.GetName().Name, typeof(AssemblyFileVersionAttribute).FullName);
+                throw new FileLoadException(message);
+            }
+
+            var attribute = (AssemblyFileVersionAttribute)attributes[0];
+            return new Version(attribute.Version);
+        }
 
         protected void Application_EndRequest() {
             _host.EndRequest();
