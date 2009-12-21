7d4bd5f47e25daa179b19aac4987ad323a64dcb4
loudej <none@none>
2009-12-21 22:54:01 +0000

    Removing unused material from Orchard.dll project folder
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4044418

--- src/Orchard/ContentManagement/IContentDisplayInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Web.Routing;
-
-namespace Orchard.ContentManagement {
-    //public interface IContentDisplayInfo : IContent {
-    //    string DisplayText { get; }
-    //    RouteValueDictionary DisplayRouteValues();
-    //    RouteValueDictionary EditRouteValues();
-    //}
-}
