3d0c236102ebb79dcac2d399a9f70e93a3f80392
loudej <none@none>
2010-01-05 10:21:40 +0000

    Adding NHiberbateProfiler activation to devtools module
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4044986

---
+++ src/Orchard.Web/Packages/Orchard.DevTools/Profiler.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.Environment;
+
+namespace Orchard.DevTools {
+    public class Profiler : IOrchardShellEvents {
+        public void Activated() {
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+        }
+
+        public void Terminating() {
+            
+        }
+    }
+}
