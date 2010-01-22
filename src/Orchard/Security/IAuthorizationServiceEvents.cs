040a6e03059a9f9e13f75d0f6001b67a0549e265
loudej <none@none>
2010-01-22 22:19:28 +0000

    Fixing a problem caused applying a patch
    --HG--
    extra : convert_revision : svn%3A5ff7c347-ad56-4c35-b696-ccb81de16e03/trunk%4045863

--- src/Orchard/Security/IAuthorizationServiceEvents.cs
+++ src/Orchard/Security/IAuthorizationServiceEvents.cs
@@ -23,28 +23,3 @@ namespace Orchard.Security {
         public virtual void Complete(CheckAccessContext context) { }
     }
 }
-using System;
-using Orchard.ContentManagement;
-using Orchard.Security.Permissions;
-
-namespace Orchard.Security {
-    public interface IAuthorizationServiceEvents : IEvents {
-        void Checking(CheckAccessContext context);
-        void Adjust(CheckAccessContext context);
-        void Complete(CheckAccessContext context);
-    }
-
-    public class CheckAccessContext {
-        public Permission Permission { get; set; }
-        public IUser User { get; set; }
-        public IContent Content { get; set; }
-        public bool Granted { get; set; }
-        public bool Adjusted { get; set; }
-    }
-
-    public abstract class AuthorizationServiceEvents : IAuthorizationServiceEvents {
-        public virtual void Checking(CheckAccessContext context) { }
-        public virtual void Adjust(CheckAccessContext context) { }
-        public virtual void Complete(CheckAccessContext context) { }
-    }
-}
