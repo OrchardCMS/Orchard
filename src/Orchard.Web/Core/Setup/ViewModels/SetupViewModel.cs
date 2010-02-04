fd68c903491b28fc417076f27c6533ce062ee79b
Erik Porter <erik.porter@microsoft.com>
2010-02-04 14:54:55 -0800

    Added some data annotations to validate SetupViewModel
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Setup/ViewModels/SetupViewModel.cs
+++ src/Orchard.Web/Core/Setup/ViewModels/SetupViewModel.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Orchard.Core.Setup.Annotations;
 using Orchard.Mvc.ViewModels;
 
 namespace Orchard.Core.Setup.ViewModels {
     public class SetupViewModel : BaseViewModel {
+        [Required, StringLength(70)]
         public string SiteName { get; set; }
+        [StringLengthMin(3), StringLength(25)]
         public string AdminUsername { get; set; }
+        [StringLengthMin(6), StringLength(20)]
         public string AdminPassword { get; set; }
+        [SqlDatabaseConnectionString]
         public string DatabaseConnectionString { get; set; }
     }
 }
