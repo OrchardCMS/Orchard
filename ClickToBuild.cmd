31ab6c5c39b470cc7739e87b63b8d8bbd796b79a
Louis DeJardin <lodejardin@microsoft.com>
2010-02-08 11:42:53 -0800

    Adding x84 variation to single-click builder
    Needed to detect vs2k8 environment batch file on x64 operating systems
    
    --HG--
    branch : dev

--- ClickToBuild.cmd
+++ ClickToBuild.cmd
@@ -1,5 +1,6 @@
 
 if "%WindowsSdkDir%" neq "" goto build
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" goto initialize2k8on64
 if exist "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" goto initialize2k8
 echo "Unable to detect suitable environment. Build may not succeed."
 goto build
@@ -7,6 +8,10 @@ goto build
 
 :initialize2k8
 call "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+goto build
+
+:initialize2k8on64
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 goto build
 
 
