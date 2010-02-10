015b26928f2c52186560c6a700b35e3c36131474
Suha Can <Suha.Can@microsoft.com>
2010-02-10 13:23:11 -0800

    More Navigation backend work. - MenuPart/record - Permissions and AdminMenu for the core nav package as well as some placeholders for the upcoming work.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Core/Navigation/AdminMenu.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.UI.Navigation;
+
+namespace Orchard.Core.Navigation {
+    public class AdminMenu : INavigationProvider {
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.Add("Navigation", "12",
+                        menu => menu
+                                    .Add("Manage Main Menu", "2.0", item => item.Action("Index", "Admin", new { area = "Navigation" }).Permission(Permissions.ManageMainMenu)));
+        }
+    }
+}
---
+++ src/Orchard.Web/Core/Navigation/Models/MenuPart.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Orchard.ContentManagement;
+using Orchard.Core.Navigation.Records;
+
+namespace Orchard.Core.Navigation.Models {
+    public class MenuPart : ContentPart<MenuPartRecord> {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get { return ContentItem.Id; } }
+
+        public string MenuText {
+            get { return Record.MenuText; }
+            set { Record.MenuText = value; }
+        }
+
+        [HiddenInput(DisplayValue = false)]
+        public string MenuPosition {
+            get { return Record.MenuPosition; }
+            set { Record.MenuPosition = value; }
+        }
+    }
+}
