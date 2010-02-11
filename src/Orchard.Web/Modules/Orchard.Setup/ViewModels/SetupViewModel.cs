d3e35ff0a5fde327b52877a952ee21f196283ef1
Erik Porter <erik.porter@microsoft.com>
2010-02-10 16:40:58 -0800

    Changed the default value for SetupViewModel to be the built-in database option and added a check to ensure a connection string was entered when the sql database option is checked
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Setup/ViewModels/SetupViewModel.cs
+++ src/Orchard.Web/Modules/Orchard.Setup/ViewModels/SetupViewModel.cs
@@ -4,14 +4,18 @@ using Orchard.Mvc.ViewModels;
 
 namespace Orchard.Setup.ViewModels {
     public class SetupViewModel : BaseViewModel {
+        public SetupViewModel() {
+            DatabaseOptions = true;
+        }
+
         [Required, StringLength(70)]
         public string SiteName { get; set; }
         [Required, StringLengthMin(3), StringLength(25)]
         public string AdminUsername { get; set; }
         [Required, StringLengthMin(6), StringLength(50)]
         public string AdminPassword { get; set; }
+        public bool DatabaseOptions { get; set; }
         [SqlDatabaseConnectionString]
         public string DatabaseConnectionString { get; set; }
-        public bool DatabaseOptions { get; set; }
     }
 }
