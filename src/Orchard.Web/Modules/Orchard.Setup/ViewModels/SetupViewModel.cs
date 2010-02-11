daefd9f62415454717ef1ebf86f6f3bc4193314c
Erik Porter <erik.porter@microsoft.com>
2010-02-10 16:46:56 -0800

    Merge 2
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Setup/ViewModels/SetupViewModel.cs
+++ src/Orchard.Web/Modules/Orchard.Setup/ViewModels/SetupViewModel.cs
@@ -7,14 +7,15 @@ namespace Orchard.Setup.ViewModels {
     	public SetupViewModel() {
     		DatabaseOptions = true;
     	}
+
         [Required(ErrorMessage = "Site name is required."), StringLength(70, ErrorMessage = "Site name can be no longer than 70 characters.")]
         public string SiteName { get; set; }
         [Required(ErrorMessage = "User name is required."), StringLengthMin(3, ErrorMessage = "User name must be longer than 3 characters."), StringLength(25, ErrorMessage = "User name can be no longer than 25 characters.")]
         public string AdminUsername { get; set; }
         [Required(ErrorMessage = "Password is required."), StringLengthMin(6, ErrorMessage = "Password must be longer than 6 characters."), StringLength(50, ErrorMessage = "Password can be no longer than 50 characters.")]
         public string AdminPassword { get; set; }
+        public bool DatabaseOptions { get; set; }
         [SqlDatabaseConnectionString]
         public string DatabaseConnectionString { get; set; }
-        public bool DatabaseOptions { get; set; }
     }
 }
