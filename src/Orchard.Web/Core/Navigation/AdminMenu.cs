f4803dec15809c73069b85d100a78874065b2ea9
Nathan Heskew <nathan.heskew@microsoft.com>
2010-02-13 00:10:52 -0800

    Consolidating Settings, Themes and Navigation admin menus under a single Site menu
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Navigation/AdminMenu.cs
+++ src/Orchard.Web/Core/Navigation/AdminMenu.cs
@@ -5,9 +5,9 @@ namespace Orchard.Core.Navigation {
         public string MenuName { get { return "admin"; } }
 
         public void GetNavigation(NavigationBuilder builder) {
-            builder.Add("Navigation", "12",
+            builder.Add("Site", "12",
                         menu => menu
-                                    .Add("Manage Main Menu", "2.0", item => item.Action("Index", "Admin", new { area = "Navigation" }).Permission(Permissions.ManageMainMenu)));
+                                    .Add("Manage Main Menu", "6.0", item => item.Action("Index", "Admin", new { area = "Navigation" }).Permission(Permissions.ManageMainMenu)));
         }
     }
 }
--- src/Orchard.Web/Core/Themes/AdminMenu.cs
+++ src/Orchard.Web/Core/Themes/AdminMenu.cs
@@ -5,9 +5,9 @@ namespace Orchard.Core.Themes {
         public string MenuName { get { return "admin"; } }
 
         public void GetNavigation(NavigationBuilder builder) {
-            builder.Add("Themes", "11",
+            builder.Add("Site", "11",
                         menu => menu
-                                    .Add("Manage Themes", "2.0", item => item.Action("Index", "Admin", new { area = "Themes" })
+                                    .Add("Manage Themes", "4.0", item => item.Action("Index", "Admin", new { area = "Themes" })
                                         .Permission(Permissions.ManageThemes).Permission(Permissions.ApplyTheme)));
         }
     }
