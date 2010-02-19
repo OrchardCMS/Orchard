f68c2d0f8a32f06f8e21ef8ddc56948553694230
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-02-19 11:18:27 -0800

    Changing comments to work with MVC 2 RC2 model validation approach
    email is no longer required (it's not required by logged in users by default)
    name is provided as hidden
    
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Comments/ViewModels/CommentsCreateViewModel.cs
+++ src/Orchard.Web/Modules/Orchard.Comments/ViewModels/CommentsCreateViewModel.cs
@@ -5,7 +5,6 @@ namespace Orchard.Comments.ViewModels {
     public class CommentsCreateViewModel : AdminViewModel {
         [Required]
         public string Name { get; set; }
-        [Required]
         public string Email { get; set; }
         public string SiteName { get; set; }
         public string CommentText { get; set; }
