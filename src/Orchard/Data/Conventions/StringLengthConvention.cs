57a4b2ad247ec0752201e1afb43ee7c33b167f37
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-02-22 13:26:28 -0800

    Fixing arbitrary spelling of the word arbetrary
    Oddly, this is far from the first time a person whose native language
    is not english has corrected my spelling.

--- src/Orchard/Data/Conventions/StringLengthConvention.cs
+++ src/Orchard/Data/Conventions/StringLengthConvention.cs
@@ -5,7 +5,7 @@ using FluentNHibernate.Conventions.Instances;
 namespace Orchard.Data.Conventions {
     public class StringLengthMaxAttribute : StringLengthAttribute {
         public StringLengthMaxAttribute() : base(10000) {
-            // 10000 is an arbetrary number large enough to be in the nvarchar(max) range 
+            // 10000 is an arbitrary number large enough to be in the nvarchar(max) range 
         }
     }
 
