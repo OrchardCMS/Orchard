349fbaa393bd1ac2242b81d2abdfd1323021df85
Nathan Heskew <nathan.heskew@microsoft.com>
2010-03-01 01:25:13 -0800

    Removing the HomeController from the Orchard assembly
    --HG--
    branch : dev

--- src/Orchard/Environment/ShellBuilders/SafeModeShellContainerFactory.cs
+++ src/Orchard/Environment/ShellBuilders/SafeModeShellContainerFactory.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Web;
-using System.Web.Mvc;
 using System.Web.Routing;
 using Autofac;
 using Orchard.ContentManagement;
