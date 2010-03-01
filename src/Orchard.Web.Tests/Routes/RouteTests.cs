2ea51a4d7288177ab533867564beaa4b61ffac25
Nathan Heskew <nathan.heskew@microsoft.com>
2010-03-01 10:56:53 -0800

    Removing tests for the default (controller/action/id) route.
    --HG--
    branch : dev

--- src/Orchard.Web.Tests/Routes/RouteTests.cs
+++ src/Orchard.Web.Tests/Routes/RouteTests.cs
@@ -21,57 +21,6 @@ namespace Orchard.Web.Tests.Routes {
         }
 
         [Test]
-        public void RouteWithControllerNoActionNoId() {
-            // Arrange
-            var context = new StubContext("~/controller1");
-            var routes = new RouteCollection();
-            MvcApplication.RegisterRoutes(routes);
-
-            // Act
-            var routeData = routes.GetRouteData(context);
-
-            // Assert
-            Assert.That(routeData, Is.Not.Null);
-            Assert.That(routeData.Values["controller"], Is.EqualTo("controller1"));
-            Assert.That(routeData.Values["action"], Is.EqualTo("Index"));
-            Assert.That(routeData.Values["id"], Is.EqualTo(""));
-        }
-
-        [Test]
-        public void RouteWithControllerWithActionNoId() {
-            // Arrange
-            var context = new StubContext("~/controller1/action2");
-            var routes = new RouteCollection();
-            MvcApplication.RegisterRoutes(routes);
-
-            // Act
-            var routeData = routes.GetRouteData(context);
-
-            // Assert
-            Assert.That(routeData, Is.Not.Null);
-            Assert.That(routeData.Values["controller"], Is.EqualTo("controller1"));
-            Assert.That(routeData.Values["action"], Is.EqualTo("action2"));
-            Assert.That(routeData.Values["id"], Is.EqualTo(""));
-        }
-
-        [Test]
-        public void RouteWithControllerWithActionWithId() {
-            // Arrange
-            var context = new StubContext("~/controller1/action2/id3");
-            var routes = new RouteCollection();
-            MvcApplication.RegisterRoutes(routes);
-
-            // Act
-            var routeData = routes.GetRouteData(context);
-
-            // Assert
-            Assert.That(routeData, Is.Not.Null);
-            Assert.That(routeData.Values["controller"], Is.EqualTo("controller1"));
-            Assert.That(routeData.Values["action"], Is.EqualTo("action2"));
-            Assert.That(routeData.Values["id"], Is.EqualTo("id3"));
-        }
-
-        [Test]
         public void RouteWithTooManySegments() {
             // Arrange
             var context = new StubContext("~/a/b/c/d");
