c281c2a313e03267b7a72a3de7214c5cef01bb17
Suha Can <Suha.Can@microsoft.com>
2010-03-02 18:11:08 -0800

    - Support for MVC areas as Orchard modules (part1)
    --HG--
    branch : dev

--- src/Orchard/Mvc/ViewEngines/ViewEngineFilter.cs
+++ src/Orchard/Mvc/ViewEngines/ViewEngineFilter.cs
@@ -53,7 +53,7 @@ namespace Orchard.Mvc.ViewEngines {
 
 
             var modules = _extensionManager.ActiveExtensions()
-                .Where(x => x.Descriptor.ExtensionType == "Module");
+                .Where(x => x.Descriptor.ExtensionType == "Module" || x.Descriptor.ExtensionType == "Area");
 
             var moduleLocations = modules.Select(x => Path.Combine(x.Descriptor.Location, x.Descriptor.Name));
             var moduleViewEngines = _viewEngineProviders
