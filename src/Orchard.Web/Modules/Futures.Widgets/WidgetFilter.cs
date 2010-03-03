3d7a6a517b019688776fff0cce8355a06c344d89
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-03-03 11:32:00 -0800

    Adding a Futures project to hold Widget concept
    Conceptual implementation of Widget and HasWidgets parts
    Adds "HtmlWidget" content type and "Widget" display type
    WidgetFilter adds widget display view models to named zones (for non-admin requests)
    
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Futures.Widgets/WidgetFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Futures.Widgets.Models;
+using Orchard.ContentManagement;
+using Orchard.Mvc.Filters;
+using Orchard.Mvc.ViewModels;
+using Orchard.Settings;
+using Orchard.UI.Admin;
+
+namespace Futures.Widgets {
+    public class WidgetFilter : FilterProvider, IActionFilter {
+        private readonly IContentManager _contentManager;
+
+        public WidgetFilter(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
+        public virtual ISite CurrentSite { get; set; }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext) {
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext) {
+            var model = BaseViewModel.From(filterContext.Result);
+            if (model == null || AdminFilter.IsApplied(filterContext.RequestContext)) {
+                return;
+            }
+
+            var siteWidgets = CurrentSite.As<HasWidgets>();
+            if (siteWidgets == null) {
+                return;
+            }
+
+            var zones = model.Zones;
+            foreach (var widget in siteWidgets.Widgets) {
+                zones.AddDisplayItem(
+                    widget.Record.Zone + ":" + widget.Record.Position,
+                    _contentManager.BuildDisplayModel(widget, "Widget"));
+            }
+        }
+    }
+}
