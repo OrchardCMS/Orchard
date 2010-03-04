a238d0e0d076a2df586a039f9d34b8e86f6f4a38
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-03-03 17:36:54 -0800

    Relocating the default role/permission assignments
    Now that there is a SetupController that is a more appropriate place to simulate
    one-time-only module activation. This avoids a problem where default permissions
    would be re-applied when the app domain restarted.
    
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Setup/Controllers/SetupController.cs
+++ src/Orchard.Web/Modules/Orchard.Setup/Controllers/SetupController.cs
@@ -8,6 +8,7 @@ using Orchard.Core.Settings.Models;
 using Orchard.Data;
 using Orchard.Environment;
 using Orchard.Environment.Configuration;
+using Orchard.Extensions;
 using Orchard.Security;
 using Orchard.Settings;
 using Orchard.Setup.ViewModels;
@@ -101,7 +102,11 @@ namespace Orchard.Setup.Controllers {
                         themeService.SetSiteTheme("Classic");
 
                         var contentManager = finiteEnvironment.Resolve<IContentManager>();
-                         
+
+                        // simulate installation-time module activation events
+                        var hackInstallationGenerator = finiteEnvironment.Resolve<IHackInstallationGenerator>();
+                        hackInstallationGenerator.GenerateInstallEvents();
+
                         // create home page as a CMS page
                         var page = contentManager.Create("page", VersionOptions.Draft);
                         page.As<BodyAspect>().Text = "<p>Welcome to Orchard!</p><p>Congratulations, you've successfully set-up your Orchard site.</p><p>This is the home page of your new site. We've taken the liberty to write here about a few things you could look at next in order to get familiar with the application. Once you feel confident you don't need this anymore, just click <a href=\"Admin/Pages/Edit/3\">Edit</a> to go into edit mode and replace this with whatever you want on your home page to make it your own.</p><p>One thing you could do (but you don't have to) is go into <a href=\"Admin/Settings\">Manage Settings</a> (follow the <a href=\"Admin\">Admin</a> link and then look for it under \"Settings\" in the menu on the left) and check that everything is configured the way you want.</p><p>You probably want to make the site your own. One of the ways you can do that is by clicking <a href=\"Admin/Themes\">Manage Themes</a> in the admin menu. A theme is a packaged look and feel that affects the whole site. We have installed a few themes already, but you'll also be able to browse through an online gallery of themes created by other users of Orchard.</p><p>Next, you can start playing with the content types that we installed. For example, go ahead and click <a href=\"Admin/Pages/Create\">Add New Page</a> in the admin menu and create an \"about\" page. Then, add it to the navigation menu by going to <a href=\"Admin/Navigation\">Manage Menu</a>. You can also click <a href=\"Admin/Blogs/Create\">Add New Blog</a> and start posting by clicking \"Add New Post\".</p><p>Finally, Orchard has been designed to be extended. It comes with a few built-in modules such as pages and blogs or themes. You can install new themes by going to <a href=\"Admin/Themes\">Manage Themes</a> and clicking <a href=\"Admin/Themes/Install\">Install a new Theme</a>. Like for themes, modules are created by other users of Orchard just like you so if you feel up to it, please <a href=\"http://www.orchardproject.net/\">consider participating</a>.</p><p>--The Orchard Crew</p>";
