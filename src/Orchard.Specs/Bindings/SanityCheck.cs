85a08396ddda350ea068200f33ebff6dbf31045a
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-04-05 13:30:02 -0700

    Initial SpecFlow project and adjusting for x64 testing
    Added bits from SpecFlow 1.2 install
    Added lib/specflow libraries and nearly empty Spec test project
    Updating Orchard.proj to use NUnit task and locate test projects by pattern instead of fixed list
    
    --HG--
    branch : dev

---
+++ src/Orchard.Specs/Bindings/SanityCheck.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Orchard.Specs.Bindings {
+    [Binding]
+    public class SanityCheck {
+        private bool _runSteps;
+        private bool _valueSet;
+        private int _value;
+
+        [Given("I have a scenario")]
+        public void GivenIHaveAScenario() {
+
+        }
+
+        [When("I run steps")]
+        public void WhenIRunSteps() {
+            _runSteps = true;
+        }
+
+
+        [When(@"they have values like ""(.*)""")]
+        public void WhenTheyHaveValuesLike(int value) {
+            Assert.That(_valueSet, Is.False);
+            _value = value;
+            _valueSet = true;
+        }
+
+        [Then("they run")]
+        public void ThenTheyRun() {
+            Assert.That(_runSteps, Is.True);
+        }
+
+        [Then("values like five are captured")]
+        public void ThenValuesLikeFiveAreCaptured() {
+            Assert.That(_valueSet, Is.True);
+            Assert.That(_value, Is.EqualTo(5));
+        }
+    }
+}
