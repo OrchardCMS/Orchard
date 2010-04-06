7e36d8bccbdf42b91c9c046af39788db38714f05
Suha Can <Suha.Can@microsoft.com>
2010-04-05 19:40:41 -0700

    - Adding Arguments to CommandContext.
    --HG--
    branch : dev

--- src/Orchard/Commands/CommandContext.cs
+++ src/Orchard/Commands/CommandContext.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace Orchard.Commands {
     public class CommandContext {
         public string Input { get; set; }
         public string Output { get; set; }
         public string Command { get; set; }
+        public IEnumerable<string> Arguments { get; set; }
         public NameValueCollection Switches { get; set; }
     }
 }
