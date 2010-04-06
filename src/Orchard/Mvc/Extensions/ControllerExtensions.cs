76b5297713b22a2723453cdd16b59242c32efde2
Suha Can <Suha.Can@microsoft.com>
2010-04-06 13:23:59 -0700

    - Fixing build break due to missing file. - Code convention enforcement to previous contribution.

--- src/Orchard/Mvc/Extensions/ControllerExtensions.cs
+++ src/Orchard/Mvc/Extensions/ControllerExtensions.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Web.Mvc;
-using Orchard.Mvc.Results;
 
 namespace Orchard.Mvc.Extensions {
     public static class ControllerExtensions {
         public static RedirectResult ReturnUrlRedirect(this Controller controller) {
             var request = controller.HttpContext.Request;
             Uri returnUrl = null;
-            try
-            {
+            try {
                 returnUrl = new Uri(request.QueryString["ReturnUrl"]);
             }
-            catch
-            {
-                try
-                {
+            catch {
+                try {
                     returnUrl =
                         new Uri(string.Format("{0}://{1}{2}{3}", request.Url.Scheme, request.Url.Host,
                                               request.Url.Port != 80 ? ":" + request.Url.Port : "",
@@ -26,14 +22,10 @@ namespace Orchard.Mvc.Extensions {
             if (returnUrl != null &&
                 returnUrl.Scheme == request.Url.Scheme &&
                 returnUrl.Port == request.Url.Port &&
-                returnUrl.Host == request.Url.Host)
-            {
+                returnUrl.Host == request.Url.Host) {
                 return new RedirectResult(returnUrl.ToString());
             }
-            else
-            {
-                return new RedirectResult("~/");
-            }
+            return new RedirectResult("~/");
         }
     }
 }
