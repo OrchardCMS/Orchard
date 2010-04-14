1c00ffb3b6bb943d9cb34b1ce573ec22dbf673b5
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-04-14 15:07:12 -0700

    Adding implementation notes to some service interfaces
    --HG--
    branch : dev

--- src/Orchard/Environment/Topology/ITopologyDescriptorProvider.cs
+++ src/Orchard/Environment/Topology/ITopologyDescriptorProvider.cs
@@ -1,7 +1,15 @@
 ﻿using Orchard.Environment.Topology.Models;
 
 namespace Orchard.Environment.Topology {
+    /// <summary>
+    /// Service resolved out of the shell container. Primarily used by host.
+    /// </summary>
     public interface ITopologyDescriptorProvider {
+        /// <summary>
+        /// Uses shell-specific database or other resources to return 
+        /// the current "correct" configuration. The host will use this information
+        /// to reinitialize the shell.
+        /// </summary>
         ShellTopologyDescriptor GetTopologyDescriptor();
     }
 }
