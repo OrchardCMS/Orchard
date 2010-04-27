bb2b8053f3337ca37ea270d818ed0db529e9acf9
Renaud Paquay <rpaquay@microsoft.com>
2010-04-26 18:11:53 -0700

    Revove unused class
    --HG--
    branch : dev

--- src/Orchard/Environment/Extensions/ShellTopology.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Orchard.Environment.Extensions {
-    public class ShellTopology_Obsolete {
-        public IEnumerable<Type> Types { get; set; }
-    }
-}
