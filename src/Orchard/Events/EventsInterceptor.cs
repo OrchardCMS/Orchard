bfc6edc1dde18f4acc825ccfb397cdfebd99de31
Suha Can <Suha.Can@microsoft.com>
2010-05-10 13:35:07 -0700

    - An autofac events module for dynamic proxy creation when an interface of type IEventHandler is resolved through DI. - Intercept method on the proxy calls Notify on the event-bus with appropriate conversion of event name and parameters.
    --HG--
    branch : dev

---
+++ src/Orchard/Events/EventsInterceptor.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Castle.Core.Interceptor;
+
+namespace Orchard.Events {
+    public class EventsInterceptor : IInterceptor {
+        private readonly IEventBus _eventBus;
+
+        public EventsInterceptor(IEventBus eventBus) {
+            _eventBus = eventBus;
+        }
+
+        public void Intercept(IInvocation invocation) {
+            var interfaceName = invocation.Method.DeclaringType.Name;
+            var methodName = invocation.Method.Name;
+
+            var data = invocation.Method.GetParameters()
+                .Select((parameter, index) => new { parameter.Name, Value = invocation.Arguments[index] })
+                .ToDictionary(kv => kv.Name, kv => kv.Value.ToString());
+
+            _eventBus.Notify(interfaceName + "_" + methodName, data);
+        }
+    }
+}
---
+++ src/Orchard/Events/EventsModule.cs
@@ -0,0 +1,10 @@
+﻿using Autofac;
+
+namespace Orchard.Events {
+    public class EventsModule : Module {
+        protected override void Load(ContainerBuilder builder) {
+            builder.RegisterSource(new EventsRegistrationSource());
+            base.Load(builder);
+        }
+    }
+}
