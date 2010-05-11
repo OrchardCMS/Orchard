e8190f0e32eb525b892c9eb0ff42d71aab998abb
Sebastien Ros <sebros@microsoft.com>
2010-05-11 15:02:43 -0700

    Corrected urls generation on multi tenants listing with Azure
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.MultiTenancy/Extensions/UrlHelperExtensions.cs
+++ src/Orchard.Web/Modules/Orchard.MultiTenancy/Extensions/UrlHelperExtensions.cs
@@ -4,15 +4,12 @@ using Orchard.Environment.Configuration;
 namespace Orchard.MultiTenancy.Extensions {
     public static class UrlHelperExtensions {
         public static string Tenant(this UrlHelper urlHelper, ShellSettings tenantShellSettings) {
-            //info: (heskew) might not keep the port insertion around beyond...
-            var port = urlHelper.RequestContext.HttpContext.Request.Url.Port;
             return string.Format(
-                "http://{0}{2}/{1}",
+                "http://{0}/{1}",
                 !string.IsNullOrEmpty(tenantShellSettings.RequestUrlHost)
                     ? tenantShellSettings.RequestUrlHost
-                    : urlHelper.RequestContext.HttpContext.Request.Url.Host,
-                tenantShellSettings.RequestUrlPrefix,
-                port != 80 ? string.Format(":{0}", port) : "");
+                    : urlHelper.RequestContext.HttpContext.Request.Headers["Host"],
+                tenantShellSettings.RequestUrlPrefix);
         }
     }
 }
