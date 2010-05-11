372010684d7d4dfa305f7e3e3cd56f9bda41ac79
Sebastien Ros <sebros@microsoft.com>
2010-05-11 16:50:25 -0700

    Change port of urls in multi tenant admin
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.MultiTenancy/Extensions/UrlHelperExtensions.cs
+++ src/Orchard.Web/Modules/Orchard.MultiTenancy/Extensions/UrlHelperExtensions.cs
@@ -4,11 +4,17 @@ using Orchard.Environment.Configuration;
 namespace Orchard.MultiTenancy.Extensions {
     public static class UrlHelperExtensions {
         public static string Tenant(this UrlHelper urlHelper, ShellSettings tenantShellSettings) {
-            return string.Format(
-                "http://{0}/{1}",
-                !string.IsNullOrEmpty(tenantShellSettings.RequestUrlHost)
-                    ? tenantShellSettings.RequestUrlHost
-                    : urlHelper.RequestContext.HttpContext.Request.Headers["Host"],
+            //info: (heskew) might not keep the port insertion around beyond...
+            var port = string.Empty;
+            string host = urlHelper.RequestContext.HttpContext.Request.Headers["Host"];
+
+            if(host.Contains(":"))
+                port = host.Substring(host.IndexOf(":"));
+
+             return string.Format(
+               "http://{0}/{1}",
+                 !string.IsNullOrEmpty(tenantShellSettings.RequestUrlHost)
+                     ? tenantShellSettings.RequestUrlHost + port : host,
                 tenantShellSettings.RequestUrlPrefix);
         }
     }
