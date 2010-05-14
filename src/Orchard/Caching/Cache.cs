d5cdc0d37e64eee033764f497752dded8f6fbe99
Suha Can <Suha.Can@microsoft.com>
2010-05-14 14:15:06 -0700

    - ICacheManager and ICache interfaces and default implementations for an Orchard cache. - Unit tests.
    --HG--
    branch : dev

---
+++ src/Orchard/Caching/Cache.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orchard.Caching {
+    public class Cache<TKey, TResult> : ICache<TKey, TResult> {
+        private readonly Dictionary<TKey, CacheEntry> _entries;
+
+        public Cache() {
+            _entries = new Dictionary<TKey, CacheEntry>();
+        }
+
+        #region Implementation of ICache<TKey,TResult>
+
+        public TResult Get(TKey key, Func<AcquireContext, TResult> acquire) {
+            CacheEntry entry;
+            if (!_entries.TryGetValue(key, out entry)) {
+                AcquireContext context = new AcquireContext();
+                entry = new CacheEntry {Result = acquire(context)};
+                _entries.Add(key, entry);
+            }
+            return entry.Result;
+        }
+
+        #endregion
+
+        public class CacheEntry {
+            public TResult Result { get; set; }
+        }
+    }
+
+}
---
+++ src/Orchard/Caching/DefaultCacheManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Castle.Core;
+
+namespace Orchard.Caching {
+    public class DefaultCacheManager : ICacheManager {
+        private readonly Dictionary<Pair<Type, Type>, object> _caches;
+
+        public DefaultCacheManager() {
+            _caches = new Dictionary<Pair<Type, Type>, object>();
+        }
+
+        #region Implementation of ICacheManager
+
+        public TResult Get<TKey, TResult>(TKey key, Func<AcquireContext, TResult> acquire) {
+            return GetCache<TKey, TResult>().Get(key, acquire);
+        }
+
+        public ICache<TKey, TResult> GetCache<TKey, TResult>() {
+            var cacheKey = new Pair<Type, Type>(typeof(TKey), typeof(TResult));
+            object value;
+            if (!_caches.TryGetValue(cacheKey, out value)) {
+                value = new Cache<TKey, TResult>();
+                _caches.Add(cacheKey, value);
+            }
+
+            return (ICache<TKey, TResult>)value;
+        }
+
+        #endregion
+    }
+}
