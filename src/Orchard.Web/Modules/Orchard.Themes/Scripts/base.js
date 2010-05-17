3341dd863e3ac80e06e69d71fa11427c61618f0d
Nathan Heskew <nathan.heskew@microsoft.com>
2010-05-17 10:13:02 -0700

    Updated the base JS (file) to use jQuery instead of $ across the board
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Themes/Scripts/base.js
+++ src/Orchard.Web/Modules/Orchard.Themes/Scripts/base.js
@@ -1,7 +1,7 @@
 ﻿//todo: (heskew) make use of the autofocus attribute instead
 jQuery.fn.extend({
     helpfullyFocus: function() {
-        var _this = $(this);
+        var _this = jQuery(this);
         var firstError = _this.find(".input-validation-error").first();
         // try to focus the first error on the page
         if (firstError.size() === 1) {
@@ -16,12 +16,12 @@ jQuery.fn.extend({
         return autofocus.focus();
     },
     toggleWhatYouControl: function() {
-        var _this = $(this);
-        var _controllees = $("[data-controllerid=" + _this.attr("id") + "]");
+        var _this = jQuery(this);
+        var _controllees = jQuery("[data-controllerid=" + _this.attr("id") + "]");
         var _controlleesAreHidden = _controllees.is(":hidden");
         if (_this.is(":checked") && _controlleesAreHidden) {
             _controllees.hide(); // <- unhook this when the following comment applies
-            $(_controllees.show()[0]).find("input").focus(); // <- aaaand a slideDown there...eventually
+            jQuery(_controllees.show()[0]).find("input").focus(); // <- aaaand a slideDown there...eventually
         } else if (!(_this.is(":checked") && _controlleesAreHidden)) {
             //_controllees.slideUp(200); <- hook this back up when chrome behaves, or when I care less
             _controllees.hide()
@@ -30,27 +30,27 @@ jQuery.fn.extend({
 });
 // collapsable areas - anything with a data-controllerid attribute has its visibility controlled by the id-ed radio/checkbox
 (function() {
-    $("[data-controllerid]").each(function() {
-        var controller = $("#" + $(this).attr("data-controllerid"));
+    jQuery("[data-controllerid]").each(function() {
+        var controller = jQuery("#" + jQuery(this).attr("data-controllerid"));
         if (controller.data("isControlling")) {
             return;
         }
         controller.data("isControlling", 1);
         if (!controller.is(":checked")) {
-            $("[data-controllerid=" + controller.attr("id") + "]").hide();
+            jQuery("[data-controllerid=" + controller.attr("id") + "]").hide();
         }
         if (controller.is(":checkbox")) {
-            controller.click($(this).toggleWhatYouControl);
+            controller.click(jQuery(this).toggleWhatYouControl);
         } else if (controller.is(":radio")) {
-            $("[name=" + controller.attr("name") + "]").click(function() { $("[name=" + $(this).attr("name") + "]").each($(this).toggleWhatYouControl); });
+            jQuery("[name=" + controller.attr("name") + "]").click(function() { jQuery("[name=" + jQuery(this).attr("name") + "]").each(jQuery(this).toggleWhatYouControl); });
         }
     });
 })();
 // inline form link buttons (form.inline.link button) swapped out for a link that submits said form
 (function() {
-    $("form.inline.link").each(function() {
-        var _this = $(this);
-        var link = $("<a class='wasFormInlineLink' href='.'/>");
+    jQuery("form.inline.link").each(function() {
+        var _this = jQuery(this);
+        var link = jQuery("<a class='wasFormInlineLink' href='.'/>");
         var button = _this.children("button").first();
         link.text(button.text())
             .addClass(button.attr("class"))
@@ -58,10 +58,10 @@ jQuery.fn.extend({
             .unload(function() { _this = 0; });
         _this.replaceWith(link);
         _this.css({ "position": "absolute", "left": "-9999em" });
-        $("body").append(_this);
+        jQuery("body").append(_this);
     });
 })();
 // a little better autofocus
-$(function() {
-    $("body").helpfullyFocus();
+jQuery(function() {
+    jQuery("body").helpfullyFocus();
 });
