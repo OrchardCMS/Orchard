a712d04743fcba3eacc8d34ad77f47d7e40e0a68
Suha Can <Suha.Can@microsoft.com>
2010-06-03 17:06:19 -0700

    - Comment in the websitefolder to remind me to fix a bug in directory monitoring.
    --HG--
    branch : dev

--- src/Orchard/Localization/Services/DefaultCultureManager.cs
+++ src/Orchard/Localization/Services/DefaultCultureManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Routing;
 using Orchard.Data;
 using Orchard.Localization.Records;
 
