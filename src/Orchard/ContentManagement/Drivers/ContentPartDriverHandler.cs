67bf391e5b85a723a713bbdbce3dc3c09962fd04
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-06-04 13:38:04 -0700

    Adding a missing file. Removing an extra div.
    --HG--
    branch : dev

--- src/Orchard/ContentManagement/Drivers/ContentPartDriverHandler.cs
+++ src/Orchard/ContentManagement/Drivers/ContentPartDriverHandler.cs
@@ -6,7 +6,7 @@ using Orchard.Logging;
 
 namespace Orchard.ContentManagement.Drivers {
     [UsedImplicitly]
-    public class ContentPartDriverHandler : IContentHandler {
+    public class ContentPartDriverHandler : ContentHandlerBase {
         private readonly IEnumerable<IContentPartDriver> _drivers;
 
         public ContentPartDriverHandler(IEnumerable<IContentPartDriver> drivers) {
@@ -16,28 +16,7 @@ namespace Orchard.ContentManagement.Drivers {
 
         public ILogger Logger { get; set; }
 
-        IEnumerable<ContentType> IContentHandler.GetContentTypes() {
-            return Enumerable.Empty<ContentType>();
-        }
-
-        void IContentHandler.Activating(ActivatingContentContext context) { }
-        void IContentHandler.Activated(ActivatedContentContext context) { }
-        void IContentHandler.Creating(CreateContentContext context) { }
-        void IContentHandler.Created(CreateContentContext context) { }
-        void IContentHandler.Loading(LoadContentContext context) { }
-        void IContentHandler.Loaded(LoadContentContext context) { }
-        void IContentHandler.Versioning(VersionContentContext context) { }
-        void IContentHandler.Versioned(VersionContentContext context) { }
-        void IContentHandler.Publishing(PublishContentContext context) { }
-        void IContentHandler.Published(PublishContentContext context) { }
-        void IContentHandler.Removing(RemoveContentContext context) { }
-        void IContentHandler.Removed(RemoveContentContext context) { }
-        void IContentHandler.Indexing(IndexContentContext context) { }
-        void IContentHandler.Indexed(IndexContentContext context) { }
-
-        void IContentHandler.GetContentItemMetadata(GetContentItemMetadataContext context) { }
-
-        void IContentHandler.BuildDisplayModel(BuildDisplayModelContext context) {
+        public override void BuildDisplayModel(BuildDisplayModelContext context) {
             _drivers.Invoke(driver => {
                                 var result = driver.BuildDisplayModel(context);
                                 if (result != null)
@@ -45,7 +24,7 @@ namespace Orchard.ContentManagement.Drivers {
                             }, Logger);
         }
 
-        void IContentHandler.BuildEditorModel(BuildEditorModelContext context) {
+        public override void BuildEditorModel(BuildEditorModelContext context) {
             _drivers.Invoke(driver => {
                                 var result = driver.BuildEditorModel(context);
                                 if (result != null)
@@ -53,7 +32,7 @@ namespace Orchard.ContentManagement.Drivers {
                             }, Logger);
         }
 
-        void IContentHandler.UpdateEditorModel(UpdateEditorModelContext context) {
+        public override void UpdateEditorModel(UpdateEditorModelContext context) {
             _drivers.Invoke(driver => {
                                 var result = driver.UpdateEditorModel(context);
                                 if (result != null)
