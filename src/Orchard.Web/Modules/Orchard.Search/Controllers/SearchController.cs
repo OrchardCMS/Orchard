182801d1fa2c89df924a0d09b5639cc66101bf68
Nathan Heskew <nathan.heskew@microsoft.com>
2010-06-05 01:32:48 -0700

    Fixing the search pager to not lose the last page
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Search/Controllers/SearchController.cs
+++ src/Orchard.Web/Modules/Orchard.Search/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Orchard.ContentManagement;
 using Orchard.Search.Services;
@@ -26,7 +27,7 @@ namespace Orchard.Search.Controllers {
             var results = _searchService.Query(q);
 
             searchViewModel.Count = results.Count();
-            searchViewModel.TotalPageCount = searchViewModel.Count/searchViewModel.PageSize;
+            searchViewModel.TotalPageCount = (int)Math.Round((decimal)searchViewModel.Count/searchViewModel.PageSize);
             //todo: deal with page requests beyond result count
             searchViewModel.ResultsPage = results
                 .Select(result => new SearchResultViewModel {
