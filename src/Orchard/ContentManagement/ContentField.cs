521b415f884b1e8c157a596821c36bc6e87bfd3e
Suha Can <Suha.Can@microsoft.com>
2010-06-08 16:56:02 -0700

    - Adding a method to the CultureManager to return the default site culture (as opposed to current culture)
    --HG--
    branch : dev

--- src/Orchard/ContentManagement/ContentField.cs
+++ src/Orchard/ContentManagement/ContentField.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Orchard.ContentManagement.MetaData.Models;
+﻿using Orchard.ContentManagement.MetaData.Models;
 
 namespace Orchard.ContentManagement {
     public class ContentField {
