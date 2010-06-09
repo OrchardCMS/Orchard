85a0f45c5de54c6bc2b6bd70813ad7028a8398a1
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-06-09 15:39:35 -0700

    Using T.Plural in code
    --HG--
    branch : dev

--- src/Orchard/Localization/Localizer.cs
+++ src/Orchard/Localization/Localizer.cs
@@ -1,8 +1,12 @@
 using System.Linq;
+using Orchard.Localization;
 
 namespace Orchard.Localization {
     public delegate LocalizedString Localizer(string text, params object[] args);
 
+}
+
+namespace Orchard.Mvc.Html {
     public static class LocalizerExtensions {
         public static LocalizedString Plural(this Localizer T, string textSingular, string textPlural, int count, params object[] args) {
             return T(count == 1 ? textSingular : textPlural, new object[] { count }.Concat(args).ToArray());
