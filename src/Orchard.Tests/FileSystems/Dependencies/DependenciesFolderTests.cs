4ad1abc4c9bf90ddeed404272ccdf6e578bf5ac4
Renaud Paquay <rpaquay@microsoft.com>
2010-06-15 18:56:37 -0700

    Add more unit test for IDependenciesFolder
    --HG--
    branch : dev

--- src/Orchard.Tests/FileSystems/Dependencies/DependenciesFolderTests.cs
+++ src/Orchard.Tests/FileSystems/Dependencies/DependenciesFolderTests.cs
@@ -1,57 +1,56 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using NUnit.Framework;
 using Orchard.FileSystems.AppData;
 using Orchard.FileSystems.Dependencies;
 using Orchard.Tests.FileSystems.AppData;
+using Orchard.Tests.Stubs;
 
 namespace Orchard.Tests.FileSystems.Dependencies {
     [TestFixture]
     public class DependenciesFolderTests {
-        private string _tempFolder;
-        private IAppDataFolder _appDataFolder;
-        private IDependenciesFolder _dependenciesFolder;
 
         [SetUp]
         public void Init() {
-            _tempFolder = Path.GetTempFileName();
-            File.Delete(_tempFolder);
-
-            _appDataFolder = AppDataFolderTests.CreateAppDataFolder(_tempFolder);
-            _dependenciesFolder = new DefaultDependenciesFolder(new Stubs.StubCacheManager(), _appDataFolder);
         }
 
         [TearDown]
         public void Term() {
-            Directory.Delete(_tempFolder, true);
         }
 
         [Test]
         public void LoadDescriptorsShouldReturnEmptyList() {
-            if (Directory.Exists(_tempFolder))
-                Directory.Delete(_tempFolder, true);
-            var e = _dependenciesFolder.LoadDescriptors();
+            var clock = new StubClock();
+            var appDataFolder = new StubAppDataFolder(clock);
+            var dependenciesFolder = new DefaultDependenciesFolder(new StubCacheManager(), appDataFolder);
+
+            var e = dependenciesFolder.LoadDescriptors();
             Assert.That(e, Is.Empty);
         }
 
         [Test]
         public void StoreDescriptorsShouldWork() {
-            if (Directory.Exists(_tempFolder))
-                Directory.Delete(_tempFolder, true);
+            var clock = new StubClock();
+            var appDataFolder = new StubAppDataFolder(clock);
+            var dependenciesFolder = new DefaultDependenciesFolder(new StubCacheManager(), appDataFolder);
+
             var d = new DependencyDescriptor {
                 LoaderName = "test",
                 Name = "name",
                 VirtualPath = "~/bin"
             };
             
-            _dependenciesFolder.StoreDescriptors(new [] { d });
-            var e = _dependenciesFolder.LoadDescriptors();
+            dependenciesFolder.StoreDescriptors(new [] { d });
+            var e = dependenciesFolder.LoadDescriptors();
             Assert.That(e, Has.Count.EqualTo(1));
         }
 
         [Test]
         public void StoreDescriptorsShouldNoOpIfNoChanges() {
-            if (Directory.Exists(_tempFolder))
-                Directory.Delete(_tempFolder, true);
+            var clock = new StubClock();
+            var appDataFolder = new StubAppDataFolder(clock);
+            var dependenciesFolder = new DefaultDependenciesFolder(new StubCacheManager(), appDataFolder);
+
             var d1 = new DependencyDescriptor {
                 LoaderName = "test1",
                 Name = "name1",
@@ -64,13 +63,42 @@ namespace Orchard.Tests.FileSystems.Dependencies {
                 VirtualPath = "~/bin2"
             };
 
-            _dependenciesFolder.StoreDescriptors(new[] { d1, d2 });
-            var dateTime1 = File.GetLastWriteTimeUtc(Path.Combine(_tempFolder, "Dependencies", "Dependencies.xml"));
+            dependenciesFolder.StoreDescriptors(new[] { d1, d2 });
+            var dateTime1 = appDataFolder.GetLastWriteTimeUtc(Path.Combine("Dependencies", "Dependencies.xml"));
+            clock.Advance(TimeSpan.FromMinutes(1));
 
-            _dependenciesFolder.StoreDescriptors(new[] { d2, d1 });
-            var dateTime2 = File.GetLastWriteTimeUtc(Path.Combine(_tempFolder, "Dependencies", "Dependencies.xml"));
+            dependenciesFolder.StoreDescriptors(new[] { d2, d1 });
+            var dateTime2 = appDataFolder.GetLastWriteTimeUtc(Path.Combine("Dependencies", "Dependencies.xml"));
             Assert.That(dateTime1, Is.EqualTo(dateTime2));
-            
+        }
+
+        [Test]
+        public void StoreDescriptorsShouldStoreIfChanges() {
+            var clock = new StubClock();
+            var appDataFolder = new StubAppDataFolder(clock);
+            var dependenciesFolder = new DefaultDependenciesFolder(new StubCacheManager(), appDataFolder);
+
+            var d1 = new DependencyDescriptor {
+                LoaderName = "test1",
+                Name = "name1",
+                VirtualPath = "~/bin1"
+            };
+
+            var d2 = new DependencyDescriptor {
+                LoaderName = "test2",
+                Name = "name2",
+                VirtualPath = "~/bin2"
+            };
+
+            dependenciesFolder.StoreDescriptors(new[] { d1, d2 });
+            var dateTime1 = appDataFolder.GetLastWriteTimeUtc(Path.Combine("Dependencies", "Dependencies.xml"));
+            clock.Advance(TimeSpan.FromMinutes(1));
+
+            d1.LoaderName = "bar";
+
+            dependenciesFolder.StoreDescriptors(new[] { d2, d1 });
+            var dateTime2 = appDataFolder.GetLastWriteTimeUtc(Path.Combine("Dependencies", "Dependencies.xml"));
+            Assert.That(dateTime1 + TimeSpan.FromMinutes(1), Is.EqualTo(dateTime2));
         }
     }
 }
---
+++ src/Orchard.Tests/Stubs/StubAppDataFolder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Orchard.Caching;
+using Orchard.FileSystems.AppData;
+using Orchard.Services;
+
+namespace Orchard.Tests.Stubs {
+    public class StubAppDataFolder : IAppDataFolder {
+        private readonly IClock _clock;
+        private readonly StubFileSystem _stubFileSystem;
+
+        public StubAppDataFolder(IClock clock) {
+            _clock = clock;
+            _stubFileSystem = new StubFileSystem(_clock);
+        }
+
+        public StubFileSystem StubFileSystem {
+            get { return _stubFileSystem; }
+        }
+
+        public IEnumerable<string> ListFiles(string path) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ListDirectories(string path) {
+            throw new NotImplementedException();
+        }
+
+        public bool FileExists(string path) {
+            return _stubFileSystem.GetFileEntry(path) != null;
+        }
+
+        public string Combine(params string[] paths) {
+            return Path.Combine(paths).Replace(Path.DirectorySeparatorChar, '/');
+        }
+
+        public void CreateFile(string path, string content) {
+            throw new NotImplementedException();
+        }
+
+        public Stream CreateFile(string path) {
+            return _stubFileSystem.CreateFile(path);
+        }
+
+        public string ReadFile(string path) {
+            throw new NotImplementedException();
+        }
+
+        public Stream OpenFile(string path) {
+            return _stubFileSystem.OpenFile(path);
+        }
+
+        public void DeleteFile(string path) {
+            throw new NotImplementedException();
+        }
+
+        public void CreateDirectory(string path) {
+            var entry = _stubFileSystem.CreateDirectoryEntry(path);
+        }
+
+        public IVolatileToken WhenPathChanges(string path) {
+            return _stubFileSystem.WhenPathChanges(path);
+        }
+
+        public string MapPath(string path) {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLastWriteTimeUtc(string path) {
+            var entry = _stubFileSystem.GetFileEntry(path);
+            if (entry == null)
+                throw new InvalidOperationException();
+            return entry.LastWriteTime;
+        }
+    }
+}
