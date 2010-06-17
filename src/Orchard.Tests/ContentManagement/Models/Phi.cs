a1cc5d579be81521821e276220e1ec8065b3861a
Suha Can <Suha.Can@microsoft.com>
2010-06-17 12:44:19 -0700

    - A couple basic test cases for welding fields to parts.
    --HG--
    branch : dev

---
+++ src/Orchard.Tests/ContentManagement/Models/Phi.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData.Models;
+
+namespace Orchard.Tests.ContentManagement.Models {
+    public class Phi : ContentField {
+        public Phi() {
+            PartFieldDefinition = new ContentPartDefinition.Field(new ContentFieldDefinition("Phi"), "Phi", new Dictionary<string, string>());
+        }
+    }
+}
