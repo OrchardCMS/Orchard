e8c5addc2d52be5c96f409b77f61511daae3e5eb
Suha Can <Suha.Can@microsoft.com>
2010-06-22 13:00:42 -0700

    - Adding an item reference content field. - Moving text content field editor from parts to fields folder.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Core/Common/Handlers/ItemReferenceContentFieldHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
+
+namespace Orchard.Core.Common.Handlers {
+    public class ItemReferenceContentFieldHandler : ContentFieldHandler {
+        public ItemReferenceContentFieldHandler(IEnumerable<IContentFieldDriver> contentFieldDrivers) : base(contentFieldDrivers) { }
+    }
+}
---
+++ src/Orchard.Web/Core/Common/Views/DisplayTemplates/Fields/Common.ItemReferenceContentField.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<ItemReferenceContentFieldDisplayViewModel>" %>
+<%@ Import Namespace="Orchard.Core.Common.ViewModels"%>
+<%= Html.ItemDisplayLink(Model.Item) %>
