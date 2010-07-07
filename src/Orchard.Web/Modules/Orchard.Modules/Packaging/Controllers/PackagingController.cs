882b1f6df5fde43851bf720f7239e71d3efeff8f
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-07-06 19:00:49 -0700

    Moving action result into its own file
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Modules/Packaging/Controllers/PackagingController.cs
+++ src/Orchard.Web/Modules/Orchard.Modules/Packaging/Controllers/PackagingController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 using Orchard.Environment.Extensions;
@@ -102,25 +101,6 @@ namespace Orchard.Modules.Packaging.Controllers {
             _packageManager.Install(packageData);
             _notifier.Information(T("Installed module"));
             return RedirectToAction("Modules");
-        }
-    }
-
-    public class DownloadStreamResult : ActionResult {
-        public string FileName { get; set; }
-        public string ContentType { get; set; }
-        public Stream Stream { get; set; }
-
-        public DownloadStreamResult(string fileName, string contentType, Stream stream) {
-            FileName = fileName;
-            ContentType = contentType;
-            Stream = stream;
-        }
-
-        public override void ExecuteResult(ControllerContext context) {
-            context.HttpContext.Response.ContentType = ContentType;
-            context.HttpContext.Response.AddHeader("content-disposition", "attachment; filename=\"" + FileName + "\"");
-            Stream.Seek(0, SeekOrigin.Begin);
-            Stream.CopyTo(context.HttpContext.Response.OutputStream);
         }
     }
 }
