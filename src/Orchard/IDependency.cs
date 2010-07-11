1c22285c3a1ccf026964659100fe28e562c84f30
Renaud Paquay <rpaquay@microsoft.com>
2010-07-11 13:35:05 -0700

    Documentation
    --HG--
    branch : dev

--- src/Orchard/IDependency.cs
+++ src/Orchard/IDependency.cs
@@ -2,12 +2,21 @@
 using Orchard.Logging;
 
 namespace Orchard {
+    /// <summary>
+    /// Base interface for services that are instantiated per unit of work (i.e. web request).
+    /// </summary>
     public interface IDependency {
     }
 
+    /// <summary>
+    /// Base interface for services that are instantiated per shell/tenant.
+    /// </summary>
     public interface ISingletonDependency : IDependency {
     }
 
+    /// <summary>
+    /// Base interface for services that are instantiated per usage.
+    /// </summary>
     public interface ITransientDependency : IDependency {
     }
 
