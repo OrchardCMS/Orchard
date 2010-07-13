9b925a2a85fcb65611370634bd6912392df534d4
Nathan Heskew <nathan.heskew@microsoft.com>
2010-07-13 11:31:39 -0700

    Small update to the localization service's GetLocalizations w/ a note to also query for scheduled items
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Localization/Services/LocalizationService.cs
+++ src/Orchard.Web/Core/Localization/Services/LocalizationService.cs
@@ -33,13 +33,15 @@ namespace Orchard.Core.Localization.Services {
         IEnumerable<IContent> ILocalizationService.GetLocalizations(IContent content) {
             var localized = content.As<Localized>();
 
+            //todo: (heskew) get scheduled content as well
+
             if (localized.MasterContentItem != null)
-                return _contentManager.Query(content.ContentItem.ContentType).Join<LocalizedRecord>()
+                return _contentManager.Query(VersionOptions.Latest, content.ContentItem.ContentType).Join<LocalizedRecord>()
                     .List()
                     .Select(i => i.As<Localized>())
                     .Where(l => l.Id != content.ContentItem.Id && (l.Id == localized.MasterContentItem.ContentItem.Id || l.MasterContentItem != null && l.MasterContentItem.ContentItem.Id == localized.MasterContentItem.ContentItem.Id));
 
-            return _contentManager.Query(content.ContentItem.ContentType).Join<LocalizedRecord>()
+            return _contentManager.Query(VersionOptions.Latest, content.ContentItem.ContentType).Join<LocalizedRecord>()
                 .List()
                 .Select(i => i.As<Localized>())
                 .Where(l => l.MasterContentItem != null && l.MasterContentItem.ContentItem.Id == content.ContentItem.Id);
