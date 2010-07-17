99d86a08f20e3bfd773f1edb99ce027d68a53612
Renaud Paquay <rpaquay@microsoft.com>
2010-07-17 15:49:33 -0700

    Temporary workaround for running SpecFlow tests with nunit
    --HG--
    branch : dev

--- src/Orchard.Specs/Hosting/WebHostAgent.cs
+++ src/Orchard.Specs/Hosting/WebHostAgent.cs
@@ -11,7 +11,11 @@ namespace Orchard.Specs.Hosting {
         }
 
         public void Shutdown() {
-            HostingEnvironment.InitiateShutdown();
+            //TODO: this line is required to properly shutdown the ASP.NET
+            //      host and release memory when running multiple SpecFlow tests.
+            //      However, nuint complains about an unhandled AppdomainUnloadedException
+            //      When we figure out a way around this, we will re-enable this line.
+            //HostingEnvironment.InitiateShutdown();
         }
     }
 }
