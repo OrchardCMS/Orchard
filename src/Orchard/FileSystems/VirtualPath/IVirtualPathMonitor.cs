0d2ad658a9943710d7f69ef00e41c956ee725768
Renaud Paquay <rpaquay@microsoft.com>
2010-07-18 13:39:46 -0700

    Remove unused method
    --HG--
    branch : dev

--- src/Orchard/FileSystems/VirtualPath/IVirtualPathMonitor.cs
+++ src/Orchard/FileSystems/VirtualPath/IVirtualPathMonitor.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Orchard.Caching;
+﻿using Orchard.Caching;
 
 namespace Orchard.FileSystems.VirtualPath {
     /// <summary>
@@ -7,9 +6,5 @@ namespace Orchard.FileSystems.VirtualPath {
     /// </summary>
     public interface IVirtualPathMonitor : IVolatileProvider {
         IVolatileToken WhenPathChanges(string virtualPath);
-        // Temporary until we have a generic mechanism for components
-        // to synchronize their dependencies through a Context.Monitor()
-        // interface
-        void WhenPathChanges(string virtualPath, Action action);
     }
 }
