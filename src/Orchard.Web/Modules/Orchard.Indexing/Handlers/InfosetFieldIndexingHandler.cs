f97351ee5994451e7c897aa5d1d71df686121f5f
Sebastien Ros <sebros@microsoft.com>
2010-07-20 14:34:01 -0700

    Index dynamic content fields - Create a new index field named {part}-{field} - Had to remove "protected" qualifier on Storage accessor
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.Indexing/Handlers/InfosetFieldIndexingHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.FieldStorage.InfosetStorage;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Indexing.Settings;
+
+namespace Orchard.Indexing.Handlers {
+    public class InfosetFieldIndexingHandler : ContentHandler {
+
+        public InfosetFieldIndexingHandler() {
+
+            OnIndexing<InfosetPart>(
+                (context, cp) => {
+                    var infosetPart = context.ContentItem.As<InfosetPart>();
+                    if ( infosetPart != null ) {
+                        foreach ( var part in infosetPart.ContentItem.Parts ) {
+                            foreach ( var field in part.PartDefinition.Fields ) {
+                                if ( field.Settings.GetModel<FieldIndexing>().Included ) {
+                                    var fieldName = field.Name;
+                                    var value = part.Fields.Where(f => f.Name == fieldName).First().Storage.Get<string>(null);
+                                    context.DocumentIndex.Add(String.Format("{0}-{1}", infosetPart.TypeDefinition.Name, fieldName.ToLower()), value).RemoveTags().Analyze();
+                                }
+                            }
+                        }
+                    }
+                });
+        }
+    }
+}
