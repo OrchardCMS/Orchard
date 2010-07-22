f4f0651c2257caf0cc96421a1bad7dfff0eb13b3
Renaud Paquay <rpaquay@microsoft.com>
2010-07-21 18:18:39 -0700

    Store default location for PublishLater part in initial settings
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Core/PublishLater/DataMigrations/PublishLaterDataMigration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Core.PublishLater.Models;
+using Orchard.Data.Migration;
+
+namespace Orchard.Core.PublishLater.DataMigrations {
+    public class PublishLaterDataMigration : DataMigrationImpl {
+        public int Create() {
+            ContentDefinitionManager.AlterPartDefinition(typeof(PublishLaterPart).Name, cfg => cfg
+                .WithLocation(new Dictionary<string, ContentLocation> {
+                    {"Display", new ContentLocation { Zone = "metadata", Position = "1" }},
+                    {"Summary", new ContentLocation { Zone = "metadata", Position = "1" }},
+                    {"SummaryAdmin", new ContentLocation { Zone = "metadata", Position = "1" }},
+                    {"Editor", new ContentLocation { Zone = "secondary", Position = "1" }}
+                }));
+            return 1;
+        }
+    }
+}
