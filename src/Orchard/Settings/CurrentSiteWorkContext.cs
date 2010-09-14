f4b7e29a073345991ab05bf11d66ab8efd8ae63f
Nathan Heskew <nathan.heskew@microsoft.com>
2010-09-13 18:27:42 -0700

    Added work context providers for the current site and user
    --HG--
    branch : theming

---
+++ src/Orchard/Settings/CurrentSiteWorkContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Orchard.Settings {
+    public class CurrentSiteWorkContext : IWorkContextStateProvider {
+        private readonly ISiteService _siteService;
+
+        public CurrentSiteWorkContext(ISiteService siteService) {
+            _siteService = siteService;
+        }
+
+        public T Get<T>(string name) {
+            if (name == "CurrentSite")
+                return (T)_siteService.GetSiteSettings();
+            return default(T);
+        }
+    }
+}
