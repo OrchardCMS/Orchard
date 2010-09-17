ae7012849a5808b4474c0608501a0e629aeedf26
Dave Reed <dareed@microsoft.com>
2010-09-17 16:43:12 -0700

    Moved all copies of jQuery/jQueryUI and related scripts, stylesheets, and images to the Orchard.jQuery module.
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.jQuery/jQueryResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.jQuery/jQueryResourceManifest.cs
@@ -2,6 +2,17 @@ namespace Orchard.UI.Resources {
     public class jQueryResourceManifest : ResourceManifest {
         public jQueryResourceManifest() {
             DefineScript("jQuery").SetUrl("jquery-1.4.2.min.js", "jquery-1.4.2.js").SetVersion("1.4.2");
+
+            // todo: include min versions
+            DefineScript("jQueryUtils").SetUrl("jquery.utils.js").SetDependencies("jQuery");
+            DefineScript("jQueryUI_Core").SetUrl("jquery.ui.core.js").SetVersion("1.8b1").SetDependencies("jQuery");
+            DefineScript("jQueryUI_Widget").SetUrl("jquery.ui.widget.js").SetVersion("1.8b1").SetDependencies("jQuery");
+            DefineScript("jQueryUI_DatePicker").SetUrl("jquery.ui.datepicker.js").SetVersion("1.8b1").SetDependencies("jQueryUI_Core", "jQueryUI_Widget");
+            DefineScript("jQueryUtils_TimePicker").SetUrl("ui.timepickr.js").SetVersion("0.7.0a").SetDependencies("jQueryUtils", "jQueryUI_Core");
+
+            DefineStyle("jQueryUtils_TimePicker").SetUrl("ui.timepickr.css");
+            DefineStyle("jQueryUI_Orchard").SetUrl("jquery-ui-1.7.2.custom.css").SetVersion("1.7.2");
+            DefineStyle("jQueryUI_DatePicker").SetUrl("ui.datepicker.css").SetDependencies("jQueryUI_Orchard").SetVersion("1.7.2");
         }
     }
 }
