7ba469cd9ad21b59c3c59316d5df2cf9ed9bbd14
Sebastien Ros <sebros@microsoft.com>
2010-09-20 16:50:12 -0700

    Security patch for MSA 2416728

--- src/Orchard/Mvc/Results/NotFoundResult.cs
+++ src/Orchard/Mvc/Results/NotFoundResult.cs
@@ -1,17 +1,11 @@
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Orchard.Mvc.Results {
     public class NotFoundResult : ViewResult {
         public override void ExecuteResult(ControllerContext context) {
-            ViewName = "NotFound";
-
-            ViewData = context.Controller.ViewData;
-            TempData = context.Controller.TempData;
-
-            base.ExecuteResult(context);
-
-            context.HttpContext.Response.StatusDescription = "File Not Found";
-            context.HttpContext.Response.StatusCode = 404;
+            throw new HttpException((int)HttpStatusCode.NotFound, "Resource not found");
         }
     }
 }
