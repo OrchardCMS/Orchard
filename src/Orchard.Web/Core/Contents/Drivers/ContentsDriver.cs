df66338fd10aba86ad4c0dc8d915ae44f1120958
Nathan Heskew <nathan.heskew@microsoft.com>
2010-09-21 21:38:20 -0700

    Finished converting Core.Contents
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Core/Contents/Drivers/ContentsDriver.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Contents.ViewModels;
+using Orchard.Core.ContentsLocation.Models;
+
+namespace Orchard.Core.Contents.Drivers {
+    public class ContentsDriver : ContentPartDriver<ContentPart> {
+        protected override DriverResult Display(ContentPart part, string displayType) {
+            var location = part.GetLocation(displayType, "secondary", null);
+            return ContentPartTemplate(new PublishContentViewModel(part.ContentItem), "Parts/Contents.Publish").LongestMatch(displayType, "Summary", "SummaryAdmin").Location(location);
+        }
+    }
+}
