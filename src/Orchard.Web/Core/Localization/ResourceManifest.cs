420baa72896fa169fd310b367fb2071b700eb0e9
Dave Reed <dareed@microsoft.com>
2010-09-27 14:22:18 -0700

    Fixed location of localization resource manfest cs file
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Localization/ResourceManifest.cs
+++ src/Orchard.Web/Core/Localization/ResourceManifest.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using Orchard.UI.Resources;
 
-namespace Orchard.Core.Localization.Resources {
+namespace Orchard.Core.Localization {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            // todo: move this file
             manifest.DefineStyle("Localization").SetUrl("base.css");
             manifest.DefineStyle("LocalizationAdmin").SetUrl("admin.css");
         }
