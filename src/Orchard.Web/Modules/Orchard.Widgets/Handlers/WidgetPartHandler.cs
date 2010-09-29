268989dad8a37c7a2b3c0aa8347b96705ae55f90
Suha Can <Suha.Can@microsoft.com>
2010-09-29 16:34:24 -0700

    - Basic persistency for the widget/layer parts.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.Widgets/Handlers/WidgetPartHandler.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using Orchard.Widgets.Models;
+
+namespace Orchard.Widgets.Handlers {
+    [UsedImplicitly]
+    public class WidgetPartHandler : ContentHandler {
+        public WidgetPartHandler(IRepository<WidgetPartRecord> widgetsRepository) {
+            Filters.Add(StorageFilter.For(widgetsRepository));
+        }
+    }
+}
