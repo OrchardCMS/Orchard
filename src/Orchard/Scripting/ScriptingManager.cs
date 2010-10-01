5fca0e53e3a64ba150694043b882137f58fbbfcf
Suha Can <Suha.Can@microsoft.com>
2010-10-01 16:33:16 -0700

    - Orchard.Scripting namespace to manage ruby scripts. - ScriptManager/ScriptRuntime implementations. - Unit Tests. - Adding IronRuby.dll, IronRuby.Libraries.dll, Microsoft.Dynamic.dll and Microsoft.Scripting.dll.
    --HG--
    branch : dev

---
+++ src/Orchard/Scripting/ScriptingManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Scripting.Hosting;
+
+namespace Orchard.Scripting {
+    public class ScriptingManager : IScriptingManager {
+        private readonly IScriptingRuntime _scriptingRuntime;
+        private ScriptScope _scriptingScope;
+
+        public ScriptingManager(IScriptingRuntime scriptingRuntime) {
+            _scriptingRuntime = scriptingRuntime;
+            _scriptingScope = _scriptingRuntime.GetRubyEngine().CreateScope();
+        }
+
+        #region IScriptingContext Members
+
+        public dynamic GetVariable(string name) {
+            return _scriptingScope.GetVariable(name);
+        }
+
+        public void SetVariable(string name, object value) {
+            _scriptingScope.SetVariable(name, value);
+        }
+
+        public dynamic Eval(string expression) {
+            var script = _scriptingRuntime.GetRubyEngine().CreateScriptSourceFromString(expression);
+            return script.Execute(_scriptingScope);
+        }
+
+        public void ExecuteFile(string fileName) {
+            _scriptingRuntime.GetRubyEngine().ExecuteFile(fileName, _scriptingScope);
+        }
+
+        public void SetScriptingScope(ScriptScope scriptScope) {
+            _scriptingScope = scriptScope;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Orchard/Scripting/ScriptingRuntime.cs
@@ -0,0 +1,18 @@
+﻿using IronRuby;
+using Microsoft.Scripting.Hosting;
+
+namespace Orchard.Scripting {
+    public class ScriptingRuntime : IScriptingRuntime {
+        private readonly ScriptRuntime _scriptingRuntime;
+
+        public ScriptingRuntime() {
+            var setup = new ScriptRuntimeSetup();
+            setup.LanguageSetups.Add(Ruby.CreateRubySetup());
+            _scriptingRuntime = new ScriptRuntime(setup);
+        }
+
+        public ScriptEngine GetRubyEngine() {
+            return _scriptingRuntime.GetEngine("ruby");
+        }
+    }
+}
