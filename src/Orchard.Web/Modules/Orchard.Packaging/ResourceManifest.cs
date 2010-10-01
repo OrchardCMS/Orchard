cd8660a884967cb91540fcb80ec8cae1590043d9
Dave Reed <dareed@microsoft.com>
2010-10-01 12:09:41 -0700

    Fix resource manifest for gallery -- should enable with the Gallery feature
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Packaging/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.Packaging/ResourceManifest.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Orchard.Environment.Extensions;
 using Orchard.UI.Resources;
 
 namespace Orchard.Packaging {
+    [OrchardFeature("Gallery")]
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             builder.Add().DefineStyle("PackagingAdmin").SetUrl("admin.css");
