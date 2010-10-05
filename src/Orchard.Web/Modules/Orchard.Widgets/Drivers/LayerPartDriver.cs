697251c06ace3afea23062d4f6d2b5f8e688388b
Andre Rodrigues <andrerod@microsoft.com>
2010-10-05 15:42:17 -0700

    Adding missing widget and layer drivers.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.Widgets/Drivers/LayerPartDriver.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.Annotations;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Widgets.Models;
+
+namespace Orchard.Widgets.Drivers {
+    [UsedImplicitly]
+    public class LayerPartDriver : ContentPartDriver<LayerPart> {
+    }
+}
