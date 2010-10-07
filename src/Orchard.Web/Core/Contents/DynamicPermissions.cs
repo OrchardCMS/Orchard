c5198b0fe80f08fbba6c2c445c49a57ff8315837
Sebastien Ros <sebros@microsoft.com>
2010-10-07 15:49:07 -0700

    Content types security refactoring - Moved AuthorizationEventHandler to Contents.Security
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Contents/DynamicPermissions.cs
+++ src/Orchard.Web/Core/Contents/DynamicPermissions.cs
@@ -9,14 +9,21 @@ using Orchard.Security.Permissions;
 
 namespace Orchard.Core.Contents {
     public class DynamicPermissions : IPermissionProvider {
-        public static readonly Permission PublishContent = new Permission { Description = "Publish or unpublish {0} for others", Name = "Publish_{0}", ImpliedBy = new[] { Permissions.PublishContent } };
-        public static readonly Permission PublishOwnContent = new Permission { Description = "Publish or unpublish {0}", Name = "PublishOwn_{0}", ImpliedBy = new[] { PublishContent, Permissions.PublishOwnContent } };
-        public static readonly Permission EditContent = new Permission { Description = "Edit {0} for others", Name = "Edit_{0}", ImpliedBy = new[] { PublishContent, Permissions.PublishContent } };
-        public static readonly Permission EditOwnContent = new Permission { Description = "Edit {0}", Name = "EditOwn_{0}", ImpliedBy = new[] { EditContent, PublishOwnContent, Permissions.EditOwnContent } };
-        public static readonly Permission DeleteContent = new Permission { Description = "Delete {0} for others", Name = "Delete_{0}", ImpliedBy = new[] { Permissions.DeleteContent } };
-        public static readonly Permission DeleteOwnContent = new Permission { Description = "Delete {0}", Name = "DeleteOwn_{0}", ImpliedBy = new[] { DeleteContent, Permissions.DeleteOwnContent } };
+        private static readonly Permission PublishContent = new Permission { Description = "Publish or unpublish {0} for others", Name = "Publish_{0}", ImpliedBy = new[] { Permissions.PublishContent } };
+        private static readonly Permission PublishOwnContent = new Permission { Description = "Publish or unpublish {0}", Name = "PublishOwn_{0}", ImpliedBy = new[] { PublishContent, Permissions.PublishOwnContent } };
+        private static readonly Permission EditContent = new Permission { Description = "Edit {0} for others", Name = "Edit_{0}", ImpliedBy = new[] { PublishContent, Permissions.PublishContent } };
+        private static readonly Permission EditOwnContent = new Permission { Description = "Edit {0}", Name = "EditOwn_{0}", ImpliedBy = new[] { EditContent, PublishOwnContent, Permissions.EditOwnContent } };
+        private static readonly Permission DeleteContent = new Permission { Description = "Delete {0} for others", Name = "Delete_{0}", ImpliedBy = new[] { Permissions.DeleteContent } };
+        private static readonly Permission DeleteOwnContent = new Permission { Description = "Delete {0}", Name = "DeleteOwn_{0}", ImpliedBy = new[] { DeleteContent, Permissions.DeleteOwnContent } };
 
-        public static readonly Permission[] PermissionTemplates = new[] {PublishContent, PublishOwnContent, EditContent, EditOwnContent, DeleteContent, DeleteOwnContent};
+        public static readonly Dictionary<string, Permission> PermissionTemplates = new Dictionary<string, Permission> {
+            {Permissions.PublishContent.Name, PublishContent},
+            {Permissions.PublishOwnContent.Name, PublishOwnContent},
+            {Permissions.EditContent.Name, EditContent},
+            {Permissions.EditOwnContent.Name, EditOwnContent},
+            {Permissions.DeleteContent.Name, DeleteContent},
+            {Permissions.DeleteOwnContent.Name, DeleteOwnContent}
+        };
 
         private readonly IContentDefinitionManager _contentDefinitionManager;
 
@@ -32,8 +39,8 @@ namespace Orchard.Core.Contents {
                 .Where(ctd => ctd.Settings.GetModel<ContentTypeSettings>().Creatable);
 
             foreach(var typeDefinition in creatableTypes) {
-                foreach ( var permissionTemplate in PermissionTemplates ) {
-                    yield return CreateDynamicPersion(permissionTemplate, typeDefinition);
+                foreach ( var permissionTemplate in PermissionTemplates.Values ) {
+                    yield return CreateDynamicPermission(permissionTemplate, typeDefinition);
                 }
             }
         }
@@ -42,12 +49,26 @@ namespace Orchard.Core.Contents {
             return Enumerable.Empty<PermissionStereotype>();
         }
 
-        public static Permission CreateDynamicPersion(Permission template, ContentTypeDefinition typeDefinition) {
+        /// <summary>
+        /// Returns a dynamic permission for a content type, based on a global content permission template
+        /// </summary>
+        public static Permission ConvertToDynamicPermission(Permission permission) {
+            if (PermissionTemplates.ContainsKey(permission.Name) ) {
+                return PermissionTemplates[permission.Name];
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Generates a permission dynamically for a content type
+        /// </summary>
+        public static Permission CreateDynamicPermission(Permission template, ContentTypeDefinition typeDefinition) {
             return new Permission {
                 Name = String.Format(template.Name, typeDefinition.Name),
                 Description = String.Format(template.Description, typeDefinition.DisplayName),
                 Category = typeDefinition.DisplayName,
-                ImpliedBy = (template.ImpliedBy ?? new Permission[0]).Select(t => CreateDynamicPersion(t, typeDefinition))
+                ImpliedBy = ( template.ImpliedBy ?? new Permission[0] ).Select(t => CreateDynamicPermission(t, typeDefinition))
             };
         }
     }
