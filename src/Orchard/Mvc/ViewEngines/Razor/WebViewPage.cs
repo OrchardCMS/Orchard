6dbbe6e1f0bb8f4652d534a331458b241ae04808
Nathan Heskew <nathan.heskew@microsoft.com>
2010-10-07 12:50:51 -0700

    Some minor post-mvc3 beta upgrade fixes
    --HG--
    branch : dev

--- src/Orchard/Mvc/ViewEngines/Razor/WebViewPage.cs
+++ src/Orchard/Mvc/ViewEngines/Razor/WebViewPage.cs
@@ -16,13 +16,15 @@ namespace Orchard.Mvc.ViewEngines.Razor {
     public abstract class WebViewPage<TModel> : System.Web.Mvc.WebViewPage<TModel>, IOrchardViewPage {
         private ScriptRegister _scriptRegister;
         private ResourceRegister _stylesheetRegister;
-        private object _display;
-        private object _new;
         private Localizer _localizer = NullLocalizer.Instance;
+        private object _display;
+        private object _layout;
         private WorkContext _workContext;
+        private object _new;
 
         public Localizer T { get { return _localizer; } }
         public dynamic Display { get { return _display; } }
+        public dynamic Layout { get { return _layout; } }
         public WorkContext WorkContext { get { return _workContext; } }
 
         public dynamic New { get { return _new; } }
@@ -73,6 +75,7 @@ namespace Orchard.Mvc.ViewEngines.Razor {
 
             _localizer = LocalizationUtilities.Resolve(ViewContext, VirtualPath);
             _display = DisplayHelperFactory.CreateHelper(ViewContext, this);
+            _layout = _workContext.Layout;
             _new = ShapeHelperFactory.CreateHelper();
         }
 
