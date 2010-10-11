36fb5672128392e09ac6224b3b7b9107f4fe7079
Andre Rodrigues <andrerod@microsoft.com>
2010-10-11 15:51:44 -0700

    Applied Louis comments.
    --HG--
    branch : dev

--- src/Orchard/ContentManagement/IContent.cs
+++ src/Orchard/ContentManagement/IContent.cs
@@ -3,5 +3,10 @@
 namespace Orchard.ContentManagement {
     public interface IContent {
         ContentItem ContentItem { get; }
+
+        /// <summary>
+        /// The ContentItem's identifier.
+        /// </summary>
+        int Id { get; }
     }
 }
