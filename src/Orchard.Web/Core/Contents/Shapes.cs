816c1a1ff566327ed4833c981c72479cc38cfae5
Nathan Heskew <nathan.heskew@microsoft.com>
2010-10-15 16:49:19 -0700

    Adding an "Edit" link for content on the front
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Contents/Shapes.cs
+++ src/Orchard.Web/Core/Contents/Shapes.cs
@@ -5,6 +5,11 @@ namespace Orchard.Core.Contents {
     public class Shapes : IShapeTableProvider {
         public void Discover(ShapeTableBuilder builder) {
             builder.Describe("Content")
+                .Configure(descriptor => descriptor.Wrappers.Add("Content_ControlWrapper"))
+                .OnCreated(created => {
+                    var content = created.Shape;
+                    content.Main.Add(created.New.PlaceChildContent(Source: content));
+                })
                 .OnDisplaying(displaying => {
                     ContentItem contentItem = displaying.Shape.ContentItem;
                     if (contentItem != null) {
