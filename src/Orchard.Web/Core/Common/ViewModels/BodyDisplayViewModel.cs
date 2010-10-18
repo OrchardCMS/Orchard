fedf68e4a4f97484145ecdb8d51db71e689a79a9
Nathan Heskew <nathan.heskew@microsoft.com>
2010-10-18 10:13:57 -0700

    Updating the Orchard.Core Common module to use the new shape API
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Common/ViewModels/BodyDisplayViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web;
-using Orchard.Core.Common.Models;
-
-namespace Orchard.Core.Common.ViewModels {
-    public class BodyDisplayViewModel {
-        public BodyPart BodyPart { get; set; }
-        public IHtmlString Html { get; set; }
-    }
-}
