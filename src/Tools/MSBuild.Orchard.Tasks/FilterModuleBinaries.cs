9fc9e5287e47587f53d1e426746ecf0f828759e0
Nathan Heskew <nathan.heskew@microsoft.com>
2010-10-20 00:17:55 -0700

    Adding another null check to the FilterModuleBinaries msbuild task
    --HG--
    branch : dev

--- src/Tools/MSBuild.Orchard.Tasks/FilterModuleBinaries.cs
+++ src/Tools/MSBuild.Orchard.Tasks/FilterModuleBinaries.cs
@@ -14,7 +14,7 @@ namespace MSBuild.Orchard.Tasks {
         public ITaskItem[] ExcludedBinaries { get; set; }
 
         public override bool Execute() {
-            if (OrchardWebBinaries == null)
+            if (ModulesBinaries == null || OrchardWebBinaries == null)
                 return true;
 
             var orchardWebAssemblies = new HashSet<string>(
