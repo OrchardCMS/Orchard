5421028cdd98c44cf22709b0909e160348729dde
Dave Reed <dareed@microsoft.com>
2010-11-04 14:00:12 -0700

    Adding str length annotation to model
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Media/Models/MediaSettingsPartRecord.cs
+++ src/Orchard.Web/Modules/Orchard.Media/Models/MediaSettingsPartRecord.cs
@@ -7,6 +7,7 @@ namespace Orchard.Media.Models {
         internal const string DefaultWhitelist = "jpg jpeg gif png txt doc docx xls xlsx pdf ppt pptx pps ppsx odt ods odp";
         private string _whitelist = DefaultWhitelist;
 
+        [StringLength(255)]
         public virtual string UploadAllowedFileTypeWhitelist {
             get { return _whitelist; }
             set { _whitelist = value; }
