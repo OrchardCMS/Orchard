2993f015a5917ff51537921b3c5cf6d312a934a6
Louis DeJardin <Louis.DeJardin@microsoft.com>
2010-11-08 15:36:24 -0800

    Adding Orchard.Lists module
    Creates generic List content type as an out-of-box container
    Will hold ListWidget when written
    
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.Lists/Migrations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Orchard.Lists {
+    public class Migrations : DataMigrationImpl {
+        public int Create() {
+            ContentDefinitionManager.AlterTypeDefinition("List", 
+                cfg=>cfg
+                    .WithPart("CommonPart")
+                    .WithPart("RoutePart")
+                    .WithPart("ContainerPart")
+                    .Creatable());
+
+            //ContentDefinitionManager.AlterTypeDefinition("ListWidget",
+            //    cfg => cfg
+            //        .WithPart("CommonPart")
+            //        .WithPart("WidgetPart")
+            //        .WithPart("ListWidgetPart")
+            //        .WithSetting("Stereotype", "Widget"));
+
+            return 1;
+        }
+    }
+}
