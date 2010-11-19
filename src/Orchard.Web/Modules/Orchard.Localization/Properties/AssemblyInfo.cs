d42a32d0b262de570d155e50211afd5e077a7c03
andrerod <none@none>
2010-11-18 23:53:07 -0800

    Updating security transparency of a few modules.
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Localization/Properties/AssemblyInfo.cs
+++ src/Orchard.Web/Modules/Orchard.Localization/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -30,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.0")]
+[assembly: AssemblyFileVersion("0.8.0")]
+[assembly: SecurityTransparent]
