15310bca8ba1889a0d5ba9e8ef94626e4100a110
Nathan Heskew <nathan.heskew@microsoft.com>
2010-11-22 22:38:08 -0800

    Fixing Core.Contents' create and remove route value metadata
    --HG--
    branch : dev

--- src/Orchard.Web/Core/Contents/Handlers/ContentsHandler.cs
+++ src/Orchard.Web/Core/Contents/Handlers/ContentsHandler.cs
@@ -7,8 +7,9 @@ namespace Orchard.Core.Contents.Handlers {
             if (context.Metadata.CreateRouteValues == null) {
                 context.Metadata.CreateRouteValues = new RouteValueDictionary {
                     {"Area", "Contents"},
-                    {"Controller", "Item"},
-                    {"Action", "Create"}
+                    {"Controller", "Admin"},
+                    {"Action", "Create"},
+                    {"Id", context.ContentItem.ContentType}
                 };
             }
             if (context.Metadata.EditorRouteValues == null) {
@@ -30,7 +31,7 @@ namespace Orchard.Core.Contents.Handlers {
             if (context.Metadata.RemoveRouteValues == null) {
                 context.Metadata.RemoveRouteValues = new RouteValueDictionary {
                     {"Area", "Contents"},
-                    {"Controller", "Item"},
+                    {"Controller", "Admin"},
                     {"Action", "Remove"},
                     {"Id", context.ContentItem.Id}
                 };
