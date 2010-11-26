85bf6158fc25e96108edb7a0072ed22dff4db120
Renaud Paquay <rpaquay@microsoft.com>
2010-11-25 18:30:15 -0800

    Make TagRecord class safer to use
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Tags/Models/TagRecord.cs
+++ src/Orchard.Web/Modules/Orchard.Tags/Models/TagRecord.cs
@@ -2,6 +2,9 @@
 
 namespace Orchard.Tags.Models {
     public class TagRecord {
+        public TagRecord() {
+            ContentTags = new List<ContentTagRecord>();
+        }
         public virtual int Id { get; set; }
         public virtual string TagName { get; set; }
         public virtual IList<ContentTagRecord> ContentTags { get; set; }
