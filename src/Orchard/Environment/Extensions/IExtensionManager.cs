e41e43bda4498ccb10d0abcd754836ccdc0990a5
Renaud Paquay <rpaquay@microsoft.com>
2010-12-07 20:35:58 -0800

    Simplifying code, and making more efficient too
    --HG--
    branch : dev

--- src/Orchard/Environment/Extensions/IExtensionManager.cs
+++ src/Orchard/Environment/Extensions/IExtensionManager.cs
@@ -15,13 +15,7 @@ namespace Orchard.Environment.Extensions {
 
     public static class ExtensionManagerExtensions {
         public static IEnumerable<FeatureDescriptor> EnabledFeatures(this IExtensionManager extensionManager, ShellDescriptor descriptor) {
-            return extensionManager.AvailableExtensions()
-                .SelectMany(extensionDescriptor => extensionDescriptor.Features)
-                .Where(featureDescriptor => IsFeatureEnabledInDescriptor(featureDescriptor, descriptor));
-        }
-
-        private static bool IsFeatureEnabledInDescriptor(FeatureDescriptor featureDescriptor, ShellDescriptor shellDescriptor) {
-            return shellDescriptor.Features.Any(shellDescriptorFeature => shellDescriptorFeature.Name == featureDescriptor.Id);
+            return extensionManager.AvailableFeatures().Where(fd => descriptor.Features.Any(sf => sf.Name == fd.Id));
         }
     }
 }
