f6b1ae4031ded2c68c991be8635129a05f01f9fd
Kevin LaBranche <klabran@hotmail.com>
2010-12-07 22:20:38 -0700

    Basic fix for Work Item 16558 - Adding too long role name causes page error.  Added Data annotation to limit role name to 255 chars to avoid YSOD.

--- src/Orchard.Web/Modules/Orchard.Roles/ViewModels/RoleCreateViewModel.cs
+++ src/Orchard.Web/Modules/Orchard.Roles/ViewModels/RoleCreateViewModel.cs
@@ -4,7 +4,7 @@ using Orchard.Security.Permissions;
 
 namespace Orchard.Roles.ViewModels {
     public class RoleCreateViewModel  {
-        [Required]
+        [Required, StringLength(255)]
         public string Name { get; set; }
         public IDictionary<string, IEnumerable<Permission>> FeaturePermissions { get; set; }
     }
