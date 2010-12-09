1295cc89496ee97fa6292948a23160200fb319ee
Dave Reed <dareed@microsoft.com>
2010-12-09 11:34:57 -0800

    #16825: Exception in background task
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Core/Common/Scheduling/OwnedScheduledTaskHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Models;
+using Orchard.Security;
+using Orchard.Tasks.Scheduling;
+
+namespace Orchard.Core.Common.Scheduling {
+    public abstract class OwnedScheduledTaskHandler : IScheduledTaskHandler {
+        private readonly IOrchardServices _orchardServices;
+
+        protected OwnedScheduledTaskHandler(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+
+        protected void SetCurrentUser(ContentItem contentItem) {
+            IUser owner = null;
+            var commonPart = contentItem.As<CommonPart>();
+            if (commonPart != null) {
+                owner = commonPart.Owner;
+            }
+            if (owner == null) {
+                var superUser = _orchardServices.WorkContext.CurrentSite.SuperUser;
+                owner = _orchardServices.WorkContext.Resolve<IMembershipService>().GetUser(superUser);
+            }
+            _orchardServices.WorkContext.Resolve<IAuthenticationService>().SetAuthenticatedUserForRequest(owner);
+        }
+
+        public abstract void Process(ScheduledTaskContext context);
+    }
+}
--- src/Orchard.Web/Modules/Orchard.PublishLater/Handlers/PublishingTaskHandler.cs
+++ src/Orchard.Web/Modules/Orchard.PublishLater/Handlers/PublishingTaskHandler.cs
@@ -1,27 +1,29 @@
 ﻿using JetBrains.Annotations;
 using Orchard.ContentManagement;
+using Orchard.Core.Common.Scheduling;
 using Orchard.Logging;
 using Orchard.Tasks.Scheduling;
 
 namespace Orchard.PublishLater.Handlers {
     [UsedImplicitly]
-    public class PublishingTaskHandler : IScheduledTaskHandler {
+    public class PublishingTaskHandler : OwnedScheduledTaskHandler {
         private readonly IContentManager _contentManager;
 
-        public PublishingTaskHandler(IContentManager contentManager) {
+        public PublishingTaskHandler(IContentManager contentManager, IOrchardServices orchardServices) : base(orchardServices) {
             _contentManager = contentManager;
             Logger = NullLogger.Instance;
         }
 
         public ILogger Logger { get; set; }
 
-        public void Process(ScheduledTaskContext context) {
+        public override void Process(ScheduledTaskContext context) {
             if (context.Task.TaskType == "Publish") {
                 Logger.Information("Publishing item #{0} version {1} scheduled at {2} utc",
                     context.Task.ContentItem.Id,
                     context.Task.ContentItem.Version,
                     context.Task.ScheduledUtc);
 
+                SetCurrentUser(context.Task.ContentItem);
                 _contentManager.Publish(context.Task.ContentItem);
             }
         }
