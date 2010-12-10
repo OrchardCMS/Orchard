9a91c4d741c4cf9ecae35e843062aecbcc9ca386
Dave Reed <dareed@microsoft.com>
2010-12-10 12:55:06 -0800

    #16825: Exception in background task. Return null for authenticated user if there is none set and there's no HttpContext. Somehow the correct owner is already being assigned, so the previously created OwnedScheduledTaskHandler was unnecessary.
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.PublishLater/Handlers/PublishingTaskHandler.cs
+++ src/Orchard.Web/Modules/Orchard.PublishLater/Handlers/PublishingTaskHandler.cs
@@ -1,29 +1,27 @@
 ﻿using JetBrains.Annotations;
 using Orchard.ContentManagement;
-using Orchard.Core.Common.Scheduling;
 using Orchard.Logging;
 using Orchard.Tasks.Scheduling;
 
 namespace Orchard.PublishLater.Handlers {
     [UsedImplicitly]
-    public class PublishingTaskHandler : OwnedScheduledTaskHandler {
+    public class PublishingTaskHandler : IScheduledTaskHandler {
         private readonly IContentManager _contentManager;
 
-        public PublishingTaskHandler(IContentManager contentManager, IOrchardServices orchardServices) : base(orchardServices) {
+        public PublishingTaskHandler(IContentManager contentManager, IOrchardServices orchardServices) {
             _contentManager = contentManager;
             Logger = NullLogger.Instance;
         }
 
         public ILogger Logger { get; set; }
 
-        public override void Process(ScheduledTaskContext context) {
+        public void Process(ScheduledTaskContext context) {
             if (context.Task.TaskType == "Publish") {
                 Logger.Information("Publishing item #{0} version {1} scheduled at {2} utc",
                     context.Task.ContentItem.Id,
                     context.Task.ContentItem.Version,
                     context.Task.ScheduledUtc);
 
-                SetCurrentUser(context.Task.ContentItem);
                 _contentManager.Publish(context.Task.ContentItem);
             }
         }
