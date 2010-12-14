ff60c81e65d4b765910219d6f3e722640b44189c
Sebastien Ros <none@none>
2010-12-13 17:43:17 -0800

    Updating to Azure SDK 1.3
    --HG--
    branch : dev

--- ClickToRunAzureInDevFabric.cmd
+++
@@ -1,5 +0,0 @@
-SET CDIR = %CD%
-call "%ProgramFiles%\Windows Azure SDK\v1.2\bin\setenv.cmd"
-csrun /devstore 
-csrun /run:"%CDIR %\build\Compile\Orchard.Azure.CloudService.csx";"%CDIR %\src\Orchard.Azure\Orchard.Azure.CloudService\ServiceConfiguration.cscfg" /launchbrowser 
-pause
