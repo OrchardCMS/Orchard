f86c4b1375e6ca3874355417a76d7515e28b695d
Sebastien Ros <none@none>
2011-01-06 15:29:28 -0800

    Converting the LogOn view to a shape, and adding a ShapeResult to render shapes as an ActionResult without the need of a dedicated view
    Work Item: 17046
    
    --HG--
    branch : 1.x

---
+++ src/Orchard/Mvc/ShapeResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace Orchard.Mvc {
+    public class ShapeResult : ViewResult {
+        public ShapeResult(dynamic shape) {
+            ViewData.Model = shape;
+            ViewName = "~/Core/Shapes/Views/ShapeResult/Display.cshtml";
+        }
+    }
+}
