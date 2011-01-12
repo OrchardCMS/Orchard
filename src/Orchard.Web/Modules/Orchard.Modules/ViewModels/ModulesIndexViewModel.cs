de42a2d4a9c49a5c94baa2d54a6ddfb0630107d9
Renaud Paquay <rpaquay@microsoft.com>
2011-01-11 19:01:46 -0800

    Adding link to Gallery in "Modules" and "Themes" pages
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Modules/ViewModels/ModulesIndexViewModel.cs
+++ src/Orchard.Web/Modules/Orchard.Modules/ViewModels/ModulesIndexViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Orchard.Environment.Extensions.Models;
 
 namespace Orchard.Modules.ViewModels {
     public class ModulesIndexViewModel {
         public bool InstallModules { get; set; }
+        public bool BrowseToGallery { get; set; }
         public IEnumerable<ExtensionDescriptor> Modules { get; set; }
     }
 }
