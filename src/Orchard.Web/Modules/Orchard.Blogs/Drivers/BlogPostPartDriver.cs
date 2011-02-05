9a0f00339ef2cb2e8310252882c5361cb058369b
andrerod <none@none>
2011-02-04 19:49:10 -0800

    Simplifying, removing dead code from blog drivers.
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Blogs/Drivers/BlogPostPartDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Blogs/Drivers/BlogPostPartDriver.cs
@@ -3,23 +3,15 @@ using Orchard.Blogs.Models;
 using Orchard.Blogs.Extensions;
 using Orchard.ContentManagement.Drivers;
 using Orchard.Core.Feeds;
-using Orchard.Localization;
 
 namespace Orchard.Blogs.Drivers {
     [UsedImplicitly]
     public class BlogPostPartDriver : ContentPartDriver<BlogPostPart> {
         private readonly IFeedManager _feedManager;
-        public IOrchardServices Services { get; set; }
 
-        public BlogPostPartDriver(IOrchardServices services, IFeedManager feedManager) {
+        public BlogPostPartDriver(IFeedManager feedManager) {
             _feedManager = feedManager;
-            Services = services;
-            T = NullLocalizer.Instance;
         }
-
-        public Localizer T { get; set; }
-
-        protected override string Prefix { get { return ""; } }
 
         protected override DriverResult Display(BlogPostPart part, string displayType, dynamic shapeHelper) {
             if (displayType.StartsWith("Detail"))
