4df8d1313ec7dba0d1281b18eec18f93a6db5c6d
Sebastien Ros <none@none>
2011-02-08 17:52:53 -0800

    Creating Orchard.DesignerTools module - Creating Shape Tracing feature from Experimental feature
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.DesignerTools/Services/ShapeTracingFactory.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.DisplayManagement.Implementation;
+using Orchard.Environment.Extensions;
+
+namespace Orchard.DesignerTools.Services {
+    [OrchardFeature("Orchard.DesignerTools")]
+    public class ShapeTracingFactory : IShapeFactoryEvents {
+        public void Creating(ShapeCreatingContext context) {
+        }
+
+        public void Created(ShapeCreatedContext context) {
+            if (context.ShapeType != "Layout" && context.ShapeType != "DocumentZone") {
+                context.Shape.Metadata.Wrappers.Add("ShapeTracing_Wrapper");
+            }
+        }
+    }
+}
