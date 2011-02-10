e0f10275b20967267f6b7a3e28d224c8e2852887
Louis DeJardin <Louis.DeJardin@microsoft.com>
2011-02-10 12:32:28 -0800

    Adding support for dynamic content expressions
    ContentItem as dynamic will respond to get property which matches a ContentPart's metadata definition name
    ContentPart as dynamic will respond to get property which matches a ContentField's metadata instance name
    
    --HG--
    branch : dev
    extra : transplant_source : Qu0%9E%09%9B%97rbq%EE%9B%1Ax3%C8p%ED%91%3E

--- src/Orchard/ContentManagement/ContentItem.cs
+++ src/Orchard/ContentManagement/ContentItem.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Linq.Expressions;
+using ClaySharp;
 using Orchard.ContentManagement.MetaData.Models;
 using Orchard.ContentManagement.Records;
 
 namespace Orchard.ContentManagement {
-    public class ContentItem : IContent {
+    public class ContentItem : IContent, IContentBehavior, IDynamicMetaObjectProvider {
         public ContentItem() {
+            _behavior = new ClayBehaviorCollection(new[] { new ContentItemBehavior(this) });
             _parts = new List<ContentPart>();
         }
 
         private readonly IList<ContentPart> _parts;
+
         ContentItem IContent.ContentItem { get { return this; } }
 
         public int Id { get { return Record == null ? 0 : Record.Id; } }
@@ -39,5 +44,15 @@ namespace Orchard.ContentManagement {
             part.ContentItem = this;
             _parts.Add(part);
         }
+
+
+        private readonly IClayBehavior _behavior;
+        IClayBehavior IContentBehavior.Behavior {
+            get { return _behavior; }
+        }
+        DynamicMetaObject IDynamicMetaObjectProvider.GetMetaObject(Expression parameter) {
+            return new ClayMetaObject(this, parameter, ex => Expression.Property(Expression.Convert(ex, typeof(IContentBehavior)), "Behavior"));
+        }
+
     }
 }
---
+++ src/Orchard/ContentManagement/ContentItemBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClaySharp;
+
+namespace Orchard.ContentManagement {
+    public class ContentItemBehavior : ClayBehavior {
+        private readonly IContent _content;
+
+        public ContentItemBehavior(IContent content) {
+            _content = content;
+        }
+
+        public override object GetMemberMissing(Func<object> proceed, object self, string name) {
+            var contentItem = _content.ContentItem;
+            foreach (var part in contentItem.Parts) {
+                if (part.PartDefinition.Name == name)
+                    return part;
+            }
+            return base.GetMemberMissing(proceed, self, name);
+        }
+    }
+}
