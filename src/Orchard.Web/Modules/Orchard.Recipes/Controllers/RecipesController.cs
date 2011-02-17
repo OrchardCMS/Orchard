ddf29e6c08695a598ae5a161b398eb225671b0b9
Suha Can <suhacan@microsoft.com>
2011-02-16 17:48:28 -0800

    Adding a front-end controller for recipes that returns recipe execution status from the journal so we can poll it when finishing setup.
    --HG--
    branch : recipe

---
+++ src/Orchard.Web/Modules/Orchard.Recipes/Controllers/RecipesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Orchard.Localization;
+using Orchard.Recipes.Services;
+
+namespace Orchard.Recipes.Controllers {
+    public class RecipesController : Controller {
+        private readonly IRecipeJournal _recipeJournal;
+
+        public RecipesController(IRecipeJournal recipeJournal) {
+            _recipeJournal = recipeJournal;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public ActionResult RecipeExecutionStatus (string executionId) {
+            var recipeStatus = _recipeJournal.GetRecipeStatus(executionId);
+            var model = recipeStatus;
+
+            return View(model);
+        }
+    }
+}
