f6579c754053d424b2ce1d872a6b78a65c940a33
Suha Can <suhacan@microsoft.com>
2011-02-17 17:13:44 -0800

    Implementation of feature handler for recipes: provides disabling/enabling of features.
    --HG--
    branch : recipe

--- src/Orchard.Web/Modules/Orchard.Recipes/RecipeHandlers/FeatureRecipeHandler.cs
+++ src/Orchard.Web/Modules/Orchard.Recipes/RecipeHandlers/FeatureRecipeHandler.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Orchard.Environment.Features;
 using Orchard.Localization;
 using Orchard.Logging;
+using Orchard.Modules.Services;
 using Orchard.Recipes.Models;
 using Orchard.Recipes.Services;
 
 namespace Orchard.Recipes.RecipeHandlers {
     public class FeatureRecipeHandler : IRecipeHandler {
-        public FeatureRecipeHandler() {
+        private readonly IFeatureManager _featureManager;
+        private readonly IModuleService _moduleService;
+
+        public FeatureRecipeHandler(IFeatureManager featureManager, IModuleService moduleService) {
+            _featureManager = featureManager;
+            _moduleService = moduleService;
             Logger = NullLogger.Instance;
             T = NullLocalizer.Instance;
         }
@@ -37,9 +44,25 @@ namespace Orchard.Recipes.RecipeHandlers {
                 }
             }
 
-            // if both, tx and disable happens first.
-            // force cascading enabling and disabling.
-            // run migrations.
+            var availableFeatures = _featureManager.GetAvailableFeatures().Select(x => x.Id).ToArray();
+            foreach (var featureName in featuresToDisable) {
+                if (!availableFeatures.Contains(featureName)) {
+                    throw new InvalidOperationException(string.Format("Could not disable feature {0} because it was not found.", featureName));
+                }
+            }
+
+            foreach (var featureName in featuresToEnable) {
+                if (!availableFeatures.Contains(featureName)) {
+                    throw new InvalidOperationException(string.Format("Could not enable feature {0} because it was not found.", featureName));
+                }
+            }
+
+            if (featuresToDisable.Count != 0) {
+                _moduleService.DisableFeatures(featuresToDisable, true);
+            }
+            if (featuresToEnable.Count != 0) {
+                _moduleService.EnableFeatures(featuresToEnable, true);
+            }
 
             recipeContext.Executed = true;
         }
