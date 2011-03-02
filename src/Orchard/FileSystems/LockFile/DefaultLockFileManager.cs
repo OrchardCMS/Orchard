919585be1acc5878708eafbdea1d2e244755d860
Sebastien Ros <none@none>
2011-03-01 22:51:09 -0800

    Adding a new file locking service - ILockFileManager to create and check lock files - ILockFile represent an acquired lock file - Lock files expire after 10 minutes by default - Lock files are created in ~/App_Data - Default implementation provided (as it is in Orchard.Framework)
    --HG--
    branch : indexing

---
+++ src/Orchard/FileSystems/LockFile/DefaultLockFileManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Orchard.FileSystems.AppData;
+using Orchard.Services;
+
+namespace Orchard.FileSystems.LockFile {
+    public class DefaultLockFileManager : ILockFileManager {
+        private readonly IAppDataFolder _appDataFolder;
+        private readonly IClock _clock;
+
+        public static TimeSpan Expiration { get; private set; }
+
+        public DefaultLockFileManager(IAppDataFolder appDataFolder, IClock clock) {
+            _appDataFolder = appDataFolder;
+            _clock = clock;
+            Expiration = TimeSpan.FromMinutes(10);
+        }
+
+        public bool TryAcquireLock(string path, ref ILockFile lockFile) {
+            try {
+                if(IsLocked(path)) {
+                    return false;
+                }
+
+                lockFile = new LockFile(_appDataFolder, path, _clock.UtcNow.ToString());
+                return true;
+            }
+            catch {
+                // an error occured while reading/creating the lock file
+                return false;
+            }
+        }
+
+        public bool IsLocked(string path) {
+            try {
+                if (_appDataFolder.FileExists(path)) {
+                    var content = _appDataFolder.ReadFile(path);
+
+                    DateTime creationUtc;
+                    if (DateTime.TryParse(content, out creationUtc)) {
+                        // if expired the file is not removed
+                        // it should be automatically as there is a finalizer in LockFile
+                        // or the next taker can do it, unless it also fails, again
+                        return creationUtc.Add(Expiration) > _clock.UtcNow;
+                    }
+                }
+            }
+            catch {
+                // an error occured while reading the file
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Orchard/FileSystems/LockFile/LockFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Orchard.FileSystems.AppData;
+
+namespace Orchard.FileSystems.LockFile {
+    /// <summary>
+    /// Represents a Lock File acquire on the file system
+    /// </summary>
+    public class LockFile : ILockFile {
+        private readonly IAppDataFolder _appDataFolder;
+        private readonly string _path;
+        private readonly string _content;
+        private bool _released;
+
+        public LockFile(IAppDataFolder appDataFolder, string path, string content) {
+            _appDataFolder = appDataFolder;
+            _path = path;
+            _content = content;
+
+            // create the physical lock file
+            _appDataFolder.CreateFile(path, content);
+        }
+
+        public void Dispose() {
+            // dispose both managed and unmanaged resources
+            Dispose(true);
+
+            // don't call the finalizer if dispose is called
+            GC.SuppressFinalize(this);
+        }
+
+        public void Release() {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if(disposing) {
+                // release managed code here
+                // nothing right now, just a placeholder to preserve the pattern
+            }
+
+            if (_released || !_appDataFolder.FileExists(_path)) {
+                // nothing to do, night happen if re-granted, and already released
+                return;
+            }
+
+            _released = true;
+
+            // check it has not been granted in the meantime
+            var current = _appDataFolder.ReadFile(_path);
+            if (current == _content) {
+                _appDataFolder.DeleteFile(_path);
+            }
+        }
+
+        ~LockFile() {
+            // dispose unmanaged resources (file)
+            Dispose(false);
+        }
+    }
+}
