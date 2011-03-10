aafdc8941fd020e1b475a5c9a379e7d20e43a0b8
Suha Can <suhacan@microsoft.com>
2011-03-10 12:47:52 -0800

    Import view and controller to permit importing data recipes.
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.ImportExport/Controllers/AdminController.cs
+++ src/Orchard.Web/Modules/Orchard.ImportExport/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Orchard.ImportExport.ViewModels;
 using Orchard.Localization;
+using Orchard.UI.Notify;
 
 namespace Orchard.ImportExport.Controllers {
     public class AdminController : Controller {
@@ -16,6 +18,25 @@ namespace Orchard.ImportExport.Controllers {
             var viewModel = new ImportViewModel();
 
             return View(viewModel);
+        }
+
+        [HttpPost, ActionName("Import")]
+        public ActionResult ImportPOST() {
+            if (!Services.Authorizer.Authorize(Permissions.Import, T("Not allowed to import.")))
+                return new HttpUnauthorizedResult();
+
+            try {
+                if (String.IsNullOrEmpty(Request.Files["RecipeFile"].FileName)) {
+                    throw new ArgumentException(T("Please choose a recipe file to import.").Text);
+                }
+
+                Services.Notifier.Information(T("Your recipe has been imported."));
+                return RedirectToAction("Import");
+            }
+            catch (Exception exception) {
+                Services.Notifier.Error(T("Import failed {0}", exception.Message));
+                return View();
+            }
         }
 
         public ActionResult Export() {
