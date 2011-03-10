ec409007d05f0685299f9a16e30eb1b6cbcd3890
Suha Can <suhacan@microsoft.com>
2011-03-10 11:42:30 -0800

    Adding menu and a tabbed import/export management in the admin.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.ImportExport/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Orchard.ImportExport.ViewModels;
+using Orchard.Localization;
+
+namespace Orchard.ImportExport.Controllers {
+    public class AdminController : Controller {
+        public AdminController(IOrchardServices services) {
+            Services = services;
+            T = NullLocalizer.Instance;
+        }
+
+        public IOrchardServices Services { get; private set; }
+        public Localizer T { get; set; }
+
+        public ActionResult Import() {
+            var viewModel = new ImportViewModel();
+
+            return View(viewModel);
+        }
+
+        public ActionResult Export() {
+            var viewModel = new ExportViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.ImportExport/Permissions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+
+namespace Orchard.ImportExport {
+    public class Permissions : IPermissionProvider {
+        public static readonly Permission Import = new Permission { Description = "Import Data", Name = "Import" };
+        public static readonly Permission Export = new Permission { Description = "Export Data", Name = "Export" };
+
+        public virtual Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions() {
+            return new[] { Import, Export };
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
+            return new[] {
+                             new PermissionStereotype {
+                                                          Name = "Administrator",
+                                                          Permissions = new[] {Import, Export}
+                                                      }
+                         };
+        }
+    }
+}
