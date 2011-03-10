deb5513737455f822f9506ffc9df81123ed1ed6e
Dave Reed <dareed@microsoft.com>
2011-03-10 11:45:07 -0800

    Initial new List Management UI.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.Lists/Handlers/ContainerPartHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Routing;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Core.Containers.Models;
+using Orchard.Data;
+
+namespace Orchard.Lists.Handlers {
+    public class ContainerPartHandler : ContentHandler {
+        public ContainerPartHandler() {
+        }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            var container = context.ContentItem.As<ContainerPart>();
+
+            if (container == null)
+                return;
+
+            // containers link to their contents in admin screens
+            context.Metadata.AdminRouteValues = new RouteValueDictionary {
+                {"Area", "Orchard.Lists"},
+                {"Controller", "Admin"},
+                {"Action", "List"},
+                {"containerId", container.Id}
+            };
+        }
+    }
+}
