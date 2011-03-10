c44ca04676c20b30b20e960f3fd7c41d790cab8b
Dave Reed <dareed@microsoft.com>
2011-03-10 11:17:04 -0800

    Deleting media files was broke. And when it failed, it double failed with a NullReferenceException while redirecting you.
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Media/Controllers/AdminController.cs
+++ src/Orchard.Web/Modules/Orchard.Media/Controllers/AdminController.cs
@@ -102,7 +102,7 @@ namespace Orchard.Media.Controllers {
                         string folderName = input[fileName];
                         if (!Services.Authorizer.Authorize(Permissions.ManageMedia, T("Couldn't delete media file")))
                             return new HttpUnauthorizedResult();
-                        _mediaService.DeleteFile(fileName, folderName);
+                        _mediaService.DeleteFile(folderName, fileName);
 
                         Services.Notifier.Information(T("Media file deleted"));
                     }
@@ -119,8 +119,7 @@ namespace Orchard.Media.Controllers {
                 return RedirectToAction("Index");
             } catch (Exception exception) {
                 this.Error(exception, T("Deleting failed: {0}", exception.Message), Logger, Services.Notifier);
-
-                return View();
+                return RedirectToAction("Index");
             }
         }
 
