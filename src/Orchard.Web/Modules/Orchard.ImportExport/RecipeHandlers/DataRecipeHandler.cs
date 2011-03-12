42336c6694200f028bbacccf5d4793a1fbfdcc1b
Suha Can <suhacan@microsoft.com>
2011-03-11 16:10:55 -0800

    Placeholder for recipe handler for importing data.
    --HG--
    branch : dev

---
+++ src/Orchard.Web/Modules/Orchard.ImportExport/RecipeHandlers/DataRecipeHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Orchard.Localization;
+using Orchard.Logging;
+using Orchard.Recipes.Models;
+using Orchard.Recipes.Services;
+
+namespace Orchard.ImportExport.RecipeHandlers {
+    public class DataRecipeHandler : IRecipeHandler {
+        public DataRecipeHandler() {
+            Logger = NullLogger.Instance;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+        ILogger Logger { get; set; }
+
+        // <Data />
+        // Import Data
+        public void ExecuteRecipeStep(RecipeContext recipeContext) {
+            if (!String.Equals(recipeContext.RecipeStep.Name, "Data", StringComparison.OrdinalIgnoreCase)) {
+                return;
+            }
+
+            recipeContext.Executed = true;
+        }
+    }
+}
