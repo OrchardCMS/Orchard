3c2cfa471b6e99cf17640f78a4b13aabb12e9109
Suha Can <suhacan@microsoft.com>
2011-03-15 16:55:50 -0700

    Moving ImportContentSession to a separate file.
    --HG--
    branch : dev

--- src/Orchard/ContentManagement/DefaultContentManager.cs
+++ src/Orchard/ContentManagement/DefaultContentManager.cs
@@ -466,39 +466,6 @@ namespace Orchard.ContentManagement {
         }
     }
 
-    public class ImportContentSession {
-        private readonly IContentManager _contentManager;
-
-        // order of x500 elements.
-        private readonly Dictionary<string, ContentItem> _dictionary;
-
-        public ImportContentSession(IContentManager contentManager) {
-            _contentManager = contentManager;
-            _dictionary = new Dictionary<string, ContentItem>();
-        }
-
-        // gets a content item for an identity string.
-        public ContentItem Get(string id) {
-            if (_dictionary.ContainsKey(id))
-                return _dictionary[id];
-
-            foreach (var item in _contentManager.Query(VersionOptions.Published).List()) {
-                var identity = _contentManager.GetItemMetadata(item).Identity.ToString();
-                if (identity == id) {
-                    _dictionary.Add(identity, item);
-                    return item;
-                }
-            }
-
-            return null;
-        }
-
-        public void Store(string id, ContentItem item) {
-            _dictionary.Add(id, item);
-        }
-
-    }
-
     class CallSiteCollection : ConcurrentDictionary<string, CallSite<Func<CallSite, object, object>>> {
         readonly Func<string, CallSite<Func<CallSite, object, object>>> _valueFactory;
 
