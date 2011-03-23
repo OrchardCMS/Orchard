f51dc240325c27d006da691eed36163837a6a8f4
Nathan Heskew <nathan.heskew@microsoft.com>
2011-03-23 14:47:35 -0700

    Putting the widget layer hinting notification into it's own feature...
    ...so it can be disabled when it becomes annoying.
    
    It's enabled by default in the blog and default recipies.
    
    --HG--
    branch : dev

--- src/Orchard.Web/Modules/Orchard.Widgets/Handlers/LayerHintHandler.cs
+++ src/Orchard.Web/Modules/Orchard.Widgets/Handlers/LayerHintHandler.cs
@@ -8,10 +8,12 @@ using Orchard.ContentManagement.Aspects;
 using Orchard.ContentManagement.Drivers;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Core.Routable.Models;
+using Orchard.Environment.Extensions;
 using Orchard.Localization;
 using Orchard.UI.Notify;
 
 namespace Orchard.Widgets.Drivers {
+    [OrchardFeature("Orchard.Widgets.PageLayerHinting")]
     public class LayerHintHandler : ContentHandler {
         public LayerHintHandler(IOrchardServices services, RequestContext requestContext) {
             T = NullLocalizer.Instance;
@@ -19,6 +21,9 @@ namespace Orchard.Widgets.Drivers {
             OnPublished<RoutePart>((context, part) => {
                 if (string.IsNullOrWhiteSpace(part.Path)) // only going to help in creating a layer if the content has a path
                     return;
+                
+
+
                 var urlHelper = new UrlHelper(requestContext);
                 var pathForLayer = "~/" + part.Path;
                 services.Notifier.Information(T("Would you like to <a href=\"{0}\">add a widget layer</a> for \"{1}\"?",
