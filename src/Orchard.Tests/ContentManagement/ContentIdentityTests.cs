fa44ab37fd2010cffa694622e6748549ba268751
Suha Can <suhacan@microsoft.com>
2011-03-29 14:18:35 -0700

    Fixing #17526: Escape "/" in content identities
    --HG--
    branch : 1.x

---
+++ src/Orchard.Tests/ContentManagement/ContentIdentityTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using Orchard.ContentManagement;
+
+namespace Orchard.Tests.ContentManagement {
+    [TestFixture]
+    public class ContentIdentityTests {
+        [Test]
+        public void ContentIdentityParsesIdentities() {
+            var identity1 = new ContentIdentity("/foo=bar");
+            Assert.That(identity1.Get("foo"), Is.EqualTo("bar"));
+
+            var identity2 = new ContentIdentity("/foo=");
+            Assert.That(identity2.Get("foo"), Is.EqualTo(String.Empty));
+
+            var identity3 = new ContentIdentity("foo");
+            Assert.That(identity3.Get("foo"), Is.Null);
+        }
+
+        [Test]
+        public void ContentIdentitiesAreEncodedWhenOutput() {
+            var identity1 = new ContentIdentity("/foo=bar");
+            Assert.That(identity1.ToString(), Is.EqualTo("/foo=bar"));
+
+            var identity2 = new ContentIdentity(@"/foo=bar/abaz=quux\/fr\\ed=foo/yarg=yiu=foo");
+            Assert.That(identity2.Get("foo"), Is.EqualTo("bar"));
+            Assert.That(identity2.Get("abaz"), Is.EqualTo(@"quux/fr\ed=foo"));
+            Assert.That(identity2.Get("yarg"), Is.EqualTo("yiu=foo"));
+            Assert.That(identity2.ToString(), Is.EqualTo(@"/foo=bar/abaz=quux\/fr\\ed=foo/yarg=yiu=foo"));
+        }
+    }
+}
+
+
+
