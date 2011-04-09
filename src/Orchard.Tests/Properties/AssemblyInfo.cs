f8aebb342f42b2c8583d7cac9cbe42f6381a23d2
Suha Can <suhacan@microsoft.com>
2011-04-08 17:24:31 -0700

    Set build version (1.1.28) for v1.1.

--- src/Orchard.Tests/Properties/AssemblyInfo.cs
+++ src/Orchard.Tests/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.20")]
-[assembly: AssemblyFileVersion("1.0.20")]
+[assembly: AssemblyVersion("1.1.28")]
+[assembly: AssemblyFileVersion("1.1.28")]
