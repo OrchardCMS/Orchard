342fc55a228e94700b25414a502cd5394b9efbc8
Andre Rodrigues <andrerod@microsoft.com>
2011-05-09 13:51:59 -0700

    #17774: Rolling files when locked. Removing unecessary custom XMLConfigurators, reconfiguring and monitoring files in the logger factory only under full trust.
    --HG--
    branch : 1.x

--- src/Orchard/Logging/OrchardFileAppender.cs
+++ src/Orchard/Logging/OrchardFileAppender.cs
@@ -1,27 +1,51 @@
-﻿using log4net.Appender;
-using log4net.Util;
+﻿using System.Collections.Generic;
+using log4net.Appender;
 
 namespace Orchard.Logging {
     public class OrchardFileAppender : RollingFileAppender {
-        public enum RollingStyleFrequencyMode {
-            Once,
-            Continuous
+        /// <summary>
+        /// Dictionary of already known suffixes (based on previous attempts) for a given filename.
+        /// </summary>
+        private static readonly Dictionary<string, int> _suffixes = new Dictionary<string, int>();
+
+        private const int Retries = 50;
+
+        /// <summary>
+        /// Opens the log file adding an incremental suffix to the filename if required due to an openning failure (usually, locking).
+        /// </summary>
+        /// <param name="fileName">The filename as specified in the configuration file.</param>
+        /// <param name="append">Boolean flag indicating weather the log file should be appended if it already exists.</param>
+        protected override void OpenFile(string fileName, bool append) {
+            lock (this) {
+                bool fileOpened = false;
+                string currentFilename = fileName;
+
+                if (!_suffixes.ContainsKey(fileName)) {
+                    _suffixes[fileName] = 0;
+                }
+
+                int newSuffix = _suffixes[fileName];
+
+                for (int i = 1; !fileOpened && i <= Retries; i++) {
+                    try {
+                        if (newSuffix > 0) {
+                            currentFilename = string.Format("{0}-{1}", fileName, newSuffix);
+                        }
+
+                        BaseOpenFile(currentFilename, append);
+
+                        fileOpened = true;
+                    } catch {
+                        newSuffix = _suffixes[fileName] + i;
+                    }
+                }
+
+                _suffixes[fileName] = newSuffix;
+            }
         }
 
-        public RollingStyleFrequencyMode RollingStyleFrequency { get; set; }
-
-        public bool RollSize {
-            get { return (RollingStyle == RollingMode.Composite || RollingStyle == RollingMode.Size); }
-        }
-
-        protected override void AdjustFileBeforeAppend() {
-            if (RollingStyle == RollingMode.Size || 
-                RollingStyleFrequency == RollingStyleFrequencyMode.Continuous) {
-                base.AdjustFileBeforeAppend();
-            }
-            else if (RollSize && ((File != null) && (((CountingQuietTextWriter) QuietWriter).Count >= MaxFileSize))) {
-                RollOverSize();
-            }
+        protected virtual void BaseOpenFile(string fileName, bool append) {
+            base.OpenFile(fileName, append);
         }
     }
 }
