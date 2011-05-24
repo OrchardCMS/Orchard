80bfa368c119c58bf8f05fce4d1d6072f3975664
Louis DeJardin <Louis.DeJardin@microsoft.com>
2011-05-24 15:52:15 -0700

    Reversing order of IEnumerable<TService> resolved from IoC
    Components are registered in dependency/priority order and
    this is also the order events and hooks should fire. The default
    order of Autofac is opposite what we need for enumerable services.
    
    The behavior of resolving a single TService is unchanged - the
    last-registered of TService is the only returned.
    
    --HG--
    branch : 1.x
    extra : transplant_source : %87%22%FC%84%A1%0FP%3C%20w%B4%1EQ%CC%90%20%97%C1%EC%BF

---
+++ src/Orchard/Environment/CollectionOrderModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using Autofac.Core;
+
+namespace Orchard.Environment {
+    internal class CollectionOrderModule : IModule {
+        public void Configure(IComponentRegistry componentRegistry) {
+            componentRegistry.Registered += (s, e) => {
+                // only bother watching enumerable resolves
+                var limitType = e.ComponentRegistration.Activator.LimitType;
+                if (typeof(IEnumerable).IsAssignableFrom(limitType)) {
+                    e.ComponentRegistration.Activated += (s2, e2) => {
+                        // Autofac's IEnumerable feature returns an Array
+                        if (e2.Instance is Array) {
+                            // Orchard needs FIFO, not FILO, component order
+                            Array.Reverse((Array)e2.Instance);
+                        }
+                    };
+                }
+            };
+        }
+    }
+}
