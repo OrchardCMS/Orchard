a605420d1152fbd5c75b8e8b7bc4d5313c415bbd
Sebastien Ros <none@none>
2011-06-03 17:44:46 -0700

    Updating Warmup UI
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Orchard.Warmup/Models/ReportEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Orchard.Warmup.Models {
+    public class ReportEntry {
+        public string RelativeUrl { get; set; }
+        public string Filename { get; set; }
+        public int StatusCode { get; set; }
+        public DateTime CreatedUtc { get; set; }
+    }
+}
