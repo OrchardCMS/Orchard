e4e359e5c1906fcccef31ec1d3adc5165107dc8f
Renaud Paquay <rpaquay@microsoft.com>
2011-06-09 12:53:33 -0700

    #17923: Modules from ~/Bin and ~/Core have higher priority
    Because of the way ASP.NET BuildManager works(*), we can't enable
    dynamic compilation of a module if the module is already
    present in ~/bin.
    
    (*) When invoking the compiler, BuildManager automatically adds
    references to all assemblies present in ~/bin. So, if you have module
    "X" in "~/bin", there is no way to enable dynamic compilation of
    module "X" by compiling "X.csproj", as there will be tons of
    compilation errors with equivalent types defined in multiple
    assemblies.
    
    --HG--
    branch : 1.x

--- src/Orchard/Environment/Extensions/Loaders/IExtensionLoader.cs
+++ src/Orchard/Environment/Extensions/Loaders/IExtensionLoader.cs
@@ -9,6 +9,7 @@ namespace Orchard.Environment.Extensions.Loaders {
     public class ExtensionProbeEntry {
         public ExtensionDescriptor Descriptor { get; set; }
         public IExtensionLoader Loader { get; set; }
+        public int Priority { get; set; }
         public string VirtualPath { get; set; }
         public IEnumerable<string> VirtualPathDependencies { get; set; }
     }
