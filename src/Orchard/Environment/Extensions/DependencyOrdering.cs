b9f903511af416c72b321bb483fdefc52e5381be
Suha Can <suhacan@microsoft.com>
2011-06-09 11:19:32 -0700

    Moving DependencyOrdering class to a better place.
    --HG--
    branch : dev

--- src/Orchard/Environment/Extensions/DependencyOrdering.cs
+++ src/Orchard/Environment/Extensions/DependencyOrdering.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Orchard.Utility {
+namespace Orchard.Environment.Extensions {
     public static class DependencyOrdering {
 
         class Linkage<T> {
