3cbcf66b194bf861a295e9efeab125214bfb4135
Sebastien Ros <none@none>
2011-09-16 15:23:35 -0700

    #17754: Localizing validation attributes
    Work Items: 17754
    
    --HG--
    branch : 1.x

---
+++ src/Orchard/Mvc/DataAnnotations/LocalizedRequiredAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Orchard.Localization;
+
+namespace Orchard.Mvc.DataAnnotations {
+    public class LocalizedRequiredAttribute : RequiredAttribute {
+        public LocalizedRequiredAttribute(RequiredAttribute attribute, Localizer t) {
+            AllowEmptyStrings = attribute.AllowEmptyStrings;
+
+            if ( !String.IsNullOrEmpty(attribute.ErrorMessage) )
+                ErrorMessage = attribute.ErrorMessage;
+
+            T = t;
+        }
+
+        public Localizer T { get; set; }
+
+        public override string FormatErrorMessage(string name) {
+            return String.IsNullOrEmpty(ErrorMessage)
+                ? T("The field {0} is required.", name).Text
+                : T(ErrorMessage, name).Text;
+        }
+    }
+}
