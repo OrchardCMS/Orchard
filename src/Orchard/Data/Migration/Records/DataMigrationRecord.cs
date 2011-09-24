93038668b726b1b859d168fff39401ed238ef323
Sebastien Ros <none@none>
2011-09-23 19:43:35 -0700

    #18116, #18082: Fixing Version column mapping
    The Version column is auto-mapped as a Versioning value. FLuent has a bug which
    prevents from altering this mapping. The simples solution is to change the type
    to int?.
    
    --HG--
    branch : 1.x

--- src/Orchard/Data/Migration/Records/DataMigrationRecord.cs
+++ src/Orchard/Data/Migration/Records/DataMigrationRecord.cs
@@ -2,6 +2,6 @@
     public class DataMigrationRecord {
         public virtual int Id { get; set; }
         public virtual string DataMigrationClass { get; set; }
-        public virtual int Version { get; set; }
+        public virtual int? Version { get; set; }
     }
 }
