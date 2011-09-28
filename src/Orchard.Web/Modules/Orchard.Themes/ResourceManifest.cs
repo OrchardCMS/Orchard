abd22c3dc56f1daee595b9a7b1419fa992e2021e
Sebastien Ros <none@none>
2011-09-27 17:24:39 -0700

    Removing reference to Contoso
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Themes/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.Themes/ResourceManifest.cs
@@ -5,10 +5,6 @@ namespace Orchard.Themes {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
             manifest.DefineStyle("ThemesAdmin").SetUrl("orchard-themes-admin.css");
-
-            // todo: include and define the min.js version too
-            // todo: move EasySlider to common location, although it does not appear to be used anywhere right now
-            manifest.DefineScript("EasySlider").SetUrl("~/themes/contoso/scripts/easySlider.js").SetDependencies("jQuery");
         }
     }
 }
