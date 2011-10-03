c2b770c91620e295392874494e497de661a83a18
Sebastien Ros <none@none>
2011-10-03 11:19:57 -0700

    Updating assembly info
    --HG--
    branch : 1.x

--- src/Orchard.Tests/Properties/AssemblyInfo.cs
+++ src/Orchard.Tests/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
--- src/Orchard.Web/Modules/Markdown/Properties/AssemblyInfo.cs
+++ src/Orchard.Web/Modules/Markdown/Properties/AssemblyInfo.cs
@@ -1,16 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Orchard.DesignerTools")]
+[assembly: AssemblyTitle("Markdown")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Orchard")]
-[assembly: AssemblyCopyright("Copyright © Outercurve Foundation 2009")]
+[assembly: AssemblyCopyright("Copyright © Outercurve Foundation 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +16,7 @@ using System.Security;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba95bc63-b148-42ce-ac32-223929b8091f")]
+[assembly: Guid("146F4A3C-C611-4B60-A046-AC5C48D25C70")]
 
 // Version information for an assembly consists of the following four values:
 //
