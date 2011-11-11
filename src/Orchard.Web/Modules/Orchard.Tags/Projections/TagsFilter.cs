971c685efb31a4707ad7fa5fb2bcdacbc643836c
Sebastien Ros <none@none>
2011-11-10 17:51:56 -0800

    Refactoring Tags filter
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Tags/Projections/TagsFilter.cs
+++ src/Orchard.Web/Modules/Orchard.Tags/Projections/TagsFilter.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Web.Mvc;
 using Orchard.ContentManagement;
-using Orchard.DisplayManagement;
-using Orchard.Environment.Extensions;
 using Orchard.Events;
 using Orchard.Localization;
 using Orchard.Tags.Models;
@@ -52,50 +49,6 @@ namespace Orchard.Tags.Projections {
             var tagNames = tags.Split(new[] { ',' }).Select(x => _tagService.GetTag(Int32.Parse(x)).TagName);
 
             return T("Tagged with {0}", String.Join(", ", tagNames));
-        }
-    }
-
-    public interface IFormProvider : IEventHandler {
-        void Describe(dynamic context);
-    }
-
-    public class TagsFilterForms : IFormProvider {
-        private readonly ITagService _tagService;
-        protected dynamic Shape { get; set; }
-        public Localizer T { get; set; }
-
-        public TagsFilterForms(
-            IShapeFactory shapeFactory,
-            ITagService tagService) {
-            _tagService = tagService;
-            Shape = shapeFactory;
-            T = NullLocalizer.Instance;
-        }
-
-        public void Describe(dynamic context) {
-            Func<IShapeFactory, dynamic> form =
-                shape => {
-
-                    var f = Shape.Form(
-                        Id: "SelectTags",
-                        _Tags: Shape.SelectList(
-                            Id: "tagids", Name: "TagIds",
-                            Title: T("Tags"),
-                            Description: T("Select some tags."),
-                            Size: 10,
-                            Multiple: true
-                            )
-                        );
-
-                    foreach (var tag in _tagService.GetTags()) {
-                        f._Tags.Add(new SelectListItem { Value = tag.Id.ToString(), Text = tag.TagName });
-                    }
-
-                    return f;
-                };
-
-            context.Form("SelectTags", form);
-
         }
     }
 }
