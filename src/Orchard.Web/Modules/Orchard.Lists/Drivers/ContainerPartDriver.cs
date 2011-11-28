f0174b6f7eb7a67f5ab8f2d4a376516283ec0101
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-11-28 21:19:38 +0100

    Removing unnecessary driver inheritance in Lists
    --HG--
    branch : 1.x
    extra : rebase_source : cc6c536d0d135da42f623d5c2ad20a5eaeb7f565

--- src/Orchard.Web/Modules/Orchard.Lists/Drivers/ContainerPartDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Lists/Drivers/ContainerPartDriver.cs
@@ -4,31 +4,12 @@ using System.Linq;
 using System.Web;
 using Orchard.ContentManagement.Drivers;
 using Orchard.Core.Containers.Models;
-using Orchard.ContentManagement.MetaData;
-using Orchard.Settings;
-using Orchard.Core.Feeds;
-using Orchard.Environment.Extensions;
 
 namespace Orchard.Lists.Drivers {
-    [OrchardSuppressDependency("Orchard.Core.Containers.Drivers.ContainerPartDriver")]
-    public class ContainerPartDriver : Orchard.Core.Containers.Drivers.ContainerPartDriver {
-        public ContainerPartDriver(
-            IContentDefinitionManager contentDefinitionManager,
-            IOrchardServices orchardServices,
-            ISiteService siteService,
-            IFeedManager feedManager)
-            : base(contentDefinitionManager, orchardServices, siteService, feedManager) {
-        }
-
+    public class ContainerPartDriver : ContentPartDriver<ContainerPart>{
         protected override DriverResult Display(ContainerPart part, string displayType, dynamic shapeHelper) {
-            var shape = base.Display(part, displayType, (object)shapeHelper); // shapeHelper casting to avoid compiler error
-
-            if (shape == null) return null;
-
-            return Combined(
-                shape,
-                ContentShape("Parts_Container_Contained_SummaryAdmin",
-                             () => shapeHelper.Parts_Container_Contained_SummaryAdmin(ContentPart: part))
+            return ContentShape("Parts_Container_Contained_SummaryAdmin",
+                             () => shapeHelper.Parts_Container_Contained_SummaryAdmin(ContentPart: part)
                 );
         }
     }
