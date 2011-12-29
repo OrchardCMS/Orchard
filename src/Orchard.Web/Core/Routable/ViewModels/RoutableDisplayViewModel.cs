ae27eb1e210b73d12c9e27064ac77d5f287c2e25
randompete <none@none>
2011-12-29 14:36:57 +0000

    Remove Routable module
    --HG--
    branch : autoroute

--- src/Orchard.Web/Core/Routable/ViewModels/RoutableDisplayViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Orchard.Core.Routable.Models;
-
-namespace Orchard.Core.Routable.ViewModels {
-   public class RoutableDisplayViewModel  {
-       public string Title { get { return RoutePart.Title; } }
-       public RoutePart RoutePart { get; set; }
-   }
-}
