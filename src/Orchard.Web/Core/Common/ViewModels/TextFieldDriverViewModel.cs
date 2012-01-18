6f53afffa4deb5e4c8197a88d54180c88026fc88
Sebastien Ros <none@none>
2012-01-17 17:07:17 -0800

    Enhancing the Text field
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Core/Common/ViewModels/TextFieldDriverViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Orchard.Core.Common.Fields;
+using Orchard.Core.Common.Settings;
+
+namespace Orchard.Core.Common.ViewModels {
+    public class TextFieldDriverViewModel {
+        public TextField Field { get; set; }
+        public string Text { get; set; }
+        public TextFieldSettings Settings { get; set; } 
+    }
+}
---
+++ src/Orchard.Web/Core/Common/ViewModels/TextFieldSettingsEventsViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Orchard.Core.Common.Settings;
+
+namespace Orchard.Core.Common.ViewModels {
+    public class TextFieldSettingsEventsViewModel {
+        public TextFieldSettings Settings { get; set; }
+        public string[] Flavors { get; set; }
+    }
+}
