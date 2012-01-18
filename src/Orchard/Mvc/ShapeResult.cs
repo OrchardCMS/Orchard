bc1f8af9a224adf1abd5edbd23bcc90d47f126dc
Sebastien Ros <none@none>
2012-01-18 10:25:12 -0800

    Adding a ShapePartialResult
    --HG--
    branch : 1.x

--- src/Orchard/Mvc/ShapeResult.cs
+++ src/Orchard/Mvc/ShapeResult.cs
@@ -9,4 +9,13 @@ namespace Orchard.Mvc {
             ViewName = "ShapeResult/Display";
         }
     }
+
+    public class ShapePartialResult : PartialViewResult {
+        public ShapePartialResult(ControllerBase controller, dynamic shape) {
+            ViewData = controller.ViewData;
+            TempData = controller.TempData;
+            ViewData.Model = shape;
+            ViewName = "ShapeResult/DisplayPartial";
+        }
+    }
 }
