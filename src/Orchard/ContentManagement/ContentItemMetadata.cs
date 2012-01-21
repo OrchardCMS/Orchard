84a64768f5c0b2db1855d88b9b8a8a0c3366cc39
randompete <none@none>
2012-01-21 20:41:56 +0000

    ContentItemMetadata has a string dictionary
    --HG--
    branch : autoroute

--- src/Orchard/ContentManagement/ContentItemMetadata.cs
+++ src/Orchard/ContentManagement/ContentItemMetadata.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Web.Routing;
+using System;
 
 namespace Orchard.ContentManagement {
     public class ContentItemMetadata {
@@ -10,7 +11,7 @@ namespace Orchard.ContentManagement {
         }
         public string DisplayText { get; set; }
         public ContentIdentity Identity { get; set; }
-        public RouteValueDictionary DisplayRouteValues { get; set; }
+        public RouteValueDictionary DisplayRouteValues { get { return RouteValues.Get("Display"); } set { RouteValues.Set("Display",()=>value); } }
         public RouteValueDictionary EditorRouteValues { get; set; }
         public RouteValueDictionary CreateRouteValues { get; set; }
         public RouteValueDictionary RemoveRouteValues { get; set; }
@@ -20,5 +21,6 @@ namespace Orchard.ContentManagement {
         }
         public readonly IList<GroupInfo> DisplayGroupInfo = new List<GroupInfo>();
         public readonly IList<GroupInfo> EditorGroupInfo = new List<GroupInfo>();
+        public readonly FuncDictionary<string, RouteValueDictionary> RouteValues = new FuncDictionary<string, RouteValueDictionary>();
     }
 }
---
+++ src/Orchard/ContentManagement/FuncDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orchard.ContentManagement {
+    public class FuncDictionary<TKey, TVal> {
+
+        private readonly IDictionary<TKey, TVal> _cached;
+        private readonly IDictionary<TKey, Func<TVal>> _factories;
+
+        public FuncDictionary() {
+            _cached = new Dictionary<TKey, TVal>();
+            _factories = new Dictionary<TKey, Func<TVal>>();
+        }
+
+        public TVal Get(TKey key) {
+            if (!_cached.ContainsKey(key)) {
+                if (!_factories.ContainsKey(key)) return default(TVal);
+                _cached[key] = _factories[key]();
+            }
+            return _cached[key];
+        }
+
+        public void Set(TKey key, Func<TVal> factory) {
+            _cached.Remove(key);
+            _factories[key] = factory;
+        }
+
+        public void Remove(TKey key) {
+            _cached.Remove(key);
+            _factories.Remove(key);
+        }
+
+    }
+}
