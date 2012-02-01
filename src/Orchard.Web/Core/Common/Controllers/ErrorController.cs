435271be217f7a12a0463ed4b306b90194bc27bb
Sebastien Ros <none@none>
2012-01-31 16:05:07 -0800

    Handling NotFound custom pages
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Core/Common/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using Orchard.Mvc;
+using Orchard.Themes;
+
+namespace Orchard.Core.Common.Controllers {
+    [Themed]
+    public class ErrorController : Controller {
+        private readonly IOrchardServices _orchardServices;
+
+        public ErrorController(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+
+        public ActionResult NotFound(string url) {
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            var model = _orchardServices.New.NotFound();
+
+            if(url == null) {
+                url = Request.Url.OriginalString;
+            }
+
+            // If the url is relative then replace with Requested path
+            model.RequestedUrl = Request.Url.OriginalString.Contains(url) & Request.Url.OriginalString != url ?
+                Request.Url.OriginalString : url;
+            
+            // Dont get the user stuck in a 'retry loop' by
+            // allowing the Referrer to be the same as the Request
+            model.ReferrerUrl = Request.UrlReferrer != null &&
+                Request.UrlReferrer.OriginalString != model.RequestedUrl ?
+                Request.UrlReferrer.OriginalString : null;
+
+            return new ShapeResult(this, model);
+        }
+    }
+}
---
+++ src/Orchard.Web/Core/Common/Routes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace Orchard.Core.Common {
+    public class Routes : IRouteProvider {
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = -9999,
+                    Route = new Route(
+                        "{*path}",
+                        new RouteValueDictionary {
+                            {"area", "Common"},
+                            {"controller", "Error"},
+                            {"action", "NotFound"}
+                        },
+                        new RouteValueDictionary {
+                        },
+                        new RouteValueDictionary {
+                            {"area", "Common"}
+                        },
+                        new MvcRouteHandler())
+                }
+            };
+        }
+
+   }
+}
