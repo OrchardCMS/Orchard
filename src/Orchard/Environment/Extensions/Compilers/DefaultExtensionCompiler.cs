df9c59734b9a9faad88a0493d1cce10f451f327e
Sebastien Ros <none@none>
2012-02-10 16:17:08 -0800

    Improving error message when a reference is not found
    --HG--
    branch : autoroute

--- src/Orchard/Environment/Extensions/Compilers/DefaultExtensionCompiler.cs
+++ src/Orchard/Environment/Extensions/Compilers/DefaultExtensionCompiler.cs
@@ -94,14 +94,14 @@ namespace Orchard.Environment.Extensions.Compilers {
                         else {
                             Logger.Error("Assembly reference '{0}' for project '{1}' cannot be loaded", assemblyReference.FullName, context.VirtualPath);
                             _criticalErrorProvider.RegisterErrorMessage(T(
-                                "The assembly reference '{0}' could not be loaded.\r\n\r\n" +
+                                "The assembly reference '{0}' could not be loaded for module '{1}'.\r\n\r\n" +
                                 "There are generally a few ways to solve this issue:\r\n" +
                                 "1. Install any dependent module.\r\n" +
                                 "2. Remove the assembly reference from the project file if it's not needed.\r\n" +
                                 "3. Ensure the assembly reference is present in the 'bin' directory of the module.\r\n" +
                                 "4. Ensure the assembly reference is present in the 'bin' directory of the application.\r\n" +
                                 "5. Specify the strong name of the assembly (name, version, culture, publickey) if the assembly is present in the GAC.",
-                                assemblyReference.FullName));
+                                assemblyReference.FullName, moduleName));
                             throw new OrchardCoreException(T("Assembly reference '{0}' for project '{1}' cannot be loaded", assemblyReference.FullName, context.VirtualPath));
                         }
                     }
