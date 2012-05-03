ab0b8b095dad2fdde5a0fc6db5c63aee83dd8992
Sebastien Ros <none@none>
2012-05-03 14:58:16 -0700

    Updating version numbers
    --HG--
    branch : 1.x

--- src/Orchard.Tests/Properties/AssemblyInfo.cs
+++ src/Orchard.Tests/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyFileVersion("1.4.1")]
--- src/Orchard.Web/Modules/Orchard.Comments/Properties/AssemblyInfo.cs
+++ src/Orchard.Web/Modules/Orchard.Comments/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyFileVersion("1.4.1")]
 [assembly:SecurityRules(SecurityRuleSet.Level2)]
