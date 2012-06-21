9ee9cb3f04d3a8fb1285ce3e6e57033384a99ac5
Sebastien Ros <none@none>
2012-06-21 11:01:11 -0700

    Calling base GetItemMetadata in  ContentMenuItemPartHandler
    --HG--
    branch : 1.x

--- src/Orchard.Web/Core/Navigation/Handlers/ContentMenuItemPartHandler.cs
+++ src/Orchard.Web/Core/Navigation/Handlers/ContentMenuItemPartHandler.cs
@@ -18,8 +18,13 @@ namespace Orchard.Core.Navigation.Handlers {
         }
 
         protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            base.GetItemMetadata(context);
+
+            if (context.ContentItem.ContentType != "ContentMenuItem") {
+                return;
+            }
+
             var contentMenuItemPart = context.ContentItem.As<ContentMenuItemPart>();
-            
             // the display route for the menu item is the one for the referenced content item
             if(contentMenuItemPart != null) {
                 context.Metadata.DisplayRouteValues = _contentManager.GetItemMetadata(contentMenuItemPart.Content).DisplayRouteValues;
