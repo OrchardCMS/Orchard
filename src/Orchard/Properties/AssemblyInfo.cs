9123cae805d0422bae636c7a7a4f6c0725c61952
Sebastien Ros <none@none>
2012-07-20 11:30:13 -0700

    Updating version numbers
    --HG--
    branch : 1.5.x

--- src/Orchard/Properties/AssemblyInfo.cs
+++ src/Orchard/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5")]
-[assembly: AssemblyFileVersion("1.5")]
+[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyFileVersion("1.5.1")]
 
