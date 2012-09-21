2ff809a32c8f96a39e1beac821cebd953e8eb7bc
Sebastien Ros <none@none>
2012-09-21 15:14:18 -0700

    #18055: Adding missing jQuery dependency to TinyMce
    Work Item: 18055
    
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/TinyMce/ResourceManifest.cs
+++ src/Orchard.Web/Modules/TinyMce/ResourceManifest.cs
@@ -4,7 +4,7 @@ namespace TinyMce {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            manifest.DefineScript("TinyMce").SetUrl("tiny_mce.js", "tiny_mce_src.js");
+            manifest.DefineScript("TinyMce").SetUrl("tiny_mce.js", "tiny_mce_src.js").SetDependencies("jQuery");
             manifest.DefineScript("OrchardTinyMce").SetUrl("orchard-tinymce.js").SetDependencies("TinyMce");
         }
     }
