ec0e0fa7d97b5db1f8d411166700995e77060c09
Sebastien Ros <none@none>
2012-10-10 14:31:37 -0700

    Backed out changeset: 021b467c8725
    --HG--
    branch : 1.x

--- src/Orchard/Caching/ICache.cs
+++ src/Orchard/Caching/ICache.cs
@@ -3,7 +3,5 @@
 namespace Orchard.Caching {
     public interface ICache<TKey, TResult> {
         TResult Get(TKey key, Func<AcquireContext<TKey>, TResult> acquire);
-        bool IsValid(TKey key);
-        bool TryGet(TKey key, out TResult value);
     }
 }
