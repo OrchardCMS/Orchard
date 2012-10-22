293288928ccc1bb72d9c8aadefb8d62fb00f5175
Sebastien Ros <none@none>
2012-10-22 16:34:06 -0700

    #19140: Fixing condition to run custom configuration on Azure Cloud Services
    Work Item: 19140
    
    --HG--
    branch : 1.x

--- src/Orchard.Azure/Orchard.Azure.Web/startup/ConfigureIIS.cmd
+++ src/Orchard.Azure/Orchard.Azure.Web/startup/ConfigureIIS.cmd
@@ -1,5 +1,5 @@
 REM   Check if this task is running on the compute emulator, if not then sets the Idle Timeout to zero
 
-IF "%ComputeEmulatorRunning%" == "true" (
+IF "%ComputeEmulatorRunning%" == "false" (
 	%windir%\system32\inetsrv\appcmd set config -section:applicationPools -applicationPoolDefaults.processModel.idleTimeout:00:00:00
 )
