5c1d767a95709de6f487dc535976ef6abb8a76e7
Sebastien Ros <none@none>
2013-02-01 11:55:35 -0800

    Adding Media Gallery Field
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Orchard.Fields/Settings/MediaGalleryFieldSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Orchard.Fields.Settings {
+    public class MediaGalleryFieldSettings {
+        public string Hint { get; set; }
+        public string AllowedExtensions { get; set; }
+        public bool Required { get; set; }
+        public bool Multiple { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Fields/ViewModels/MediaGalleryFieldViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Orchard.Fields.Fields;
+
+namespace Orchard.Fields.ViewModels {
+
+    public class MediaGalleryFieldViewModel {
+
+        public ICollection<MediaGalleryItem> Items { get; set; }
+        public string SelectedItems { get; set; }
+        public MediaGalleryField Field { get; set; }
+    }
+}
