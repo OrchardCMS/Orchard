8651e33326812585139c18e4660929e0b985caae
Sebastien Ros <none@none>
2013-02-10 15:32:38 -0800

    Adding ShapeMenuItem
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Core/Navigation/Handlers/ShapeMenuItemPartHandler.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.Core.Navigation.Models;
+using Orchard.Data;
+using Orchard.ContentManagement.Handlers;
+
+namespace Orchard.Core.Navigation.Handlers {
+    public class ShapeMenuItemPartHandler : ContentHandler {
+        public ShapeMenuItemPartHandler(IRepository<ShapeMenuItemPartRecord> repository) {
+            Filters.Add(StorageFilter.For(repository));
+        }
+    }
+}
