9ff39b377a9e856aecb4c152fd39d3d5daea5f2c
Sebastien Ros <none@none>
2013-02-28 16:38:15 -0800

    Adding a new overload for T.Plural
    --HG--
    branch : 1.x

--- src/Orchard/Localization/Localizer.cs
+++ src/Orchard/Localization/Localizer.cs
@@ -10,5 +10,16 @@ namespace Orchard.Mvc.Html {
         public static LocalizedString Plural(this Localizer T, string textSingular, string textPlural, int count, params object[] args) {
             return T(count == 1 ? textSingular : textPlural, new object[] { count }.Concat(args).ToArray());
         }
+
+        public static LocalizedString Plural(this Localizer T, string textNone, string textSingular, string textPlural, int count, params object[] args) {
+            switch (count) {
+                case 0:
+                    return T(textNone, new object[] {count}.Concat(args).ToArray());
+                case 1:
+                    return T(textSingular, new object[] {count}.Concat(args).ToArray());
+                default:
+                    return T(textPlural, new object[] {count}.Concat(args).ToArray());
+            }
+        }
     }
 }
