48bed76df4c887d9141698f2d041859dfe788166
Sebastien Ros <none@none>
2013-03-09 10:53:14 -0800

    Fixing how search settings are read in IndexTaskExecutor
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Indexing/Services/IndexingTaskExecutor.cs
+++ src/Orchard.Web/Modules/Orchard.Indexing/Services/IndexingTaskExecutor.cs
@@ -169,7 +169,7 @@ namespace Orchard.Indexing.Services {
 
                         // skip items from types which are not indexed
                         var settings = GetTypeIndexingSettings(item);
-                        if (!settings.Indexes.Contains(indexName)) {
+                        if (!settings.List.Contains(indexName)) {
                             continue;
                         }
 
@@ -209,7 +209,7 @@ namespace Orchard.Indexing.Services {
                         if (item.ContentItem != null) {
                             // skip items from types which are not indexed
                             var settings = GetTypeIndexingSettings(item.ContentItem);
-                            if (settings.Indexes.Contains(indexName)) {
+                            if (settings.List.Contains(indexName)) {
                                 documentIndex = ExtractDocumentIndex(item.ContentItem);
                             }
                         }
