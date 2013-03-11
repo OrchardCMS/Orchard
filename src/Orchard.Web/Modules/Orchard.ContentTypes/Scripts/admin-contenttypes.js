e8640fda0c4210ebe67c79d0836f941c373bd9b6
Sipke Schoorstra <sipke@skywalkersd.net>
2013-03-11 22:21:20 +0100

    Implementing search filter on Content Types and Content Parts screens.
    --HG--
    branch : 1.x
    extra : rebase_source : 81dd70ddb4fadb4efc4d7d943fdd59d94bba9dd1

---
+++ src/Orchard.Web/Modules/Orchard.ContentTypes/Scripts/admin-contenttypes.js
@@ -0,0 +1,16 @@
+﻿(function($) {
+    $(function() {
+        $("#search-box").focus().on("keyup", function() {
+            var text = $(this).val().toLowerCase();
+            
+            if (text == "") {
+                $("[data-record-text]").show();
+            } else {
+                $("[data-record-text]").each(function() {
+                    var recordText = $(this).data("record-text").toLowerCase();
+                    $(this).toggle(recordText.indexOf(text) >= 0);
+                });
+            }
+        });
+    });
+})(jQuery);
