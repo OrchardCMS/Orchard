ffe345bfe07fee4d531bb7349ba805387933a7e2
Sipke Schoorstra <sipke@skywalkersd.net>
2013-03-11 19:30:42 +0100

    #19094: Moving "Content Types" and "Content Parts" tabs from "Content" into a new admin menu which has two tabs: Content Types and Content Parts.
    Work Item: 19094
    
    --HG--
    branch : 1.x
    extra : rebase_source : 7ac0edb9f01094222e52b82f5f8eda9f3f6d926a

---
+++ src/Orchard.Web/Modules/Orchard.ContentTypes/Services/IStereotypesProvider.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Orchard.ContentTypes.Services {
+    public interface IStereotypesProvider : IDependency {
+        IEnumerable<StereotypeDescription> GetStereotypes();
+    }
+
+    public class StereotypeDescription {
+        public string Stereotype { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
