856f0517645a13473fdbc50244e363393013cf15
Sebastien Ros <none@none>
2013-03-18 09:52:12 -0700

    #19510: EventRecord and ActionRecord in Orchard.Rules missing length attribute
    Work Item: 19510
    
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Rules/Models/EventRecord.cs
+++ src/Orchard.Web/Modules/Orchard.Rules/Models/EventRecord.cs
@@ -1,8 +1,11 @@
-﻿namespace Orchard.Rules.Models {
+﻿using Orchard.Data.Conventions;
+
+namespace Orchard.Rules.Models {
     public class EventRecord {
         public virtual int Id { get; set; }
         public virtual string Category { get; set; }
         public virtual string Type { get; set; }
+        [StringLengthMax]
         public virtual string Parameters { get; set; }
 
         // Parent property
