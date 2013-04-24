28f0c90f5f4f51dd9f58fa7afced04e4f4c931c5
Sipke Schoorstra <sipke@skywalkersd.net>
2013-04-24 16:41:00 +0200

    #19643: Fixing type mis-cast issue.
    Work Item: 19643
    
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Localization/ViewModels/AddLocalizationViewModel.cs
+++ src/Orchard.Web/Modules/Orchard.Localization/ViewModels/AddLocalizationViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using Orchard.ContentManagement;
 
 namespace Orchard.Localization.ViewModels {
     public class AddLocalizationViewModel  {
@@ -8,6 +7,6 @@ namespace Orchard.Localization.ViewModels {
         [Required]
         public string SelectedCulture { get; set; }
         public IEnumerable<string> SiteCultures { get; set; }
-        public IContent Content { get; set; }
+        public dynamic Content { get; set; }
     }
 }
