dd613616071ae381bdb4fc1d016a79deb8dd8b1e
Sipke Schoorstra <sipke@skywalkersd.net>
2013-05-02 18:51:58 +0200

    Adding WebRequest, WebResponse and Redirect activities.
    --HG--
    branch : 1.x
    extra : source : fe92e546e2636ccd59663b7c33158c0ac66a8dc4

---
+++ src/Orchard.Web/Modules/Orchard.Workflows/Activities/WebResponseActivity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Orchard.Localization;
+using Orchard.Workflows.Models;
+using Orchard.Workflows.Services;
+
+namespace Orchard.Workflows.Activities {
+    public class WebResponseActivity : Event {
+        public WebResponseActivity() {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public override bool CanExecute(WorkflowContext workflowContext, ActivityContext activityContext) {
+            return true;
+        }
+
+        public override bool CanStartWorkflow {
+            get { return true; }
+        }
+
+        public override IEnumerable<LocalizedString> GetPossibleOutcomes(WorkflowContext workflowContext, ActivityContext activityContext) {
+            yield return T("Done");
+        }
+
+        public override IEnumerable<LocalizedString> Execute(WorkflowContext workflowContext, ActivityContext activityContext) {
+            yield return T("Done");
+        }
+
+        public override string Name {
+            get { return "WebResponse"; }
+        }
+
+        public override LocalizedString Category {
+            get { return T("HTTP"); }
+        }
+
+        public override LocalizedString Description {
+            get { return T("Suspends the workflow until an HTTP request comes in."); }
+        }
+    }
+}
