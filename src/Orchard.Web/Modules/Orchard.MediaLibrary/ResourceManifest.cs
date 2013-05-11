dfbcc7c00e4807d25024e97135db8e9a9491bede
Sebastien Ros <none@none>
2013-05-11 13:09:20 -0700

    Using FontAwesome in Media Library
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.MediaLibrary/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/ResourceManifest.cs
@@ -4,6 +4,7 @@ namespace Orchard.MediaLibrary {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             builder.Add().DefineStyle("MediaManagerAdmin").SetUrl("orchard-medialibrary-admin.css").SetDependencies("~/Themes/TheAdmin/Styles/Site.css");
+            builder.Add().DefineStyle("FontAwesome").SetUrl("//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css");
         }
     }
 }
