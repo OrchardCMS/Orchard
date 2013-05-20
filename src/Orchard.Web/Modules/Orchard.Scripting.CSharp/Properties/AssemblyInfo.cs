d84a0f6e05f7ef5ae96b8de9f942d14b290d72e8
Sebastien Ros <none@none>
2013-05-20 10:13:45 -0700

    Removing mention of Jint
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.Scripting.CSharp/Properties/AssemblyInfo.cs
+++ src/Orchard.Web/Modules/Orchard.Scripting.CSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jint")]
+[assembly: AssemblyTitle("Orchard.Scripting.CSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Orchard")]
