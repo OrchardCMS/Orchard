86385c8e1de4d2b494eb93dc33c2537eac0ebe93
Sebastien Ros <none@none>
2013-06-03 16:20:10 -0700

    Creating an Api Key setting for Web Search in Media Library
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/Handlers/WebSearchSettingsPartHandler.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+using Orchard.MediaLibrary.Models;
+
+namespace Orchard.MediaLibrary.Handlers {
+    [UsedImplicitly]
+    public class WebSearchSettingsPartHandler : ContentHandler {
+        public WebSearchSettingsPartHandler() {
+            T = NullLocalizer.Instance;
+            Filters.Add(new ActivatingFilter<WebSearchSettingsPart>("Site"));
+            Filters.Add(new TemplateFilterForPart<WebSearchSettingsPart>("WebSearchSettings", "Parts/WebSearch.WebSearchSettings", "media"));
+        }
+
+        public Localizer T { get; set; }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            if (context.ContentItem.ContentType != "Site")
+                return;
+            base.GetItemMetadata(context);
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("Media")));
+        }
+    }
+}
