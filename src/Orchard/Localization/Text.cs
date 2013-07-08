33bfa792074781ccdf5c36fcb9b929a32794703d
agriffard <none@none>
2013-07-08 13:30:22 +0200

    #17313: Localized translations with " are not found
    Work Item: 17313
    
    --HG--
    branch : 1.x

--- src/Orchard/Localization/Text.cs
+++ src/Orchard/Localization/Text.cs
@@ -25,6 +25,7 @@ namespace Orchard.Localization {
 
             var workContext = _workContextAccessor.GetContext();
             var currentCulture = workContext.CurrentCulture;
+            textHint = textHint.Replace(@"""", @"""""");
             var localizedFormat = _localizedStringManager.GetLocalizedString(_scope, textHint, currentCulture);
 
             return args.Length == 0 
