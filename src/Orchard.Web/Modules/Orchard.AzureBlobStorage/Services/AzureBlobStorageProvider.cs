6bd43591ac6613420e1e8fd936c19d342b114fde
Sebastien Ros <sebastien.ros@microsoft.com>
2013-08-06 17:36:32 -0700

    Implementing Windows Azure Blob Storage provider module

---
+++ src/Orchard.Web/Modules/Orchard.AzureBlobStorage/Services/AzureBlobStorageProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using Microsoft.WindowsAzure;
+using Orchard.Environment.Configuration;
+using Orchard.Environment.Extensions;
+using Orchard.FileSystems.Media;
+
+namespace Orchard.AzureBlobStorage.Services {
+    [OrchardSuppressDependency("Orchard.FileSystems.Media.FileSystemStorageProvider")]
+    public class AzureBlobStorageProvider : AzureFileSystem, IStorageProvider {
+        public AzureBlobStorageProvider(ShellSettings shellSettings, IMimeTypeProvider mimeTypeProvider)
+            : this(shellSettings, CloudConfigurationManager.GetSetting("StorageConnectionString"), mimeTypeProvider) {
+        }
+
+        public AzureBlobStorageProvider(ShellSettings shellSettings, string storageConnectionString, IMimeTypeProvider mimeTypeProvider) : base("media", shellSettings.Name, false, storageConnectionString, mimeTypeProvider) { }
+
+        public bool TrySaveStream(string path, Stream inputStream) {
+            try {
+                SaveStream(path, inputStream);
+            }
+            catch {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void SaveStream(string path, Stream inputStream) {
+            // Create the file.
+            // The CreateFile method will map the still relative path
+            var file = CreateFile(path);
+
+            using(var outputStream = file.OpenWrite()) {
+                var buffer = new byte[8192];
+                for (;;) {
+                    var length = inputStream.Read(buffer, 0, buffer.Length);
+                    if (length <= 0)
+                        break;
+                    outputStream.Write(buffer, 0, length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the local path for a given url within the storage provider.
+        /// </summary>
+        /// <param name="url">The public url of the media.</param>
+        /// <returns>The local path.</returns>
+        public string GetStoragePath(string url) {
+            if (url.StartsWith(_absoluteRoot)) {
+                return url.Substring(Combine(_absoluteRoot, "/").Length);
+            }
+
+            return null;
+        }
+
+        public string GetRelativePath(string path) {
+            return GetPublicUrl(path);
+        }
+
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.AzureBlobStorage/Services/CloudBlobContainerExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Orchard.AzureBlobStorage.Services {
+    public static class CloudBlobContainerExtensions {
+
+        public static bool BlobExists(this CloudBlobContainer container, string path) {
+            if (String.IsNullOrEmpty(path) || path.Trim() == String.Empty)
+                throw new ArgumentException("Path can't be empty");
+
+            try {
+                var blob = container.GetBlockBlobReference(path);
+                blob.FetchAttributes();
+                return true;
+            }
+            catch (StorageException) {
+                return false;
+            }
+        }
+
+        public static void EnsureBlobExists(this CloudBlobContainer container, string path) {
+            if (!BlobExists(container, path)) {
+                throw new ArgumentException("File " + path + " does not exist");
+            }
+        }
+
+        public static void EnsureBlobDoesNotExist(this CloudBlobContainer container, string path) {
+            if (BlobExists(container, path)) {
+                throw new ArgumentException("File " + path + " already exists");
+            }
+        }
+
+        public static bool DirectoryExists(this CloudBlobContainer container, string path) {
+            if (String.IsNullOrEmpty(path) || path.Trim() == String.Empty)
+                throw new ArgumentException("Path can't be empty");
+
+            return container.GetDirectoryReference(path).ListBlobs().Any();
+        }
+
+        public static void EnsureDirectoryExists(this CloudBlobContainer container, string path) {
+            if (!DirectoryExists(container, path)) {
+                throw new ArgumentException("Directory " + path + " does not exist");
+            }
+        }
+
+        public static void EnsureDirectoryDoesNotExist(this CloudBlobContainer container, string path) {
+            if (DirectoryExists(container, path)) {
+                throw new ArgumentException("Directory " + path + " already exists");
+            }
+        }
+    }
+}
