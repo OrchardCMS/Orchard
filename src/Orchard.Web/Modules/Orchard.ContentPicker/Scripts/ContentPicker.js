e6d83b57bd438389681c34c8d99d1e84dc47fbd9
Sipke Schoorstra <skywalker@darkskydevelopment.net>
2013-08-11 01:20:18 -0700

    #20012: Refactoring content picker field view and script to a shape for re-usability.
    Work Item: 20012

--- src/Orchard.Web/Modules/Orchard.ContentPicker/Scripts/ContentPicker.js
+++ src/Orchard.Web/Modules/Orchard.ContentPicker/Scripts/ContentPicker.js
@@ -36,4 +36,71 @@
 
         var w = window.open(url, "_blank", data.windowFeatures || "width=685,height=700,status=no,toolbar=no,location=no,menubar=no,resizable=no,scrollbars=yes");
     });
+    
+    // Render content items table and initialize controls.
+    (function() {
+        $("fieldset.content-picker-field").each(function() {
+            var self = this;
+            var required = $(self).data("required");
+            var multiple = $(self).data("multiple");
+            var addButton = $(self).find(".button.add");
+            var removeText = $(self).data("remove-text");
+            var template = '<tr><td>&nbsp;</td><td><span data-id="{contentItemId}" data-fieldid="@idsFieldId" class="content-picker-item">{edit-link}</span></td><td><span data-id="{contentItemId}" class="content-picker-remove button grey">' + removeText + '</span></td></tr>';
+            var selectedItemsFieldname = $(self).data("selected-items-fieldname");
+            var baseUrl = $(self).data("base-url");
+            var partName = $(self).data("part-name");
+            var fieldName = $(self).data("field-name");
+            
+            var refreshIds = function() {
+                var id = $("[name='" + selectedItemsFieldname + "']");
+                var fieldId = $(self).find("span[data-fieldid]");
+                
+                id.val("");
+                fieldId.each(function() {
+                    id.val(id.val() + "," + $(this).attr("data-id"));
+                });
+
+                var itemsCount = fieldId.length;
+            
+                if(!multiple && itemsCount > 0) {
+                    addButton.hide();
+                }
+                else {
+                    addButton.show();
+                }
+            };
+
+            refreshIds();
+        
+            addButton.click(function() {
+                addButton.trigger("orchard-admin-contentpicker-open", {
+                    callback: function(data) {
+                        var tmpl = template.replace( /\{contentItemId\}/g , data.id).replace( /\{edit-link\}/g , data.editLink);
+                        var content = $(tmpl);
+                        $(self).find('table.content-picker tbody').append(content);
+
+                        refreshIds();
+                        $(self).find('.content-picker-message').show();
+                    },
+                    baseUrl: baseUrl,
+                    part: partName,
+                    field: fieldName
+                });
+            });
+
+            $(self).on("click",'table.content-picker .content-picker-remove',function() {
+                $(this).closest('tr').remove();
+                refreshIds();
+                $(self).find('.content-picker-message').show();
+            });
+            
+            $(self).find("table.content-picker tbody").sortable({
+                handle: 'td:first',
+                stop: function(event, ui) {
+                    refreshIds();
+                    $(self).find('.content-picker-message').show();
+                }
+            }).disableSelection();
+        });
+    })();
 });
