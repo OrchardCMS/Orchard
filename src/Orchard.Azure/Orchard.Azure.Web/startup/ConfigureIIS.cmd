e094066194a679da16af57d0f150ee61b472ebd1
DanielStolt <daniel.stolt@perceptible.net>
2013-08-14 21:48:28 +0200

    Upgrading Orchard.Azure to Windows Azure SDK 2.1. Implementing dynamic inclusion of theme and module content via role content folders. Moving Orchard.Azure.Web startup task to Orchard.Azure.CloudService.

--- src/Orchard.Azure/Orchard.Azure.Web/startup/ConfigureIIS.cmd
+++
@@ -1,5 +0,0 @@
-REM   Check if this task is running on the compute emulator, if not then sets the Idle Timeout to zero
-
-IF "%ComputeEmulatorRunning%" == "false" (
-	%windir%\system32\inetsrv\appcmd set config -section:applicationPools -applicationPoolDefaults.processModel.idleTimeout:00:00:00
-)
