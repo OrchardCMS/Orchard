bf29469a7c2def87b83576dd582dab0c4634b8d3
DanielStolt <daniel.stolt@perceptible.net>
2013-08-14 22:53:34 +0200

    Added missing file.

---
+++ src/Orchard.Azure/Orchard.Azure.CloudService/Orchard.Azure.WebContent/Bin/Startup/ConfigureIIS.cmd
@@ -0,0 +1,5 @@
+REM   Check if this task is running on the compute emulator, if not then sets the Idle Timeout to zero
+
+IF "%ComputeEmulatorRunning%" == "false" (
+	%windir%\system32\inetsrv\appcmd set config -section:applicationPools -applicationPoolDefaults.processModel.idleTimeout:00:00:00
+)
