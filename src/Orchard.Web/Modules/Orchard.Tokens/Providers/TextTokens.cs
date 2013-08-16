ad23c73e819f38be6e7f0afd2f923e776c1b343c
Sebastien Ros <sebastien.ros@microsoft.com>
2013-08-16 12:25:14 -0700

    #19971: Adding TrimEnd token
    Work Item: 19971

--- src/Orchard.Web/Modules/Orchard.Tokens/Providers/TextTokens.cs
+++ src/Orchard.Web/Modules/Orchard.Tokens/Providers/TextTokens.cs
@@ -14,6 +14,7 @@ namespace Orchard.Tokens.Providers {
             context.For("Text", T("Text"), T("Tokens for text strings"))
                 .Token("Limit:*", T("Limit:<text length>[,<ellipsis>]"), T("Limit text to specified length and append an optional ellipsis text."))
                 .Token("Format:*", T("Format:<text format>"), T("Optional format specifier (e.g. foo{0}bar). See format strings at <a target=\"_blank\" href=\"http://msdn.microsoft.com/en-us/library/az4se3k1.aspx\">Standard Formats</a> and <a target=\"_blank\" href=\"http://msdn.microsoft.com/en-us/library/8kb3ddd4.aspx\">Custom Formats</a>"), "DateTime")
+                .Token("TrimEnd:*", T("TrimEnd:<chars|number>"), T("Trims the specified characters or number of them from the end of the string."), "Text")
                 .Token("UrlEncode", T("Url Encode"), T("Encodes a URL string."), "Text")
                 .Token("HtmlEncode", T("Html Encode"), T("Encodes an HTML string."), "Text")
                 .Token("LineEncode", T("Line Encode"), T("Replaces new lines with <br /> tags."))
@@ -38,6 +39,7 @@ namespace Orchard.Tokens.Providers {
                 .Token(
                     token => token.StartsWith("Format:", StringComparison.OrdinalIgnoreCase) ? token.Substring("Format:".Length) : null,
                     (token, d) => String.Format(d,token))
+                .Token(token => token.StartsWith("TrimEnd:", StringComparison.OrdinalIgnoreCase) ? token.Substring("TrimEnd:".Length) : null, TrimEnd)
                 .Token("UrlEncode", HttpUtility.UrlEncode)
                 .Token("HtmlEncode", HttpUtility.HtmlEncode)
                 .Token("LineEncode", text => text.Replace(System.Environment.NewLine, "<br />"))
@@ -45,7 +47,21 @@ namespace Orchard.Tokens.Providers {
                 
         }
 
-        private string Limit(string token, string param) {
+        private static string TrimEnd(string token, string param) {
+            if (String.IsNullOrEmpty(param)) {
+                return token;
+            }
+
+            int n;
+            if (!int.TryParse(param, out n)) {
+                return token.TrimEnd(param.ToCharArray());
+            }
+
+            n = Math.Max(0, n); // prevent negative numbers
+            return token.Substring(0, token.Length - n);
+        }
+
+        private static string Limit(string token, string param) {
             if(String.IsNullOrEmpty(token)) {
                 return String.Empty;
             }
