abac56cf58b6b05e8e9146d3dfc6b22bba2e4b36
Sebastien Ros <sebastien.ros@microsoft.com>
2013-08-16 16:01:13 -0700

    #19987: Decoupling worfklows from other modules
    Work Item: 19987

---
+++ src/Orchard.Web/Modules/Orchard.Email/ResourceManifest.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.UI.Resources;
+
+namespace Orchard.Email {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            //builder.Add().DefineStyle("WorkflowsActivities-SendEmail").SetUrl("workflows-activity-sendemail.css").SetDependencies("WorkflowsActivities");
+        }
+    }
+}
--- src/Orchard.Web/Modules/Orchard.Workflows/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.Workflows/ResourceManifest.cs
@@ -1,43 +1,32 @@
-﻿using Orchard.UI.Resources;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Orchard.Environment;
+using Orchard.UI.Resources;
+using Orchard.Utility.Extensions;
+using Orchard.Workflows.Services;
 
 namespace Orchard.Workflows {
     public class ResourceManifest : IResourceManifestProvider {
+        private readonly Work<IActivitiesManager> _activitiesManager;
+
+        public ResourceManifest(Work<IActivitiesManager> activitiesManager) {
+            _activitiesManager = activitiesManager;
+        }
+
         public void BuildManifests(ResourceManifestBuilder builder) {
-            builder.Add().DefineStyle("WorkflowsAdmin").SetUrl("orchard-workflows-admin.css").SetDependencies("~/Themes/TheAdmin/Styles/Site.css");
+            var manifest = builder.Add();
 
+            manifest.DefineStyle("WorkflowsAdmin").SetUrl("orchard-workflows-admin.css").SetDependencies("~/Themes/TheAdmin/Styles/Site.css");
 
-            builder.Add().DefineStyle("WorkflowsActivities-Branch").SetUrl("workflows-activity-branch.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-ContentCreate").SetUrl("workflows-activity-contentcreated.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-ContentPublished").SetUrl("workflows-activity-contentpublished.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-ContentRemoved").SetUrl("workflows-activity-contentremoved.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-ContentVersioned").SetUrl("workflows-activity-contentversioned.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-Delete-Publish").SetUrl("workflows-activity-delete-publish.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-Decision").SetUrl("workflows-activity-decision.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-IsInRole").SetUrl("workflows-activity-isinrole.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-Notify").SetUrl("workflows-activity-notify.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-Publish").SetUrl("workflows-activity-publish.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-SendEmail").SetUrl("workflows-activity-sendemail.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-Timer").SetUrl("workflows-activity-timer.css").SetDependencies("WorkflowsAdmin");
-            builder.Add().DefineStyle("WorkflowsActivities-UserTask").SetUrl("workflows-activity-usertask.css").SetDependencies("WorkflowsAdmin");
+            var activityNames = _activitiesManager.Value.GetActivities().Select(x => "WorkflowsActivity-" + x.Name).ToArray();
 
-            builder.Add().DefineStyle("WorkflowsActivities").SetUrl("workflows-activity.css")
-                .SetDependencies(
-                "WorkflowsActivities-Branch",
-                "WorkflowsActivities-ContentCreate",
-                "WorkflowsActivities-ContentPublished",
-                "WorkflowsActivities-ContentRemoved",
-                "WorkflowsActivities-ContentVersioned",
-                "WorkflowsActivities-Delete-Publish",
-                "WorkflowsActivities-Decision",
-                "WorkflowsActivities-IsInRole",
-                "WorkflowsActivities-Notify",
-                "WorkflowsActivities-Publish",
-                "WorkflowsActivities-SendEmail",
-                "WorkflowsActivities-Timer",
-                "WorkflowsActivities-UserTask"
-                );
+            foreach (var activityName in activityNames) {
+                manifest.DefineStyle(activityName).SetUrl(activityName.HtmlClassify()).SetDependencies("WorkflowsAdmin");
+            }
 
-            builder.Add().DefineScript("jsPlumb").SetUrl("jquery.jsPlumb-1.4.1-all-min.js").SetDependencies("jQueryUI");
+            manifest.DefineStyle("WorkflowsActivities").SetUrl("workflows-activity.css").SetDependencies(activityNames);
+
+            manifest.DefineScript("jsPlumb").SetUrl("jquery.jsPlumb-1.4.1-all-min.js").SetDependencies("jQueryUI");
         }
     }
 }
