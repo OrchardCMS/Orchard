e42644a39b3d36c9bb7c4686243e0350efab866d
Sebastien Ros <sebastien.ros@microsoft.com>
2013-08-19 12:20:47 -0700

    Removing unused files

--- src/Orchard.Web/Modules/Orchard.Email/ResourceManifest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Orchard.UI.Resources;
-
-namespace Orchard.Email {
-    public class ResourceManifest : IResourceManifestProvider {
-        public void BuildManifests(ResourceManifestBuilder builder) {
-            //builder.Add().DefineStyle("WorkflowsActivities-SendEmail").SetUrl("workflows-activity-sendemail.css").SetDependencies("WorkflowsActivities");
-        }
-    }
-}
