3a73e77afeb43ca0df68ad6026be1b9ed264a579
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-08-27 18:44:05 +0200

    Fixing file name typo

---
+++ src/Orchard/Mvc/DataAnnotations/LocalizedRegularExpressionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Orchard.Localization;
+
+namespace Orchard.Mvc.DataAnnotations {
+    public class LocalizedRegularExpressionAttribute : RegularExpressionAttribute {
+        public LocalizedRegularExpressionAttribute(RegularExpressionAttribute attribute, Localizer t)
+            : base(attribute.Pattern) {
+            if ( !String.IsNullOrEmpty(attribute.ErrorMessage) )
+                ErrorMessage = attribute.ErrorMessage;
+
+            T = t;
+        }
+
+        public Localizer T { get; set; }
+
+        public override string FormatErrorMessage(string name) {
+            return String.IsNullOrEmpty(ErrorMessage)
+                ? T("The field {0} must match the regular expression '{1}'.", name, Pattern).Text
+                : T(ErrorMessage, name, Pattern).Text;
+        }
+    }
+}
