8020447191df898cab0bc8773e2f9a69b88b3f44
Daniel Stolt <daniel.stolt@perceptible.net>
2013-08-28 03:24:23 +0200

    Corrected the fake record in Orchard.Azure.

--- src/Orchard.Web/Modules/Orchard.Azure/Services/Caching/Database/AzureCacheRecord.cs
+++ src/Orchard.Web/Modules/Orchard.Azure/Services/Caching/Database/AzureCacheRecord.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Orchard.Environment.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +8,9 @@ namespace Orchard.Azure.Services.Caching.Database.Models {
 
     /// <summary>
     /// Fake record in order to force the mappings to be updated
-    /// once the modules is enabled/disabled.
+    /// whenever the feature is enabled/disabled.
     /// </summary>
+    [OrchardFeature(Constants.DatabaseCacheFeatureName)]
     public class AzureCacheRecord {
         public virtual int Id {
             get;
