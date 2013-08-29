1086aaf0b71c3c9a83c5cd76a9a7e9ad13c8f4eb
Walance <g.keulens@obec.nl>
2013-08-29 10:53:47 +0200

    #20000: Moving the logic of replacing newlines to a filter.
    Work Item: 20000

---
+++ src/Orchard.Web/Core/Common/Services/TextFieldFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using Orchard.Services;
+using Orchard.Utility.Extensions;
+
+namespace Orchard.Core.Common.Services {
+    public class TextFieldFilter : IHtmlFilter {
+        public string ProcessContent(string text, string flavor) {
+            // Flavor is null for a normal input/text field
+            return flavor == null || string.Equals(flavor, "textarea", StringComparison.OrdinalIgnoreCase) ? ReplaceNewLines(text) : text;
+        }
+
+        private static string ReplaceNewLines(string text) {
+            if (string.IsNullOrEmpty(text))
+                return string.Empty;
+
+            return HttpUtility.HtmlEncode(text).ReplaceNewLinesWith("<br />");
+        }
+    }
+} 
