4e218a536220e5accee4396b024f57aeba4d2987
Sebastien Ros <sebastien.ros@microsoft.com>
2013-10-29 18:20:29 -0700

    Fixing SpamFilterPart filenames

--- src/Orchard.Web/Modules/Orchard.AntiSpam/Models/SpamFilterPart.cs
+++ src/Orchard.Web/Modules/Orchard.AntiSpam/Models/SpamFilterPart.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Orchard.ContentManagement.Records;
+﻿using Orchard.ContentManagement;
 
 namespace Orchard.AntiSpam.Models {
-    public class SpamFilterPartRecord : ContentPartRecord {
-        public virtual SpamStatus Status { get; set; }
+    public class SpamFilterPart : ContentPart<SpamFilterPartRecord> {
+
+        public SpamStatus Status {
+            get { return Record.Status; }
+            set { Record.Status = value; }
+        }
     }
 }
--- src/Orchard.Web/Modules/Orchard.AntiSpam/Models/SpamFilterPartRecord.cs
+++ src/Orchard.Web/Modules/Orchard.AntiSpam/Models/SpamFilterPartRecord.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Orchard.ContentManagement;
-using Orchard.ContentManagement.Records;
+﻿using Orchard.ContentManagement.Records;
 
 namespace Orchard.AntiSpam.Models {
-    public class SpamFilterPart : ContentPart<SpamFilterPartRecord> {
-
-        public SpamStatus Status {
-            get { return Record.Status; }
-            set { Record.Status = value; }
-        }
+    public class SpamFilterPartRecord : ContentPartRecord {
+        public virtual SpamStatus Status { get; set; }
     }
 }
