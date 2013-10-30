44f2d4d3aa6d79e49c25c09a591b65aa68a8c53f
Sebastien Ros <sebastien.ros@microsoft.com>
2013-10-29 18:28:59 -0700

    Cleaning ArchiveLaterPart

--- src/Orchard.Web/Modules/Orchard.ArchiveLater/Models/ArchiveLaterPart.cs
+++ src/Orchard.Web/Modules/Orchard.ArchiveLater/Models/ArchiveLaterPart.cs
@@ -3,7 +3,7 @@ using Orchard.ContentManagement;
 using Orchard.ContentManagement.Utilities;
 
 namespace Orchard.ArchiveLater.Models {
-    public class ArchiveLaterPart : ContentPart<ArchiveLaterPart> {
+    public class ArchiveLaterPart : ContentPart {
         private readonly LazyField<DateTime?> _scheduledArchiveUtc = new LazyField<DateTime?>();
         public LazyField<DateTime?> ScheduledArchiveUtc { get { return _scheduledArchiveUtc; } }
     }
