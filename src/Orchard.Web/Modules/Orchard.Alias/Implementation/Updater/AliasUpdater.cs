5c24810427ede1fe887d2c0827ed655d853715fd
Sebastien Ros <sebastien.ros@microsoft.com>
2013-11-06 13:54:13 -0800

    Optimizing IAliasHolder updates

--- src/Orchard.Web/Modules/Orchard.Alias/Implementation/Updater/AliasUpdater.cs
+++ src/Orchard.Web/Modules/Orchard.Alias/Implementation/Updater/AliasUpdater.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using Orchard.Alias.Implementation.Holder;
 using Orchard.Alias.Implementation.Storage;
-using Orchard.Data;
 using Orchard.Environment;
 using Orchard.Tasks;
 using Orchard.Logging;
@@ -13,12 +10,14 @@ namespace Orchard.Alias.Implementation.Updater {
     public class AliasHolderUpdater : IOrchardShellEvents, IBackgroundTask {
         private readonly IAliasHolder _aliasHolder;
         private readonly IAliasStorage _storage;
+        private readonly IAliasUpdateCursor _cursor;
 
         public ILogger Logger { get; set; }
 
-        public AliasHolderUpdater(IAliasHolder aliasHolder, IAliasStorage storage) {
+        public AliasHolderUpdater(IAliasHolder aliasHolder, IAliasStorage storage, IAliasUpdateCursor cursor) {
             _aliasHolder = aliasHolder;
             _storage = storage;
+            _cursor = cursor;
             Logger = NullLogger.Instance;
         }
 
@@ -31,7 +30,14 @@ namespace Orchard.Alias.Implementation.Updater {
 
         private void Refresh() {
             try {
-                var aliases = _storage.List();
+                // only retreive aliases which have not been processed yet
+                var aliases = _storage.List(x => x.Id > _cursor.Cursor).ToArray();
+
+                // update the last processed id
+                if (aliases.Any()) {
+                    _cursor.Cursor = aliases.Last().Item5;
+                }
+                
                 _aliasHolder.SetAliases(aliases.Select(alias => new AliasInfo { Path = alias.Item1, Area = alias.Item2, RouteValues = alias.Item3 }));
             }
             catch (Exception ex) {
