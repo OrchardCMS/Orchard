9a07d8b4198971958ffe9b427a93dde8b29bec75
Lombiq <registrator@lombiq.com>
2013-12-27 01:27:04 +0100

    Adding some inline documentation to shell management API

--- src/Orchard/Environment/Configuration/IShellSettingsManager.cs
+++ src/Orchard/Environment/Configuration/IShellSettingsManager.cs
@@ -2,7 +2,16 @@ using System.Collections.Generic;
 
 namespace Orchard.Environment.Configuration {
     public interface IShellSettingsManager {
+        /// <summary>
+        /// Retrieves all shell settings stored.
+        /// </summary>
+        /// <returns>All shell settings.</returns>
         IEnumerable<ShellSettings> LoadSettings();
+
+        /// <summary>
+        /// Persists shell settings to the storage.
+        /// </summary>
+        /// <param name="settings">The shell settings to store.</param>
         void SaveSettings(ShellSettings settings);
     }
 }
