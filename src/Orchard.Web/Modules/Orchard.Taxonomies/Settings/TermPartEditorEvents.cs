529a43c1ed60efca512690bece452888ddb30f9a
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-01-18 18:36:11 -0800

    Added a part setting for TermPart so the display type can be changed on child items when displaying the details of the term.

---
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Settings/TermPartEditorEvents.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.ContentManagement.MetaData.Models;
+using Orchard.ContentManagement.ViewModels;
+
+namespace Orchard.Taxonomies.Settings {
+    public class TermPartEditorEvents : ContentDefinitionEditorEventsBase {
+        public override IEnumerable<TemplateViewModel> TypePartEditor(ContentTypePartDefinition definition) {
+            if (definition.PartDefinition.Name == "TermPart") {
+                var model = definition.Settings.GetModel<TermPartSettings>();
+                yield return DefinitionTemplate(model);
+            }
+        }
+
+        public override IEnumerable<TemplateViewModel> TypePartEditorUpdate(ContentTypePartDefinitionBuilder builder, IUpdateModel updateModel) {
+            if (builder.Name != "TermPart") {
+                yield break;
+            }
+
+            var model = new TermPartSettings();
+
+            if (updateModel.TryUpdateModel(model, "TermPartSettings", null, null)) {
+                builder
+                    .WithSetting("TermPartSettings.ChildDisplayType", model.ChildDisplayType);
+            }
+
+            yield return DefinitionTemplate(model);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Settings/TermPartSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Orchard.Taxonomies.Settings {
+    public class TermPartSettings {
+        /// <summary>
+        /// The display type to use for the child items of the term.
+        /// </summary>
+        public string ChildDisplayType { get; set; }
+    }
+}
