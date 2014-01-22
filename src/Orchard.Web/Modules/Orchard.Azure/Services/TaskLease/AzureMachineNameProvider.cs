ece9cff117aa985a981e5bfa10b9c2d8d57be512
Daniel Stolt <daniel.stolt@perceptible.net>
2014-01-22 21:27:16 +0100

    #20433: Added specialized implementation of IMachineNameProvider that uses the role instance ID instead of the Windows machine name for task lease records, and activated this implementation for cloud services through Host.config. This makes task lease records resilient to role instance reimaging.
    Work Item: 20433

---
+++ src/Orchard.Web/Modules/Orchard.Azure/Services/TaskLease/AzureMachineNameProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Orchard.TaskLease.Services;
+
+namespace Orchard.Azure.Services.TaskLease
+{
+	public class AzureMachineNameProvider : IMachineNameProvider
+	{
+		public string GetMachineName()
+		{
+			return RoleEnvironment.CurrentRoleInstance.Id;
+		}
+	}
+}
