83df0ebdf07c50f6381e426ca034c7f90a8aa01a
Sebastien Ros <sebastien.ros@microsoft.com>
2014-02-05 10:28:24 -0800

    Adding tags to RSS feeds

---
+++ src/Orchard.Web/Modules/Orchard.Tags/Feeds/TagsFeedItemBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using Orchard.ContentManagement;
+using Orchard.Core.Feeds;
+using Orchard.Core.Feeds.Models;
+using Orchard.Environment.Extensions;
+using Orchard.Tags.Models;
+
+namespace Orchard.Tags.Feeds {
+    [OrchardFeature("Orchard.Tags.Feeds")]
+    public class TagsFeedItemBuilder : IFeedItemBuilder {
+        public void Populate(FeedContext context) {
+            foreach (var feedItem in context.Response.Items.OfType<FeedItem<ContentItem>>()) {
+                // add to known formats
+                if (context.Format == "rss") {
+                    
+                    // adding tags to the rss item
+                    var tagsPart = feedItem.Item.As<TagsPart>();
+                    if (tagsPart != null) {
+                        tagsPart.CurrentTags.ToList().ForEach(x => 
+                            feedItem.Element.Add(new XElement("category", x)));
+                    }
+                }
+            }
+        }
+    }
+}
