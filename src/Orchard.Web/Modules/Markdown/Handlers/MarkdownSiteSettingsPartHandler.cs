1f81355afd96e6cdbb1889930c7b2a5a92fe73e6
Sebastien Ros <sebastien.ros@microsoft.com>
2014-02-17 17:15:37 -0800

    Improving Markdown support
    - Adding a settings page to enable Markdown on Blogs
    - Improving live preview Markdown styles

---
+++ src/Orchard.Web/Modules/Markdown/Handlers/MarkdownSiteSettingsPartHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Markdown.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Localization;
+
+namespace Markdown.Handlers {
+    public class MarkdownSiteSettingsPartHandler : ContentHandler {
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+
+        public MarkdownSiteSettingsPartHandler(IContentDefinitionManager contentDefinitionManager) {
+            _contentDefinitionManager = contentDefinitionManager;
+            Filters.Add(new ActivatingFilter<MarkdownSiteSettingsPart>("Site"));
+            Filters.Add(new TemplateFilterForPart<MarkdownSiteSettingsPart>("MarkdownSiteSettings", "Parts/Markdown.MarkdownSiteSettings", "markdown"));
+            OnInitializing<MarkdownSiteSettingsPart>((context, part) => {
+                part.UseMarkdownForBlogs = false;
+            });
+
+            OnUpdated<MarkdownSiteSettingsPart>((context, part) => {
+                var blogPost = _contentDefinitionManager.GetTypeDefinition("BlogPost");
+                if (blogPost == null) {
+                        return;
+                }
+
+                var bodyPart = blogPost.Parts.FirstOrDefault(x => x.PartDefinition.Name == "BodyPart");
+                if (bodyPart == null) {
+                    return;
+                }
+
+                _contentDefinitionManager.AlterTypeDefinition("BlogPost", build => build
+                    .WithPart("BodyPart", cfg => cfg
+                        .WithSetting("BodyTypePartSettings.Flavor", part.UseMarkdownForBlogs ? "markdown" : "html")
+                    )
+                );
+            });
+
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            if (context.ContentItem.ContentType != "Site")
+                return;
+            base.GetItemMetadata(context);
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("Markdown")));
+        }
+    }
+}
