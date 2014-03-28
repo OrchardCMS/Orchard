129133f6ba083f1e4d7f68e83de858a67e500f86
Sipke Schoorstra <sipke@ideliverable.com>
2014-03-28 01:28:23 +0100

    Added Lists projection filter and sort criterion provider.

---
+++ src/Orchard.Web/Modules/Orchard.Lists/Forms/ListFilterForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
+using Orchard.ContentManagement;
+using Orchard.Core.Containers.Models;
+using Orchard.Core.Containers.Services;
+using Orchard.DisplayManagement;
+using Orchard.Lists.Services;
+
+namespace Orchard.Lists.Forms {
+    public class ListFilterForm : Component, IFormProvider {
+        private readonly IContainerService _containerService;
+        private readonly IContentManager _contentManager;
+
+        public ListFilterForm(IShapeFactory shapeFactory, IContainerService containerService, IContentManager contentManager) {
+            _containerService = containerService;
+            _contentManager = contentManager;
+            New = shapeFactory;
+        }
+        protected dynamic New { get; set; }
+
+        public void Describe(dynamic context) {
+            Func<IShapeFactory, object> form =
+                shape => {
+                    var f = New.Form(
+                        Id: "List",
+                        _Lists: New.SelectList(
+                            Id: "listId", Name: "ListId",
+                            Title: T("List"),
+                            Description: T("Select a list."),
+                            Multiple: false));
+
+                    foreach (var list in _containerService.GetContainers(VersionOptions.Latest).OrderBy(GetListName)) {
+                        f._Lists.Add(new SelectListItem {Value = list.Id.ToString(CultureInfo.InvariantCulture), Text = GetListName(list)});
+                    }
+
+                    return f;
+                };
+
+            context.Form("ListFilter", form);
+        }
+
+        private string GetListName(ContainerPart containerPart) {
+            return _contentManager.GetItemMetadata(containerPart).DisplayText;
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Lists/Projections/ListFilter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Models;
+using Orchard.Core.Containers.Models;
+using Orchard.Lists.Services;
+using Orchard.Localization;
+
+namespace Orchard.Lists.Projections {
+    public class ListFilter : Component, IFilterProvider {
+        private readonly IContentManager _contentManager;
+
+        public ListFilter(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
+        public void Describe(dynamic describe) {
+            describe.For("Lists", T("Lists"), T("Lists"))
+                .Element("List", T("List"), T("Specific list"),
+                    (Action<dynamic>)ApplyFilter,
+                    (Func<dynamic, LocalizedString>)DisplayFilter,
+                    "ListFilter"
+                );
+        }
+
+        public void ApplyFilter(dynamic context) {
+            var selectedList = (string)context.State.ListId;
+
+            if (String.IsNullOrWhiteSpace(selectedList))
+                return;
+
+            var query = (IHqlQuery)context.Query;
+            var listId = Int32.Parse(selectedList);
+            context.Query = query.Where(alias => alias.ContentPartRecord<CommonPartRecord>(), item => item.Eq("Container", listId));
+        }
+
+        public LocalizedString DisplayFilter(dynamic context) {
+            var selectedList = (string)context.State.ListId;
+
+            if (String.IsNullOrWhiteSpace(selectedList))
+                return T("No list was selected");
+
+            var listId = Int32.Parse(selectedList);
+            var list = _contentManager.Get<ContainerPart>(listId, VersionOptions.Latest, QueryHints.Empty);
+            var listName = _contentManager.GetItemMetadata(list).DisplayText;
+            return T("Show items from the following list: {0}", listName);
+        }
+    }
+}
