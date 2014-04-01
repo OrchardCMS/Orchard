6bee52e950b43809cede79bede173c4ec7c258c8
Sebastien Ros <sebastien.ros@microsoft.com>
2014-04-01 15:49:26 -0700

    Using the blog's description in RSS feeds

---
+++ src/Orchard.Web/Modules/Orchard.Blogs/Feeds/BlogPartFeedItemBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Xml.Linq;
+using JetBrains.Annotations;
+using Orchard.Blogs.Models;
+using Orchard.ContentManagement;
+using Orchard.Core.Feeds;
+using Orchard.Core.Feeds.Models;
+using Orchard.Mvc.Extensions;
+using Orchard.Services;
+
+namespace Orchard.Blogs.Feeds {
+    public class BlogPartFeedItemBuilder : IFeedItemBuilder {
+        private IContentManager _contentManager;
+        public BlogPartFeedItemBuilder(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
+        public void Populate(FeedContext context) {
+            var containerIdValue = context.ValueProvider.GetValue("containerid");
+            if (containerIdValue == null)
+                return;
+
+            var containerId = (int)containerIdValue.ConvertTo(typeof(int));
+            var container = _contentManager.Get(containerId);
+
+            if (container == null) {
+                return;
+            }
+
+            if (container.ContentType != "Blog") {
+                return;
+            }
+
+            var blog = container.As<BlogPart>();
+
+            if (context.Format == "rss") {
+                context.Response.Element.SetElementValue("description", blog.Description);
+            }
+            else {
+                context.Builder.AddProperty(context, null, "description", blog.Description);
+            }
+        }
+    }
+}
