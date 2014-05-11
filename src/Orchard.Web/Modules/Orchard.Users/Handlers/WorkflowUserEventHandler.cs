68e9972579a2c99dab31915033ab2576b842b7de
Nicholas Mayne <Jetski5822@gmail.com>
2014-05-11 13:49:11 +0100

    Making sure previous commit follows naming conventions

--- src/Orchard.Web/Modules/Orchard.Users/Handlers/WorkflowUserEventHandler.cs
+++ src/Orchard.Web/Modules/Orchard.Users/Handlers/WorkflowUserEventHandler.cs
@@ -6,62 +6,62 @@ using Orchard.Workflows.Services;
 namespace Orchard.Users.Handlers {
     [OrchardFeature("Orchard.Users.Workflows")]
     public class WorkflowUserEventHandler : IUserEventHandler {
-        private IWorkflowManager workflowManager;
+        private readonly IWorkflowManager _workflowManager;
 
         public WorkflowUserEventHandler(IWorkflowManager workflowManager) {
-            this.workflowManager = workflowManager;
+            _workflowManager = workflowManager;
         }
 
         public void Creating(UserContext context) {
-            workflowManager.TriggerEvent("UserCreating",
+            _workflowManager.TriggerEvent("UserCreating",
                                          context.User,
                                          () => new Dictionary<string, object> {{"User", context}});
         }
 
         public void Created(UserContext context) {
-            workflowManager.TriggerEvent("UserCreated",
+            _workflowManager.TriggerEvent("UserCreated",
                                          context.User,
                                          () => new Dictionary<string, object> {{"User", context}});
         }
 
         public void LoggedIn(Security.IUser user) {
-            workflowManager.TriggerEvent("UserLoggedIn",
+            _workflowManager.TriggerEvent("UserLoggedIn",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
 
         public void LoggedOut(Security.IUser user) {
-            workflowManager.TriggerEvent("UserLoggedOut",
+            _workflowManager.TriggerEvent("UserLoggedOut",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
 
         public void AccessDenied(Security.IUser user) {
-            workflowManager.TriggerEvent("UserAccessDenied",
+            _workflowManager.TriggerEvent("UserAccessDenied",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
 
         public void ChangedPassword(Security.IUser user) {
-            workflowManager.TriggerEvent("UserChangedPassword",
+            _workflowManager.TriggerEvent("UserChangedPassword",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
 
         public void SentChallengeEmail(Security.IUser user) {
-            workflowManager.TriggerEvent("UserSentChallengeEmail",
+            _workflowManager.TriggerEvent("UserSentChallengeEmail",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
 
         public void ConfirmedEmail(Security.IUser user) {
-            workflowManager.TriggerEvent("UserConfirmedEmail",
+            _workflowManager.TriggerEvent("UserConfirmedEmail",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
 
         public void Approved(Security.IUser user) {
-            workflowManager.TriggerEvent("UserApproved",
+            _workflowManager.TriggerEvent("UserApproved",
                                          user,
                                          () => new Dictionary<string, object> {{"User", user}});
         }
