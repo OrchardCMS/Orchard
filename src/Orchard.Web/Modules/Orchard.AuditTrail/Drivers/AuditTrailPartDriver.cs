780d1248d3c239e932ebdb9479834f9cfe978f6b
sfmskywalker <sipke@ideliverable.com>
2014-05-26 01:36:05 +0200

    Implementing inline audit trail on content editor view.

--- src/Orchard.Web/Modules/Orchard.AuditTrail/Drivers/AuditTrailPartDriver.cs
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/Drivers/AuditTrailPartDriver.cs
@@ -1,20 +1,72 @@
-﻿using Orchard.AuditTrail.Models;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Orchard.AuditTrail.Models;
+using Orchard.AuditTrail.Services;
+using Orchard.AuditTrail.Settings;
+using Orchard.AuditTrail.ViewModels;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.UI.Navigation;
 
 namespace Orchard.AuditTrail.Drivers {
     public class AuditTrailPartDriver : ContentPartDriver<AuditTrailPart> {
+        private readonly IOrchardServices _services;
+        private readonly IAuditTrailManager _auditTrailManager;
+        private readonly IAuditTrailEventDisplayBuilder _displayBuilder;
+
+        public AuditTrailPartDriver(IOrchardServices services, IAuditTrailManager auditTrailManager, IAuditTrailEventDisplayBuilder displayBuilder) {
+            _services = services;
+            _auditTrailManager = auditTrailManager;
+            _displayBuilder = displayBuilder;
+        }
+
         protected override DriverResult Editor(AuditTrailPart part, dynamic shapeHelper) {
             return Editor(part, null, shapeHelper);
         }
 
         protected override DriverResult Editor(AuditTrailPart part, IUpdateModel updater, dynamic shapeHelper) {
-            return ContentShape("Parts_AuditTrail_Edit", () => {
-                if (updater != null) {
-                    updater.TryUpdateModel(part, Prefix, null, null);
-                }
-                return shapeHelper.EditorTemplate(Model: part, TemplateName: "Parts.AuditTrail", Prefix: Prefix);
-            });
+            var settings = part.Settings.GetModel<AuditTrailPartSettings>();
+            var results = new List<DriverResult>();
+
+            if (settings.ShowAuditTrailCommentInput) {
+                results.Add(ContentShape("Parts_AuditTrail_Comment", () => {
+                    if (updater != null) {
+                        updater.TryUpdateModel(part, Prefix, null, null);
+                    }
+                    return shapeHelper.EditorTemplate(Model: part, TemplateName: "Parts.AuditTrail.Comment", Prefix: Prefix);
+                }));
+            }
+
+            if (settings.ShowAuditTrailLink) {
+                results.Add(ContentShape("Parts_AuditTrail_Link", () => shapeHelper.Parts_AuditTrail_Link()));
+            }
+
+            if (settings.ShowAuditTrail) {
+                results.Add(ContentShape("Parts_AuditTrail", () => {
+                    var pager = new Pager(_services.WorkContext.CurrentSite, null, null);
+                    var pageOfData = _auditTrailManager.GetRecords(pager.Page, pager.PageSize, new AuditTrailFilterParameters {
+                        FilterKey = "content",
+                        FilterValue = part.Id.ToString(CultureInfo.InvariantCulture)
+                    });
+                    var pagerShape = shapeHelper.Pager(pager).TotalItemCount(pageOfData.TotalItemCount);
+                    var eventDescriptors = from c in _auditTrailManager.Describe()
+                                           from e in c.Events
+                                           select e;
+                    var recordViewModels = from record in pageOfData
+                                           let descriptor = eventDescriptors.FirstOrDefault(x => x.Event == record.Event)
+                                           where descriptor != null
+                                           select new AuditTrailEventSummaryViewModel {
+                                               Record = record,
+                                               EventDescriptor = descriptor,
+                                               CategoryDescriptor = descriptor.CategoryDescriptor,
+                                               SummaryShape = _displayBuilder.BuildDisplay(record, "SummaryAdmin")
+                                           };
+                    return shapeHelper.Parts_AuditTrail(Records: recordViewModels, Pager: pagerShape);
+                }));
+            }
+
+            return Combined(results.ToArray());
         }
     }
 }
---
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/Scripts/audit-trail-admin.js
@@ -0,0 +1,10 @@
+﻿(function($) {
+    // Initialize Expando control
+    $(".expando-wrapper legend").expandoControl(
+        function (controller) {
+            return controller.nextAll(".expando");
+        }, {
+            collapse: true,
+            remember: true
+        });
+})(jQuery);
