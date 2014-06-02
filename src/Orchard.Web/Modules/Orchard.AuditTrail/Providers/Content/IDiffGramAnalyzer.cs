3c159fbd9add7d81ab4d4a85f3b7435137badfab
sfmskywalker <sipke@ideliverable.com>
2014-06-02 20:14:43 +0200

    Adding descriptive XML comments.

--- src/Orchard.Web/Modules/Orchard.AuditTrail/Providers/Content/IDiffGramAnalyzer.cs
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/Providers/Content/IDiffGramAnalyzer.cs
@@ -3,7 +3,15 @@ using System.Xml.Linq;
 
 namespace Orchard.AuditTrail.Providers.Content {
     public interface IDiffGramAnalyzer : IDependency {
+        /// <summary>
+        /// Compares the specified XML elements and returns a DiffGram XML element.
+        /// </summary>
         XElement GenerateDiffGram(XElement element1, XElement element2);
+
+        /// <summary>
+        /// Analyzes the specified DiffGram against the specified original XML element and returns a list of diff nodes,
+        /// where each node describes the difference between the original and updated document.
+        /// </summary>
         IEnumerable<DiffNode> Analyze(XElement original, XElement diffGram);
     }
 }
