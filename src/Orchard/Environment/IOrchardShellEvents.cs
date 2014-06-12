3561d573248909ff5792874aa57af749ef8aac51
Sipke Schoorstra <sipke@orchardpros.net>
2014-06-11 20:52:09 -0700

    Moving IFeatureEventHandler to its own file.

--- src/Orchard/Environment/IOrchardShellEvents.cs
+++ src/Orchard/Environment/IOrchardShellEvents.cs
@@ -1,20 +1,8 @@
-﻿using Orchard.Environment.Extensions.Models;
-using Orchard.Events;
+﻿using Orchard.Events;
 
 namespace Orchard.Environment {
     public interface IOrchardShellEvents : IEventHandler {
         void Activated();
         void Terminating();
-    }
-
-    public interface IFeatureEventHandler : IEventHandler {
-        void Installing(Feature feature);
-        void Installed(Feature feature);
-        void Enabling(Feature feature);
-        void Enabled(Feature feature);
-        void Disabling(Feature feature);
-        void Disabled(Feature feature);
-        void Uninstalling(Feature feature);
-        void Uninstalled(Feature feature);
     }
 }
