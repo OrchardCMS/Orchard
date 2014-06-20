ac1684b0f916433b41d28fcf9127b7f33adaf7d3
Sebastien Ros <sebastien.ros@microsoft.com>
2014-06-20 15:16:49 -0700

    Fixing Widget layers deletion

--- src/Orchard.Web/Modules/Orchard.Widgets/Services/WidgetsService.cs
+++ src/Orchard.Web/Modules/Orchard.Widgets/Services/WidgetsService.cs
@@ -57,7 +57,7 @@ namespace Orchard.Widgets.Services {
 
         public IEnumerable<WidgetPart> GetOrphanedWidgets() {
             return _contentManager
-                .Query<WidgetPart, WidgetPartRecord>()
+                .Query<WidgetPart>()
                 .ForVersion(VersionOptions.Latest)
                 .WithQueryHints(new QueryHints().ExpandParts<CommonPart>())
                 .Where<CommonPartRecord>(x => x.Container == null)
@@ -66,7 +66,8 @@ namespace Orchard.Widgets.Services {
 
         public IEnumerable<WidgetPart> GetWidgets(int layerId) {
             return _contentManager
-                .Query<WidgetPart, WidgetPartRecord>()
+                .Query<WidgetPart>()
+                .ForVersion(VersionOptions.Latest)
                 .WithQueryHints(new QueryHints().ExpandParts<CommonPart>())
                 .Where<CommonPartRecord>(x => x.Container.Id == layerId)
                 .List();
