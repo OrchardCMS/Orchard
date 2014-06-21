a507074b9a9ddd9135657bac8bf86db58151695c
Nicholas Mayne <Jetski5822@gmail.com>
2014-06-21 17:13:23 +0100

    #19803: Adding TinyMCE
    Work Item: 19803

--- src/Orchard.Web/Modules/TinyMce/Scripts/orchard-tinymce.js
+++ src/Orchard.Web/Modules/TinyMce/Scripts/orchard-tinymce.js
@@ -9,18 +9,15 @@ if (mediaLibraryEnabled) {
 }
 
 tinyMCE.init({
-    theme: "advanced",
+    selector: "textarea.tinymce",
+    theme: "modern",
     schema: "html5",
-    mode: "specific_textareas",
-    editor_selector: "tinymce",
-    plugins: "fullscreen,autoresize,searchreplace,inlinepopups" + mediaPlugins.substr(2),
-    theme_advanced_toolbar_location: "top",
-    theme_advanced_toolbar_align: "left",
-    theme_advanced_buttons1: "search,replace,|,cut,copy,paste,|,undo,redo" + mediaPlugins + ",|,link,unlink,charmap,emoticon,codeblock,|,bold,italic,|,numlist,bullist,formatselect,|,code,fullscreen,",
-    theme_advanced_buttons2: "",
-    theme_advanced_buttons3: "",
+    plugins: "fullscreen,autoresize,searchreplace" + mediaPlugins.substr(2),
+    toolbar: "search,replace,|,cut,copy,paste,|,undo,redo" + mediaPlugins + ",|,link,unlink,charmap,emoticon,codeblock,|,bold,italic,|,numlist,bullist,formatselect,|,code,fullscreen,",
     convert_urls: false,
     valid_elements: "*[*]",
     // shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
-    extended_valid_elements: "script[type|defer|src|language]"
+    extended_valid_elements: "script[type|defer|src|language]",
+    menubar: false,
+    statusbar: false
 });
