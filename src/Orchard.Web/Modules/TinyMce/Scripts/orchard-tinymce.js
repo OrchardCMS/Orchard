aed9793fc02d3c4fe12d0387fbbed7a4f2a2238c
Nicholas Mayne <Jetski5822@gmail.com>
2014-06-22 19:09:22 +0100

    Adding plugins and fixing styling conflict with admin theme (TinyMCE)

--- src/Orchard.Web/Modules/TinyMce/Scripts/orchard-tinymce.js
+++ src/Orchard.Web/Modules/TinyMce/Scripts/orchard-tinymce.js
@@ -12,12 +12,13 @@ tinyMCE.init({
     selector: "textarea.tinymce",
     theme: "modern",
     schema: "html5",
-    plugins: "fullscreen,autoresize,searchreplace" + mediaPlugins.substr(2),
-    toolbar: "search,replace,|,cut,copy,paste,|,undo,redo" + mediaPlugins + ",|,link,unlink,charmap,emoticon,codeblock,|,bold,italic,|,numlist,bullist,formatselect,|,code,fullscreen,",
+    plugins: "fullscreen,autoresize,searchreplace,link,charmap,code" + mediaPlugins.substr(2),
+    toolbar: "searchreplace,|,cut,copy,paste,|,undo,redo" + mediaPlugins + ",|,link,unlink,charmap,|,bold,italic,|,numlist,bullist,formatselect,|,code,fullscreen,",
     convert_urls: false,
     valid_elements: "*[*]",
     // shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
     extended_valid_elements: "script[type|defer|src|language]",
     menubar: false,
-    statusbar: false
+    statusbar: false,
+    skin: 'orchardlightgray'
 });
