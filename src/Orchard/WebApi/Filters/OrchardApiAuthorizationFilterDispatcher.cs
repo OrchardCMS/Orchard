64e1a745f56eb0103b1c4c39fe28c9baaee05df1
Nicholas Mayne <Jetski5822@gmail.com>
2014-06-22 22:55:19 +0100

    Adding IAuthorizationFilter support for WebApi

---
+++ src/Orchard/WebApi/Filters/OrchardApiAuthorizationFilterDispatcher.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Orchard.WebApi.Filters {
+    public class OrchardApiAuthorizationFilterDispatcher : IAuthorizationFilter {
+        public bool AllowMultiple { get; private set; }
+        public async Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation) {
+            var workContext = actionContext.ControllerContext.GetWorkContext();
+
+            foreach (var actionFilter in workContext.Resolve<IEnumerable<IApiFilterProvider>>().OfType<IAuthorizationFilter>()) {
+                continuation = () => actionFilter.ExecuteAuthorizationFilterAsync(actionContext, cancellationToken, continuation);
+            }
+
+            return await continuation();
+        }
+    }
+}
