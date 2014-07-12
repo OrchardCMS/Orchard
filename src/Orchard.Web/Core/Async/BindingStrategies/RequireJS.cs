f947f75252469c01811ead345793937fd846e032
Sipke Schoorstra <sipke@orchardpros.net>
2014-07-12 01:33:55 -0700

    Implementing RequireJS dependency loading.

--- src/Orchard.Web/Core/Async/BindingStrategies/RequireJS.cs
+++ src/Orchard.Web/Core/Async/BindingStrategies/RequireJS.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Orchard.DisplayManagement;
@@ -27,20 +28,68 @@ namespace Orchard.Core.Async.BindingStrategies {
             if (!resources.Any())
                 return;
 
-            var scripts = resources.Select(x => String.Format("\"{0}\"", GetUrl(defaultSettings, appPath, x)));
+            var dictionary = resources.ToDictionary(x => x.Resource.Name);
+            var resourcesWithDependencies = resources.Where(x => x.Resource.Dependencies != null && x.Resource.Dependencies.Any()).ToArray();
+            var scriptBuilder = new StringBuilder();
 
-            var tagBuilder = new TagBuilder("script");
+            // Configure dependencies
+            if (resourcesWithDependencies.Any()) {
+                scriptBuilder.AppendFormat(
+                    "requirejs.config({{" +
+                    "shim: {{\r\n");
+
+                var index = 0;
+                foreach (var resource in resourcesWithDependencies) {
+                    var resourceName = GetModuleName(defaultSettings, appPath, resource);
+                    var dependencies = String.Join(",", resource.Resource.Dependencies.Select(x => String.Format("\"{0}\"", GetModuleName(defaultSettings, appPath, dictionary[x]))));
+                    scriptBuilder.AppendFormat("\"{0}\": [{1}]", resourceName, dependencies);
+
+                    if (index++ < resourcesWithDependencies.Length - 1) {
+                        scriptBuilder.AppendLine(",");
+                    }
+                }
+                
+                scriptBuilder.AppendFormat("}}" +
+                    "}});\r\n");
+            }
+
+            var scripts = resources.Select(x => String.Format("\"{0}\"", GetModuleName(defaultSettings, appPath, x)));            
+            scriptBuilder.AppendFormat("requirejs([{0}]);\r\n", String.Join("\r\n,", scripts));
+
+            var tagBuilder = new TagBuilder("script") {
+                InnerHtml = scriptBuilder.ToString()
+            };
             tagBuilder.Attributes["type"] = "text/javascript";
-            tagBuilder.InnerHtml = String.Format("requirejs([{0}])", String.Join(",", scripts));
             Output.Write(tagBuilder.ToString(TagRenderMode.Normal));
         }
 
-        private static string GetUrl(RequireSettings defaultSettings, string appPath, ResourceRequiredContext resource) {
+        private static string GetModuleName(RequireSettings defaultSettings, string appPath, ResourceRequiredContext resource) {
             var url = resource.GetResourceUrl(defaultSettings, appPath);
-            if (VirtualPathUtility.IsAppRelative(url)) {
-                url = VirtualPathUtility.ToAbsolute(url);
-            }
-            return url;
+
+            if (IsAbsoluteUrl(url))
+                return url;
+
+            var path = url.Substring(appPath.Length);
+            var name = TrimStartingSlash(GetPathWithoutExtension(path).ToLowerInvariant());
+            return name;
+        }
+
+        private static string TrimStartingSlash(string path) {
+            return path.StartsWith("/") ? path.Substring(1) : path;
+        }
+
+        private static string GetPathWithoutExtension(string path) {
+            var index = path.LastIndexOf('.');
+            return index > 0 ? path.Substring(0, index) : path;
+        }
+
+        private static bool IsAbsoluteUrl(string url) {
+            Uri uri;
+
+            if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out uri))
+                return false;
+
+            return uri.IsAbsoluteUri;
         }
     }
 }
