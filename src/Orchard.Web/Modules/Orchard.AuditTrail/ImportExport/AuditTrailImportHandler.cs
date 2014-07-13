655467d823d8edb839d98c44134164a4e449ac5b
Sipke Schoorstra <sipke@orchardpros.net>
2014-07-12 23:49:20 -0700

    Adding and implementing ImportAuditTrail permission.

--- src/Orchard.Web/Modules/Orchard.AuditTrail/ImportExport/AuditTrailImportHandler.cs
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/ImportExport/AuditTrailImportHandler.cs
@@ -3,20 +3,32 @@ using Orchard.AuditTrail.Models;
 using Orchard.ContentManagement;
 using Orchard.Data;
 using Orchard.Environment.Extensions;
+using Orchard.Logging;
 using Orchard.Recipes.Models;
 using Orchard.Recipes.Services;
+using Orchard.Security;
 
 namespace Orchard.AuditTrail.ImportExport {
     [OrchardFeature("Orchard.AuditTrail.ImportExport")]
     public class AuditTrailImportHandler : Component, IRecipeHandler {
         private readonly IRepository<AuditTrailEventRecord> _auditTrailEventRepository;
+        private readonly IAuthorizer _authorizer;
+        private readonly IWorkContextAccessor _wca;
 
-        public AuditTrailImportHandler(IRepository<AuditTrailEventRecord> auditTrailEventRepository) {
+        public AuditTrailImportHandler(IRepository<AuditTrailEventRecord> auditTrailEventRepository, IAuthorizer authorizer, IWorkContextAccessor wca) {
             _auditTrailEventRepository = auditTrailEventRepository;
+            _authorizer = authorizer;
+            _wca = wca;
         }
 
         public void ExecuteRecipeStep(RecipeContext recipeContext) {
             if (!String.Equals(recipeContext.RecipeStep.Name, "AuditTrail", StringComparison.OrdinalIgnoreCase)) {
+                return;
+            }
+
+            if (!_authorizer.Authorize(Permissions.ImportAuditTrail)) {
+                Logger.Warning("Blocked {0} from importing an audit trail because this user does not have the ImportauditTrail permission.", _wca.GetContext().CurrentUser.UserName);
+                recipeContext.Executed = false;
                 return;
             }
 
--- src/Orchard.Web/Modules/Orchard.AuditTrail/Permissions.cs
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/Permissions.cs
@@ -6,19 +6,23 @@ namespace Orchard.AuditTrail {
     public class Permissions : IPermissionProvider {
         public static readonly Permission ViewAuditTrail = new Permission { Description = "View audit trail", Name = "ViewAuditTrail" };
         public static readonly Permission ManageAuditTrailSettings = new Permission { Description = "Manage audit trail settings", Name = "ManageAuditTrailSettings" };
+        public static readonly Permission ImportAuditTrail = new Permission { Description = "Import audit trail", Name = "ImportAuditTrail" };
 
         public virtual Feature Feature { get; set; }
 
         public IEnumerable<Permission> GetPermissions() {
             yield return ViewAuditTrail;
             yield return ManageAuditTrailSettings;
+            yield return ImportAuditTrail;
         }
 
         public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
-            return new[] {
-                new PermissionStereotype {
-                    Name = "Administrator",
-                    Permissions = new[] {ViewAuditTrail, ManageAuditTrailSettings}
+            yield return new PermissionStereotype {
+                Name = "Administrator",
+                Permissions = new[] {
+                    ViewAuditTrail, 
+                    ManageAuditTrailSettings,
+                    /* Not even an administrator will get the ImportAuditTrail permission. */
                 }
             };
         }
