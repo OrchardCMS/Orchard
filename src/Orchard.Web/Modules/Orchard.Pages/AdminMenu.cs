313dc0015f72324d2d773c84403a75c4bf270e96
Sipke Schoorstra <sipke@orchardpros.net>
2014-07-22 11:08:56 -0700

    Fixing namespace.

--- src/Orchard.Web/Modules/Orchard.Pages/AdminMenu.cs
+++ src/Orchard.Web/Modules/Orchard.Pages/AdminMenu.cs
@@ -1,7 +1,7 @@
 ﻿using Orchard.Localization;
 using Orchard.UI.Navigation;
 
-namespace Orchard.Core.Dashboard {
+namespace Orchard.Pages {
     public class AdminMenu : INavigationProvider {
         public Localizer T { get; set; }
         public string MenuName { get { return "admin"; } }
