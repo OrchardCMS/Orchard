2b37a6ed6e2fc848ee1daf4b6e6bf98efe3a67d0
Daniel Stolt <daniel@ideliverable.com>
2014-07-25 23:11:02 +0200

    Added IDependency as base interface of IDateFormatter.

--- src/Orchard/Localization/Services/IDateFormatter.cs
+++ src/Orchard/Localization/Services/IDateFormatter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Orchard.Framework.Localization.Models;
 
 namespace Orchard.Framework.Localization.Services {
-    public interface IDateFormatter {
+    public interface IDateFormatter : IDependency {
         DateTimeParts ParseDateTime(string dateTimeString, CultureInfo culture);
         DateParts ParseDate(string dateString, CultureInfo culture);
         TimeParts ParseTime(string timeString, CultureInfo culture);
