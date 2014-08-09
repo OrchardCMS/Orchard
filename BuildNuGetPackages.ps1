0c62997865dabe5e7bf854c8aa8e42054e8d23f0
Lombiq <registrator@lombiq.com>
2014-08-09 22:54:57 +0200

    Implementing NuGet package generation from libs and Orchard.Framework

---
+++ BuildNuGetPackages.ps1
@@ -0,0 +1,20 @@
+$dotNetFolderName = "net45"
+$outputDirectory = "build/NuGet"
+
+.\nuget update -self
+
+If (Test-Path $outputDirectory){
+	rmdir $outputDirectory -Force -Recurse
+}
+mkdir $outputDirectory
+
+# Orchard.Libraries
+mkdir Orchard.Libraries/lib/$dotNetFolderName
+copy lib/* Orchard.Libraries/lib/$dotNetFolderName -Recurse
+move Orchard.Libraries/lib/$dotNetFolderName/Orchard.Libraries.nuspec Orchard.Libraries/Orchard.Libraries.nuspec
+rm Orchard.Libraries.*.nupkg
+.\nuget pack Orchard.Libraries/Orchard.Libraries.nuspec -OutputDirectory $outputDirectory
+rmdir Orchard.Libraries -Force -Recurse
+
+# Orchard.Framework
+.\nuget pack src/Orchard/Orchard.Framework.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory $outputDirectory
