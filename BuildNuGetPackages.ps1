0e061a8b38794eebf6201a7119f818bfbc9d296d
Lombiq <registrator@lombiq.com>
2014-08-09 23:52:34 +0200

    Moving NuGet.exe to .nuget folder

--- BuildNuGetPackages.ps1
+++ BuildNuGetPackages.ps1
@@ -1,7 +1,8 @@
 $dotNetFolderName = "net45"
 $outputDirectory = "build/NuGet"
+Set-Alias nuget src/.nuget/nuget
 
-.\nuget update -self
+nuget update -self
 
 If (Test-Path $outputDirectory){
 	rmdir $outputDirectory -Force -Recurse
@@ -13,8 +14,8 @@ mkdir Orchard.Libraries/lib/$dotNetFolderName
 copy lib/* Orchard.Libraries/lib/$dotNetFolderName -Recurse
 move Orchard.Libraries/lib/$dotNetFolderName/Orchard.Libraries.nuspec Orchard.Libraries/Orchard.Libraries.nuspec
 rm Orchard.Libraries.*.nupkg
-.\nuget pack Orchard.Libraries/Orchard.Libraries.nuspec -OutputDirectory $outputDirectory
+nuget pack Orchard.Libraries/Orchard.Libraries.nuspec -OutputDirectory $outputDirectory
 rmdir Orchard.Libraries -Force -Recurse
 
 # Orchard.Framework
-.\nuget pack src/Orchard/Orchard.Framework.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory $outputDirectory
+nuget pack src/Orchard/Orchard.Framework.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory $outputDirectory
