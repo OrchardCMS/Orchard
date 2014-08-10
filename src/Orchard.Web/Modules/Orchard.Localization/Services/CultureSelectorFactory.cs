fbd68f1ea1834b4fee56e7a52abb025d868ef7c3
Nicholas Mayne <Jetski5822@gmail.com>
2014-08-10 18:00:26 +0100

    Moving initialization of UICulture Shape to a Event

---
+++ src/Orchard.Web/Modules/Orchard.Localization/Services/CultureSelectorFactory.cs
@@ -0,0 +1,37 @@
+using System.Web.Routing;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.Implementation;
+using Orchard.Environment.Extensions;
+using Orchard.UI.Admin;
+
+namespace Orchard.Localization.Services {
+    [OrchardFeature("Orchard.Localization.CutlureSelector")]
+    public class AdminCultureSelectorFactory : ShapeDisplayEvents {
+        private readonly WorkContext _workContext;
+
+        public AdminCultureSelectorFactory(
+            IWorkContextAccessor workContextAccessor, 
+            IShapeFactory shapeFactory) {
+            _workContext = workContextAccessor.GetContext();
+            Shape = shapeFactory;
+        }
+        dynamic Shape { get; set; }
+
+        private bool IsActivable() {
+            // activate on front-end only
+            if (AdminFilter.IsApplied(new RequestContext(_workContext.HttpContext, new RouteData())))
+                return true;
+
+            return false;
+        }
+
+        public override void Displaying(ShapeDisplayingContext context) {
+            context.ShapeMetadata.OnDisplaying(displayedContext => {
+                if (IsActivable() && displayedContext.ShapeMetadata.Type == "Layout") {
+                    _workContext.Layout.Header.Add(Shape.UICultureSelector());
+                }
+            });
+
+        }
+    }
+}
