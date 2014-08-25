cf02023a2ae48c321c7961b6ad88f94438e62503
Nicholas Mayne <Jetski5822@gmail.com>
2014-08-25 12:06:40 +0100

    Adding Transliteration Lib

---
+++ src/Orchard.Web/Modules/Orchard.Localization/Providers/ContentTokens.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.NaturalLanguage.Tools;
+//using Orchard.ContentManagement;
+//using Orchard.Core.Settings.Tokens;
+
+//namespace Orchard.Localization.Providers {
+//    public class TransliterationTokens : ITokenProvider {
+//        private readonly IContentManager _contentManager;
+
+//        public TransliterationTokens(IContentManager contentManager) {
+//            _contentManager = contentManager;
+//        }
+
+//        public Localizer T { get; set; }
+
+
+//        public void Describe(dynamic context) {
+//            var x = new Transliterator()
+//        }
+
+//        public void Evaluate(dynamic context) {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
