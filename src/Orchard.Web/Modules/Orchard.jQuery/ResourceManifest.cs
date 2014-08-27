8087e46c127a0ac34f1482aad5d6459ef1f70be8
Nicholas Mayne <Jetski5822@gmail.com>
2014-08-27 23:46:11 +0100

    RTL fixes for Media Lib, and upgrading color box to 1.5.13

--- src/Orchard.Web/Modules/Orchard.jQuery/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.jQuery/ResourceManifest.cs
@@ -85,8 +85,8 @@ namespace Orchard.jQuery {
             manifest.DefineScript("jQueryFileUpload").SetUrl("jquery.fileupload.min.js", "jquery.fileupload.js").SetVersion("5.41.0").SetDependencies("jQueryIFrameTransport").SetDependencies("jQueryUI_Widget");
 
             // jquer Color Box
-            manifest.DefineScript("jQueryColorBox").SetUrl("jquery.colorbox.min.js", "jquery.colorbox.js").SetVersion("1.4.10").SetDependencies("jQuery");
-            manifest.DefineStyle("jQueryColorBox").SetUrl("colorbox.css").SetVersion("1.4.10");
+            manifest.DefineScript("jQueryColorBox").SetUrl("jquery.colorbox-min.js", "jquery.colorbox.js").SetVersion("1.5.13").SetDependencies("jQuery");
+            manifest.DefineStyle("jQueryColorBox").SetUrl("colorbox.css").SetVersion("1.5.13");
         }
     }
 }
