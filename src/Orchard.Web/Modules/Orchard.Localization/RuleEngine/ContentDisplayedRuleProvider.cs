79042774bd3042362b094a2f44b48032aa64a733
Nicholas Mayne <Jetski5822@gmail.com>
2014-08-30 00:45:55 +0100

    Adding Culture Rule Provider. this required a change to the IRuleProvider to be an IEventHandler rather than a direct IDependency

---
+++ src/Orchard.Web/Modules/Orchard.Localization/RuleEngine/ContentDisplayedRuleProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Orchard.Events;
+using Orchard.Localization.Services;
+using Orchard.Mvc;
+
+namespace Orchard.Localization.RuleEngine {
+    public interface IRuleProvider : IEventHandler {
+        void Process(dynamic ruleContext);
+    }
+
+    public class CultureRuleProvider : IRuleProvider {
+        private readonly ICultureManager _cultureManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CultureRuleProvider(ICultureManager cultureManager, IHttpContextAccessor httpContextAccessor) {
+            _cultureManager = cultureManager;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Process(dynamic ruleContext) {
+            if (!String.Equals(ruleContext.FunctionName, "culture-code", StringComparison.OrdinalIgnoreCase)) {
+                ProcessCultureCode(ruleContext);
+            }
+
+            if (!String.Equals(ruleContext.FunctionName, "culture-lcid", StringComparison.OrdinalIgnoreCase)) {
+                ProcessCultureId(ruleContext);
+            }
+        }
+
+        private void ProcessCultureCode(dynamic ruleContext) {
+            var httpContext = _httpContextAccessor.Current();
+            var currentUserCulture = CultureInfo.GetCultureInfo(_cultureManager.GetCurrentCulture(httpContext));
+
+            ruleContext.Result = ((object[])ruleContext.Arguments)
+                .Cast<string>()
+                .Select(CultureInfo.GetCultureInfo)
+                .Any(c => c.Name == currentUserCulture.Name);
+        }
+
+        private void ProcessCultureId(dynamic ruleContext) {
+            var httpContext = _httpContextAccessor.Current();
+            var currentUserCulture = CultureInfo.GetCultureInfo(_cultureManager.GetCurrentCulture(httpContext));
+
+            ruleContext.Result = ((object[])ruleContext.Arguments)
+                .Cast<int>()
+                .Select(CultureInfo.GetCultureInfo)
+                .Any(c => c.Name == currentUserCulture.Name);
+        }
+    }
+}
