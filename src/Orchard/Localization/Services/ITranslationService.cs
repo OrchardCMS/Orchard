690aa633b324fcd9fe84896710bcd1ecedda41b2
Nicholas Mayne <Jetski5822@gmail.com>
2014-08-30 15:43:25 +0100

    Adding TranslationService to Core

---
+++ src/Orchard/Localization/Services/ITranslationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Orchard.Localization.Services {
+    public interface ITranslationService : IDependency {
+        string Translate(CultureInfo from, CultureInfo to, string value);
+    }
+
+    public class DefaultTranslationService : ITranslationService {
+        public string Translate(CultureInfo from, CultureInfo to, string value) {
+            return value;
+        }
+    }
+}
