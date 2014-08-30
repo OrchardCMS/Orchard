8edd49244a6c05d42eb862ad2aff6222a7e24d31
Nicholas Mayne <Jetski5822@gmail.com>
2014-08-30 16:08:50 +0100

    Changing the way we check for files for TinyMCE - should increase performance of loading the editor

---
+++ src/Orchard.Web/Modules/TinyMce/Services/TinyMceShapeDisplayEvent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Orchard;
+using Orchard.Caching;
+using Orchard.DisplayManagement.Implementation;
+using Orchard.FileSystems.VirtualPath;
+
+namespace TinyMce.Services {
+    public class TinyMceShapeDisplayEvent : ShapeDisplayEvents {
+        private readonly ICacheManager _cacheManager;
+        private readonly ISignals _signals;
+        private readonly IVirtualPathProvider _virtualPathProvider;
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        private const string CacheKeyFormat = "tinymce-locales-{0}";
+        private const string DefaultLanguage = "en";
+
+        public TinyMceShapeDisplayEvent(
+            ICacheManager cacheManager,
+            IVirtualPathProvider virtualPathProvider,
+            IWorkContextAccessor workContextAccessor,
+            ISignals signals) {
+            _signals = signals;
+            _cacheManager = cacheManager;
+            _virtualPathProvider = virtualPathProvider;
+            _workContextAccessor = workContextAccessor;
+        }
+
+        public override void Displaying(ShapeDisplayingContext context) {
+            if (String.CompareOrdinal(context.ShapeMetadata.Type, "Body_Editor") != 0) {
+                return;
+            }
+
+            if (String.CompareOrdinal(context.Shape.EditorFlavor, "html") != 0) {
+                return;
+            }
+
+            context.Shape.Language = GetTinyMceLanguageIdentifier();
+        }
+
+        private string GetTinyMceLanguageIdentifier() {
+            var currentCulture = CultureInfo.GetCultureInfo(_workContextAccessor.GetContext().CurrentCulture);
+
+            if (currentCulture.Name.Equals(DefaultLanguage, StringComparison.OrdinalIgnoreCase))
+                return currentCulture.Name;
+
+
+            return _cacheManager.Get(string.Format(CacheKeyFormat, currentCulture.Name), ctx => {
+                ctx.Monitor(_signals.When("culturesChanged"));
+
+                var customLanguage = currentCulture.Name.Replace('-', '_');
+
+                var languageFiles = _virtualPathProvider
+                    .ListFiles("~/modules/tinymce/scripts/langs")
+                    .ToList();
+
+                if (languageFiles.Any(x => x == string.Format("{0}.js", customLanguage)))
+                    return customLanguage;
+
+                if (!DefaultLanguage.Equals(currentCulture.TwoLetterISOLanguageName, StringComparison.OrdinalIgnoreCase) &&
+                    languageFiles.Any(x => x == string.Format("{0}.js", currentCulture.TwoLetterISOLanguageName))) {
+                    return currentCulture.TwoLetterISOLanguageName;
+                }
+
+                return DefaultLanguage;
+            });
+        }
+    }
+}
