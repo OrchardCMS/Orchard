180f76d351adfe661ae48ee7da431cd82192a401
Stanley Goldman <Stanley.Goldman@gmail.com>
2014-10-19 14:56:24 -0400

    #21016: Adding Import/Export Functionality to TagCloudDriver
    WorkItem: 21016

--- src/Orchard.Web/Modules/Orchard.Tags/Drivers/TagCloudDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Tags/Drivers/TagCloudDriver.cs
@@ -1,5 +1,7 @@
-﻿using Orchard.ContentManagement;
+﻿using System;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
 using Orchard.Environment.Extensions;
 using Orchard.Tags.Models;
 
@@ -33,6 +35,16 @@ namespace Orchard.Tags.Drivers {
         protected override DriverResult Editor(TagCloudPart part, IUpdateModel updater, dynamic shapeHelper) {
             updater.TryUpdateModel(part, Prefix, null, null);
             return Editor(part, shapeHelper);
+        }
+
+        protected override void Exporting(TagCloudPart part, ExportContentContext context) {
+            context.Element(part.PartDefinition.Name).SetAttributeValue("Slug", part.Slug);
+            context.Element(part.PartDefinition.Name).SetAttributeValue("Buckets", part.Buckets);
+        }
+
+        protected override void Importing(TagCloudPart part, ImportContentContext context) {
+            part.Slug = context.Attribute(part.PartDefinition.Name, "Slug");
+            part.Buckets = Convert.ToInt32(context.Attribute(part.PartDefinition.Name, "Buckets"));
         }
     }
 }
