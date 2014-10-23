4f4ad4d48fc66067e7d3b0fb39eea75dd00c0fd7
Stanley Goldman <Stanley.Goldman@gmail.com>
2014-10-23 13:01:12 -0400

    Revert "TagCloud Functionality to filter by User"
    This reverts commit ff1e1bf25afcdd3867f675b7b3208ebf917c038d.

--- src/Orchard.Web/Modules/Orchard.Tags/Handlers/TagCloudHandler.cs
+++ src/Orchard.Web/Modules/Orchard.Tags/Handlers/TagCloudHandler.cs
@@ -4,27 +4,22 @@ using Orchard.ContentManagement.Handlers;
 using Orchard.Environment.Extensions;
 using Orchard.Tags.Models;
 using Orchard.Tags.Services;
-using Orchard.Tokens;
 
 namespace Orchard.Tags.Handlers {
     [OrchardFeature("Orchard.Tags.TagCloud")]
     public class TagCloudHandler : ContentHandler {
         private readonly ISignals _signals;
-        private readonly ITokenizer _tokenizer;
 
         public TagCloudHandler(
             ITagCloudService tagCloudService,
-            ISignals signals,
-            ITokenizer tokenizer) {
+            ISignals signals) {
 
             _signals = signals;
-            _tokenizer = tokenizer;
 
             OnInitializing<TagCloudPart>((context, part) => part
-                ._tagCountField.Loader(tags => {
-                    var username = _tokenizer.Replace(part.Username, new object());
-                    return tagCloudService.GetPopularTags(part.Buckets, part.Slug, username).ToList();
-                }));
+                ._tagCountField.Loader(tags =>
+                    tagCloudService.GetPopularTags(part.Buckets, part.Slug).ToList()
+                    ));
 
             OnPublished<TagsPart>((context, part) => InvalidateTagCloudCache());
             OnRemoved<TagsPart>((context, part) => InvalidateTagCloudCache());
