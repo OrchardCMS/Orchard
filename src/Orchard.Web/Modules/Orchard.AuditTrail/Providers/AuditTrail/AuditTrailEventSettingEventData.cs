da72fc980988953cadc980f630c55c998debdb03
Sipke Schoorstra <sipke@ideliverable.com>
2014-11-27 16:55:18 -0800

    #21091: Fixing audit trail bug.
    This fixes an issue causing a YSOD on the AuditTrail screen by recording all relevant data for the AuditTrailSettings event provider, such that the event providers themselves can be disabled without breaking the recorded events (which relied on the Event Descriptor to be available when rendering the event, which is unavailable if its providing feature is disabled).
    
    Work Item: 21091

---
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/Providers/AuditTrail/AuditTrailEventSettingEventData.cs
@@ -0,0 +1,9 @@
+﻿namespace Orchard.AuditTrail.Providers.AuditTrail {
+    public class AuditTrailEventSettingEventData
+    {
+        public string EventName { get; set; }
+        public string EventDisplayName { get; set; }
+        public string EventCategory { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
--- src/Orchard.Web/Modules/Orchard.AuditTrail/Providers/AuditTrail/AuditTrailSettingsEventShape.cs
+++ src/Orchard.Web/Modules/Orchard.AuditTrail/Providers/AuditTrail/AuditTrailSettingsEventShape.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Orchard.AuditTrail.Models;
 using Orchard.AuditTrail.Services;
 using Orchard.AuditTrail.Shapes;
 using Orchard.AuditTrail.ViewModels;
 using Orchard.DisplayManagement.Implementation;
 using Orchard.Environment;
+using Orchard.Logging;
 
 namespace Orchard.AuditTrail.Providers.AuditTrail {
     public class AuditTrailSettingsEventShape : AuditTrailEventShapeAlteration<AuditTrailSettingsEventProvider> {
@@ -13,7 +13,10 @@ namespace Orchard.AuditTrail.Providers.AuditTrail {
 
         public AuditTrailSettingsEventShape(Work<IAuditTrailManager> auditTrailManager) {
             _auditTrailManager = auditTrailManager;
+            Logger = NullLogger.Instance;
         }
+
+        public ILogger Logger { get; set; }
 
         protected override string EventName {
             get { return AuditTrailSettingsEventProvider.EventsChanged; }
@@ -21,8 +24,8 @@ namespace Orchard.AuditTrail.Providers.AuditTrail {
 
         protected override void OnAlterShape(ShapeDisplayingContext context) {
             var eventData = (IDictionary<string, object>)context.Shape.EventData;
-            var oldSettings = _auditTrailManager.Value.DeserializeProviderConfiguration((string)eventData["OldSettings"]);
-            var newSettings = _auditTrailManager.Value.DeserializeProviderConfiguration((string)eventData["NewSettings"]);
+            var oldSettings = AuditTrailManagerExtensions.DeserializeEventData((string)eventData["OldSettings"], Logger);
+            var newSettings = AuditTrailManagerExtensions.DeserializeEventData((string)eventData["NewSettings"], Logger);
             var diff = GetDiffQuery(oldSettings, newSettings).ToArray();
 
             context.Shape.OldSettings = oldSettings;
@@ -30,16 +33,17 @@ namespace Orchard.AuditTrail.Providers.AuditTrail {
             context.Shape.Diff = diff;
         }
 
-        private IEnumerable<AuditTrailEventDescriptorSettingViewModel> GetDiffQuery(IEnumerable<AuditTrailEventSetting> oldSettings, IEnumerable<AuditTrailEventSetting> newSettings) {
+        private IEnumerable<AuditTrailEventDescriptorSettingViewModel> GetDiffQuery(IEnumerable<AuditTrailEventSettingEventData> oldSettings, IEnumerable<AuditTrailEventSettingEventData> newSettings) {
             var oldDictionary = oldSettings.ToDictionary(x => x.EventName);
 
             return
                 from newSetting in newSettings
-                let oldSetting = oldDictionary.ContainsKey(newSetting.EventName) ? oldDictionary[newSetting.EventName] : default(AuditTrailEventSetting)
+                let oldSetting = oldDictionary.ContainsKey(newSetting.EventName) ? oldDictionary[newSetting.EventName] : default(AuditTrailEventSettingEventData)
                 where oldSetting == null || oldSetting.IsEnabled != newSetting.IsEnabled
+                let descriptor = _auditTrailManager.Value.DescribeEvent(newSetting.EventName)
                 select new AuditTrailEventDescriptorSettingViewModel {
                     Setting = newSetting,
-                    Descriptor = _auditTrailManager.Value.DescribeEvent(newSetting.EventName)
+                    Descriptor = descriptor
                 };
         }
     }
