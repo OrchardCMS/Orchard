6340e504bc2b35a1f64f5d40e7630d1c0b2fd951
Daniel Stolt <daniel@ideliverable.com>
2014-12-04 19:41:05 +0800

    Various improvements to OutputCache module.
    - Added synchronization of multiple concurrent content regeneration on cache miss.
    - Added support for grace time, configurable per route.
    - Refactored and cleaned up code and comments througout the module.
    - Made many members "protected virtual" on OutputCacheFilter for easier substitution.
    - Made various usability improvements in admin UI.
    - Added support for "vary by authentication state".

--- src/Orchard.Web/Modules/Orchard.OutputCache/Migrations.cs
+++ src/Orchard.Web/Modules/Orchard.OutputCache/Migrations.cs
@@ -8,11 +8,11 @@ namespace Orchard.OutputCache {
                     table => table
                         .Column<int>("Id", c => c.PrimaryKey().Identity())
                         .Column<int>("Duration")
-                        .Column<int>("MaxAge")
+                        .Column<int>("GraceTime")
                         .Column<string>("RouteKey", c => c.WithLength(255))
                     );
 
-            return 6;
+            return 7;
         }
 
         public int UpdateFrom1() {
@@ -31,7 +31,6 @@ namespace Orchard.OutputCache {
         }
 
         public int UpdateFrom3() {
-
             SchemaBuilder.AlterTable("CacheParameterRecord",
                     table => table
                         .AddColumn<int>("MaxAge")
@@ -46,6 +45,16 @@ namespace Orchard.OutputCache {
 
         public int UpdateFrom5() {
             return 6;
+        }
+
+        public int UpdateFrom6() {
+            SchemaBuilder.AlterTable("CacheParameterRecord",
+                    table => {
+                        table.DropColumn("MaxAge");
+                        table.AddColumn<int>("GraceTime");
+                    });
+
+            return 7;
         }
     }
 }
