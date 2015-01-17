af40271734b012bc76433e32de2b9576d65feeb3
Lombiq <registrator@lombiq.com>
2015-01-17 19:24:27 +0100

    Adding AppConfigurationAccessor to access appSettings or connectionStrings in an injected, loosely-coupled way.
    Copied from Helpful Libraries, adapted to Orchard conventions.

---
+++ src/Orchard/Environment/Configuration/IAppConfigurationAccessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchard.Environment.Configuration {
+    /// <summary>
+    /// Exposes application configuration (can be e.g. AppSettings from Web.config or CloudConfiguration on Azure).
+    /// </summary>
+    public interface IAppConfigurationAccessor : IDependency {
+        /// <summary>
+        /// Gets an application configuration value with the given name (can be e.g. AppSettings from Web.config or CloudConfiguration on Azure).
+        /// </summary>
+        /// <param name="name">The name of the application configuration entry.</param>
+        /// <returns>The string value of the application configuration entry.</returns>
+        string GetConfiguration(string name);
+    }
+}
