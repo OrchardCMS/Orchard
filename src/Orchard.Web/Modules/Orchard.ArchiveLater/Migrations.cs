604dfb67bf42e62f36628f481d535c34beaab3cc
Lombiq <registrator@lombiq.com>
2015-01-20 18:55:51 +0100

    Streamlining Migrations.cs for Orchard.PublishLater and adding description for ArchiveLaterPart

--- src/Orchard.Web/Modules/Orchard.ArchiveLater/Migrations.cs
+++ src/Orchard.Web/Modules/Orchard.ArchiveLater/Migrations.cs
@@ -5,8 +5,18 @@ using Orchard.Data.Migration;
 namespace Orchard.ArchiveLater {
     public class Migrations : DataMigrationImpl {
         public int Create() {
-            ContentDefinitionManager.AlterPartDefinition("ArchiveLaterPart", builder => builder.Attachable());
-            return 1;
+            ContentDefinitionManager.AlterPartDefinition("ArchiveLaterPart", builder => builder
+                .Attachable()
+                .WithDescription("Adds the ability to delay the unpublishing of a content item to a later date and time."));
+
+            return 2;
+        }
+
+        public int UpdateFrom1() {
+            ContentDefinitionManager.AlterPartDefinition("ArchiveLaterPart", builder => builder
+                .WithDescription("Adds the ability to delay the unpublising of a content item to a later date and time."));
+
+            return 2;
         }
     }
 }
