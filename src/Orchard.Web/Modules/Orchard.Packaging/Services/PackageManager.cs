10c0baabf43697d1d8f7dba48cae592b2548dd26
Lombiq <registrator@lombiq.com>
2015-01-27 18:33:09 +0100

    Implementing proper package uninstall process with features being disabled and uninstalled, then the package being removed.

--- src/Orchard.Web/Modules/Orchard.Packaging/Services/PackageManager.cs
+++ src/Orchard.Web/Modules/Orchard.Packaging/Services/PackageManager.cs
@@ -5,6 +5,8 @@ using NuGet;
 using Orchard.Environment.Extensions;
 using Orchard.Environment.Extensions.Folders;
 using Orchard.Environment.Extensions.Models;
+using Orchard.Environment.Features;
+using Orchard.Environment.State;
 using Orchard.Localization;
 using Orchard.Packaging.Models;
 
@@ -14,14 +16,23 @@ namespace Orchard.Packaging.Services {
         private readonly IExtensionManager _extensionManager;
         private readonly IPackageBuilder _packageBuilder;
         private readonly IPackageInstaller _packageInstaller;
+        private readonly IShellStateManager _shellStateManager;
+        private readonly IFeatureManager _featureManager;
+        private readonly IPackageUninstallHandler _packageUninstallHandler;
 
         public PackageManager(
             IExtensionManager extensionManager,
             IPackageBuilder packageBuilder,
-            IPackageInstaller packageInstaller) {
+            IPackageInstaller packageInstaller,
+            IShellStateManager shellStateManager,
+            IFeatureManager featureManager,
+            IPackageUninstallHandler packageUninstallHandler) {
             _extensionManager = extensionManager;
             _packageBuilder = packageBuilder;
             _packageInstaller = packageInstaller;
+            _shellStateManager = shellStateManager;
+            _featureManager = featureManager;
+            _packageUninstallHandler = packageUninstallHandler;
 
             T = NullLocalizer.Instance;
         }
@@ -69,7 +80,30 @@ namespace Orchard.Packaging.Services {
         }
 
         public void Uninstall(string packageId, string applicationPath) {
-            _packageInstaller.Uninstall(packageId, applicationPath);
+            var extensionToUninstall = _extensionManager.AvailableExtensions()
+                .FirstOrDefault(extension => PackageBuilder.BuildPackageId(extension.Id, extension.ExtensionType) == packageId);
+
+            if (extensionToUninstall == null) {
+                throw new OrchardException(T("There is no extension that has the package ID \"{0}\".", packageId));
+            }
+
+            var featureIdsToUninstall = extensionToUninstall.Features.Select(feature => feature.Id);
+            var shellState = _shellStateManager.GetShellState();
+            var featureStates = shellState.Features.Where(featureState => featureIdsToUninstall.Contains(featureState.Name));
+
+            // This means that no feature from this extension wasn enabled yet, can be uninstalled directly.
+            if (!featureStates.Any()) {
+                _packageUninstallHandler.QueuePackageUninstall(packageId);
+            }
+            else {
+                _featureManager.DisableFeatures(extensionToUninstall.Features.Select(feature => feature.Id), true);
+
+                // Installed state can't be deduced from the shell state changes like for enabled state, so have to
+                // set that explicitly.
+                foreach (var featureState in featureStates) {
+                    _shellStateManager.UpdateInstalledState(featureState, Environment.State.Models.ShellFeatureState.State.Falling);
+                }
+            }
         }
 
         #endregion
