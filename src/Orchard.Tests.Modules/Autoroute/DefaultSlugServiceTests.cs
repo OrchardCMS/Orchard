46fd3d22f05b26e4f224fa2e29761b202c2b0cf8
Codinlab <thierry@codinlab.fr>
2015-02-08 13:00:29 +0000

    #18787 : slugify function
    Work Item: 18787

--- src/Orchard.Tests.Modules/Autoroute/DefaultSlugServiceTests.cs
+++ src/Orchard.Tests.Modules/Autoroute/DefaultSlugServiceTests.cs
@@ -35,12 +35,42 @@ namespace Orchard.Tests.Modules.Autoroute {
 
         [Test]
         public void ShouldMakeSureFunkycharactersAndHyphansOnlyReturnSingleHyphan() {
-            Assert.That(_slugService.Slugify("a-%-.d"), Is.EqualTo("a-d"));
+            Assert.That(_slugService.Slugify("«a»-%-.d"), Is.EqualTo("a-d"));
+        }
+
+        [Test]
+        public void ShouldConvertToLowercase() {
+            Assert.That(_slugService.Slugify("ABCDE"), Is.EqualTo("abcde"));
+        }
+
+        [Test]
+        public void ShouldRemoveDiacritics() {
+            Assert.That(_slugService.Slugify("àçéïôù"), Is.EqualTo("aceiou"));
         }
 
         [Test]
         public void ShouldPreserveCyrilicCharacters() {
             Assert.That(_slugService.Slugify("джинсы_клеш"), Is.EqualTo("джинсы_клеш"));
+        }
+
+        [Test]
+        public void ShouldPreserveHebrewCharacters() {
+            Assert.That(_slugService.Slugify("צוות_אורצ_רד"), Is.EqualTo("צוות_אורצ_רד"));
+        }
+
+        [Test]
+        public void ShouldPreserveChineseCharacters() {
+            Assert.That(_slugService.Slugify("调度模块允许后台任务调度"), Is.EqualTo("调度模块允许后台任务调度"));
+        }
+
+        [Test]
+        public void ShouldPreserveArabicCharacters() {
+            Assert.That(_slugService.Slugify("فريق_الاورشارد"), Is.EqualTo("فريق_الاورشارد"));
+        }
+
+        [Test]
+        public void ShouldPreserveJapaneseCharacters() {
+            Assert.That(_slugService.Slugify("不正なコンテナ"), Is.EqualTo("不正なコンテナ"));
         }
     }
 }
