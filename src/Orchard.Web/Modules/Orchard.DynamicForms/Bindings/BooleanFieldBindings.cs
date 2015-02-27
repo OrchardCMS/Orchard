3fd51616ae2fd11bc6a9a9f527a7cd36ba0ace23
Sipke Schoorstra <sipke@ideliverable.com>
2015-02-27 22:39:22 +0100

    Removed T property that was hiding T from base.

--- src/Orchard.Web/Modules/Orchard.DynamicForms/Bindings/BooleanFieldBindings.cs
+++ src/Orchard.Web/Modules/Orchard.DynamicForms/Bindings/BooleanFieldBindings.cs
@@ -2,14 +2,10 @@ using System;
 using Orchard.DynamicForms.Services;
 using Orchard.DynamicForms.Services.Models;
 using Orchard.Fields.Fields;
-using Orchard.Localization;
 
 namespace Orchard.DynamicForms.Bindings {
     public class BooleanFieldBindings : Component, IBindingProvider {
-        public BooleanFieldBindings() {
-            T = NullLocalizer.Instance;
-        }
-
+        
         public void Describe(BindingDescribeContext context) {
             context.For<BooleanField>()
                 .Binding("Value", (contentItem, field, s) => field.Value = IsTrueish(s));
