2d4c1c817d963f40505e981650bd5e7336ffe74c
Daniel Stolt <daniel@ideliverable.com>
2015-03-18 12:34:29 +0100

    #21249: Removed build dependency on TypeScript.
    Instead added a gulpfile to enable compilation of TypeScript files when needed.
    
    Work Item: 21249

---
+++ src/Orchard.Web/Modules/Orchard.Azure.MediaServices/gulpfile.js
@@ -0,0 +1,40 @@
+﻿/*
+ * This gulpfile enables compilation of the TypeScript files in this project. The TypeScript 
+ * project properties have been removed, and this glupfile created instead, to remove the requirement 
+ * on Visual Studio and TypeScript for compiling/building Orchard while still allowing TypeScript 
+ * code in this project to be maintained.
+ * 
+ * To use this file you will need to:
+ * - Install Node.js on your machine
+ * - Install the following Node.js modules in this project folder:
+ *    npm install gulp
+ *    npm install gulp-typescript
+ * 
+ * NOTE: The above Node.js modules should not be committed to version control. They should only be used
+ * locally on your dev machine, while you have the need to edit and compile the TypeScript files in
+ * this project.
+ * 
+ * NOTE: Optionally you can use this gulpfile with the Task Runner Explorer extension in Visual Studio
+ * if you want a more integrated/automated workflow.
+ */
+
+var gulp = require("gulp"),
+    ts = require("gulp-typescript");
+
+gulp.task("default", ["scripts"]);
+
+var project = ts.createProject({
+    declarationFiles: false,
+    noExternalResolve: true
+});
+
+gulp.task("scripts", function() {
+    var result = gulp
+        .src([
+            "Scripts/*.ts",
+            "Scripts/typings/*.d.ts"
+        ])
+        .pipe(ts(project));
+    
+    return result.js.pipe(gulp.dest("Scripts"));
+});
