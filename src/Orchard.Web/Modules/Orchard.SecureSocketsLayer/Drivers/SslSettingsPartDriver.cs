2339bdc9663cdc27efb17ae5b020f9c29ebaf985
mvarblow <matt@varblow.net>
2015-05-13 16:45:55 -0400

    Update SslSettingsPartDriver.cs
    Fixed issue 5274

--- src/Orchard.Web/Modules/Orchard.SecureSocketsLayer/Drivers/SslSettingsPartDriver.cs
+++ src/Orchard.Web/Modules/Orchard.SecureSocketsLayer/Drivers/SslSettingsPartDriver.cs
@@ -36,25 +36,8 @@ namespace Orchard.SecureSocketsLayer.Drivers {
         }
 
         protected override void Importing(SslSettingsPart part, ImportContentContext context) {
-            var elementName = part.PartDefinition.Name;
-            part.Enabled = bool.Parse(context.Attribute(elementName, "Enabled") ?? "false");
-            part.SecureEverything = bool.Parse(context.Attribute(elementName, "SecureEverything") ?? "true");
-            part.CustomEnabled = bool.Parse(context.Attribute(elementName, "CustomEnabled") ?? "false");
-            part.Urls = context.Attribute(elementName, "Urls") ?? "";
-            part.InsecureHostName = context.Attribute(elementName, "InsecureHostName") ?? "";
-            part.SecureHostName = context.Attribute(elementName, "SecureHostName") ?? "";
-
+            base.Importing(part, context);
             _signals.Trigger(SslSettingsPart.CacheKey);
-        }
-
-        protected override void Exporting(SslSettingsPart part, ExportContentContext context) {
-            var el = context.Element(part.PartDefinition.Name);
-            el.SetAttributeValue("Enabled", part.Enabled);
-            el.SetAttributeValue("SecureEverything", part.SecureEverything);
-            el.SetAttributeValue("CustomEnabled", part.CustomEnabled);
-            el.SetAttributeValue("Urls", part.Urls);
-            el.SetAttributeValue("InsecureHostName", part.InsecureHostName);
-            el.SetAttributeValue("SecureHostName", part.SecureHostName);
         }
     }
 }
