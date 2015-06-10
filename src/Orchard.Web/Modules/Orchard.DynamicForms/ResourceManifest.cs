e8de82b611063dd59969ed5f4fcd62b79f6e30c9
Daniel Stolt <daniel@ideliverable.com>
2015-06-10 20:43:59 +0300

    Added Gulp build to Orchard.DynamicForms.

--- src/Orchard.Web/Modules/Orchard.DynamicForms/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.DynamicForms/ResourceManifest.cs
@@ -4,8 +4,8 @@ namespace Orchard.DynamicForms {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            manifest.DefineStyle("DynamicForms.FormElements").SetUrl("forms-admin.css");
-            manifest.DefineScript("DynamicForms.FormElements").SetUrl("Forms.min.js", "Forms.js").SetDependencies("Layouts.LayoutEditor");
+            manifest.DefineStyle("DynamicForms.FormElements").SetUrl("forms-admin.min.css", "forms-admin.css");
+            manifest.DefineScript("DynamicForms.FormElements").SetUrl("LayoutEditor.min.js", "LayoutEditor.js").SetDependencies("Layouts.LayoutEditor");
         }
     }
 }
