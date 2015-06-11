32cd707bd0186f9754a007e89a175b1d314c4475
Sipke Schoorstra <sipke@ideliverable.com>
2015-06-11 14:22:26 +0300

    Restructured IDeliverable Licensing folders a bit.

--- src/IDeliverable/IDeliverable.Licensing.Service/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ src/IDeliverable/IDeliverable.Licensing.Service/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -2,21 +2,15 @@
 // package to your project.
 ////#define Handle_PageResultOfT
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Net.Http.Headers;
-using System.Reflection;
-using System.Web;
 using System.Web.Http;
+
 #if Handle_PageResultOfT
 using System.Web.Http.OData;
 #endif
 
-namespace IDeliverable.Licensing.Service.Areas.HelpPage
+namespace IDeliverable.Licensing.Service.Areas.HelpPage.App_Start
 {
     /// <summary>
     /// Use this class to customize the Help Page.
--- src/IDeliverable/IDeliverable.Licensing.Service/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ src/IDeliverable/IDeliverable.Licensing.Service/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -1,5 +1,6 @@
 using System.Web.Http;
 using System.Web.Mvc;
+using IDeliverable.Licensing.Service.Areas.HelpPage.App_Start;
 
 namespace IDeliverable.Licensing.Service.Areas.HelpPage
 {
