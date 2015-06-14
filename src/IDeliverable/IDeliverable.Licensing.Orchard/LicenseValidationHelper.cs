b88fc651f8a316671f5bf6042402b70ebbdf4724
Daniel Stolt <daniel@ideliverable.com>
2015-06-14 15:56:15 +0300

    Removed unnecessary using statement.

--- src/IDeliverable/IDeliverable.Licensing.Orchard/LicenseValidationHelper.cs
+++ src/IDeliverable/IDeliverable.Licensing.Orchard/LicenseValidationHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Caching;
 using IDeliverable.Licensing.Validation;
 using IDeliverable.Licensing.VerificationTokens;
 using Orchard;
