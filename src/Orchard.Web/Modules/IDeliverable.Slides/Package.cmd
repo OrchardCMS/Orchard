25f1047922b2a94b7e8151c2498dea90abc18ad1
Daniel Stolt <daniel@ideliverable.com>
2015-06-15 22:58:16 +0300

    Added a bit of cleanup to the packaging script.

--- src/Orchard.Web/Modules/IDeliverable.Slides/Package.cmd
+++ src/Orchard.Web/Modules/IDeliverable.Slides/Package.cmd
@@ -1,6 +1,13 @@
 ..\..\..\..\..\IDeliverable\Packaging\ILMerge\ILMerge.exe /v4 /ndebug /out:bin\IDeliverable.Slides.dll bin\IDeliverable.Slides.dll bin\IDeliverable.Licensing.Orchard.dll bin\IDeliverable.Licensing.dll
 
-del bin\IDeliverable.Licensing.dll
-del bin\IDeliverable.Licensing.Orchard.dll
+del /q bin\IDeliverable.Licensing.dll
+del /q bin\IDeliverable.Licensing.Orchard.dll
 
 ..\..\..\..\..\IDeliverable\Packaging\Confuser\Confuser.CLI.exe Confuser.crproj
+
+for %%i in (bin\*.*) do if not "%%i"=="bin\IDeliverable.Slides.dll" del /q "%%i"
+
+rmdir /s /q Properties
+
+del /q Confuser.crproj
+del /q Package.cmd
