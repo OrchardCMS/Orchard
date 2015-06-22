723c177fe3da433725e84b72d940b8dbcda9e0d6
Daniel Stolt <daniel@ideliverable.com>
2015-06-22 21:12:43 +0300

    Fixed merge artifacts.

--- src/Orchard.Web/Modules/Orchard.Layouts/Drivers/HeadingElementDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Layouts/Drivers/HeadingElementDriver.cs
@@ -29,7 +29,7 @@ namespace Orchard.Layouts.Drivers {
             return Editor(context, editor);
         }
 
-        protected override void OnDisplaying(Heading element, ElementDisplayContext context) {
+        protected override void OnDisplaying(Heading element, ElementDisplayingContext context) {
             var text = element.Content;
             var flavor = "html";
             var processedText = ApplyHtmlFilters(text, flavor);
