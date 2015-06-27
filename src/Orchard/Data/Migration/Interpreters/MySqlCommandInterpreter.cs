a7adb1bdad7f8341edfc46a9b76af23e798924b3
Murray <minglei84@126.com>
2015-06-27 08:47:55 +0800

    Revert "Fixed [default value] For Mysql"
    This reverts commit 8f5cc1528846d904271d5f887294dcd4e9d410ef.

--- src/Orchard/Data/Migration/Interpreters/MySqlCommandInterpreter.cs
+++ src/Orchard/Data/Migration/Interpreters/MySqlCommandInterpreter.cs
@@ -32,55 +32,29 @@ namespace Orchard.Data.Migration.Interpreters {
 
         public string[] CreateStatements(AlterColumnCommand command) {
             var builder = new StringBuilder();
+            
             builder.AppendFormat("alter table {0} modify column {1} ",
-                _dialectLazy.Value.QuoteForTableName(PrefixTableName(command.TableName)),
-                _dialectLazy.Value.QuoteForColumnName(command.ColumnName));
-            var initLength = builder.Length;
+                            _dialectLazy.Value.QuoteForTableName(PrefixTableName(command.TableName)),
+                            _dialectLazy.Value.QuoteForColumnName(command.ColumnName));
 
             // type
-            if (command.DbType != DbType.Object)
-            {
-                builder.Append(DefaultDataMigrationInterpreter.GetTypeName(_dialectLazy.Value, command.DbType,
-                    command.Length, command.Precision, command.Scale));
+            if (command.DbType != DbType.Object) {
+                builder.Append(DefaultDataMigrationInterpreter.GetTypeName(_dialectLazy.Value, command.DbType, command.Length, command.Precision, command.Scale));
             }
-            else
-            {
-                if (command.Length > 0 || command.Precision > 0 || command.Scale > 0)
-                {
-                    throw new OrchardException(
-                        T(
-                            "Error while executing data migration: you need to specify the field's type in order to change its properties"));
+            else {
+                if (command.Length > 0 || command.Precision > 0 || command.Scale > 0) {
+                    throw new OrchardException(T("Error while executing data migration: you need to specify the field's type in order to change its properties"));
                 }
             }
 
             // [default value]
-            var builder2 = new StringBuilder();
-            builder2.AppendFormat("alter table {0} alter column {1} ",
-                _dialectLazy.Value.QuoteForTableName(PrefixTableName(command.TableName)),
-                _dialectLazy.Value.QuoteForColumnName(command.ColumnName));
-            var initLength2 = builder2.Length;
-
-            if (command.Default != null)
-            {
-                builder2.Append(" set default ")
-                    .Append(DefaultDataMigrationInterpreter.ConvertToSqlValue(command.Default))
-                    .Append(" ");
+            if (command.Default != null) {
+                builder.Append(" set default ").Append(_dataMigrationInterpreter.ConvertToSqlValue(command.Default)).Append(" ");
             }
 
-            // result
-            var result = new List<string>();
-
-            if (builder.Length > initLength)
-            {
-                result.Add(builder.ToString());
-            }
-
-            if (builder2.Length > initLength2)
-            {
-                result.Add(builder2.ToString());
-            }
-
-            return result.ToArray();
+            return new [] {
+                builder.ToString()
+            };
         }
 
         private string PrefixTableName(string tableName) {
