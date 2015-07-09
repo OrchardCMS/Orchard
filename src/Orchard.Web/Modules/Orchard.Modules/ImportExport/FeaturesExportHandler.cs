76132f26916077bb3312d40b4e39a6f0efbcb43f
Sipke Schoorstra <sipke@ideliverable.com>
2015-07-09 12:29:05 +0100

    #5056: Added Export Features recipe step.
    Fixes #5056

---
+++ src/Orchard.Web/Modules/Orchard.Modules/ImportExport/FeaturesExportHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Orchard.Environment.Features;
+using Orchard.Events;
+
+namespace Orchard.Modules.ImportExport {
+    public interface IExportEventHandler : IEventHandler {
+        void Exporting(dynamic context);
+        void Exported(dynamic context);
+    }
+
+    public class FeaturesExportHandler : IExportEventHandler {
+        private readonly IFeatureManager _featureManager;
+
+        public FeaturesExportHandler(IFeatureManager featureManager) {
+            _featureManager = featureManager;
+        }
+
+        public void Exporting(dynamic context) {
+            if (!((IEnumerable<string>)context.ExportOptions.CustomSteps).Contains("Features")) {
+                return;
+            }
+
+            var enabledFeatures = _featureManager.GetEnabledFeatures();
+
+            var root = new XElement("Feature", new XAttribute("enable", String.Join(", ", enabledFeatures.Select(x => x.Id))));
+            context.Document.Element("Orchard").Add(root);
+        }
+
+        public void Exported(dynamic context) {
+        }
+    }
+}
+
