f82132102d1548775708469df6f64e3a997ab1a1
Sipke Schoorstra <sipke@ideliverable.com>
2015-07-17 12:57:50 +0100

    Refactored CurrentTheme import/export step.

---
+++ src/Orchard.Web/Modules/Orchard.Themes/Recipes/Executors/CurrentThemeStep.cs
@@ -0,0 +1,25 @@
+using Orchard.Environment.Extensions;
+using Orchard.Recipes.Models;
+using Orchard.Recipes.Services;
+using Orchard.Themes.Services;
+
+namespace Orchard.Themes.Recipes.Executors {
+
+    [OrchardFeature("Orchard.Themes.ImportExportCurrentTheme")]
+    public class CurrentThemeStep : RecipeExecutionStep {
+        private readonly ISiteThemeService _siteThemeService;
+
+        public override string Name {
+            get { return "CurrentTheme"; }
+        }
+
+        public CurrentThemeStep(ISiteThemeService siteThemeService) {
+            _siteThemeService = siteThemeService;
+        }
+
+        public override void Execute(RecipeExecutionContext context) {
+            var themeId = context.RecipeStep.Step.Attribute("id").Value;
+            _siteThemeService.SetSiteTheme(themeId);
+        }
+    }
+}
