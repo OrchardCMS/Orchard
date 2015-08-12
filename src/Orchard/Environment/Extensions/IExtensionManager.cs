7d7340fbab2ae95b72d930547f80f691a6ca7c1d
Sipke Schoorstra <sipke@ideliverable.com>
2015-08-12 15:33:10 +0100

    Resolved merge conflicts.
    The dev branch had the ExtensionManagerExtensions class as a separate file while the 1.9.x branch had this class in the same file as IExtensionManager.cs.

--- src/Orchard/Environment/Extensions/IExtensionManager.cs
+++ src/Orchard/Environment/Extensions/IExtensionManager.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Orchard.Environment.Descriptor.Models;
 using Orchard.Environment.Extensions.Models;
 
 namespace Orchard.Environment.Extensions {
@@ -18,6 +20,10 @@ namespace Orchard.Environment.Extensions {
 
         public static IEnumerable<FeatureDescriptor> EnabledFeatures(this IExtensionManager extensionManager, IEnumerable<ShellFeature> features) {
             return extensionManager.AvailableFeatures().Where(fd => features.Any(sf => sf.Name == fd.Id));
+        }
+
+        public static IEnumerable<FeatureDescriptor> DisabledFeatures(this IExtensionManager extensionManager, ShellDescriptor descriptor) {
+            return extensionManager.AvailableFeatures().Where(fd => descriptor.Features.All(sf => sf.Name != fd.Id));
         }
     }
 }
