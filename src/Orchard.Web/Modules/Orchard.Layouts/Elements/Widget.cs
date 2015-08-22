de3d205ca98cb2ec81bdc0b9cb3193b54b5207dd
Sipke Schoorstra <sipke@ideliverable.com>
2015-08-22 16:59:17 +0100

    Implemented a Widget element harvester.

---
+++ src/Orchard.Web/Modules/Orchard.Layouts/Elements/Widget.cs
@@ -0,0 +1,23 @@
+﻿using Orchard.Layouts.Framework.Elements;
+using Orchard.Layouts.Helpers;
+
+namespace Orchard.Layouts.Elements {
+    public class Widget : Element {
+        public override string Category {
+            get { return "Widgets"; }
+        }
+
+        public override bool IsSystemElement {
+            get { return true; }
+        }
+
+        public override bool HasEditor {
+            get { return false; }
+        }
+
+        public int? WidgetId {
+            get { return this.Retrieve(x => x.WidgetId); }
+            set { this.Store(x => x.WidgetId, value); }
+        }
+    }
+}
