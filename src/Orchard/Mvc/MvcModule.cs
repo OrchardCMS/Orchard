9b91b1b2609439bb50dfc0321e72e5f39a95022f
Sebastien Ros <sebastienros@gmail.com>
2015-09-04 13:20:08 -0700

    Removing unnecessary resolution

--- src/Orchard/Mvc/MvcModule.cs
+++ src/Orchard/Mvc/MvcModule.cs
@@ -52,7 +52,6 @@ namespace Orchard.Mvc {
             var baseUrl = new Func<string>(() => siteService.GetSiteSettings().BaseUrl);
             var httpContextBase = new HttpContextPlaceholder(baseUrl);
 
-            context.Resolve<IWorkContextAccessor>().CreateWorkContextScope(httpContextBase);
             return httpContextBase;
         }
 
