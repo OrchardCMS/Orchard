59d95f11ee0b23f95f0019b52181335bba571588
Erik Scofield <escofield@naxosusa.com>
2015-09-23 00:11:43 -0500

    Enhancement to allow multi-select for the content picker

--- src/Orchard.Web/Modules/Orchard.ContentPicker/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.ContentPicker/ResourceManifest.cs
@@ -5,6 +5,7 @@ namespace Orchard.ContentPicker {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
             manifest.DefineScript("ContentPicker").SetUrl("ContentPicker.js", "ContentPicker.js").SetDependencies("jQuery");
+            manifest.DefineScript("RecentContentTab").SetUrl("RecentContentTab.js", "RecentContentTab.js").SetDependencies("jQuery");
         }
     }
 }
