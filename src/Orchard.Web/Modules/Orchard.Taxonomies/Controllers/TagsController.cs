c81fc702516981087766706d1f3f3282c27f6e26
Jeff <jbullock@rvc.ac.uk>
2015-09-28 15:52:43 +0100

    Added admin check and moved tag class to separate file with serialization attributes for lowercasing property names.

--- src/Orchard.Web/Modules/Orchard.Taxonomies/Controllers/TagsController.cs
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Controllers/TagsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Web.Http;
 using Orchard.ContentManagement;
@@ -9,25 +10,33 @@ using Orchard.Taxonomies.Helpers;
 using Orchard.Taxonomies.Models;
 using Orchard.Taxonomies.Services;
 using System.Linq;
+using Orchard.Security;
+using Orchard.Taxonomies.ViewModels;
 
 namespace Orchard.Taxonomies.Controllers {
     public class TagsController : ApiController {
         private readonly ITaxonomyService _taxonomyService;
         private readonly IContentManager _contentManager;
+	    private readonly IAuthorizer _authorizer;
         public Localizer T { get; set; }
         protected ILogger Logger { get; set; }
 
         public TagsController(
             ITaxonomyService taxonomyService,
-            IContentManager contentManager) {
+            IContentManager contentManager,
+			IAuthorizer authorizer) {
             _taxonomyService = taxonomyService;
             T = NullLocalizer.Instance;
             _contentManager = contentManager;
+	        _authorizer = authorizer;
             Logger = NullLogger.Instance;
         }
 
-        public IEnumerable<TagDto> Get(int taxonomyId, bool leavesOnly, string query) {
-            if (string.IsNullOrEmpty(query)) return new List<TagDto>();
+        public IEnumerable<Tag> Get(int taxonomyId, bool leavesOnly, string query) {
+	        if (!_authorizer.Authorize(StandardPermissions.AccessAdminPanel)) {
+		        throw new UnauthorizedAccessException("Can't access the admin");
+	        }
+            if (string.IsNullOrEmpty(query)) return new List<Tag>();
             var allTerms = leavesOnly
                                ? _taxonomyService.GetTerms(taxonomyId).ToList()
                                : new List<TermPart>();
@@ -36,25 +45,19 @@ namespace Orchard.Taxonomies.Controllers {
                                                .Join<TitlePartRecord>()
                                                .Where(r => r.Title.Contains(query))
                                                .List()
-                                               .Select(t => CreateTagDto(t, leavesOnly, allTerms))
-                                               .OrderBy(t => t.label)
+                                               .Select(t => BuildTag(t, leavesOnly, allTerms))
+                                               .OrderBy(t => t.Label)
                                                .ToList();
             return matchingTerms;
         }
 
-        private static TagDto CreateTagDto(TermPart term, bool leavesOnly, IEnumerable<TermPart> terms) {
-            return new TagDto {
-                value = term.Id,
-                label = term.Name,
-                disabled = !term.Selectable || (leavesOnly && terms.Any(t => t.Path.Contains(term.Path + term.Id))),
-                levels = term.GetLevels()
+        private static Tag BuildTag(TermPart term, bool leavesOnly, IEnumerable<TermPart> terms) {
+            return new Tag {
+                Value = term.Id,
+                Label = term.Name,
+                Disabled = !term.Selectable || (leavesOnly && terms.Any(t => t.Path.Contains(term.Path + term.Id))),
+                Levels = term.GetLevels()
             };
         }
-    }
-    public class TagDto {
-        public string label { get; set; }
-        public int value { get; set; }
-        public int levels { get; set; }
-        public bool disabled { get; set; }
     }
 }
