0abe2c765713d76c0de5dc0ad0b572120731089f
Baruch Nissenbaum <baruch@ibn-labs.com>
2015-10-17 19:18:37 +0300

    formatting fixed

--- src/Orchard/Environment/Configuration/ExtensionLocations.cs
+++ src/Orchard/Environment/Configuration/ExtensionLocations.cs
@@ -12,19 +12,16 @@ namespace Orchard.Environment.Configuration {
         public string[] ModuleAndThemeLocations;
         public string[] ExtensionsVirtualPathPrefixes;  // Modules+Themes (no core)
 
-        public ExtensionLocations()
-        {
+        public ExtensionLocations() {
             Init(new DefaultAppConfigurationAccessor());
         }
 
         // This optional constructor can be used to create an environment that takes AppConfigurations from IAppConfigurationAccessor instead of from the global ConfigurationManager.AppSettings
-        public ExtensionLocations(IAppConfigurationAccessor appConfigurationAccessor)
-        {
+        public ExtensionLocations(IAppConfigurationAccessor appConfigurationAccessor) {
             Init(appConfigurationAccessor);
         }
 
-        public virtual void Init(IAppConfigurationAccessor appConfigurationAccessor)
-        {
+        public virtual void Init(IAppConfigurationAccessor appConfigurationAccessor) {
             CoreLocations = new string[] {"~/Core"};
             ModuleLocations = GetConfigPaths(appConfigurationAccessor, "Modules", "~/Modules");
             ThemeLocations = GetConfigPaths(appConfigurationAccessor, "Themes", "~/Themes" );
@@ -52,8 +49,7 @@ namespace Orchard.Environment.Configuration {
         /// </summary>
         /// <param name="virtualPath"></param>
         /// <returns>the module - or null of not found</returns>
-        public static string ModuleMatch(string virtualPath, IEnumerable<string> pathPrefixes)
-        {
+        public static string ModuleMatch(string virtualPath, IEnumerable<string> pathPrefixes) {
             foreach(string prefix in pathPrefixes) {
                 if(virtualPath.StartsWith(prefix)) {
                     int index = virtualPath.IndexOf('/', prefix.Length, virtualPath.Length - prefix.Length);
@@ -73,8 +69,7 @@ namespace Orchard.Environment.Configuration {
         /// </summary>
         /// <param name="virtualPath"></param>
         /// <returns>the module - or null of not found</returns>
-        public string ExtensionsModuleMatch(string virtualPath)
-        {
+        public string ExtensionsModuleMatch(string virtualPath) {
             ModuleMatch(virtualPath, ExtensionsVirtualPathPrefixes);
             return null;
         }
@@ -82,13 +77,11 @@ namespace Orchard.Environment.Configuration {
         /// <summary>
         /// Return true if the virtual path starts with any of the prefixes
         /// </summary>
-        public static bool PrefixMatch(string virtualPath, IEnumerable<string> pathPrefixes)
-        {
+        public static bool PrefixMatch(string virtualPath, IEnumerable<string> pathPrefixes) {
             return pathPrefixes.Any(p => virtualPath.StartsWith(p));
         }
 
-        public bool ExtensionsPrefixMatch(string virtualPath)
-        {
+        public bool ExtensionsPrefixMatch(string virtualPath) {
             return PrefixMatch(virtualPath, ExtensionsVirtualPathPrefixes);
         }
 
@@ -96,25 +89,19 @@ namespace Orchard.Environment.Configuration {
         /// Get list of comma separated paths from web.config appSettings
         /// Also return the default path
         /// </summary>
-        static string[] GetConfigPaths(IAppConfigurationAccessor appConfigurationAccessor, string key, string defaultPath)
-        {
-            char[] delim = new char[] { ',' };
+        static string[] GetConfigPaths(IAppConfigurationAccessor appConfigurationAccessor, string key, string defaultPath) {
+            char[] delim = { ',' };
             string configuration = appConfigurationAccessor.GetConfiguration(key) ?? "";
             return configuration.Split(delim, StringSplitOptions.RemoveEmptyEntries).Concat(new string[] { defaultPath }).Select(s => s.Trim()).Distinct(StringComparer.OrdinalIgnoreCase).ToArray();
         }
 
-        private class DefaultAppConfigurationAccessor : IAppConfigurationAccessor
-        {
-            public DefaultAppConfigurationAccessor()
-            {
-            }
+        private class DefaultAppConfigurationAccessor : IAppConfigurationAccessor {
+            public DefaultAppConfigurationAccessor() {}
 
-            public string GetConfiguration(string name)
-            {
+            public string GetConfiguration(string name) {
                 return ConfigurationManager.AppSettings[name];
             }
         }
-
 
     }
 }
