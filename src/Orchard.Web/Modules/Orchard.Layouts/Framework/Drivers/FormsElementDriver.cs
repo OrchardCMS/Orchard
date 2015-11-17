30a5db7f4e3bc1f11837ed9d5c47601781a84123
Katsuyuki Ohmuro <harmony7@pex2.jp>
2015-11-17 17:48:09 +0900

    Fix for element editor when submitting forms created using Forms API.

--- src/Orchard.Web/Modules/Orchard.Layouts/Framework/Drivers/FormsElementDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Layouts/Framework/Drivers/FormsElementDriver.cs
@@ -14,6 +14,7 @@ namespace Orchard.Layouts.Framework.Drivers {
 
         protected dynamic BuildForm(ElementEditorContext context, string formName, string position = null) {
             // TODO: Fix Forms API so that it works with prefixes. Right now only binding implements prefix, but building a form ignores the specified prefix.
+            UpdateElementData(context, new[] { formName });
             var form = _formManager.Bind(_formManager.Build(formName), context.ValueProvider);
 
             if (!String.IsNullOrWhiteSpace(position)) {
@@ -32,8 +33,33 @@ namespace Orchard.Layouts.Framework.Drivers {
             return Editor(context, formShape);
         }
 
+        /// <summary>
+        /// If we are currently updating the element, then based on the involved forms,
+        /// update the Element's data dictionary with submitted data.
+        /// </summary>
+        protected void UpdateElementData(ElementEditorContext context, IEnumerable<string> formNames, string prefix = "") {
+
+            if (context.Updater != null) {
+
+                // Build forms and look for entires in the Element Data that have the same name.
+                foreach (var formName in formNames) {
+                    var form = _formManager.Build(formName);
+                    FormNodesProcessor.ProcessForm(form, (Action<object>) (shape => {
+                        var name = ((dynamic) shape).Name as string;
+                        if (name != null) {
+                            var value = context.ValueProvider.GetValue(prefix + name);
+                            if (value != null) {
+                                context.Element.Data[name] = value.AttemptedValue;
+                            }
+                        }
+                    }));
+                }
+            }
+        }
+
         protected dynamic BuildForms(ElementEditorContext context) {
             // TODO: Fix Forms API so that it works with prefixes. Right now only binding implements prefix, but building a form ignores the specified prefix.
+            UpdateElementData(context, FormNames);
             var forms = FormNames.Reverse().Select(x => _formManager.Bind(_formManager.Build(x), context.ValueProvider)).ToArray();
             var formShape = context.ShapeFactory.ElementEditor__Forms(Forms: forms);
             return formShape;
