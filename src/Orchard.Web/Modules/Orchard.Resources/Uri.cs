3da3ab8a49576a54e08e287210c5287fc2764659
Sipke Schoorstra <sipke@ideliverable.com>
2015-11-21 15:49:23 +0100

    Added Uri.

---
+++ src/Orchard.Web/Modules/Orchard.Resources/Uri.cs
@@ -0,0 +1,12 @@
+using Orchard.Environment.Extensions;
+using Orchard.UI.Resources;
+
+namespace Orchard.Resources {
+    [OrchardFeature("Orchard.Resources.Uri")]
+    public class Uri : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+            manifest.DefineScript("Uri").SetUrl("uri.min.js", "uri.js").SetVersion("1.16.1");
+        }
+    }
+}
