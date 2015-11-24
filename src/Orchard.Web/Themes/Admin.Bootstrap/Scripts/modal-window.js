ad2877f33772b3948e08ef54b66cbdfd0dae3152
Sipke Schoorstra <sipke@ideliverable.com>
2015-11-24 01:24:48 +0100

    Cleaning.

--- src/Orchard.Web/Themes/Admin.Bootstrap/Scripts/modal-window.js
+++
@@ -1,8 +0,0 @@
-﻿var w = document.defaultView || document.parentWindow;
-var frames = w.parent.document.getElementsByTagName('iframe');
-for (var i = frames.length; i-- > 0;) {
-    var frame = frames[i];
-    if (frame.className.indexOf('cboxIframe') > -1) {
-        frame.contentWindow.document.documentElement.className += ' media-library-modal-window';
-    }
-}
