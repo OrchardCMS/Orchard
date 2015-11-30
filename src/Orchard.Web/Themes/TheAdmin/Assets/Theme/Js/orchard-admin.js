1b209f1c8d8fdf8f877d6b6d2359f17fac87ccb0
Sipke Schoorstra <sipke@ideliverable.com>
2015-11-30 19:33:41 +0100

    Implemented pagination.

--- src/Orchard.Web/Themes/TheAdmin/Assets/Theme/Js/orchard-admin.js
+++ src/Orchard.Web/Themes/TheAdmin/Assets/Theme/Js/orchard-admin.js
@@ -1,8 +1,14 @@
 (function ($) {
+
+    var initializePagers = function () {
+        $('select.pager.selector').change(function() {
+            window.location = $(this).attr("disabled", true).val();
+        });
+    };
+
     $(document).ready(function () {
         App.init();
 
-        // Select2.
-        $(".select2").select2();
+        initializePagers();
     });
 })(jQuery);
