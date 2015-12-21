63ee01a4a0c8b613591f3a800cf6aa5022f28309
Thierry Fleury <thierry@codinlab.fr>
2015-12-21 09:35:30 +0100

    Move RoleRuleProvider to RoleCondition

---
+++ src/Orchard.Web/Modules/Orchard.Roles/Conditions/RoleCondition.cs
@@ -0,0 +1,32 @@
+﻿using Orchard.Conditions.Services;
+using Orchard.ContentManagement;
+using Orchard.Roles.Models;
+using Orchard.Security;
+using System;
+using System.Linq;
+
+namespace Orchard.Roles.Conditions {
+    public class RoleRuleProvider : IConditionProvider {
+        private readonly IAuthenticationService _authenticationService;
+
+        public RoleRuleProvider(IAuthenticationService authenticationService) {
+            _authenticationService = authenticationService;
+        }
+
+        public void Evaluate(ConditionEvaluationContext evaluationContext) {
+            if (!String.Equals(evaluationContext.FunctionName, "role", StringComparison.OrdinalIgnoreCase)) {
+                return;
+            }
+
+            var user = _authenticationService.GetAuthenticatedUser();
+            if (user == null) {
+                evaluationContext.Result = false;
+                return;
+            }
+
+            var roles = evaluationContext.Arguments.Cast<string>();
+            var userRoles = user.As<IUserRoles>();
+            evaluationContext.Result = userRoles != null ? userRoles.Roles.Intersect(roles).Count() > 0 : false;
+        }
+    }
+}
