70dd70655c5e1c6dfcc3cf041724cd4879755637
Chris Payne <chris.payne@bedegaming.com>
2015-12-28 20:32:20 +0000

    Tidyup to make use of new .net syntax

--- src/Orchard.Web/Modules/Orchard.Glimpse/TimelineCategories.cs
+++ src/Orchard.Web/Modules/Orchard.Glimpse/TimelineCategories.cs
@@ -2,32 +2,12 @@
 
 namespace Orchard.Glimpse {
     public class TimelineCategories {
-        public static TimelineCategoryItem Cache {
-            get { return new TimelineCategoryItem("Cache", "#154B87", "#154B87"); }
-        }
-
-        public static TimelineCategoryItem Layers {
-            get { return new TimelineCategoryItem("Layers", "#E6261D", "#E6261D"); }
-        }
-
-        public static TimelineCategoryItem Parts {
-            get { return new TimelineCategoryItem("Parts", "#169871", "#169871"); }
-        }
-
-        public static TimelineCategoryItem ContentManager {
-            get { return new TimelineCategoryItem("Content Manager", "#603182", "#603182"); }
-        }
-
-        public static TimelineCategoryItem Shapes {
-            get { return new TimelineCategoryItem("Shapes", "#bcbb27", "#bcbb27"); }
-        }
-
-        public static TimelineCategoryItem Authorizer {
-            get { return new TimelineCategoryItem("Authorizer", "#da7520", "#da7520"); }
-        }
-
-        public static TimelineCategoryItem Widgets {
-            get { return new TimelineCategoryItem("Widgets", "#84DCC6", "#84DCC6"); }
-        }
+        public static TimelineCategoryItem Cache => new TimelineCategoryItem("Cache", "#154B87", "#154B87");
+        public static TimelineCategoryItem Layers => new TimelineCategoryItem("Layers", "#E6261D", "#E6261D");
+        public static TimelineCategoryItem Parts => new TimelineCategoryItem("Parts", "#169871", "#169871");
+        public static TimelineCategoryItem ContentManager => new TimelineCategoryItem("Content Manager", "#603182", "#603182");
+        public static TimelineCategoryItem Shapes => new TimelineCategoryItem("Shapes", "#bcbb27", "#bcbb27");
+        public static TimelineCategoryItem Authorizer => new TimelineCategoryItem("Authorizer", "#da7520", "#da7520");
+        public static TimelineCategoryItem Widgets => new TimelineCategoryItem("Widgets", "#84DCC6", "#84DCC6");
     }
 }
