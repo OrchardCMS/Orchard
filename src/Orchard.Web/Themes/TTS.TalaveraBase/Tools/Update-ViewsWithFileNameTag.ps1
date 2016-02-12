45dbf61966e5fb0423c789e0431d2d3564cd9173
NelsonLamprecht <nelson.lamprecht@live.com>
2016-02-12 17:39:46 -0600

    Added and executed script that adds the file name to each view in the current theme.

---
+++ src/Orchard.Web/Themes/TTS.TalaveraBase/Tools/Update-ViewsWithFileNameTag.ps1
@@ -0,0 +1,43 @@
+﻿cls
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value;
+    if($Invocation.PSScriptRoot)
+    {
+        return $Invocation.PSScriptRoot;
+    }
+    Elseif($Invocation.MyCommand.Path)
+    {
+        return Split-Path $Invocation.MyCommand.Path
+    }
+    else
+    {
+        return $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+    }
+}
+
+function Insert-Content 
+{
+    param ([String]$path)
+    process 
+    {
+        $( ,$_; Get-Content $path -ea SilentlyContinue) | Out-File $path
+    }
+ }
+
+
+$themeDirectory = [System.IO.Directory]::GetParent((Get-ScriptDirectory)).FullName
+$files = Get-ChildItem -Path $themeDirectory -Recurse -Filter "*.cshtml"
+
+foreach ($file in $files)
+{      
+    Write-Output "Processing $($file.FullName)"
+    $fileContent = $null
+    $fileContent = Get-Content $file.FullName -TotalCount 1
+    $contentToAdd = "<!-- $($file.Name) -->"
+    if ($fileContent -ne $contentToAdd)
+    {
+        Write-Output "`t*`t Modifying File Name: $($file.FullName)"
+        $contentToAdd | Insert-Content $file.FullName       
+    }       
+}
