6b72f5130f6d5f4a1d784f2ff75a86eab530c72a
NelsonLamprecht <nelson.lamprecht@live.com>
2016-02-12 17:00:58 -0600

    Replaced with bootstrap 3.3.6 to fix issue with popover and tooltip

--- src/Orchard.Web/Themes/TTS.TalaveraBase/Code/ResourceManifest.cs
+++ src/Orchard.Web/Themes/TTS.TalaveraBase/Code/ResourceManifest.cs
@@ -5,15 +5,14 @@ namespace TTS.TalaveraBase {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
 
-            manifest.DefineScript("Bootstrap").SetUrl("bootstrap-3.3.5/js/bootstrap.min.js", "bootstrap-3.3.5/js/bootstrap.js").SetVersion("3.3.4").SetDependencies("jQuery");
-            manifest.DefineScript("HoverDropdown").SetUrl("hover-dropdown.js").SetDependencies("Bootstrap");
+            manifest.DefineScript("Bootstrap").SetUrl("bootstrap/js/bootstrap.min.js", "bootstrap/js/bootstrap.js").SetVersion("3.3.6").SetDependencies("jQuery");
+            manifest.DefineScript("HoverDropdown").SetUrl("hover-dropdown.js").SetDependencies("Bootstrap");            
             manifest.DefineScript("Stapel-Modernizr").SetUrl("stapel/modernizr.custom.63321.js");
             manifest.DefineScript("Stapel").SetUrl("stapel/jquery.stapel.js").SetDependencies("jQuery", "Stapel-Modernizr");
             manifest.DefineScript("prettyPhoto").SetUrl("prettyPhoto/jquery.prettyPhoto.js").SetDependencies("jQuery");
             manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
 
             manifest.DefineStyle("Stapel").SetUrl("stapel/stapel.css");
-            manifest.DefineStyle("prettyPhoto").SetUrl("prettyPhoto/prettyPhoto.css");
-        }
+            manifest.DefineStyle("prettyPhoto").SetUrl("prettyPhoto/prettyPhoto.css");        }
     }
 }
