c36a7c7f553f0c47ed5dc820a8909c2311cb7675
NelsonLamprecht <nelson.lamprecht@live.com>
2016-02-18 14:02:34 -0600

    Added style to resources.html to get custom theme css to load right

---
+++ src/Orchard.Web/Themes/AEFC/Code/ResourceManifest.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.UI.Resources;
+
+namespace AEFC
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineStyle("customer_theme").SetUrl("customer_theme.css");
+        }
+    }
+}
