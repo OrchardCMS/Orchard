bf4cecd527c99bbf6fb710d30af6c3fc2e9dea20
NelsonLamprecht <nelson.lamprecht@live.com>
2016-02-25 10:26:52 -0600

    wip

---
+++ src/Orchard.Web/Themes/Talavera.AEFC/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("American Energy Fitness Center")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Talavera Technology Solutions")]
+[assembly: AssemblyProduct("Talavera.AEFCv2.Properties")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a85d50f4-71ff-41cc-aaf0-d9bee3773095")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Orchard.Web/Themes/Talavera.Base/Code/ResourceManifest.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.UI.Resources;
+
+namespace Talavera.Base {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("Bootstrap").SetUrl("bootstrap/js/bootstrap.min.js", "bootstrap/js/bootstrap.js").SetVersion("3.3.6").SetDependencies("jQuery");
+            manifest.DefineScript("HoverDropdown").SetUrl("hover-dropdown.js").SetDependencies("Bootstrap");
+            manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
+
+            //manifest.DefineScript("Stapel-Modernizr").SetUrl("stapel/modernizr.custom.63321.js");
+            //manifest.DefineScript("Stapel").SetUrl("stapel/jquery.stapel.js").SetDependencies("jQuery", "Stapel-Modernizr");
+            //manifest.DefineScript("prettyPhoto").SetUrl("prettyPhoto/jquery.prettyPhoto.js").SetDependencies("jQuery");
+            //manifest.DefineStyle("Stapel").SetUrl("stapel/stapel.css");
+            //manifest.DefineStyle("prettyPhoto").SetUrl("prettyPhoto/prettyPhoto.css");        
+        }
+    }
+}
