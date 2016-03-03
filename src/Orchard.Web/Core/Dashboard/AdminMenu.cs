040f2ce867383ecae4569e6059f6c52f443a87c7
Sipke Schoorstra <sipke@ideliverable.com>
2016-03-04 00:43:40 +0100

    Fixed menu structure issue with local nav.

--- src/Orchard.Web/Core/Dashboard/AdminMenu.cs
+++ src/Orchard.Web/Core/Dashboard/AdminMenu.cs
@@ -3,17 +3,18 @@ using Orchard.Security;
 using Orchard.UI.Navigation;
 
 namespace Orchard.Core.Dashboard {
+    // We don't want to display the Dashboard in the "admin" menu, but we need to register this menu item nonetheless
+    // as it needs to be the root of the menu structure.
     public class AdminMenu : INavigationProvider {
         public Localizer T { get; set; }
-        public string MenuName { get { return "top_admin"; } }
+        public string MenuName { get { return "admin"; } }
 
         public void GetNavigation(NavigationBuilder builder) {
             builder.AddImageSet("dashboard")
                 .Add(T("Dashboard"), "-5",
-                    menu => menu.Add(T("Orchard"), "-5",
-                        item => item
-                            .Action("Index", "Admin", new { area = "Dashboard" })
-                            .Permission(StandardPermissions.AccessAdminPanel)));
+                    item => item
+                        .Action("Index", "Admin", new { area = "Dashboard" })
+                        .Permission(StandardPermissions.AccessAdminPanel), new[] { "hidden" });
         }
     }
 }
