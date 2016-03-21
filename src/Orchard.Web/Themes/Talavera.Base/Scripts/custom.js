9a914a65d221bd0ca3ed38b27237afd33f7e9eed
NelsonLamprecht <nelson.lamprecht@live.com>
2016-03-21 09:46:47 -0500

    Footer background

--- src/Orchard.Web/Themes/Talavera.Base/Scripts/custom.js
+++ src/Orchard.Web/Themes/Talavera.Base/Scripts/custom.js
@@ -6,58 +6,7 @@
     // make sure submenu item links work
     $('.dropdown-menu > .dropdown > a').click(function () {
         window.location.href = $(this).attr('href');
-    });
-
-    // add top margin to first container below menu when using fixed navbar
-    $('.navbar-wrapper .navbar-fixed-top').parent('div').next('div').css('margin-top', ($('.navbar-fixed-top').height() + 20) + 'px');
-    $(window).resize(function () {
-        $('.navbar-wrapper .navbar-fixed-top').parent('div').next('div').css('margin-top', ($('.navbar-fixed-top').height() + 20) + 'px');
-    });
-
-    var navigationVertical = {
-        attach: function () {
-            $('ul.menu-main-menu > li.dropdown').on('shown.bs.dropdown', function (event) {
-                var next = $(event.target.nextElementSibling);
-                if (next.length) {
-                    var height = $(this)[0].scrollHeight;
-                    next.css("padding-top", height);
-                }
-            })        
-        
-            $('ul.menu-main-menu > li.dropdown').on('hidden.bs.dropdown', function (event) {
-                var next = $(event.target.nextElementSibling);
-                if (next.length) {
-                    next.css("padding-top", "");
-                }
-            })
-        },
-        remove: function () {
-            $('ul.menu-main-menu > li.dropdown').unbind("shown.bs.dropdown");
-            $('ul.menu-main-menu > li.dropdown').unbind("hidden.bs.dropdown");           
-        }
-    };
-
-    //add padding top when menu is clicked 
-    if (matchMedia) {
-        var matchMedia767 = window.matchMedia("(max-width:767px)");
-        matchMedia767.addListener(function (data) {
-            if (data.matches)
-            {                
-                navigationVertical.attach();
-            }
-            else
-            {
-               navigationVertical.remove();
-            }
-        });
-    }
-
-    // add bottom margin to body equal to the footer height when using sticky footer
-    if ($('html').hasClass('sticky-footer')) {
-        var footerHeight = $('#layout-footer').height();
-
-        $('body').css('margin-bottom', footerHeight + 'px');
-    }
+    });    
 
     // ALERTS
     $('.alert').alert();
