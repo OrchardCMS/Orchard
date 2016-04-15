7d603684f0dd9ee2c153d293ec53eaad81d9cdf1
Sipke Schoorstra <sipke@ideliverable.com>
2016-04-15 16:27:01 +0200

    Added Blockquote element (#6773)
    * Added Blockquote element.
    
    Fixes #6772
    
    * Added Citation property to Blockquote.

---
+++ src/Orchard.Web/Modules/Orchard.Layouts/Drivers/BlockquoteElementDriver.cs
@@ -0,0 +1,36 @@
+﻿using Orchard.Layouts.Elements;
+using Orchard.Layouts.Framework.Display;
+using Orchard.Layouts.Framework.Drivers;
+using Orchard.Layouts.Helpers;
+using Orchard.Layouts.Services;
+using Orchard.Layouts.ViewModels;
+
+namespace Orchard.Layouts.Drivers {
+    public class BlockquoteElementDriver : ElementDriver<Blockquote> {
+        private readonly IElementFilterProcessor _processor;
+
+        public BlockquoteElementDriver(IElementFilterProcessor processor) {
+            _processor = processor;
+        }
+
+        protected override EditorResult OnBuildEditor(Blockquote element, ElementEditorContext context) {
+            var viewModel = new BlockquoteEditorViewModel {
+                Text = element.Content,
+                Citation = element.Citation
+            };
+            var editor = context.ShapeFactory.EditorTemplate(TemplateName: "Elements.Blockquote", Model: viewModel);
+
+            if (context.Updater != null) {
+                context.Updater.TryUpdateModel(viewModel, context.Prefix, null, null);
+                element.Content = viewModel.Text;
+                element.Citation = viewModel.Citation;
+            }
+            
+            return Editor(context, editor);
+        }
+
+        protected override void OnDisplaying(Blockquote element, ElementDisplayingContext context) {
+            context.ElementShape.ProcessedContent = _processor.ProcessContent(element.Content, "html", context.GetTokenData());
+        }
+    }
+}
