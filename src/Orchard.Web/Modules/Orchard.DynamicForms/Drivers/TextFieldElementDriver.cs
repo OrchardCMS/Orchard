a9be63d089246ecbd49514775e0046993bedeca8
Sergio Navarro <jersio@hotmail.com>
2016-04-23 09:45:05 +0200

    Replaced RegularExpression per ValidationExpression

--- src/Orchard.Web/Modules/Orchard.DynamicForms/Drivers/TextFieldElementDriver.cs
+++ src/Orchard.Web/Modules/Orchard.DynamicForms/Drivers/TextFieldElementDriver.cs
@@ -59,10 +59,10 @@ namespace Orchard.DynamicForms.Drivers {
                         Title: "Maximum Length",
                         Classes: new[] { "text", "medium" },
                         Description: T("The maximum length allowed.")),
-                    _RegularExpression: shape.Textbox(
-                        Id: "RegularExpression",
-                        Name: "RegularExpression",
-                        Title: "Regular Expression",
+                    _ValidationExpression: shape.Textbox(
+                        Id: "ValidationExpression",
+                        Name: "ValidationExpression",
+                        Title: "Validation Expression",
                         Classes: new[] { "text", "large" },
                         Description: T("The regular expression the text must match with.")),
                     _CustomValidationMessage: shape.Textbox(
