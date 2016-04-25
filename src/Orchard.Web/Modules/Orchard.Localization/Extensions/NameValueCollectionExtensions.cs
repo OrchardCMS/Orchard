ad33725fd76e62cf260ba60f9b4bf52231fa7c45
psp589 <pablosanchez589@gmail.com>
2016-04-25 16:47:06 +0200

    fixed query string culture change

---
+++ src/Orchard.Web/Modules/Orchard.Localization/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace Orchard.Localization
+{
+    public static class NameValueCollectionExtensions {
+        public static string ToQueryString(this NameValueCollection nameValues) {
+            return String.Join("&", (from string name in nameValues select String.Concat(name, "=", HttpUtility.UrlEncode(nameValues[name]))).ToArray());
+        }
+    }
+}
