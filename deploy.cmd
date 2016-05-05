06778eb584c59737992a8cccc7a4a7d3bd3bbfc6
Sebastien Ros <sebastienros@gmail.com>
2016-05-05 12:46:48 -0700

    Setting correct nuget path for Kudu
    Fixes #6810

--- deploy.cmd
+++ deploy.cmd
@@ -69,7 +69,7 @@ echo Handling .NET Web Application deployment.
 
 :: 1. Restore NuGet packages
 IF /I "" NEQ "" (
-  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\"
+  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\src\Orchard.sln"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
