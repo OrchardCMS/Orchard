4cdf53539ee4925db69011619a208c3b9375742b
Jasmin Savard <jasmin.savard@gmail.com>
2016-05-07 23:53:00 -0400

    Work on Reports admin views.
    Fixing Redis bug that made the website/admin crash when trying to enable the different associated modules.

---
+++ src/Orchard.Web/Modules/Orchard.Redis/Configuration/RedisNotificationProvider.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.Environment.Configuration;
+using Orchard.Localization;
+using Orchard.MessageBus.Services;
+using Orchard.UI.Admin.Notification;
+using Orchard.UI.Notify;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Orchard.Redis.Configuration {
+    public class RedisNotificationProvider : INotificationProvider {
+        private readonly ShellSettings _shellSettings;
+
+        public RedisNotificationProvider(ShellSettings shellSettings) {
+            _shellSettings = shellSettings;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public IEnumerable<NotifyEntry> GetNotifications() {
+            var _defaultSettingsKey = "Orchard.Redis.MessageBus";
+            var _tenantSettingsKey = _shellSettings.Name + ":" + _defaultSettingsKey;
+
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings[_tenantSettingsKey] ?? ConfigurationManager.ConnectionStrings[_defaultSettingsKey];
+
+            if (connectionStringSettings == null) {
+                yield return new NotifyEntry { Message = T("You need to configure Redis connection string."), Type = NotifyType.Warning };
+            }
+        }
+    }
+}
