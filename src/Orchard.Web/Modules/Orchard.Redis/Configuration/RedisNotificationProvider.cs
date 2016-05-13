dbc0c8a911b3da5ce546eaa575bb2b91be5cfa52
Jasmin Savard <jasmin.savard@gmail.com>
2016-05-13 00:55:03 -0400

    Validating that we have a connection string for Orchard.Redis.Cache and Orchard.Redis.OutputCache.
    This won't support any other third party service.

--- src/Orchard.Web/Modules/Orchard.Redis/Configuration/RedisNotificationProvider.cs
+++ src/Orchard.Web/Modules/Orchard.Redis/Configuration/RedisNotificationProvider.cs
@@ -1,24 +1,52 @@
-﻿using Orchard.Environment.Configuration;
+﻿using Orchard.Environment.Features;
 using Orchard.Localization;
 using Orchard.UI.Admin.Notification;
 using Orchard.UI.Notify;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Orchard.Redis.Configuration {
     public class RedisNotificationProvider : INotificationProvider {
         private readonly RedisConnectionProvider _redisConnectionProvider;
-        public const string ConnectionStringKey = "Orchard.Redis.Cache";
+        private readonly FeatureManager _featureManager;
 
-        public RedisNotificationProvider(ShellSettings shellSettings, RedisConnectionProvider redisConnectionProvider) {
+        public RedisNotificationProvider(FeatureManager featureManager, RedisConnectionProvider redisConnectionProvider) {
             _redisConnectionProvider = redisConnectionProvider;
+            _featureManager = featureManager;
             T = NullLocalizer.Instance;
         }
 
         public Localizer T { get; set; }
 
         public IEnumerable<NotifyEntry> GetNotifications() {
-            if (_redisConnectionProvider.GetConnectionString(ConnectionStringKey) == null) {
-                yield return new NotifyEntry { Message = T("You need to configure Redis connection string."), Type = NotifyType.Warning };
+            string ConnectionStringKey = "";
+
+            var enabledModules = _featureManager.GetEnabledFeatures();
+            var verifyModule = enabledModules.Where(f => f.Id == "Orchard.Redis.Caching").FirstOrDefault();
+
+            if (verifyModule != null) {
+                ConnectionStringKey = "Orchard.Redis.Cache";
+                if (_redisConnectionProvider.GetConnectionString(ConnectionStringKey) == null) {
+                    yield return new NotifyEntry { Message = T("You need to configure Redis Cache connection string."), Type = NotifyType.Warning };
+                }
+            }
+
+            verifyModule = enabledModules.Where(f => f.Id == "Orchard.Redis.OutputCache").FirstOrDefault();
+
+            if (verifyModule != null) {
+                ConnectionStringKey = "Orchard.Redis.OutputCache";
+                if (_redisConnectionProvider.GetConnectionString(ConnectionStringKey) == null) {
+                    yield return new NotifyEntry { Message = T("You need to configure Redis OutputCache connection string."), Type = NotifyType.Warning };
+                }
+            }
+
+            verifyModule = enabledModules.Where(f => f.Id == "Orchard.Redis.MessageBus").FirstOrDefault();
+
+            if (verifyModule != null) {
+                ConnectionStringKey = "Orchard.Redis.MessageBus";
+                if (_redisConnectionProvider.GetConnectionString(ConnectionStringKey) == null) {
+                    yield return new NotifyEntry { Message = T("You need to configure Redis MessageBus connection string."), Type = NotifyType.Warning };
+                }
             }
         }
     }
