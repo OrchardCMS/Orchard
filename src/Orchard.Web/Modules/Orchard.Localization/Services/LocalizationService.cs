42090ebdf8d258ffd9ab7de612e7ee300e0a150f
Alex Petryakov <alexpetryakov@gmail.com>
2016-05-30 15:11:25 +0300

    Fix for getting other content item localzation.
    Signed-off-by: Alex Petryakov <alexpetryakov@gmail.com>

--- src/Orchard.Web/Modules/Orchard.Localization/Services/LocalizationService.cs
+++ src/Orchard.Web/Modules/Orchard.Localization/Services/LocalizationService.cs
@@ -29,11 +29,17 @@ namespace Orchard.Localization.Services {
             if (localized == null)
                 return null;
 
+            if (localized.Record.CultureId == cultureRecord.Id) {
+                return localized;
+            }
+
+            int masterContentItemId = localized.HasTranslationGroup ? localized.Record.MasterContentItemId : localized.Id;
+
             // Warning: Returns only the first of same culture localizations.
             return _contentManager
                 .Query<LocalizationPart>(versionOptions, content.ContentItem.ContentType)
                 .Where<LocalizationPartRecord>(l =>
-                (l.Id == content.ContentItem.Id || l.MasterContentItemId == content.ContentItem.Id)
+                (l.Id == masterContentItemId || l.MasterContentItemId == masterContentItemId)
                 && l.CultureId == cultureRecord.Id)
                 .Slice(1)
                 .FirstOrDefault();
