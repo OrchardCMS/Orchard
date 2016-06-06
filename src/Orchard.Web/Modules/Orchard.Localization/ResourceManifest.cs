7c8ab76ec80d3a9e4252af688d07d8859215bbba
Antoine Griffard <agriffard@hotmail.com>
2016-06-06 13:17:27 +0200

    Fix missing zones and admin culture selector.

--- src/Orchard.Web/Modules/Orchard.Localization/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.Localization/ResourceManifest.cs
@@ -4,6 +4,7 @@ namespace Orchard.Localization {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
+            manifest.DefineStyle("Flags").SetUrl("flags.css");
             manifest.DefineStyle("Localization").SetUrl("orchard-localization-base.css");
             manifest.DefineStyle("LocalizationAdmin").SetUrl("orchard-localization-admin.css");
         }
