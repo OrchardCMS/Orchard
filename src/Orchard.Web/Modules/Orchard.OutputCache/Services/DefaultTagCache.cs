10ec0c953bbdb0f74d2b59c43a37884783a46de6
Hannan Azam <hannan.azam@gmail.com>
2016-10-11 01:37:49 +0500

    Default Tag Cache from HttpContext

--- src/Orchard.Web/Modules/Orchard.OutputCache/Services/DefaultTagCache.cs
+++ src/Orchard.Web/Modules/Orchard.OutputCache/Services/DefaultTagCache.cs
@@ -2,23 +2,25 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using Orchard.OutputCache.Models;
 using Orchard.Utility.Extensions;
+using Orchard.Environment.Configuration;
+using System.Web.Caching;
 
 namespace Orchard.OutputCache.Services {
     /// <summary>
     /// Tenant wide case insensitive reverse index for <see cref="CacheItem"/> tags.
     /// </summary>
     public class DefaultTagCache : ITagCache {
+        private readonly string _tenantName;
+        private readonly WorkContext _workContext;
 
-        private readonly ConcurrentDictionary<string, HashSet<string>> _dictionary;
-
-        public DefaultTagCache() {
-            _dictionary = new ConcurrentDictionary<string, HashSet<string>>(StringComparer.OrdinalIgnoreCase);
+        public DefaultTagCache(IWorkContextAccessor workContextAccessor, ShellSettings shellSettings) {
+            _workContext = workContextAccessor.GetContext();
+            _tenantName = shellSettings.Name;
         }
 
         public void Tag(string tag, params string[] keys) {
-            var set = _dictionary.GetOrAdd(tag, x => new HashSet<string>());
+            var set = Dictionary.GetOrAdd(tag, x => new HashSet<string>());
 
             lock (set) {
                 foreach (var key in keys) {
@@ -26,10 +28,10 @@ namespace Orchard.OutputCache.Services {
                 }
             }
         }
-        
+
         public IEnumerable<string> GetTaggedItems(string tag) {
             HashSet<string> set;
-            if (_dictionary.TryGetValue(tag, out set)) {
+            if (Dictionary.TryGetValue(tag, out set)) {
                 lock (set) {
                     return set.ToReadOnlyCollection();
                 }
@@ -40,7 +42,21 @@ namespace Orchard.OutputCache.Services {
 
         public void RemoveTag(string tag) {
             HashSet<string> set;
-            _dictionary.TryRemove(tag, out set);
+            Dictionary.TryRemove(tag, out set);
+        }
+
+        private ConcurrentDictionary<string, HashSet<string>> Dictionary {
+            get {
+                var key = _tenantName + ":TagCache";
+                var dictionary = _workContext.HttpContext.Cache.Get(key) as ConcurrentDictionary<string, HashSet<string>>;
+
+                if (dictionary == null) {
+                    dictionary = new ConcurrentDictionary<string, HashSet<string>>(StringComparer.OrdinalIgnoreCase);
+                    _workContext.HttpContext.Cache.Add(key, dictionary, null, Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.Normal, null);
+                }
+
+                return dictionary;
+            }
         }
     }
 }
--- src/Orchard.Web/Modules/Orchard.OutputCache/Services/ITagCache.cs
+++ src/Orchard.Web/Modules/Orchard.OutputCache/Services/ITagCache.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 
 namespace Orchard.OutputCache.Services {
-    public interface ITagCache : ISingletonDependency {
+    public interface ITagCache : IDependency {
         void Tag(string tag, params string[] keys);
         IEnumerable<string> GetTaggedItems(string tag);
         void RemoveTag(string tag);
