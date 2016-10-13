13979fd1da358f6f2b848be8905899fbb74d5330
Vinícius Fernandes <viniciusdfol@gmail.com>
2016-10-13 13:13:29 -0300

    Fixed an issue with incorrect log message at Indexing module
    * Fixed incorrect log message in Indexing module
    * Fixed an issue with incorrect log message at Indexing module
    * Fixed identation
    
    Fixes #7225

--- src/Orchard.Web/Modules/Orchard.Indexing/Services/IndexingTaskExecutor.cs
+++ src/Orchard.Web/Modules/Orchard.Indexing/Services/IndexingTaskExecutor.cs
@@ -295,7 +295,7 @@ namespace Orchard.Indexing.Services {
             try {
                 if (deleteFromIndex.Count > 0) {
                     _indexProvider.Delete(indexName, deleteFromIndex);
-                    Logger.Information("Added content items to index: {0}", addToIndex.Count);
+                    Logger.Information("Deleted content items from index: {0}",  deleteFromIndex.Count);
                 }
             }
             catch (Exception ex) {
