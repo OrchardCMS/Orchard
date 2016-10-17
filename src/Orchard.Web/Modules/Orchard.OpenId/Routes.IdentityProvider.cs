1015bf649e7ffc82b4c4fa6efe57b459b6f15aa4
talajlouni <talajlouni@avertra.com>
2016-10-17 11:05:29 -0400

    Add the OpenId Module and its features

---
+++ src/Orchard.Web/Modules/Orchard.OpenId/Routes.IdentityProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Orchard.Environment.Extensions;
+using Orchard.Mvc.Routes;
+
+namespace Orchard.OpenId
+{
+    [OrchardFeature("Orchard.IdentityProvider")]
+    public class IdentityProviderRoutes : IRouteProvider
+    {
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var route in GetRoutes())
+            {
+                routes.Add(route);
+            }
+        }
+    }
+}
