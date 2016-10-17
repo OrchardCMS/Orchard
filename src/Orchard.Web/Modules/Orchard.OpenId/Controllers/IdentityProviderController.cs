4d08b8ca154155f899277687b305b5c901c36644
talajlouni <talajlouni@avertra.com>
2016-10-17 11:05:29 -0400

    Add the OpenId Module and its features

---
+++ src/Orchard.Web/Modules/Orchard.OpenId/Controllers/IdentityProviderController.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.Environment.Extensions;
+
+namespace Orchard.OpenId.Controllers
+{
+    [OrchardFeature("Orchard.IdentityProvider")]
+    public class IdentityProviderController
+    {
+    }
+}
