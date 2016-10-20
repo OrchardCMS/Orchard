e03b52a34f55839db242562b1136260554fbb1cc
talajlouni <talajlouni@avertra.com>
2016-10-19 22:09:13 -0400

    Implement the logic that supports OpenId Authentication against some default providers

---
+++ src/Orchard.Web/Modules/Orchard.OpenId/Handlers/AzureActiveDirectorySettingsPartHandler.cs
@@ -0,0 +1,24 @@
+﻿using Orchard.OpenId.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+
+namespace Orchard.OpenId.Handlers {
+    public class AzureActiveDirectorySettingsPartHandler : ContentHandler {
+        public Localizer T { get; set; }
+
+        public AzureActiveDirectorySettingsPartHandler() {
+            T= NullLocalizer.Instance;
+            Filters.Add(new ActivatingFilter<AzureActiveDirectorySettingsPart>("Site"));
+        }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            if (context.ContentItem.ContentType != "Site") {
+                return;
+            }
+
+            base.GetItemMetadata(context);
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("Azure AD Authentication")));
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.OpenId/OwinMiddlewares/Twitter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Twitter;
+using Orchard.Environment.Extensions;
+using Orchard.Owin;
+using Owin;
+
+namespace Orchard.OpenId.OwinMiddlewares {
+    [OrchardFeature("Orchard.OpenId.Twitter")]
+    public class Twitter : IOwinMiddlewareProvider {
+
+        public IEnumerable<OwinMiddlewareRegistration> GetOwinMiddlewares() {
+            // TODO: change these into site settings
+            var consumerKey = "";
+            var consumerSecret = "";
+
+            var twitterOptions = new TwitterAuthenticationOptions {
+                ConsumerKey = consumerKey,
+                ConsumerSecret = consumerSecret,
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+                {
+                    Constants.VeriSignClass3SecureServerCA_G2,
+                    Constants.VeriSignClass3SecureServerCA_G3,
+                    Constants.VeriSignClass3PublicPrimaryCA_G5,
+                    Constants.SymantecClass3SecureServerCA_G4,
+                    Constants.DigiCertSHA2HighAssuranceServerCA,
+                    Constants.DigiCertHighAssuranceEVRootCA 
+                })
+            };
+
+            return new List<OwinMiddlewareRegistration> {
+                new OwinMiddlewareRegistration {
+                    Priority = Constants.OpenIdOwinMiddlewarePriority,
+                    Configure = app => {
+                        app.UseTwitterAuthentication(twitterOptions);
+                    }
+                }
+            };
+        }
+    }
+}
