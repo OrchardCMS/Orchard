cab67b3c55ceee7d3789c60ad6803c9999f1ac46
Tha'er Al-Ajlouni <talajlouni@avertra.com>
2016-10-21 18:14:55 -0400

    Add the login page

---
+++ src/Orchard.Web/Modules/Orchard.OpenId/Providers/AzureActiveDirectory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using Orchard.OpenId.Models;
+using Orchard.OpenId.Services;
+using Orchard.Settings;
+
+namespace Orchard.OpenId.Providers {
+    [OrchardFeature("Orchard.OpenId.AzureActiveDirectory")]
+    public class AzureActiveDirectory : IOpenIdProvider {
+        private readonly IWorkContextAccessor _workContextAccessor;
+        private readonly ISiteService _siteService;
+
+        public AzureActiveDirectory(
+            IWorkContextAccessor workContextAccessor,
+            ISiteService siteService) {
+
+            _workContextAccessor = workContextAccessor;
+            _siteService = siteService;
+        }
+
+        public string AuthenticationType {
+            get { return "OpenIdConnect"; }
+        }
+
+        public string Name {
+            get { return "AzureAD"; }
+        }
+
+        public string DisplayName {
+            get { return "Azure Active Directory"; }
+        }
+
+        public bool IsValid {
+            get { return IsProviderValid(); }
+        }
+
+        private bool IsProviderValid() {
+            try {
+                AzureActiveDirectorySettingsPart settings;
+                ISite site;
+
+                if (_siteService == null) { // happens if the provider was called early in the pipeline
+                    var scope = _workContextAccessor.GetContext() ?? _workContextAccessor.CreateWorkContextScope().WorkContext;
+                    site = scope.Resolve<ISiteService>().GetSiteSettings();
+                }
+                else {
+                    site = _siteService.GetSiteSettings();
+                }
+
+                settings = site.As<AzureActiveDirectorySettingsPart>();
+
+                return (settings != null && settings.IsValid);
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.OpenId/Providers/Twitter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using Orchard.OpenId.Models;
+using Orchard.OpenId.Services;
+using Orchard.Settings;
+
+namespace Orchard.OpenId.Providers {
+    [OrchardFeature("Orchard.OpenId.Twitter")]
+    public class Twitter : IOpenIdProvider {
+        private readonly IWorkContextAccessor _workContextAccessor;
+        private readonly ISiteService _siteService;
+
+        public Twitter(
+            IWorkContextAccessor workContextAccessor,
+            ISiteService siteService) {
+
+            _workContextAccessor = workContextAccessor;
+            _siteService = siteService;
+        }
+
+        public string AuthenticationType {
+            get { return "Twitter"; }
+        }
+
+        public string Name {
+            get { return "Twitter"; }
+        }
+
+        public string DisplayName {
+            get { return "Twitter"; }
+        }
+
+        public bool IsValid {
+            get { return IsProviderValid(); }
+        }
+
+        private bool IsProviderValid() {
+            try {
+                TwitterSettingsPart settings;
+                ISite site;
+
+                if (_siteService == null) { // happens if the provider was called early in the pipeline
+                    var scope = _workContextAccessor.GetContext() ?? _workContextAccessor.CreateWorkContextScope().WorkContext;
+                    site = scope.Resolve<ISiteService>().GetSiteSettings();
+                }
+                else {
+                    site = _siteService.GetSiteSettings();
+                }
+
+                settings = site.As<TwitterSettingsPart>();
+
+                return (settings != null && settings.IsValid);
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+    }
+}
