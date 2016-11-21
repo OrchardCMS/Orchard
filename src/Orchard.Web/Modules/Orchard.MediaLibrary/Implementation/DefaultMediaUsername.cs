87073d9760c73c5fa5ccfdfcd8c2dc6cc246bfe5
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2016-11-21 11:28:24 +0100

    Manage Own Media permission, created Users/UserName folder that sounds as having some problems. Now creates Users/UserNameEncoded folder in order to prevent:
    Media Folder name broken with special chars
    
    Added interface for change the default behavior

---
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/Implementation/DefaultMediaUsername.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.MediaLibrary.Providers;
+using Orchard.Users.Models;
+
+namespace Orchard.MediaLibrary.Implementation {
+    public class DefaultMediaUsername : IMediaFolderProvider {
+        public virtual string GetFolderName(ContentItem content) {
+            if (content.As<UserPart>() != null) {
+                string folder = "";
+                foreach (char c in content.As<UserPart>().UserName) {
+                    if (char.IsLetterOrDigit(c)) {
+                        folder += c;
+                    }
+                    else
+                        folder += "_" + String.Format("{0:X}", Convert.ToInt32(c));
+                }
+                return folder;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
