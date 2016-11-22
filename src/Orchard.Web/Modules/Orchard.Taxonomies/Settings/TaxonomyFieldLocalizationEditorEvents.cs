c07f1dc1cdfff672cc1b80f10bdbb51485b686bc
marco.viglione <marco.viglione@laser-group.com>
2016-11-22 17:55:12 +0100

    Settings for localized taxonomy field.

---
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Settings/TaxonomyFieldLocalizationEditorEvents.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.ContentManagement.MetaData.Models;
+using Orchard.ContentManagement.ViewModels;
+using Orchard.Environment.Extensions;
+
+namespace Orchard.Taxonomies.Settings {
+    [OrchardFeature("Orchard.Taxonomies.LocalizationExtensions")]
+    public class TaxonomyFieldLocalizationEditorEvents : ContentDefinitionEditorEventsBase {
+
+        public override IEnumerable<TemplateViewModel> PartFieldEditor(ContentPartFieldDefinition definition) {
+            if (definition.FieldDefinition.Name == "TaxonomyField") {
+                var model = definition.Settings.GetModel<TaxonomyFieldLocalizationSettings>();
+                yield return DefinitionTemplate(model);
+            }
+        }
+
+        public override IEnumerable<TemplateViewModel> PartFieldEditorUpdate(ContentPartFieldDefinitionBuilder builder, IUpdateModel updateModel) {
+            if (builder.FieldType != "TaxonomyField") {
+                yield break;
+            }
+
+            var model = new TaxonomyFieldLocalizationSettings();
+            if (updateModel.TryUpdateModel(model, "TaxonomyFieldLocalizationSettings", null, null)) {
+                builder.WithSetting("TaxonomyFieldLocalizationSettings.AssertSameCulture", model.AssertSameCulture.ToString(CultureInfo.InvariantCulture));
+                builder.WithSetting("TaxonomyFieldLocalizationSettings.TryToLocalize", model.TryToLocalize.ToString(CultureInfo.InvariantCulture));
+                builder.WithSetting("TaxonomyFieldLocalizationSettings.RemoveItemsWithoutLocalization", model.RemoveItemsWithoutLocalization.ToString(CultureInfo.InvariantCulture));
+            }
+
+            yield return DefinitionTemplate(model);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Settings/TaxonomyFieldLocalizationSettings.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.Environment.Extensions;
+
+namespace Orchard.Taxonomies.Settings {
+    [OrchardFeature("Orchard.Taxonomies.LocalizationExtensions")]
+    public class TaxonomyFieldLocalizationSettings {
+
+        public bool AssertSameCulture { get; set; }
+        public bool TryToLocalize { get; set; }
+        public bool RemoveItemsWithoutLocalization { get; set; }
+    }
+}
