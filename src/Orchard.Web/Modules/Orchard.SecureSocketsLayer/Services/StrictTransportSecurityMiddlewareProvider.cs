eed3ee687148bc311b0140ac570d164e09fec102
Lombiq <github@lombiq.com>
2016-11-24 23:38:20 +0100

    Adding the ability to enable HTTP Strict Transport Security from the SSL module, fixes #6612

---
+++ src/Orchard.Web/Modules/Orchard.SecureSocketsLayer/Services/StrictTransportSecurityMiddlewareProvider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.Logging;
+using Orchard.Owin;
+using Orchard.SecureSocketsLayer.Models;
+using Owin;
+
+namespace Orchard.SecureSocketsLayer.Services {
+    public class StrictTransportSecurityMiddlewareProvider : IOwinMiddlewareProvider {
+        private readonly IWorkContextAccessor _wca;
+
+        public ILogger Logger { get; set; }
+
+        public StrictTransportSecurityMiddlewareProvider(IWorkContextAccessor wca) {
+            _wca = wca;
+
+            Logger = NullLogger.Instance;
+        }
+
+        public IEnumerable<OwinMiddlewareRegistration> GetOwinMiddlewares() {
+            return new[] {
+                new OwinMiddlewareRegistration {
+                    Configure = app =>
+                        app.Use(async (context, next) => {
+                            var sslSettings = _wca.GetContext().CurrentSite.As<SslSettingsPart>();
+
+                            if (sslSettings.SendStrictTransportSecurityHeaders) {
+                                string responseValue = "max-age=" + sslSettings.StrictTransportSecurityMaxAge;
+
+                                if (sslSettings.StrictTransportSecurityIncludeSubdomains) {
+                                    responseValue += "; includeSubDomains";
+                                }
+
+                                if (sslSettings.StrictTransportSecurityPreload) {
+                                    responseValue += "; preload";
+                                }
+                                context.Response.Headers.Append("Strict-Transport-Security", responseValue);
+                            }
+
+                            await next.Invoke();
+                        })
+                }
+            };
+        }
+    }
+}
