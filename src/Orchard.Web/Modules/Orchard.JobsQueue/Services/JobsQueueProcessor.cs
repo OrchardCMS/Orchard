48e2a6a1b4d1d25da82074485e0f3960fd79e5c1
Lombiq <github@lombiq.com>
2016-12-28 20:16:47 +0100

    Fixing that JobsQueue will fail if there are a large number of tasks, fixes #5365
    JobsQueue now works in batches to prevent transaction timeouts and the manual job execution feature uses batches as well. This is a fix for the immediate issue, further fixes are needed around the background task infrastructure, see the linked issue.

--- src/Orchard.Web/Modules/Orchard.JobsQueue/Services/JobsQueueProcessor.cs
+++ src/Orchard.Web/Modules/Orchard.JobsQueue/Services/JobsQueueProcessor.cs
@@ -7,44 +7,51 @@ using Orchard.Events;
 using Orchard.JobsQueue.Models;
 using Orchard.Logging;
 using Orchard.Tasks.Locking.Services;
+using Orchard.Data;
 
 namespace Orchard.JobsQueue.Services {
     public class JobsQueueProcessor : IJobsQueueProcessor {
         private readonly Work<IJobsQueueManager> _jobsQueueManager;
         private readonly Work<IEventBus> _eventBus;
         private readonly Work<IDistributedLockService> _distributedLockService;
+        private readonly Work<ITransactionManager> _transactionManager;
 
         public JobsQueueProcessor(
             Work<IJobsQueueManager> jobsQueueManager,
             Work<IEventBus> eventBus,
-            Work<IDistributedLockService> distributedLockService) {
+            Work<IDistributedLockService> distributedLockService,
+            Work<ITransactionManager> transactionManager) {
 
             _jobsQueueManager = jobsQueueManager;
             _eventBus = eventBus;
             _distributedLockService = distributedLockService;
+            _transactionManager = transactionManager;
             Logger = NullLogger.Instance;
         }
 
         public ILogger Logger { get; set; }
 
-        public void ProcessQueue() {
+        public void ProcessQueue(int batchSize, uint batchCount) {
             IDistributedLock @lock;
             if (_distributedLockService.Value.TryAcquireLock(GetType().FullName, TimeSpan.FromMinutes(5), out @lock)) {
                 using (@lock) {
                     IEnumerable<QueuedJobRecord> messages;
-
-                    while ((messages = _jobsQueueManager.Value.GetJobs(0, 10).ToArray()).Any()) {
+                    var currentBatch = 0;
+                    while (batchCount > currentBatch && (messages = _jobsQueueManager.Value.GetJobs(0, batchSize).ToArray()).Any()) {
                         foreach (var message in messages) {
                             ProcessMessage(message);
                         }
+
+                        currentBatch++;
                     }
                 }
             }
         }
 
         private void ProcessMessage(QueuedJobRecord job) {
-
             Logger.Debug("Processing job {0}.", job.Id);
+
+            _transactionManager.Value.RequireNew();
 
             try {
                 var payload = JObject.Parse(job.Parameters);
@@ -52,13 +59,13 @@ namespace Orchard.JobsQueue.Services {
 
                 _eventBus.Value.Notify(job.Message, parameters);
 
+                _jobsQueueManager.Value.Delete(job);
+
                 Logger.Debug("Processed job Id {0}.", job.Id);
             }
             catch (Exception e) {
+                _transactionManager.Value.Cancel();
                 Logger.Error(e, "An unexpected error while processing job {0}. Error message: {1}.", job.Id, e);
-            }
-            finally {
-                _jobsQueueManager.Value.Delete(job);
             }
         }
     }
