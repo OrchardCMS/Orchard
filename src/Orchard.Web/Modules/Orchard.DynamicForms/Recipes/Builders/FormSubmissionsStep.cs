b717e4626391a6f68de474e5e33885870f77f7eb
Hannan Azam <hannan.azam@gmail.com>
2016-12-30 17:47:34 +0500

    #7486 Possibility of exporting individual Form Submissions

--- src/Orchard.Web/Modules/Orchard.DynamicForms/Recipes/Builders/FormSubmissionsStep.cs
+++ src/Orchard.Web/Modules/Orchard.DynamicForms/Recipes/Builders/FormSubmissionsStep.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Xml.Linq;
-using Orchard.ContentManagement;
 using Orchard.DynamicForms.Services;
-using Orchard.DynamicForms.ViewModels;
 using Orchard.Localization;
-using Orchard.Recipes.Models;
 using Orchard.Recipes.Services;
+using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.DynamicForms.ViewModels;
 
 namespace Orchard.DynamicForms.Recipes.Builders {
     public class FormSubmissionsStep : RecipeBuilderStep {
         private readonly IFormService _formService;
+
         public FormSubmissionsStep(IFormService formService) {
             _formService = formService;
-            SelectedSubmissions = new List<string>();
+            SelectedForms = new List<string>();
         }
 
         public override string Name {
@@ -28,65 +27,44 @@ namespace Orchard.DynamicForms.Recipes.Builders {
         public override LocalizedString Description {
             get { return T("Exports submitted forms."); }
         }
-        public IList<string> SelectedSubmissions { get; set; }
+
+        public IList<string> SelectedForms { get; set; }
 
         public override dynamic BuildEditor(dynamic shapeFactory) {
-            // TODO: Implement an editor that enables the user to select which forms to export.
             return UpdateEditor(shapeFactory, null);
         }
+
         public override dynamic UpdateEditor(dynamic shapeFactory, IUpdateModel updater) {
-            var submissions = _formService.GetSubmissions().OrderBy(x => x.FormName).GroupBy(y => y.FormName)
-                .Select(x => new FormSubmissionsEntry { Name = x.FirstOrDefault().FormName })
-                .ToList(); 
+            List<FormExportEntry> forms = new List<FormExportEntry>();
 
-            if (!submissions.Any()) {
-                return null;
+            if (updater != null && updater.TryUpdateModel(forms, Prefix, null, null)) {
+                SelectedForms = forms.Where(x => x.Export).Select(x => x.FormName).ToList();
             }
-         
-            var viewModel = new FormSubmissionsBuilderStepViewModel {
-                Submissions = submissions
-            };
-
-            if (updater != null && updater.TryUpdateModel(viewModel, Prefix, null, null)) {
-                SelectedSubmissions = viewModel.Submissions.Where(x => x.ExportFormData).Select(x => x.Name).ToList();
+            else {
+                forms = _formService.GetSubmissions().OrderBy(x => x.FormName).GroupBy(y => y.FormName)
+                    .Select(x => new FormExportEntry { FormName = x.FirstOrDefault().FormName })
+                    .ToList();
             }
 
-            return shapeFactory.EditorTemplate(TemplateName: "BuilderSteps/Submissions", Model: viewModel, Prefix: Prefix);
-        }
-        public override void Configure(RecipeBuilderStepConfigurationContext context) {
-        
-            var selectedSubmissions = context.ConfigurationElement.Attr("SelectedSubmissions");
-        
-            if (!String.IsNullOrWhiteSpace(selectedSubmissions))
-                SelectedSubmissions = selectedSubmissions.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-
-
+            return shapeFactory.EditorTemplate(TemplateName: "BuilderSteps/FormSubmissions", Model: forms, Prefix: Prefix);
         }
 
-        public override void ConfigureDefault() {
-
-            SelectedSubmissions = _formService.GetSubmissions().OrderBy(x => x.FormName).GroupBy(y => y.FormName)
-               .Select(x => x.FirstOrDefault().FormName).ToList();
-                
-        }
         public override void Build(BuildContext context) {
-            var submissions = _formService.GetSubmissions();
-            var selectedSubmissions = SelectedSubmissions;
-            var forms = submissions.Where(a => SelectedSubmissions.Contains(a.FormName)).ToArray().GroupBy(x => x.FormName);
-            if (!forms.Any()) {
-                return;
-            }
-            
-            var root = new XElement("Forms");
-            context.RecipeDocument.Element("Orchard").Add(root);
+            if (SelectedForms.Count() > 0) {
+                var root = new XElement("Forms");
+                context.RecipeDocument.Element("Orchard").Add(root);
 
-            foreach (var form in forms) {
-                root.Add(new XElement("Form",
-                    new XAttribute("Name", form.Key),
-                    new XElement("Submissions", form.Select(submission =>
-                        new XElement("Submission",
-                            new XAttribute("CreatedUtc", submission.CreatedUtc),
-                            new XCData(submission.FormData))))));
+                foreach (var form in SelectedForms) {
+                    var submissions = _formService.GetSubmissions(form);
+                    if (submissions.Count() > 0) {
+                        root.Add(new XElement("Form",
+                            new XAttribute("Name", form),
+                            new XElement("Submissions", submissions.Select(submission =>
+                                new XElement("Submission",
+                                    new XAttribute("CreatedUtc", submission.CreatedUtc),
+                                    new XCData(submission.FormData))))));
+                    }
+                }
             }
         }
     }
--- src/Orchard.Web/Modules/Orchard.DynamicForms/ViewModels/FormSubmissionsEntry.cs
+++
@@ -1,6 +0,0 @@
-namespace Orchard.DynamicForms.ViewModels {
-    public class FormSubmissionsEntry {
-        public string Name { get; set; }
-        public bool ExportFormData { get; set; }
-    }
-}
