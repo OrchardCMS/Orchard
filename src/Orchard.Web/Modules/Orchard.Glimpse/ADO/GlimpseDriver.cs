9a52612bcfaec13b6f9adcaea71a4e1ceae600ae
paynecrl97 <chris.payne@bedegaming.com>
2017-01-04 23:33:25 +0000

    Uses GlimpseDbConnection as the IDbConnection

--- src/Orchard.Web/Modules/Orchard.Glimpse/ADO/GlimpseDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Glimpse/ADO/GlimpseDriver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using Glimpse.Ado.AlternateType;
@@ -20,7 +21,7 @@ namespace Orchard.Glimpse.ADO {
         }
 
         public IDbConnection CreateConnection() {
-            return _decoratedService.CreateConnection();
+            return new GlimpseDbConnection(_decoratedService.CreateConnection() as DbConnection);
         }
 
         public IDbCommand GenerateCommand(CommandType type, SqlString sqlString, SqlType[] parameterTypes) {
