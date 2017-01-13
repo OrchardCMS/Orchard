7e5bdb28e326901988084c29d480ee425a872397
Daniel Stolt <daniel@ideliverable.com>
2017-01-13 17:56:36 +0100

    Updated Gulp pipeline to use latest version of TypeScript.
    This also required a change to flow all .js source files through the TypeScript transpiler and let TypeScript do the concatenation/bundling where applicable. This has the added bonus of catching more errors in plain .js files as the TypeScript compiler applies its structural and semantic understanding of the JavaScript code.

--- src/Orchard.Web/Modules/Orchard.Resources/Scripts/Calendars/jquery.calendars-de-CH.js
+++ src/Orchard.Web/Modules/Orchard.Resources/Scripts/Calendars/jquery.calendars-de-CH.js
@@ -6,23 +6,23 @@
 /* http://keith-wood.name/calendars.html
    Swiss-German localisation for Gregorian/Julian calendars for jQuery.
    Written by Douglas Jose & Juerg Meier. */
-(function($) {
-	$.calendars.calendars.gregorian.prototype.regionalOptions['de-CH'] = {
-		name: 'Gregorian',
-		epochs: ['BCE', 'CE'],
-		monthNames: ['Januar','Februar','März','April','Mai','Juni',
-		'Juli','August','September','Oktober','November','Dezember'],
-		monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
-		'Jul','Aug','Sep','Okt','Nov','Dez'],
-		dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
-		dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-		dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-		dateFormat: 'dd.mm.yyyy',
-		firstDay: 1,
-		isRTL: false
-	};
-	if ($.calendars.calendars.julian) {
-		$.calendars.calendars.julian.prototype.regionalOptions['de-CH'] =
-			$.calendars.calendars.gregorian.prototype.regionalOptions['de-CH'];
-	}
+(function ($) {
+    $.calendars.calendars.gregorian.prototype.regionalOptions['de-CH'] = {
+        name: 'Gregorian',
+        epochs: ['BCE', 'CE'],
+        monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+            'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+        monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+        dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+        dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+        dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+        dateFormat: 'dd.mm.yyyy',
+        firstDay: 1,
+        isRTL: false
+    };
+    if ($.calendars.calendars.julian) {
+        $.calendars.calendars.julian.prototype.regionalOptions['de-CH'] =
+            $.calendars.calendars.gregorian.prototype.regionalOptions['de-CH'];
+    }
 })(jQuery);
