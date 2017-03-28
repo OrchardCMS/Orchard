69a425120d5f7afc52f0ca6760aab8c22ced22fa
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2017-03-28 10:24:15 +0200

    Fix trytotranslate  a true settato impostazione iniziale per nostra versione di tassonomia

--- src/Orchard.Web/Modules/Orchard.Taxonomies/Settings/TaxonomyFieldLocalizationSettings.cs
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Settings/TaxonomyFieldLocalizationSettings.cs
@@ -4,5 +4,8 @@ namespace Orchard.Taxonomies.Settings {
     [OrchardFeature("Orchard.Taxonomies.LocalizationExtensions")]
     public class TaxonomyFieldLocalizationSettings {
         public bool TryToLocalize { get; set; }
+        public TaxonomyFieldLocalizationSettings() {
+            TryToLocalize = true;  // aggiunto nella nostra versione, default messo a true
+        }
     }
 }
