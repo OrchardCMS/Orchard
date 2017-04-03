4cb0b3b5c4bf4c5f3ce274447e388ecb89918415
Matteo Piovanelli <MatteoPiovanelli-Laser@users.noreply.github.com>
2017-04-03 19:33:14 +0200

    Localization improvements (#7596)
    Updates to handling of MasterContentItem (as they are in 1.10.x)
    Synchronization of CultureNeutral Fields and Parts (based on Cloning)
    Support for multi-language Blogs (in its own feature in the Blogs module)
    Support for multi-language Taxonomies (in its own feature in the Taxonomies module)

---
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/LocalizedTaxonomyMigration.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Data.Migration;
+using Orchard.Environment.Extensions;
+
+namespace Orchard.Taxonomies {
+    [OrchardFeature("Orchard.Taxonomies.LocalizationExtensions")]
+    public class LocalizedTaxonomyMigration : DataMigrationImpl {
+
+        public int Create() {
+            ContentDefinitionManager.AlterTypeDefinition("Taxonomy", cfg => cfg
+                  .WithPart("LocalizationPart")
+                  );
+            return 1;
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Taxonomies/Services/ITaxonomySource.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.Taxonomies.Fields;
+using Orchard.Taxonomies.Models;
+
+namespace Orchard.Taxonomies.Services {
+    public interface ITaxonomySource : IDependency {
+        TaxonomyPart GetTaxonomy(string name, ContentItem item);
+    }
+}
