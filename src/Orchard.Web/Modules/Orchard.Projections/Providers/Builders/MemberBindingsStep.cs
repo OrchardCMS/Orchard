dac521e6a3e02945caad8397859443ce6a9e03d9
lihengzhe <li-0803@163.com>
2017-12-23 09:30:49 +0800

    typo fixing and reformat the code per jersiovic review

--- src/Orchard.Web/Modules/Orchard.Projections/Providers/Builders/MemberBindingsStep.cs
+++ src/Orchard.Web/Modules/Orchard.Projections/Providers/Builders/MemberBindingsStep.cs
@@ -6,44 +6,41 @@ using Orchard.Localization;
 using Orchard.Projections.Services;
 using Orchard.Recipes.Services;
 
-namespace Orchard.Projections.Providers.Builders
-{
-    public class MemberBindingsStep : RecipeBuilderStep
-    {
-
+namespace Orchard.Projections.Providers.Builders {
+    public class MemberBindingsStep : RecipeBuilderStep {
         private readonly IMemberBindingProvider _memberBindingProvider;
-        public MemberBindingsStep(IMemberBindingProvider memberBindingProvider)
-        {
+        public MemberBindingsStep(IMemberBindingProvider memberBindingProvider) {
             _memberBindingProvider = memberBindingProvider;
         }
 
-        public override string Name
-        {
+        public override string Name {
             get { return "MemberBindings"; }
         }
 
-        public override LocalizedString DisplayName
-        {
+        public override LocalizedString DisplayName {
             get { return T("Member Bindings"); }
         }
 
-        public override LocalizedString Description
-        {
+        public override LocalizedString Description {
             get { return T("Exports query member bindings."); }
         }
-        public override int Priority { get { return 25; } }
-        public override int Position { get { return 25; } }
 
-        public override void Build(BuildContext context)
-        {
+        public override int Priority {
+            get { return 25; }
+        }
+
+        public override int Position {
+            get { return 25; }
+        }
+
+        public override void Build(BuildContext context) {
             var memberBindings = new XElement("MemberBindings");
             context.RecipeDocument.Element("Orchard").Add(memberBindings);
 
             var bindingBuilder = new BindingBuilder();
             _memberBindingProvider.GetMemberBindings(bindingBuilder);
 
-            foreach (var bindingItem in bindingBuilder.Build())
-            {
+            foreach (var bindingItem in bindingBuilder.Build()) {
                 var declaringType = bindingItem.Property.DeclaringType;
 
                 var memberBinding = new XElement("MemberBinding",
--- src/Orchard.Web/Modules/Orchard.Projections/Providers/Executors/MemberBindingsStep.cs
+++ src/Orchard.Web/Modules/Orchard.Projections/Providers/Executors/MemberBindingsStep.cs
@@ -6,48 +6,37 @@ using Orchard.Projections.Models;
 using Orchard.Recipes.Models;
 using Orchard.Recipes.Services;
 
-namespace Orchard.Projections.Providers.Executors
-{
-    public class MemberBindingsStep : RecipeExecutionStep
-    {
+namespace Orchard.Projections.Providers.Executors {
+    public class MemberBindingsStep : RecipeExecutionStep {
         private readonly IRepository<MemberBindingRecord> _repository;
 
-        public MemberBindingsStep(IRepository<MemberBindingRecord> repository, RecipeExecutionLogger logger) : base(logger)
-        {
+        public MemberBindingsStep(IRepository<MemberBindingRecord> repository, RecipeExecutionLogger logger) : base(logger) {
             _repository = repository;
             Logger = NullLogger.Instance;
         }
 
         public ILogger Logger { get; set; }
 
-        public override string Name
-        {
+        public override string Name {
             get { return "MemberBindings"; }
         }
 
-        public override void Execute(RecipeExecutionContext context)
-        {
-
-            foreach (var memberBindingElement in context.RecipeStep.Step.Elements())
-            {
+        public override void Execute(RecipeExecutionContext context) {
+            foreach (var memberBindingElement in context.RecipeStep.Step.Elements()) {
                 Logger.Information("Importing member bindings.");
-
-                try
-                {
+                try {
                     var member = memberBindingElement.Attribute("Member").Value;
                     var type = memberBindingElement.Attribute("Type").Value;
                     if (_repository.Get(b => b.Member == member && b.Type == type) != null)
                         continue;
-                    _repository.Create(new MemberBindingRecord
-                    {
+                    _repository.Create(new MemberBindingRecord {
                         Member = member,
                         Type = type,
                         DisplayName = memberBindingElement.Attribute("DisplayName").Value,
                         Description = memberBindingElement.Attribute("Description").Value,
                     });
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     Logger.Error(ex, "Error while importing member bindings.");
                     throw;
                 }
