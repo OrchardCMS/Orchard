9446665c954683cad6838d3fd873ebf71782ce6f
Jasmin Savard <jasmin.savard@gmail.com>
2018-04-23 00:42:19 -0400

    Fix write concurrency issue with LayoutPart

--- src/Orchard.Web/Modules/Orchard.Layouts/Services/LayoutEditorFactory.cs
+++ src/Orchard.Web/Modules/Orchard.Layouts/Services/LayoutEditorFactory.cs
@@ -9,37 +9,59 @@ using Orchard.Layouts.Framework.Elements;
 using Orchard.Layouts.Helpers;
 using Orchard.Layouts.Models;
 using Orchard.Layouts.ViewModels;
+using Orchard.Locking;
 using Orchard.Utility.Extensions;
 
-namespace Orchard.Layouts.Services {
-    public class LayoutEditorFactory : ILayoutEditorFactory {
+namespace Orchard.Layouts.Services
+{
+    public class LayoutEditorFactory : ILayoutEditorFactory
+    {
         private readonly ILayoutModelMapper _mapper;
         private readonly ILayoutManager _layoutManager;
         private readonly IElementManager _elementManager;
         private readonly IElementDisplay _elementDisplay;
         private readonly IShapeDisplay _shapeDisplay;
+        private readonly IOrchardServices _orchardServices;
+        private readonly ILockingProvider _lockingProvider;
 
         public LayoutEditorFactory(
-            ILayoutModelMapper mapper, 
-            ILayoutManager layoutManager, 
-            IElementManager elementManager, 
-            IElementDisplay elementDisplay, 
-            IShapeDisplay shapeDisplay) {
+            ILayoutModelMapper mapper,
+            ILayoutManager layoutManager,
+            IElementManager elementManager,
+            IElementDisplay elementDisplay,
+            IShapeDisplay shapeDisplay,
+            IOrchardServices orchardServices,
+            ILockingProvider lockingProvider)
+        {
 
             _mapper = mapper;
             _layoutManager = layoutManager;
             _elementManager = elementManager;
             _elementDisplay = elementDisplay;
             _shapeDisplay = shapeDisplay;
+            _orchardServices = orchardServices;
+            _lockingProvider = lockingProvider;
         }
 
-        public LayoutEditor Create(LayoutPart layoutPart) {
+        private string LockString {
+            get {
+                return string.Join(".",
+                    _orchardServices.WorkContext?.CurrentSite?.BaseUrl ?? "",
+                    _orchardServices.WorkContext?.CurrentSite?.SiteName ?? "",
+                    "LayoutEditorFactory");
+            }
+        }
+
+        public LayoutEditor Create(LayoutPart layoutPart)
+        {
             return Create(layoutPart.LayoutData, layoutPart.SessionKey, layoutPart.TemplateId, layoutPart);
         }
 
-        public LayoutEditor Create(string layoutData, string sessionKey, int? templateId = null, IContent content = null) {
-            return new LayoutEditor {
-                Data = _mapper.ToEditorModel(layoutData, new DescribeElementsContext {Content = content}).ToJson(),
+        public LayoutEditor Create(string layoutData, string sessionKey, int? templateId = null, IContent content = null)
+        {
+            return new LayoutEditor
+            {
+                Data = _mapper.ToEditorModel(layoutData, new DescribeElementsContext { Content = content }).ToJson(),
                 ConfigurationData = GetConfigurationData(content),
                 TemplateId = templateId,
                 Content = content,
@@ -48,26 +70,40 @@ namespace Orchard.Layouts.Services {
             };
         }
 
-        private IList<LayoutPart> GetTemplates(IContent content = null) {
-            var query = _layoutManager.GetTemplates();
-            var layoutPart = content != null ? content.As<LayoutPart>() : null;
+        private IList<LayoutPart> GetTemplates(IContent content = null)
+        {
+            IEnumerable<LayoutPart> query = null;
 
-            if (layoutPart != null) {
-                query = query.Where(x => x.Id != layoutPart.Id);
-            }
+            _lockingProvider.Lock(string.Join(".", LockString, content.Id),
+            () =>
+            {
+                query = _layoutManager.GetTemplates();
 
+                var layoutPart = content != null ? content.As<LayoutPart>() : null;
+
+                if (layoutPart != null)
+                {
+                    query = query.Where(x => x.Id != layoutPart.Id);
+                }
+            });
+            
             return query.ToList();
         }
 
-        private string GetConfigurationData(IContent content) {
+        private string GetConfigurationData(IContent content)
+        {
             var elementCategories = GetCategories(content).ToArray();
-            var config = new {
-                categories = elementCategories.Select(category => new {
+            var config = new
+            {
+                categories = elementCategories.Select(category => new
+                {
                     name = category.DisplayName.Text,
-                    contentTypes = category.Elements.Where(x => !x.IsSystemElement).Select(descriptor => {
+                    contentTypes = category.Elements.Where(x => !x.IsSystemElement).Select(descriptor =>
+                    {
                         var element = _elementManager.ActivateElement(descriptor);
                         var map = _mapper.GetMapFor(element);
-                        return new {
+                        return new
+                        {
                             label = descriptor.DisplayText.Text,
                             id = descriptor.TypeName,
                             type = map.LayoutElementType,
@@ -87,11 +123,13 @@ namespace Orchard.Layouts.Services {
             return JToken.FromObject(config).ToString(Formatting.None);
         }
 
-        private string RenderElement(Element element, DescribeElementsContext describeContext, string displayType = "Design") {
+        private string RenderElement(Element element, DescribeElementsContext describeContext, string displayType = "Design")
+        {
             return _shapeDisplay.Display(_elementDisplay.DisplayElement(element, describeContext.Content, displayType));
         }
 
-        private IEnumerable<CategoryDescriptor> GetCategories(IContent content) {
+        private IEnumerable<CategoryDescriptor> GetCategories(IContent content)
+        {
             var describeContext = new DescribeElementsContext { Content = content };
             var elementCategories = _elementManager.GetCategories(describeContext).ToArray();
 
