8b4b5e362811d99de4ae4c6665340c0f914ec9cc
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2018-08-07 17:32:53 +0200

    Added NOLOCK to the IndexingTaskRecord table to prevent locking everything while reading from it.

---
+++ src/Orchard.Web/Modules/Orchard.Indexing/Services/IndexingNoLockTableProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Orchard.Data.Providers;
+
+namespace Orchard.Indexing.Services {
+    public class IndexingNoLockTableProvider : INoLockTableProvider {
+        public IEnumerable<string> GetTableNames() {
+            return new string[] { "Orchard_Indexing_IndexingTaskRecord" };
+        }
+    }
+}
