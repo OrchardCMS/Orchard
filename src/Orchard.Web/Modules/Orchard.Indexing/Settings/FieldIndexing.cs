0d021d5a7df622feab167f608d47ae868356c4b1
Payne <Chris.Payne@dentsplysirona.com>
2019-03-22 17:23:09 +0000

    Adds ability to configure field indexing options

--- src/Orchard.Web/Modules/Orchard.Indexing/Settings/FieldIndexing.cs
+++ src/Orchard.Web/Modules/Orchard.Indexing/Settings/FieldIndexing.cs
@@ -1,5 +1,13 @@
 ﻿namespace Orchard.Indexing.Settings {
     public class FieldIndexing {
+        public FieldIndexing() {
+            Analyzed = true;
+            TagsRemoved = true;
+        }
+
         public bool Included { get; set; }
+        public bool Stored { get; set; }
+        public bool Analyzed { get; set; }
+        public bool TagsRemoved { get; set; }
     }
 }
