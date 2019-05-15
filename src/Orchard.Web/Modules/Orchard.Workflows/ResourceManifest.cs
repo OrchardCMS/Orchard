978e0efce0543e2d96cce6c0959bfa31285e4f43
Márk Bartha <mark.bartha@lombiq.com>
2019-05-15 18:12:24 +0200

    Fixing that Orchard.Workflows requires styles that not exists

--- src/Orchard.Web/Modules/Orchard.Workflows/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.Workflows/ResourceManifest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Web;
 using Orchard.Environment;
 using Orchard.UI.Resources;
 using Orchard.Utility.Extensions;
@@ -8,9 +9,11 @@ using Orchard.Workflows.Services;
 namespace Orchard.Workflows {
     public class ResourceManifest : IResourceManifestProvider {
         private readonly Work<IActivitiesManager> _activitiesManager;
+        private readonly IHostEnvironment _hostEnvironment;
 
-        public ResourceManifest(Work<IActivitiesManager> activitiesManager) {
+        public ResourceManifest(Work<IActivitiesManager> activitiesManager, IHostEnvironment hostEnvironment) {
             _activitiesManager = activitiesManager;
+            _hostEnvironment = hostEnvironment;
         }
 
         public void BuildManifests(ResourceManifestBuilder builder) {
@@ -18,13 +21,24 @@ namespace Orchard.Workflows {
 
             manifest.DefineStyle("WorkflowsAdmin").SetUrl("orchard-workflows-admin.css").SetDependencies("~/Themes/TheAdmin/Styles/Site.css");
 
-            var activityNames = _activitiesManager.Value.GetActivities().Select(x => "WorkflowsActivity-" + x.Name).ToArray();
+            var resourceNames = new List<string>();
 
-            foreach (var activityName in activityNames) {
-                manifest.DefineStyle(activityName).SetUrl(activityName.HtmlClassify()).SetDependencies("WorkflowsAdmin");
+            foreach (var activity in _activitiesManager.Value.GetActivities()) {
+                var assemblyName = activity.GetType().Assembly.GetName().Name;
+                var basePath = VirtualPathUtility.AppendTrailingSlash("~/Modules/" + assemblyName + "/styles/");
+                var resourceName = "WorkflowsActivity-" + activity.Name;
+                var filename = resourceName.HtmlClassify() + ".css";
+
+                if (File.Exists(_hostEnvironment.MapPath(basePath) + filename)) {
+                    resourceNames.Add(resourceName);
+
+                    manifest.DefineStyle(resourceName)
+                        .SetUrl(filename)
+                        .SetDependencies("WorkflowsAdmin");
+                }
             }
 
-            manifest.DefineStyle("WorkflowsActivities").SetUrl("workflows-activity.css").SetDependencies(activityNames);
+            manifest.DefineStyle("WorkflowsActivities").SetUrl("workflows-activity.css").SetDependencies(resourceNames.ToArray());
 
             manifest.DefineScript("jsPlumb").SetUrl("jquery.jsPlumb-1.4.1-all-min.js").SetDependencies("jQueryUI");
         }
