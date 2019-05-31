c599eef8bfa44140ca1bc2ac41e3c4051dc2d6d7
Milán Keszthelyi <milan.keszthelyi@lombiq.com>
2019-05-31 14:24:31 +0200

    Passing the exact Content Item to the ContentPreview.Button

--- src/Orchard.Web/Modules/Orchard.ContentPreview/Drivers/ContentPreviewDriver.cs
+++ src/Orchard.Web/Modules/Orchard.ContentPreview/Drivers/ContentPreviewDriver.cs
@@ -4,6 +4,6 @@ using Orchard.Core.Common.Models;
 namespace Orchard.ContentPreview.Drivers {
     public class ContentPreviewDriver : ContentPartDriver<CommonPart> {
         protected override DriverResult Editor(CommonPart part, dynamic shapeHelper) =>
-            ContentShape("ContentPreview_Button", () => shapeHelper.ContentPreview_Button());
+            ContentShape("ContentPreview_Button", () => shapeHelper.ContentPreview_Button(Model: part.ContentItem));
     }
 }
