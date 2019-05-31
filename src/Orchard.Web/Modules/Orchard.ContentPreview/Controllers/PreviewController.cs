3e34fcdc8ae43bf2f8a0d13e738cac8f7eecd9aa
Milán Keszthelyi <milan.keszthelyi@lombiq.com>
2019-05-31 15:33:19 +0200

    Code cleanup of PreviewController

--- src/Orchard.Web/Modules/Orchard.ContentPreview/Controllers/PreviewController.cs
+++ src/Orchard.Web/Modules/Orchard.ContentPreview/Controllers/PreviewController.cs
@@ -1,15 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System.Net;
 using System.Web.Mvc;
 using Orchard.ContentManagement;
-using Orchard.ContentManagement.MetaData;
 using Orchard.Core.Common.Models;
 using Orchard.Localization;
-using Orchard.Logging;
 using Orchard.Security;
 using Orchard.Services;
-using Orchard.Settings;
 using Orchard.Themes;
 using Orchard.UI.Notify;
 
@@ -17,9 +12,6 @@ namespace Orchard.ContentPreview.Controllers {
     [Themed]
     public class PreviewController : Controller, IUpdateModel {
         private readonly IContentManager _contentManager;
-        private readonly IContentDefinitionManager _contentDefinitionManager;
-        private readonly ISiteService _siteService;
-        //private readonly IContentItemDisplayManager _contentItemDisplayManager;
         private readonly INotifier _notifier;
         private readonly IClock _clock;
         private readonly IAuthorizer _authorizer;
@@ -27,27 +19,15 @@ namespace Orchard.ContentPreview.Controllers {
 
         public PreviewController(
             IContentManager contentManager,
-            //IContentItemDisplayManager contentItemDisplayManager,
-            IContentDefinitionManager contentDefinitionManager,
-            ISiteService siteService,
             INotifier notifier,
-            //ILogger<PreviewController> logger,
-            //IHtmlLocalizer<PreviewController> localizer,
             IClock clock,
             IAuthorizer authorizer) {
             _clock = clock;
             _notifier = notifier;
-            _siteService = siteService;
             _contentManager = contentManager;
-            _contentDefinitionManager = contentDefinitionManager;
             _authorizer = authorizer;
-
-            T = NullLocalizer.Instance;
-            Logger = NullLogger.Instance;
         }
 
-        public ILogger Logger { get; set; }
-        public Localizer T { get; set; }
 
         public ActionResult Index() {
             return View();
@@ -55,7 +35,7 @@ namespace Orchard.ContentPreview.Controllers {
 
         [HttpPost]
         [ValidateInput(false)]
-        public async Task<ActionResult> Render() {
+        public ActionResult Render() {
             if (!_authorizer.Authorize(Permissions.ContentPreview)) {
                 return new HttpUnauthorizedResult();
             }
@@ -63,44 +43,20 @@ namespace Orchard.ContentPreview.Controllers {
             var contentItemType = Request.Form["ContentItemType"];
             var contentItem = _contentManager.New(contentItemType);
 
-            // Assign the ids from the currently edited item so that validation thinks
-            // it's working on the same item. For instance if drivers are checking name unicity
-            // they need to think this is the same existing item (AutoroutePart).
-
-            var contentItemId = Request.Form["PreviewContentItemId"];
-            var contentItemVersionId = Request.Form["PreviewContentItemVersionId"];
-            int.TryParse(Request.Form["PreviewId"], out var contentId);
-
-            //contentItem.As<CommonPart>().Id = contentId;
-            // contentItem.ContentItemId = contentItemId;
-            // contentItem.ContentItemVersionId = contentItemVersionId;
-            contentItem.As<CommonPart>().CreatedUtc = _clock.UtcNow;
-            contentItem.As<CommonPart>().ModifiedUtc = _clock.UtcNow;
-            contentItem.As<CommonPart>().PublishedUtc = _clock.UtcNow;
-
-            // TODO: we should probably get this value from the main editor as it might impact validators
-            //var model = await _contentItemDisplayManager.UpdateEditorAsync(contentItem, this, true);
+            var commonPart = contentItem.As<CommonPart>();
+            commonPart.CreatedUtc = commonPart.ModifiedUtc= commonPart.PublishedUtc = _clock.UtcNow;
+            
             var model = _contentManager.UpdateEditor(contentItem, this);
 
             if (!ModelState.IsValid) {
-                var errors = new List<string>();
-                //foreach (var modelState in ValidationHelpers.GetModelStateList(ViewData, false)) {
-                //    for (var i = 0; i < modelState.Errors.Count; i++) {
-                //        var modelError = modelState.Errors[i];
-                //        var errorText = ValidationHelpers.GetModelErrorMessageOrDefault(modelError);
-                //        errors.Add(errorText);
-                //    }
-                //}
-
-                //return StatusCode(HttpStatusCode.InternalServerError, new { errors = errors });
                 return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
             }
 
-            //model = await _contentItemDisplayManager.BuildDisplayAsync(contentItem, this, "Detail");
             model = _contentManager.BuildDisplay(contentItem, "Detail");
 
             return View(model);
         }
+
 
         [ValidateInput(false)]
         bool IUpdateModel.TryUpdateModel<TModel>(TModel model, string prefix, string[] includeProperties, string[] excludeProperties) {
