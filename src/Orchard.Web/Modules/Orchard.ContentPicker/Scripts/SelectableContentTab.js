97a114e59ee5f57eff0c8ffbd0d8f5bbe0c3abcb
marco.viglione <marco.viglione@laser-group.com>
2019-07-18 15:31:58 +0200

    Revert "Allows selected contents to not be lost after changing page"
    This reverts commit bc4aed04d23e95001f2da99d99e1a7ae28d03556.

--- src/Orchard.Web/Modules/Orchard.ContentPicker/Scripts/SelectableContentTab.js
+++ src/Orchard.Web/Modules/Orchard.ContentPicker/Scripts/SelectableContentTab.js
@@ -1,18 +1,12 @@
 ﻿jQuery(function ($) {
 
     Initialize = function () {
-        $('.content-picker-itemCheck').each(function () {
-            var related = $(this).siblings('.content-picker-item').children('.related');
-
-            if (window.sessionStorage.getItem(related.data("id")) != null) {
-                $(this).prop('checked', true);
-            }
-        });
-
-        $('.content-picker-itemCheck').change(function () {
-            var related = $(this).siblings('.content-picker-item').children('.related');
-
-            if (this.checked) {
+ 
+        $('.button.addSelected').on('click', function () {
+            var selectedItems = $('.content-picker-itemCheck:checked');
+            var itemsToAdd = new Array();
+            $.each(selectedItems, function (index, item) {
+                var related = $(item).siblings('.content-picker-item').children('.related');
                 var data = {
                     id: related.data("id"),
                     displayText: related.data("display-text"),
@@ -22,27 +16,13 @@
                     displayLink: related.data("display-link"),
                     published: related.data("published")
                 };
-
-                window.sessionStorage.setItem(related.data("id"), JSON.stringify(data));
-            } else {
-                window.sessionStorage.removeItem(related.data("id"));
-            }
-        });
-
-        $('.button.addSelected').on('click', function () {
-            var itemsToAdd = new Array();
-            for (var i = 0; i < sessionStorage.length; i++) {
-                var data = window.sessionStorage.getItem(sessionStorage.key(i));
-                itemsToAdd.push(JSON.parse(data));
-            }
-            window.sessionStorage.clear();
+                return itemsToAdd.push(data);
+            });
             window.opener.jQuery[query("callback")](itemsToAdd);
             window.close();
         });
-
         $('.content-picker-SelectAll').on('click', function () {
             $('.content-picker-itemCheck').prop('checked', $(this).prop("checked"));
-            $('.content-picker-itemCheck').change();
         });
     };
 
