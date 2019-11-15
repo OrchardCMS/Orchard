6e5b21448169e1e6f5606529c10cfcc94ca4462f
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-11-15 17:34:34 +0100

    caching sitecultureselector and sslsettingspart

--- src/Orchard.Web/Modules/Orchard.SecureSocketsLayer/Handlers/SslSettingsPartHandler.cs
+++ src/Orchard.Web/Modules/Orchard.SecureSocketsLayer/Handlers/SslSettingsPartHandler.cs
@@ -3,12 +3,24 @@ using Orchard.Data;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Localization;
 using Orchard.SecureSocketsLayer.Models;
+using Orchard.Caching;
 
 namespace Orchard.SecureSocketsLayer.Handlers {
     public class SslSettingsPartHandler : ContentHandler {
-        public SslSettingsPartHandler() {
+        private readonly ISignals _signals;
+
+        public SslSettingsPartHandler(ISignals signals) {
+
+            _signals = signals;
+
             T = NullLocalizer.Instance;
+
             Filters.Add(new ActivatingFilter<SslSettingsPart>("Site"));
+
+            // Evict cached content when updated, removed or destroyed.
+            OnPublished<SslSettingsPart>((context, part) => Invalidate(part));
+            OnRemoved<SslSettingsPart>((context, part) => Invalidate(part));
+            OnDestroyed<SslSettingsPart>((context, part) => Invalidate(part));
         }
 
         public Localizer T { get; set; }
@@ -21,6 +33,11 @@ namespace Orchard.SecureSocketsLayer.Handlers {
                 Id = "Ssl",
                 Position = "2"
             });
+        }
+
+        private void Invalidate(SslSettingsPart content) {
+            _signals.Trigger($"SslSettingsPart_{content.Id}");
+            _signals.Trigger("SslSettingsPart_EvictAll");
         }
     }
 }
