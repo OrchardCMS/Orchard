0e6872be70c8ab15770912c25669d6acae444665
elena.lampugnani <elena.lampugnani@laser-group.com>
2019-12-19 14:27:30 +0100

    add pager to blogpostarchive

--- src/Orchard.Web/Modules/Orchard.Blogs/Controllers/BlogPostController.cs
+++ src/Orchard.Web/Modules/Orchard.Blogs/Controllers/BlogPostController.cs
@@ -9,7 +9,9 @@ using Orchard.Core.Feeds;
 using Orchard.DisplayManagement;
 using Orchard.Localization;
 using Orchard.Mvc;
+using Orchard.Settings;
 using Orchard.Themes;
+using Orchard.UI.Navigation;
 
 namespace Orchard.Blogs.Controllers {
     [Themed]
@@ -19,19 +21,22 @@ namespace Orchard.Blogs.Controllers {
         private readonly IBlogPostService _blogPostService;
         private readonly IFeedManager _feedManager;
         private readonly IArchiveConstraint _archiveConstraint;
-
+        private readonly ISiteService _siteService;
+        
         public BlogPostController(
             IOrchardServices services, 
             IBlogService blogService, 
             IBlogPostService blogPostService,
             IFeedManager feedManager,
             IShapeFactory shapeFactory,
-            IArchiveConstraint archiveConstraint) {
+            IArchiveConstraint archiveConstraint,
+            ISiteService siteService) {
             _services = services;
             _blogService = blogService;
             _blogPostService = blogPostService;
             _feedManager = feedManager;
             _archiveConstraint = archiveConstraint;
+            _siteService = siteService;
             T = NullLocalizer.Instance;
             Shape = shapeFactory;
         }
@@ -39,7 +44,8 @@ namespace Orchard.Blogs.Controllers {
         dynamic Shape { get; set; }
         public Localizer T { get; set; }
 
-        public ActionResult ListByArchive(string path) {
+        public ActionResult ListByArchive(string path, PagerParameters pagerParameters) {
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
 
             var blogPath = _archiveConstraint.FindPath(path);
             var archive = _archiveConstraint.FindArchiveData(path);
@@ -60,15 +66,21 @@ namespace Orchard.Blogs.Controllers {
                 return new ShapeResult(this, Shape.Parts_Blogs_BlogArchives(Blog: blogPart, Archives: _blogPostService.GetArchives(blogPart)));
             }
 
+            pager.PageSize = blogPart.PostsPerPage;
+
             var list = Shape.List();
-            list.AddRange(_blogPostService.Get(blogPart, archive).Select(b => _services.ContentManager.BuildDisplay(b, "Summary")));
+            list.AddRange(_blogPostService.Get(blogPart, archive, pager.GetStartIndex(), pager.PageSize)
+                .Select(b => _services.ContentManager.BuildDisplay(b, "Summary")));
+
+            var totalItemCount = _blogPostService.Get(blogPart, archive).Count();
 
             _feedManager.Register(blogPart, _services.ContentManager.GetItemMetadata(blogPart).DisplayText);
 
             var viewModel = Shape.ViewModel()
                 .ContentItems(list)
                 .Blog(blogPart)
-                .ArchiveData(archive);
+                .ArchiveData(archive)
+                .Pager(Shape.Pager(pager).TotalItemCount(totalItemCount));
 
             return View(viewModel);
         }
