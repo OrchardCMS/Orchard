5baefd726f75be370bf5c4d1cf1f2f21718abe68
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-02-07 14:50:00 +0100

    Add button manage approve disable in edit user
    # Conflicts:
    #	src/Orchard.Web/Modules/Orchard.Users/Orchard.Users.csproj

---
+++ src/Orchard.Web/Modules/Orchard.Users/Drivers/UserApprovePartDriver.cs
@@ -0,0 +1,57 @@
+﻿using Laser.Orchard.StartupConfig.ApproveUserExtension.Services;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+using Orchard.Mvc;
+using Orchard.Users.Models;
+using Orchard.Users.ViewModels;
+using Orchard.Users;
+using Orchard;
+
+namespace Laser.Orchard.StartupConfig.ApproveUserExtension.Drivers {
+    public class UserApprovePartDriver : ContentPartDriver<UserPart> {
+        private const string TemplateName = "Parts/User.Approve";
+        private readonly IApproveUserService _approveUserService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IOrchardServices _orchardServices;
+
+        public UserApprovePartDriver(
+            IApproveUserService approveUserService,
+            IHttpContextAccessor httpContextAccessor,
+            IOrchardServices orchardServices
+            ) {
+
+            _approveUserService = approveUserService;
+            _httpContextAccessor = httpContextAccessor;
+            _orchardServices = orchardServices;
+
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        protected override DriverResult Editor(UserPart part, dynamic shapeHelper) {
+            var model = new UserEditViewModel { User = part };
+
+            return ContentShape("Parts_UserApprove_Edit",
+                                () => {
+                                    if (!_orchardServices.Authorizer.Authorize(Permissions.ManageUsers, T("Not authorized to manage users"))) {
+                                        return null;
+                                    }
+                                    return shapeHelper.EditorTemplate(TemplateName: TemplateName, Model: model, Prefix: Prefix);
+                                 });
+        }
+
+        protected override DriverResult Editor(UserPart part, IUpdateModel updater, dynamic shapeHelper) {
+            var model = new UserEditViewModel { User = part };
+
+            return ContentShape("Parts_UserApprove_Edit",
+                              () => {
+                                  if (!_orchardServices.Authorizer.Authorize(Permissions.ManageUsers, T("Not authorized to manage users"))) { 
+                                      return null;
+                                  }
+                                  return shapeHelper.EditorTemplate(TemplateName: TemplateName, Model: model, Prefix: Prefix);
+                              });
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.Users/Handlers/ApproveUserHandler.cs
@@ -0,0 +1,34 @@
+﻿using Laser.Orchard.StartupConfig.ApproveUserExtension.Services;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Mvc;
+using Orchard.Users.Models;
+
+namespace Laser.Orchard.StartupConfig.ApproveUserExtension.Handler {
+    public class ApproveUserHandler : ContentHandler {
+        private readonly IApproveUserService _approveUserService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ApproveUserHandler(
+           IApproveUserService approveUserService,
+           IHttpContextAccessor httpContextAccessor) {
+            _approveUserService = approveUserService;
+            _httpContextAccessor = httpContextAccessor;
+
+            OnPublished<UserPart>((context, part) => {
+                var httpContext = _httpContextAccessor.Current();
+                // verify user click correct button and 
+                // registration status is correct to approve/disable
+                if (httpContext.Request.Form["submit.Save"] == "submit.ApproveUser" &&
+                    part.RegistrationStatus == UserStatus.Pending) {
+                    _approveUserService.Approve(part);
+                }
+
+                if (httpContext.Request.Form["submit.Save"] == "submit.DisableUser" &&
+                    part.RegistrationStatus == UserStatus.Approved) {
+                    _approveUserService.Disable(part);
+                }
+            });
+        }
+    }
+}
