f43a9cbb97a58b609018635bc7c3849b74908012
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-09-08 10:12:57 +0200

    New version of Boolean Binder Provider

---
+++ src/Orchard/Mvc/ModelBinders/BooleanBinderProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Orchard.Mvc.ModelBinders {
+    public class BooleanBinderProvider : IModelBinderProvider, IModelBinder {
+
+        public IEnumerable<ModelBinderDescriptor> GetModelBinders() {
+            return new[] {
+                             new ModelBinderDescriptor {
+                                                           ModelBinder = this,
+                                                           Type = typeof(bool)
+                                                       }
+                         };
+        }
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext) {
+            var value = false;
+            try {
+                value = (bool)bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ConvertTo(typeof(bool));
+            } catch {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, new FormatException());
+            }
+            return value;
+        }
+    }
+}
