b528ecd1e0c2b6169f8756240bdbae38207d588c
Matteo Piovanelli <MatteoPiovanelli-Laser@users.noreply.github.com>
2020-09-11 11:02:53 +0200

    New version of Boolean Binder Provider (#8413)
    * New version of Boolean Binder Provider
    * Use Convert.ToBoolean(string) rather than ValueProviderResult.ConvertTo(bool)

---
+++ src/Orchard/Mvc/ModelBinders/BooleanBinderProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Orchard.Mvc.ModelBinders {
+    public class BooleanBinderProvider : IModelBinderProvider, IModelBinder {
+
+        public IEnumerable<ModelBinderDescriptor> GetModelBinders() {
+            return new[] {
+                             new ModelBinderDescriptor {
+                                                           ModelBinder = this,
+                                                           Type = typeof(bool)
+                                                       }
+                         };
+        }
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext) {
+            var value = false;
+            try {
+                value = Convert.ToBoolean(bindingContext.ValueProvider.GetValue(bindingContext.ModelName).AttemptedValue);
+            } catch {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, new FormatException());
+            }
+            return value;
+        }
+    }
+}
