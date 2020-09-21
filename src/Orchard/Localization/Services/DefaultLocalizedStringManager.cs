96884e6dbdcfdb155f081fc0bda074ef24ac8c2e
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-09-21 12:11:38 +0200

    Prevent NREs for null text When the text we are localizing is null (which potentially happens when localizing dynamic content), it could result in a null parent translation, which would throw when used as "basis" for comparisons.

--- src/Orchard/Localization/Services/DefaultLocalizedStringManager.cs
+++ src/Orchard/Localization/Services/DefaultLocalizedStringManager.cs
@@ -51,6 +51,7 @@ namespace Orchard.Localization.Services {
 
         public FormatForScope GetLocalizedString(IEnumerable<string> scopes, string text, string cultureName) {
             var culture = LoadCulture(cultureName);
+            text = text ?? string.Empty;
             foreach (var scope in scopes) {
                 string scopedKey = (scope + "|" + text).ToLowerInvariant();
                 if (culture.Translations.ContainsKey(scopedKey)) {
