755fd0e660e32a6c16bd39f357cd2e017230d522
aaronamm <theeranitp@gmail.com>
2020-10-07 00:25:22 +0700

    Improve build script to not fail when JS warning wrote to standard error

--- deploy.ps1
+++ deploy.ps1
@@ -1,107 +1,117 @@
-#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-strictmode?view=powershell-7
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-strictmode?view=powershell-7
 Set-StrictMode -Version Latest
 
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7#erroractionpreference
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Continue" # Just explicit set it
 
-Import-Module -Name .\DeploymentUtility
+Import-Module -Name ./DeploymentUtility -Force
 
 "Verify if Node.js installed"
 if (-not (Get-Command -Name node -ErrorAction Ignore)) {
-    throw "Missing node.js executable, please install node.js." +
-    "If already installed, make sure it can be reached from the current Environment."
+    throw  (
+		"Missing Node.js executable, please install Node.js." +
+		"If already installed, make sure it can be reached from the current environment."
+	)
 }
 
-# Setup
 $ARTIFACTS = "$PSScriptRoot\..\artifacts"
-
+# Set deployment source folder
 if (-not $Env:DEPLOYMENT_SOURCE) {
+    'Set $DEPLOYMENT_SOURCE variable from the current directory'
     $Env:DEPLOYMENT_SOURCE = $PSScriptRoot
 }
 
 if (-not $Env:DEPLOYMENT_TARGET) {
+    'Set $DEPLOYMENT_TARGET variable'
     $Env:DEPLOYMENT_TARGET = "$ARTIFACTS\wwwroot"
 }
 
 if (-not $Env:NEXT_MANIFEST_PATH) {
+    'Set $NEXT_MANIFEST_PATH variable'
     $Env:NEXT_MANIFEST_PATH = "$ARTIFACTS\manifest"
 
-    if (-not $Env:PREVIOUS_MANIFEST_PATH) {
-        $Env:PREVIOUS_MANIFEST_PATH = "$ARTIFACTS\manifest"
-    }
+	if (-not $Env:PREVIOUS_MANIFEST_PATH) {
+		'Set $PREVIOUS_MANIFEST_PATH variable'
+		$Env:PREVIOUS_MANIFEST_PATH = "$ARTIFACTS\manifest"
+	}
 }
 
-if (-not $Env:KUDU_SYNC_CMD) {
-    "Installing Kudu Sync"
-    Invoke-ExternalCommand -ScriptBlock { & npm install kudusync -g --silent }
-
-    # Locally just running "kuduSync" would also work
-    $Env:KUDU_SYNC_CMD = "$Env:AppData\npm\kuduSync.cmd"
-}
-
-# Log Environment variables
-$EnvironmentNameToWriteValue = @(
+# Log environment variables
+$environmentNameToWriteValue = @(
     "DEPLOYMENT_SOURCE"
     "DEPLOYMENT_TARGET"
     "NEXT_MANIFEST_PATH"
     "PREVIOUS_MANIFEST_PATH"
-    "KUDU_SYNC_CMD"
     "WEBSITE_NODE_DEFAULT_VERSION"
+    "WEBSITE_NPM_DEFAULT_VERSION"
     "SCM_REPOSITORY_PATH"
-    "Path" 
     "SOLUTION_PATH"
     "PROJECT_PATH"
     "MSBUILD_PATH"
+    "Path" 
 )
-Write-EnviromentValue -EnvironmentName $EnvironmentNameToWriteValue
+Write-EnviromentValue -EnvironmentName $environmentNameToWriteValue
 
-"Current node version: $(& node --version)"
-"Current npm version: $(& npm --version)"
-"Current MSBUILD version: $(& $Env:MSBUILD_PATH -version)"
+################ Build Node.js project with yarn if there is yarn.lock file ################
+$nodeProjectsDir = Get-ChildItem -Path . -Recurse -Filter "yarn.lock" |
+	Select-Object -ExpandProperty DirectoryName -Unique |
+    Where-Object { $_ -NotMatch "node_modules" }
 
-###########################################################
-# Deployment
-###########################################################
+"Node projects directory:"
+$nodeProjectsDir
 
-"Handling .NET Web Application deployment."
-"Restore NuGet packages"
-"Current nuget version: $(nuget help | Select -First 1)"
+Install-Yarn
 
-Invoke-ExternalCommand -ScriptBlock { 
-	& nuget restore "$Env:SOLUTION_PATH" -MSBuildPath "$(Split-Path -Path $Env:MSBUILD_PATH)"  
+$nodeProjectsDir | Foreach-Object {
+	$projectDir = $_
+	Push-Location -Path $projectDir
+
+	"Current Node project directory is $(Get-Location)"
+	"Installing npm packages with yarn"
+	Invoke-ExternalCommand -ScriptBlock { yarn install }
+
+	"Building Node.js project with yarn" 
+	Invoke-ExternalCommand -ScriptBlock { yarn build }
+	Pop-Location
 }
+###########################################################################################
 
-"Build .NET project to the temp directory"
+# Build .NET project
+"Restore NuGet packages"
+Invoke-ExternalCommand -ScriptBlock { nuget restore "$Env:SOLUTION_PATH" }
+
+"Build .NET project to the pre-compiled directory"
 $preCompiledDir = "$Env:DEPLOYMENT_SOURCE\build\Precompiled"
 
-"Building with MSBUILD to '$preCompiledDir'" 
+"Build .NET project to the temp directory"
+"Building the project with MSBuild to '$preCompiledDir'" 
 Invoke-ExternalCommand -ScriptBlock { 
-    # https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-command-line-reference?view=vs-2019
-    & "$Env:MSBUILD_PATH" `
-        "$Env:PROJECT_PATH" `
-        /t:Precompiled `
-        /verbosity:minimal `
-        /maxcpucount `
-        /nologo `
-        $Env:SCM_BUILD_ARGS
-    # Set SCM_BUILD_ARGS App Services Apps Settings to string you want to append to the msbuild command line.
+	cmd /c "$Env:MSBUILD_PATH" `
+		"$Env:PROJECT_PATH" `
+		/t:Precompiled `
+		/p:PreCompiledDir=$preCompiledDir `
+		/verbosity:minimal `
+		/maxcpucount `
+		/nologo `
+		$Env:SCM_BUILD_ARGS
+		# Set SCM_BUILD_ARGS as App Service Configuration to any string you want to append to the MSBuild command line.
 }
 
-"Kudu syncing" 
-Invoke-ExternalCommand -ScriptBlock { 
-    & "$Env:KUDU_SYNC_CMD" `
-        -v 50 `
-        -f "$preCompiledDir" `
-        -t "$Env:DEPLOYMENT_TARGET" `
-        -n "$Env:NEXT_MANIFEST_PATH" `
-        -p "$Env:PREVIOUS_MANIFEST_PATH" `
-        -i ".git;.hg;.deployment;deploy.cmd;deploy.ps1;node_modules;"
+Install-KuduSync
+
+"Syncing a build output to a deployment folder" 
+Invoke-ExternalCommand -ScriptBlock {
+	cmd /c kudusync `
+		-f "$preCompiledDir" `
+		-t "$Env:DEPLOYMENT_TARGET" `
+		-n "$Env:NEXT_MANIFEST_PATH" `
+		-p "$Env:PREVIOUS_MANIFEST_PATH" `
+		-i ".git;.hg;.deployment;deploy.cmd;deploy.ps1;node_modules;"
 }
 
 if ($Env:POST_DEPLOYMENT_ACTION) {
     "Post deployment stub"
-    Invoke-ExternalCommand -ScriptBlock { & $Env:POST_DEPLOYMENT_ACTION }
+    Invoke-ExternalCommand -ScriptBlock { $Env:POST_DEPLOYMENT_ACTION }
 }
 
 "Deployment successfully"
-
