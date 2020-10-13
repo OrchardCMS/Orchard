e49546fdf28e1ab764d1ecbb8a279b8ecdc66023
aaronamm <theeranitp@gmail.com>
2020-10-13 15:59:34 +0700

    Add database migration to copy SmtpSettingsPart.Address's value to SmtpSettingsPart.FromAddress

--- src/Orchard.Web/Modules/Orchard.Email/Migrations.cs
+++ src/Orchard.Web/Modules/Orchard.Email/Migrations.cs
@@ -1,11 +1,39 @@
-﻿using Orchard.Data.Migration;
+﻿using System.Linq;
+using System.Xml;
+using Orchard.ContentManagement;
+using Orchard.Data.Migration;
+using Orchard.Email.Models;
 
 namespace Orchard.Email {
     public class Migrations : DataMigrationImpl {
+        private readonly IContentManager _contentManager;
 
-        public int Create() {
+        public Migrations(IContentManager contentManager) => _contentManager = contentManager;
 
-            return 1;
+        // The first migration without any content should not exist but it has been deployed so we need to keep it.
+        public int Create() => 1;
+
+        public int UpdateFrom1() {
+            // Migrate existing SmtpSettingPart.Address because we rename it to FromAddress.
+            var siteSettingsItem = _contentManager.Query(contentTypeNames: "Site")
+                .Slice(1)
+                .SingleOrDefault();
+
+            var siteSettingsRecord = siteSettingsItem?.Record;
+
+            if (siteSettingsRecord != null) {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(siteSettingsRecord.Data);
+
+                var smtpSettingNode = xmlDoc.SelectSingleNode("//SmtpSettingsPart");
+                if (smtpSettingNode != null) {
+                    var smtpSettingsPart = siteSettingsItem.As<SmtpSettingsPart>();
+                    smtpSettingsPart.FromAddress = smtpSettingNode.Attributes["Address"]?.Value;
+                }
+            }
+
+            return 2;
         }
     }
 }
+
