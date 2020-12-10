cca308def20ed86273b7de5f9924a4b7507ec71d
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-12-10 08:38:49 +0100

    Added an event activity for workflows that activates on the first Updated event after creation of a ContentItem

--- src/Orchard.Web/Modules/Orchard.Workflows/Handlers/WorkflowContentHandler.cs
+++ src/Orchard.Web/Modules/Orchard.Workflows/Handlers/WorkflowContentHandler.cs
@@ -6,8 +6,16 @@ using Orchard.Workflows.Services;
 namespace Orchard.Workflows.Handlers {
 
     public class WorkflowContentHandler : ContentHandler {
+        // Used to memorize the ids of ContentItems for which we go through the
+        // OnCreated handler.
+        private List<int> _createdItems;
+        // Used to memorize the ids of ContentItems for which we go through the
+        // OnUpdated handler.
+        private List<int> _updatedItems;
 
         public WorkflowContentHandler(IWorkflowManager workflowManager) {
+            _createdItems = new List<int>();
+            _updatedItems = new List<int>();
 
             OnPublished<ContentPart>(
                 (context, part) =>
@@ -34,9 +42,15 @@ namespace Orchard.Workflows.Handlers {
                     () => new Dictionary<string, object> { { "Content", context.BuildingContentItem } }));
 
             OnCreated<ContentPart>(
-                (context, part) =>
+                (context, part) => {
+
                     workflowManager.TriggerEvent("ContentCreated", context.ContentItem,
-                    () => new Dictionary<string, object> { { "Content", context.ContentItem } }));
+                        () => new Dictionary<string, object> { { "Content", context.ContentItem } });
+
+                    if (context.ContentItem != null) { // sanity check
+                        _createdItems.Add(context.ContentItem.Id);
+                    }
+                });
 
             OnUpdated<ContentPart>(
                 (context, part) => {
@@ -49,6 +63,22 @@ namespace Orchard.Workflows.Handlers {
                         context.ContentItem,
                         () => new Dictionary<string, object> { { "Content", context.ContentItem } }
                     );
+
+                    if (context.ContentItem != null) { // sanity check
+                        if (_createdItems.Contains(context.ContentItem.Id)) {
+                            if (!_updatedItems.Contains(context.ContentItem.Id)) {
+                                // first update after creation of item
+                                workflowManager.TriggerEvent(
+                                    "ContentFirstUpdated",
+                                    context.ContentItem,
+                                    () => new Dictionary<string, object> { { "Content", context.ContentItem } }
+                                );
+                            }
+                        }
+                        // in case a further update is invoked, this would prevent
+                        // the FirstUpdate event to be fired again
+                        _updatedItems.Add(context.ContentItem.Id);
+                    }
                 });
         }
     }
