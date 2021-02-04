0c34ca3dd51fbdd11122446daaf79aa89286ec1e
Hermes Sbicego <hermes.sbicego@laser-group.com>
2021-02-04 19:05:50 +0100

    Feature/8445 libraries update (#8446)

--- src/Orchard.Web/Modules/Orchard.Layouts/Serialization/LocalizedStringYamlConverter.cs
+++ src/Orchard.Web/Modules/Orchard.Layouts/Serialization/LocalizedStringYamlConverter.cs
@@ -8,7 +8,7 @@ namespace Orchard.Layouts.Serialization {
     public class LocalizedStringYamlConverter : IYamlTypeConverter {
         public bool Accepts(Type type) => type == typeof(LocalizedString);
 
-        public object ReadYaml(IParser parser, Type type) => new LocalizedString(parser.Expect<Scalar>()?.Value);
+        public object ReadYaml(IParser parser, Type type) => new LocalizedString(parser.Consume<Scalar>()?.Value);
 
         public void WriteYaml(IEmitter emitter, object value, Type type) =>
             emitter.Emit(new Scalar((value as LocalizedString)?.Text ?? ""));
