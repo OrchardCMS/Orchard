100c0c353cb46722b69e4365b06d9563693591c5
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-09-13 10:49:31 +0200

    added generic permission

--- src/Orchard.Web/Modules/Orchard.Widgets/Permissions.cs
+++ src/Orchard.Web/Modules/Orchard.Widgets/Permissions.cs
@@ -5,12 +5,13 @@ using Orchard.Security.Permissions;
 namespace Orchard.Widgets {
     public class Permissions : IPermissionProvider {
         public static readonly Permission ManageWidgets = new Permission { Description = "Managing Widgets", Name = "ManageWidgets" };
+        public static readonly Permission EditWidgetWrapper = new Permission { Description = "Edit Widget Wrapper in front-end", Name = "EditWidgetWrapper" };
 
         public virtual Feature Feature { get; set; }
 
         public IEnumerable<Permission> GetPermissions() {
             return new[] {
-                ManageWidgets,
+                ManageWidgets,EditWidgetWrapper
             };
         }
 
@@ -18,7 +19,7 @@ namespace Orchard.Widgets {
             return new[] {
                 new PermissionStereotype {
                     Name = "Administrator",
-                    Permissions = new[] {ManageWidgets}
+                    Permissions = new[] {ManageWidgets, EditWidgetWrapper }
                 },
             };
         }
