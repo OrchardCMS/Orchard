694bd559312dfff42e63b197edb92010d156458b
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-09-17 10:46:11 +0200

    feature: adds role of the user in to cache key and evict when changed or deleted the role

---
+++ src/Orchard.Web/Modules/Orchard.OutputCache/Filters/CacheByRoleFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using Orchard.Mvc.Filters;
+using Orchard.Roles.Models;
+using Orchard.Security;
+
+namespace Orchard.OutputCache.Filters {
+    [OrchardFeature("Orchard.OutputCache.CacheByRole")]
+    public class CacheByRoleFilter : FilterProvider, IActionFilter, IResultFilter, ICachingEventHandler {
+        private readonly IAuthenticationService _authenticationService;
+        public CacheByRoleFilter(IAuthenticationService authenticationService) {
+            _authenticationService = authenticationService;
+        }
+
+        public void KeyGenerated(StringBuilder key) {
+            var currentUser = _authenticationService.GetAuthenticatedUser();
+            if (currentUser != null) {
+                var roles = currentUser.As<UserRolesPart>().Roles;
+                if (roles.Any()) {
+                    // append roles in alphabetical order
+                    key.Append("UserRoles=" + 
+                        String.Join("|",roles.OrderBy(r => r))+";");
+                }
+                else {
+                    key.Append("UserRoles=empty-role;");
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext) {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext) {
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) {
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext) {
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.OutputCache/Handlers/CacheByRoleHandler.cs
@@ -0,0 +1,78 @@
+﻿using Orchard.Environment.Configuration;
+using Orchard.Environment.Extensions;
+using Orchard.OutputCache.Models;
+using Orchard.OutputCache.Services;
+using Orchard.Roles.Events;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace Orchard.OutputCache.Handlers {
+    [OrchardFeature("Orchard.OutputCache.CacheByRole")]
+    public class CacheByRoleHandler : IRoleEventHandler {
+        private readonly string _tenantName;
+        private readonly WorkContext _workContext;
+        private readonly IOutputCacheStorageProvider _cacheStorageProvider;
+
+        // used to manage the role evict once 
+        // the role evict
+        private string roleEvict = string.Empty;
+
+        public CacheByRoleHandler(
+            IWorkContextAccessor workContextAccessor,
+            IOutputCacheStorageProvider cacheStorageProvider,
+            ShellSettings shellSettings) {
+            _workContext = workContextAccessor.GetContext();
+            _cacheStorageProvider = cacheStorageProvider;
+            _tenantName = shellSettings.Name;
+        }
+        public void PermissionAdded(PermissionAddedContext context) {
+            if(roleEvict != context.Role.Name) {
+                roleEvict = context.Role.Name;
+                EvictCache(context.Role.Name);
+            }
+        }
+
+        public void PermissionRemoved(PermissionRemovedContext context) {
+            if (roleEvict != context.Role.Name) {
+                roleEvict = context.Role.Name;
+                EvictCache(context.Role.Name);
+            }
+        }
+
+        public void Removed(RoleRemovedContext context) {
+            EvictCache(context.Role.Name);
+        }
+
+        public void Created(RoleCreatedContext context) {
+        }
+
+        public void Renamed(RoleRenamedContext context) {
+        }
+
+        public void UserAdded(UserAddedContext context) {
+        }
+
+        public void UserRemoved(UserRemovedContext context) {
+        }
+
+        private void EvictCache(string role) {
+            var cacheItems = _workContext.HttpContext.Cache.AsParallel()
+                 .Cast<DictionaryEntry>()
+                 .Select(x => x.Value)
+                 .OfType<CacheItem>()
+                 // get all cache items for my tenant
+                 .Where(x => x.Tenant.Equals(_tenantName, StringComparison.OrdinalIgnoreCase))
+                 // split of the cache key string
+                 .Where(ci => ci.CacheKey.Split(';')
+                    // each key should contain the property added for the roles
+                    .Any(key => key.StartsWith("UserRoles=", StringComparison.OrdinalIgnoreCase)
+                        // and the role indicated
+                        && key.Substring(10).Contains(role)));
+
+            foreach (var item in cacheItems) {
+                _cacheStorageProvider.Remove(item.CacheKey);
+            }
+        }
+    }
+}
