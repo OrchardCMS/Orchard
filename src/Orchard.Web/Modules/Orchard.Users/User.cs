817fb8dc80edf50f08d36e97c68522a03b12f869
Idan Elor <idanelor@apiiro.com>
2021-12-14 13:17:57 +0200

    Add user data

---
+++ src/Orchard.Web/Modules/Orchard.Users/User.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public int SSN { get; set; }
+        public int CreditCardNumber { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+        
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
