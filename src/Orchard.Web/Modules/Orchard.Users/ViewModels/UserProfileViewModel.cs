56d0753b414a6a8081922d65febc5f471d19f41a
Idan Elor <idanelor@apiiro.com>
2021-12-14 11:02:10 +0200

    Make some changes

---
+++ src/Orchard.Web/Modules/Orchard.Users/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orchard.Users.ViewModels
+{
+    [OrchardFeature("Orchard.Users.EditPasswordByAdmin")]
+    public class UserEditPasswordViewModel {
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public IContent User { get; set; }
+    }
+}
