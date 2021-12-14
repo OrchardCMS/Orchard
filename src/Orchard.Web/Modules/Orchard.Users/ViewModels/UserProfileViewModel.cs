857fdb264cfb3c18a73d5c34124eab28dd540f6e
Idan Elor <idanelor@apiiro.com>
2021-12-14 10:44:43 +0200

    Make some changes

---
+++ src/Orchard.Web/Modules/Orchard.Users/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orchard.Users.ViewModels
+{
+    [OrchardFeature("Orchard.Users.ROUser")]
+    public class UserProfileViewModel {
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public IContent User { get; set; }
+        public String SSN { get; set; }
+        public String Address { get; set }
+    }
+}
