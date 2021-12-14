26cc49bc6d14f29359a56d7177f88c19a58c17bc
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2021-12-14 11:27:56 +0100

    Provider to prevent suspension of users based on assigned roles

---
+++ src/Orchard.Web/Modules/Orchard.Roles/Handlers/RolesUserSuspensionSettingsPartHandler.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+using Orchard.Roles.Models;
+
+namespace Orchard.Roles.Handlers {
+    public class RolesUserSuspensionSettingsPartHandler : ContentHandler {
+
+        public RolesUserSuspensionSettingsPartHandler() {
+
+            T = NullLocalizer.Instance;
+            Filters.Add(new ActivatingFilter<RolesUserSuspensionSettingsPart>("Site"));
+        }
+
+        public Localizer T { get; set; }
+
+    }
+}
