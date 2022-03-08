5a9ce63bd3715c03ea55aa84907ba6c12c44129c
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2022-03-08 14:11:26 +0100

    Added specific permission to change owner of content item

---
+++ src/Orchard.Web/Core/Common/OwnerEditor/OwnerEditorPermissions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Environment.Extensions.Models;
+using Orchard.Security;
+using Orchard.Security.Permissions;
+
+namespace Orchard.Core.Common.OwnerEditor {
+    public class OwnerEditorPermissions : IPermissionProvider {
+
+        public static readonly Permission MayEditContentOwner = new Permission {
+            Description = "Edit the Owner of content items",
+            Name = "MayEditContentOwner",
+            ImpliedBy = new[] { StandardPermissions.SiteOwner } };
+
+        public virtual Feature Feature { get; set; }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
+            return new[] {new PermissionStereotype {
+                    Name = "Administrator",
+                    Permissions = new[] { MayEditContentOwner }
+                } };
+        }
+
+        public IEnumerable<Permission> GetPermissions() {
+            return new[] { MayEditContentOwner };
+        }
+    }
+}
