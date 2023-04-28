4eeb2dffeea350e5d56f5a8c34c8555eea6452be
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2023-04-28 15:57:11 +0200

    Tests if dependencies are enabled before activating the content links settings

--- src/Orchard.Web/Modules/TinyMce/Settings/EditorEvents.cs
+++ src/Orchard.Web/Modules/TinyMce/Settings/EditorEvents.cs
@@ -6,26 +6,30 @@ using Orchard.ContentManagement.MetaData;
 using Orchard.ContentManagement.MetaData.Builders;
 using Orchard.ContentManagement.MetaData.Models;
 using Orchard.ContentManagement.ViewModels;
+using Orchard.Environment.Descriptor.Models;
 
 namespace TinyMce.Settings {
     public class EditorEvents : ContentDefinitionEditorEventsBase {
 
         private string[] _htmlParts = new string[] { "BodyPart", "LayoutPart" };
         private string[] _htmlFields = new string[] { "TextField" };
+        private bool _contentLinksDependenciesEnabled = false;
 
-
-        public EditorEvents() {
+        public EditorEvents(ShellDescriptor shellDescriptor) {
+            var contenPickerEnabled = shellDescriptor.Features.Any(x => x.Name == "Orchard.ContentPicker") ? true : false;
+            var tokensHtmlFilterEnabled = shellDescriptor.Features.Any(x => x.Name == "Orchard.Tokens.HtmlFilter") ? true : false;
+            _contentLinksDependenciesEnabled = contenPickerEnabled && tokensHtmlFilterEnabled;
         }
 
         public override IEnumerable<TemplateViewModel> PartFieldEditor(ContentPartFieldDefinition definition) {
-            if (!_htmlFields.Any(x => x.Equals(definition.FieldDefinition.Name, StringComparison.InvariantCultureIgnoreCase)))
+            if (!_contentLinksDependenciesEnabled || !_htmlFields.Any(x => x.Equals(definition.FieldDefinition.Name, StringComparison.InvariantCultureIgnoreCase)))
                 yield break;
             var model = definition.Settings.GetModel<ContentLinksSettings>();
             yield return DefinitionTemplate(model);
         }
 
         public override IEnumerable<TemplateViewModel> PartFieldEditorUpdate(ContentPartFieldDefinitionBuilder builder, IUpdateModel updateModel) {
-            if (!_htmlFields.Any(x => x.Equals(builder.Name, StringComparison.InvariantCultureIgnoreCase)))
+            if (!_contentLinksDependenciesEnabled || !_htmlFields.Any(x => x.Equals(builder.Name, StringComparison.InvariantCultureIgnoreCase)))
                 yield break;
 
             var model = new ContentLinksSettings();
@@ -36,7 +40,7 @@ namespace TinyMce.Settings {
         }
 
         public override IEnumerable<TemplateViewModel> TypePartEditor(ContentTypePartDefinition definition) {
-            if (!_htmlParts.Any(x => x.Equals(definition.PartDefinition.Name, StringComparison.InvariantCultureIgnoreCase)))
+            if (!_contentLinksDependenciesEnabled || !_htmlParts.Any(x => x.Equals(definition.PartDefinition.Name, StringComparison.InvariantCultureIgnoreCase)))
                 yield break;
             var model = definition.Settings.GetModel<ContentLinksSettings>();
             yield return DefinitionTemplate(model);
@@ -44,7 +48,7 @@ namespace TinyMce.Settings {
 
 
         public override IEnumerable<TemplateViewModel> TypePartEditorUpdate(ContentTypePartDefinitionBuilder builder, IUpdateModel updateModel) {
-            if (!_htmlParts.Any(x => x.Equals(builder.Name, StringComparison.InvariantCultureIgnoreCase)))
+            if (!_contentLinksDependenciesEnabled || !_htmlParts.Any(x => x.Equals(builder.Name, StringComparison.InvariantCultureIgnoreCase)))
                 yield break;
             var model = new ContentLinksSettings();
             updateModel.TryUpdateModel(model, "ContentLinksSettings", null, null);
