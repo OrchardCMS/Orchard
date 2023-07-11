8a6958cec2edf28bfdabaadd19088c7a866be963
Lombiq <github@lombiq.com>
2023-07-11 22:11:08 +0200

    Adding migration step to migrate from EnableSsl to EncryptionMethod in SmtpSettingsPart

--- src/Orchard.Web/Modules/Orchard.Email/Models/SmtpSettingsPart.cs
+++ src/Orchard.Web/Modules/Orchard.Email/Models/SmtpSettingsPart.cs
@@ -37,7 +37,7 @@ namespace Orchard.Email.Models {
         [Obsolete($"To keep configuration compatible with {nameof(EmailMessagingChannel)}.")]
 #pragma warning restore CS0618 // Type or member is obsolete
         public bool EnableSsl {
-            get { return EncryptionMethod != SmtpEncryptionMethod.None; }
+            get { return this.Retrieve(x => x.EnableSsl); }
         }
 
         public SmtpEncryptionMethod EncryptionMethod {
