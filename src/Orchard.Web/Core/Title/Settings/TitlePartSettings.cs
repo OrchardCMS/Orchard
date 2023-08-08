c32beabbca015ee235ba6b369465e83c1c46dbfc
Alessandro Agostini <alessandro.agostini@laser-group.com>
2023-08-08 17:05:38 +0200

    added missing files

---
+++ src/Orchard.Web/Core/Title/Settings/TitlePartSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Orchard.Core.Title.Settings {
+    public class TitlePartSettings {
+        [Range(0, 1024)]
+        [DisplayName("Maximum Length")]
+        public int MaxLength {get; set;}
+
+    }
+}
