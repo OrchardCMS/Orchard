44a4b3249e615c37ca0a3ca80cd0c8e8fa947089
Alessandro Agostini <alessandro.agostini@laser-group.com>
2023-08-28 17:01:23 +0200

    added comment to remind people to update the migration when they change the constant introduced previously

--- src/Orchard.Web/Core/Title/Settings/TitlePartSettings.cs
+++ src/Orchard.Web/Core/Title/Settings/TitlePartSettings.cs
@@ -9,6 +9,7 @@ namespace Orchard.Core.Title.Settings {
 
     
     public class TitlePartSettings {
+        // Whenever this constant is changed a new migration step must be created to update the length of the field on the DB
         public const int MAX_TITLE_LENGTH = 1024;
         [Range(0, MAX_TITLE_LENGTH)]
         [DisplayName("Maximum Length")]
