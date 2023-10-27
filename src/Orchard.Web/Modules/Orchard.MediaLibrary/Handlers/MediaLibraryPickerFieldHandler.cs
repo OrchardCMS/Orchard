03884cbd6431721ffe69b03c69ba672a1cb44dc1
Andrea Piovanelli <83577153+AndreaPiovanelliLaser@users.noreply.github.com>
2023-10-27 09:22:24 +0200

    Lazy field loading using content item ContentManager instead of the injected one. (#8725)
    * Lazy field loading using content item ContentManager instead of the injected one.
    
    * Removed unused _contentManager variable

--- src/Orchard.Web/Modules/Orchard.MediaLibrary/Handlers/MediaLibraryPickerFieldHandler.cs
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/Handlers/MediaLibraryPickerFieldHandler.cs
@@ -9,16 +9,12 @@ using Orchard.MediaLibrary.Models;
 
 namespace Orchard.MediaLibrary.Handlers {
     public class MediaLibraryPickerFieldHandler : ContentHandler {
-        private readonly IContentManager _contentManager;
         private readonly IContentDefinitionManager _contentDefinitionManager;
 
         public MediaLibraryPickerFieldHandler(
-            IContentManager contentManager, 
             IContentDefinitionManager contentDefinitionManager) {
             
-            _contentManager = contentManager;
             _contentDefinitionManager = contentDefinitionManager;
-
         }
 
         protected override void Loaded(LoadContentContext context) {
@@ -37,7 +33,8 @@ namespace Orchard.MediaLibrary.Handlers {
 
             foreach (var field in fields) {
                 var localField = field;
-                localField._contentItems = new Lazy<IEnumerable<MediaPart>>(() => _contentManager.GetMany<MediaPart>(localField.Ids, VersionOptions.Published, QueryHints.Empty).ToList());
+                // Using context content item's ContentManager instead of injected one to avoid lifetime scope exceptions in case of LazyFields.
+                localField._contentItems = new Lazy<IEnumerable<MediaPart>>(() => contentItem.ContentManager.GetMany<MediaPart>(localField.Ids, VersionOptions.Published, QueryHints.Empty).ToList());
             }
         }
     }
