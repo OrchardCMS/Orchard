2f7deb7fef1a9f01d9199dfda0da74300f95ce6a
Benedek Farkas <benedek.farkas@lombiq.com>
2025-09-19 18:48:40 +0200

    Fixing the rendering of buttons from the merge in ContentsDriver

--- src/Orchard.Web/Core/Contents/Drivers/ContentsDriver.cs
+++ src/Orchard.Web/Core/Contents/Drivers/ContentsDriver.cs
@@ -17,18 +17,19 @@ namespace Orchard.Core.Contents.Drivers {
         }
 
         protected override DriverResult Editor(ContentPart part, dynamic shapeHelper) {
-            var results = new List<DriverResult> { ContentShape("Content_SaveButton", saveButton => saveButton) };
+            var results = new List<DriverResult>();
 
             if (part.TypeDefinition.Settings.GetModel<ContentTypeSettings>().Draftable) {
-                results.Add(ContentShape("Content_PublishButton", publishButton => publishButton));
-                results.Add(ContentShape("Content_UnpublishButton", unpublishButton => unpublishButton));
+                results.Add(ContentShape("Content_SaveButton", saveButton => saveButton));
             }
+
+            results.Add(ContentShape("Content_PublishButton", publishButton => publishButton));
+            results.Add(ContentShape("Content_UnpublishButton", unpublishButton => unpublishButton));
+            results.Add(ContentShape("Content_CancelButton", cancelButton => cancelButton));
 
             if (part.Id > 0) {
                 results.Add(ContentShape("Content_DeleteButton", deleteButton => deleteButton));
             }
-
-            results.Add(ContentShape("Content_CancelButton", cancelButton => cancelButton));
 
             return Combined(results.ToArray());
         }
