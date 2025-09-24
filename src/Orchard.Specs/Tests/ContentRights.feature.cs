811f7a9c33646d911f2952bd971c6fc68994a8cc
Benedek Farkas <benedek.farkas@lombiq.com>
2025-09-24 22:29:50 +0200

    Updating SpecFlow generated files

--- src/Orchard.Specs/Tests/ContentRights.feature.cs
+++ src/Orchard.Specs/Tests/ContentRights.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Orchard.Specs
+namespace Orchard.Specs.Tests
 {
     using TechTalk.SpecFlow;
     
