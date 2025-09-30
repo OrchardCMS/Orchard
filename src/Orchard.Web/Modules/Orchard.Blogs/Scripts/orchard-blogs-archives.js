84d4cbf32b7aaac0e4e2295c16bda805bd08d6ba
Benedek Farkas <benedek.farkas@lombiq.com>
2025-09-30 22:21:08 +0200

    Updating Blog Archives scripts

--- src/Orchard.Web/Modules/Orchard.Blogs/Scripts/orchard-blogs-archives.js
+++ src/Orchard.Web/Modules/Orchard.Blogs/Scripts/orchard-blogs-archives.js
@@ -1,16 +1,13 @@
-﻿/** archives **/
 (function ($) {
-    $(function() {
-        $('.archives ul.years li.previous').each(function() {
-            $(this).click(function(ev) {
+    $(function () {
+        $('.archives ul.years li.previous').each(function () {
+            $(this).click(function (ev) {
                 if (!ev || $(ev.target).not("a").length) {
                     $(this).toggleClass("open");
                     $(this).find("h4>span").toggle();
                     $(this).children("ul").toggle();
                 }
             });
-
-            //$(this).hoverClassIfy();
         });
     });
 })(jQuery);
