826f0a2cd3b1cb9f5df26ae6595595f658b3d0e1
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-01 22:05:25 +0200

    Updating resource version numbers for jQuery Calendars

--- src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/jQueryUI.cs
+++ src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/jQueryUI.cs
@@ -51,16 +51,16 @@ namespace Orchard.Resources.ResourceManifests {
             manifest.DefineScript("jQueryCalendars")
                 .SetUrl("Calendars/jquery.calendars.all.min.js", "Calendars/jquery.calendars.all.js")
                 .SetDependencies("jQueryPlugin")
-                .SetVersion("2.0.1");
+                .SetVersion("2.2.0");
 
             manifest.DefineScript("jQueryCalendars_Picker")
                 .SetUrl("Calendars/jquery.calendars.picker.full.min.js", "Calendars/jquery.calendars.picker.full.js")
                 .SetDependencies("jQueryCalendars")
-                .SetVersion("2.0.1");
+                .SetVersion("2.2.0");
             manifest.DefineStyle("jQueryCalendars_Picker")
                 .SetUrl("Calendars/jquery.calendars.picker.full.min.css", "Calendars/jquery.calendars.picker.full.css")
                 .SetDependencies("jQueryUI_Orchard")
-                .SetVersion("2.0.1");
+                .SetVersion("2.0.0"); // The styles are still at version 2.0.0 in the 2.2.0 release.
 
             // jQuery Time Entry.
             manifest.DefineScript("jQueryTimeEntry")
