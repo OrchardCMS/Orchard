148360aa97d40282e2c6f35a815d0d4934f2d751
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-01 17:21:13 +0200

    Upgrading Bootstrap to 4.6.2 source

--- src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/Bootstrap.cs
+++ src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/Bootstrap.cs
@@ -4,14 +4,20 @@ namespace Orchard.Resources.ResourceManifests {
     public class Bootstrap : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
+
             manifest.DefineStyle("Bootstrap")
                 .SetUrl("bootstrap.min.css", "bootstrap.css")
-                .SetCdn("//cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css", "//cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.css")
-                .SetVersion("4.6.1");
+                .SetCdn(
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css",
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css")
+                .SetVersion("4.6.2");
+
             manifest.DefineScript("Bootstrap").SetUrl("bootstrap.bundle.min.js", "bootstrap.bundle.js")
                 .SetDependencies("jQuery")
-                .SetCdn("//cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js", "//cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.js")
-                .SetVersion("4.6.1");
+                .SetCdn(
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.js")
+                .SetVersion("4.6.2");
         }
     }
 }
