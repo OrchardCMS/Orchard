63a0a48757b6ae6b3c2afe03a88b16d68ffde717
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-01 17:57:46 +0200

    Moving AngularJS output files into a separate subfolder

--- src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/AngularJs.cs
+++ src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/AngularJs.cs
@@ -4,12 +4,23 @@ namespace Orchard.Resources.ResourceManifests {
     public class AngularJs : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            manifest.DefineScript("AngularJs").SetUrl("angular.min.js", "angular.js").SetVersion("1.3.3");
-            manifest.DefineScript("AngularJs_Sanitize").SetUrl("angular-sanitize.min.js", "angular-sanitize.js").SetVersion("1.3.3").SetDependencies("AngularJs");
-            manifest.DefineScript("AngularJs_Resource").SetUrl("angular-resource.min.js", "angular-resource.js").SetVersion("1.3.3").SetDependencies("AngularJs");
-            manifest.DefineScript("AngularJs_Sortable").SetUrl("angular-sortable.min.js", "angular-sortable.js").SetDependencies("AngularJs", "jQueryUI_Sortable");
+            manifest.DefineScript("AngularJs")
+                .SetUrl("Angular/angular.min.js", "Angular/angular.js")
+                .SetVersion("1.3.3");
+            manifest.DefineScript("AngularJs_Sanitize")
+                .SetUrl("Angular/angular-sanitize.min.js", "Angular/angular-sanitize.js")
+                .SetVersion("1.3.3")
+                .SetDependencies("AngularJs");
+            manifest.DefineScript("AngularJs_Resource")
+                .SetUrl("Angular/angular-resource.min.js", "Angular/angular-resource.js")
+                .SetVersion("1.3.3")
+                .SetDependencies("AngularJs");
+            manifest.DefineScript("AngularJs_Sortable")
+                .SetUrl("Angular/angular-sortable.min.js", "Angular/angular-sortable.js")
+                .SetDependencies("AngularJs", "jQueryUI_Sortable");
 
-            manifest.DefineScript("AngularJs_Full").SetDependencies("AngularJs", "AngularJs_Sanitize", "AngularJs_Resource", "AngularJs_Sortable");
+            manifest.DefineScript("AngularJs_Full")
+                .SetDependencies("AngularJs", "AngularJs_Sanitize", "AngularJs_Resource", "AngularJs_Sortable");
         }
     }
 }
