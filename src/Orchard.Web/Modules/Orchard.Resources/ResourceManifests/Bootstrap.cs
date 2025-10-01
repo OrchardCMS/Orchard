d0227cb022d4542ee4194d5d51b812e84f300c82
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-01 18:25:25 +0200

    Adding new Bootstrap files, moving scripts and styles into separate subfolders

--- src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/Bootstrap.cs
+++ src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/Bootstrap.cs
@@ -6,13 +6,28 @@ namespace Orchard.Resources.ResourceManifests {
             var manifest = builder.Add();
 
             manifest.DefineStyle("Bootstrap")
-                .SetUrl("bootstrap.min.css", "bootstrap.css")
+                .SetUrl("Bootstrap/bootstrap.min.css", "Bootstrap/bootstrap.css")
                 .SetCdn(
                     "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css",
                     "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css")
                 .SetVersion("4.6.2");
 
-            manifest.DefineScript("Bootstrap").SetUrl("bootstrap.bundle.min.js", "bootstrap.bundle.js")
+            manifest.DefineStyle("Bootstrap.Grid")
+                .SetUrl("Bootstrap/bootstrap-grid.min.css", "Bootstrap/bootstrap-grid.css")
+                .SetCdn(
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap-grid.min.css",
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap-grid.css")
+                .SetVersion("4.6.2");
+
+            manifest.DefineStyle("Bootstrap.Reboot")
+                .SetUrl("Bootstrap/bootstrap-reboot.min.css", "Bootstrap/bootstrap-reboot.css")
+                .SetCdn(
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap-reboot.min.css",
+                    "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap-reboot.css")
+                .SetVersion("4.6.2");
+
+            manifest.DefineScript("Bootstrap")
+                .SetUrl("Bootstrap/bootstrap.bundle.min.js", "Bootstrap/bootstrap.bundle.js")
                 .SetDependencies("jQuery")
                 .SetCdn(
                     "//cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
