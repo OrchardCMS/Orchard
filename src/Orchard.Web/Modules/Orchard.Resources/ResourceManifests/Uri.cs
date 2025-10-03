1d4dfcb6a5e6f511964b5b22805fdd1802d8035f
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-03 16:47:51 +0200

    Moving URI JS files to a subfolder

--- src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/Uri.cs
+++ src/Orchard.Web/Modules/Orchard.Resources/ResourceManifests/Uri.cs
@@ -4,7 +4,7 @@ namespace Orchard.Resources.ResourceManifests {
     public class Uri : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            manifest.DefineScript("Uri").SetUrl("uri.min.js", "uri.js").SetVersion("1.16.1");
+            manifest.DefineScript("Uri").SetUrl("Uri/uri.min.js", "Uri/uri.js").SetVersion("1.16.1");
         }
     }
 }
