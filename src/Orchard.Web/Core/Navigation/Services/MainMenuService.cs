14d19b9ccc5ef77748cdda8d50bc24dbb1376008
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-03 21:42:21 +0200

    MainMenuService: Fixing that unpublished menu items could not be deleted

--- src/Orchard.Web/Core/Navigation/Services/MainMenuService.cs
+++ src/Orchard.Web/Core/Navigation/Services/MainMenuService.cs
@@ -25,7 +25,7 @@ namespace Orchard.Core.Navigation.Services {
         }
 
         public IContent GetMenu(string menuName) {
-            if(string.IsNullOrWhiteSpace(menuName)) {
+            if (string.IsNullOrWhiteSpace(menuName)) {
                 return null;
             }
 
@@ -37,19 +37,19 @@ namespace Orchard.Core.Navigation.Services {
         }
 
         public IContent GetMenu(int menuId) {
-            return _contentManager.Get(menuId, VersionOptions.Published);    
+            return _contentManager.Get(menuId, VersionOptions.Published);
         }
 
         public MenuPart Get(int menuPartId) {
-            return _contentManager.Get<MenuPart>(menuPartId);
+            return _contentManager.Get<MenuPart>(menuPartId, VersionOptions.Latest);
         }
 
         public IContent Create(string name) {
-            
-            if(string.IsNullOrWhiteSpace(name)) {
+
+            if (string.IsNullOrWhiteSpace(name)) {
                 throw new ArgumentNullException(name);
             }
-            
+
             var menu = _contentManager.Create("Menu");
             menu.As<TitlePart>().Title = name;
 
