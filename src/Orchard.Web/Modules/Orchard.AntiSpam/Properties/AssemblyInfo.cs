78ee4efb4f37b3ce75abd65692f64a7ea17e72e6
Benedek Farkas <benedek.farkas@lombiq.com>
2025-10-10 18:49:26 +0200

    #8850: Release 1.10.4 (#8851)
    * Updating version numbers to 1.10.4
    
    * Updating Advisory Dashboard Widget iframe URL
    
    * Dashboards: Removing empty Scripts folder
    
    * Dashboards: Updating the Advisory element's iframe URL
    
    * Updating solution with GHA workflow files

--- src/Orchard.Web/Modules/Orchard.AntiSpam/Properties/AssemblyInfo.cs
+++ src/Orchard.Web/Modules/Orchard.AntiSpam/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Orchard")]
-[assembly: AssemblyCopyright("Copyright © .NET Foundation")]
+[assembly: AssemblyCopyright("Copyright ï¿½ .NET Foundation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.10.3")]
-[assembly: AssemblyFileVersion("1.10.3")]
+[assembly: AssemblyVersion("1.10.4")]
+[assembly: AssemblyFileVersion("1.10.4")]
 
