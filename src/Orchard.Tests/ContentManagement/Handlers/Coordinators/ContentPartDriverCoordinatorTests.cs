a941219e564d6199f7b5c3f750041c6da5cfdfa7
Gábor Domonkos <gabor.domonkos@lombiq.com>
2025-11-17 16:14:00 +0100

    Removing obsoleted ContentShapeResult ContentShape(IShape shape) method from ContentPartDriver.

--- src/Orchard.Tests/ContentManagement/Handlers/Coordinators/ContentPartDriverCoordinatorTests.cs
+++ src/Orchard.Tests/ContentManagement/Handlers/Coordinators/ContentPartDriverCoordinatorTests.cs
@@ -87,7 +87,9 @@ namespace Orchard.Tests.ContentManagement.Handlers.Coordinators
                 var stub = shapeHelper.Stub(Foo: string.Join(",", part.Foo));
                 if (!string.IsNullOrWhiteSpace(displayType))
                     stub.Metadata.Type = string.Format("{0}.{1}", stub.Metadata.Type, displayType);
-                return ContentShape(stub).Location("TopMeta");
+                return ContentShape("Parts_Stub", () => {
+                    return string.Empty;
+                }).Location("TopMeta");
 
                 //var viewModel = new StubViewModel { Foo = string.Join(",", part.Foo) };
                 //if (displayType.StartsWith("Summary"))
