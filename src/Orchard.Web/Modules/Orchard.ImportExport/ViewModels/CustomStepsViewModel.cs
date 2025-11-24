c582c66ef7e231a950ea05d8cee208bff6952eb8
Gábor Domonkos <gabor.domonkos@lombiq.com>
2025-11-24 12:24:26 +0100

    Removing obsoleted CustomStepsStep. Don't need the CustomStepsStep RecipeBuilderStep because you can implement IRecipeBuilderStep and IRecipeExecutionStep instead of implementing custom export steps.

--- src/Orchard.Web/Modules/Orchard.ImportExport/ViewModels/CustomStepsViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Orchard.ImportExport.ViewModels
-{
-    public class CustomStepsViewModel
-    {
-        public IList<CustomStepEntry> CustomSteps { get; set; }
-    }
-}
