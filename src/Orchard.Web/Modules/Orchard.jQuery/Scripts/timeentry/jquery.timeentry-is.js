6df533d868fe085b1cc321c3a6ddfe0ce9044e47
Benedek Farkas <benedek.farkas@lombiq.com>
2025-11-25 18:52:55 +0100

    Removing Orchard.jQuery too since its quite outdated and Orchard.Resources has been available since 1.9

--- src/Orchard.Web/Modules/Orchard.jQuery/Scripts/timeentry/jquery.timeentry-is.js
+++
@@ -1,9 +0,0 @@
-﻿/* http://keith-wood.name/timeEntry.html
-   Icelandic initialisation for the jQuery time entry extension
-   Written by Már Örlygsson (http://mar.anomy.net/) */
-(function($) {
-	$.timeEntry.regionalOptions['is'] = {show24Hours: true, separator: ':',
-		ampmPrefix: '', ampmNames: ['fh', 'eh'],
-		spinnerTexts: ['Núna', 'Fyrra svæði', 'Næsta svæði', 'Hækka', 'Lækka']};
-	$.timeEntry.setDefaults($.timeEntry.regionalOptions['is']);
-})(jQuery);
